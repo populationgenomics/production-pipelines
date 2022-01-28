bec170d2a945d662708e20c366271fec9e822f41
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-01-29 02:53:16 +1100

    HaplotypeCaller refactoring, using resource helprs, pulling data with retry_gs_cp

--- cpg_pipes/jobs/haplotype_caller.py
+++ cpg_pipes/jobs/haplotype_caller.py
@@ -4,7 +4,7 @@ Create Hail Batch jobs for variant calling in individual samples.
 
 import logging
 from os.path import join, basename
-from typing import Optional, List
+from typing import Optional, List, Tuple
 
 import hailtop.batch as hb
 from hailtop.batch.job import Job
@@ -12,7 +12,7 @@ from hailtop.batch.job import Job
 from cpg_pipes import resources, utils
 from cpg_pipes.smdb import SMDB
 from cpg_pipes.jobs import split_intervals
-from cpg_pipes.hailbatch import wrap_command
+from cpg_pipes.hailbatch import wrap_command, STANDARD
 
 logger = logging.getLogger(__file__)
 logging.basicConfig(format='%(levelname)s (%(name)s %(lineno)s): %(message)s')
@@ -27,10 +27,9 @@ def produce_gvcf(
     cram_path: str,
     crai_path: Optional[str] = None,
     output_path: Optional[str] = None,
-    number_of_intervals: Optional[int] = 1,
+    number_of_intervals: int = 1,
     intervals: Optional[hb.ResourceGroup] = None,
     overwrite: bool = True,
-    check_existence: bool = True,
     depends_on: Optional[List[Job]] = None,
     smdb: Optional[SMDB] = None,
     dragen_mode: bool = False,
@@ -42,76 +41,24 @@ def produce_gvcf(
     HaplotypeCaller is run in an interval-based sharded way, with per-interval
     HaplotypeCaller jobs defined in a nested loop.
     """
-    if output_path and check_existence and utils.can_reuse(output_path, overwrite):
+    if output_path and utils.can_reuse(output_path, overwrite):
         return b.new_job('Make GVCF [reuse]', dict(sample=sample_name, project=project_name))
 
-    logger.info(
-        f'Submitting the variant calling jobs' + 
-        (f' to write {output_path}' if output_path else '') + 
-        ' for {sample_name}'
+    depends_on = depends_on or []
+
+    first_j, last_j, hc_gvcf_path = haplotype_caller(
+        b=b,
+        sample_name=sample_name,
+        project_name=project_name,
+        tmp_bucket=tmp_bucket,
+        cram_path=cram_path,
+        crai_path=crai_path,
+        number_of_intervals=number_of_intervals, 
+        intervals=intervals,
+        overwrite=overwrite, 
+        depends_on=depends_on,
+        dragen_mode=dragen_mode,
     )
-
-    reference = b.read_input_group(
-        base=resources.REF_FASTA,
-        fai=resources.REF_FASTA + '.fai',
-        dict=resources.REF_FASTA.replace('.fasta', '')
-        .replace('.fna', '')
-        .replace('.fa', '')
-        + '.dict',
-    )
-
-    hc_gvcf_path = join(tmp_bucket, 'haplotypecaller', f'{sample_name}.g.vcf.gz')
-    hc_jobs = []
-    if number_of_intervals is not None and number_of_intervals > 1:
-        if intervals is None:
-            intervals = split_intervals.get_intervals(
-                b=b,
-                scatter_count=number_of_intervals,
-                out_bucket=join(tmp_bucket, 'intervals'),
-            )
-
-        # Splitting variant calling by intervals
-        for idx in range(number_of_intervals):
-            hc_jobs.append(
-                hc_job(
-                    b,
-                    sample_name=sample_name,
-                    project_name=project_name,
-                    reference=reference,
-                    cram_fpath=cram_path,
-                    crai_fpath=crai_path,
-                    interval=intervals[f'interval_{idx}'],
-                    interval_idx=idx,
-                    number_of_intervals=number_of_intervals,
-                    depends_on=depends_on,
-                    dragen_mode=dragen_mode,
-                    overwrite=overwrite,
-                )
-            )
-        hc_j = merge_gvcfs_job(
-            b=b,
-            sample_name=sample_name,
-            project_name=project_name,
-            gvcfs=[j.output_gvcf for j in hc_jobs],
-            out_gvcf_path=hc_gvcf_path,
-            overwrite=overwrite,
-        )
-    else:
-        hc_j = hc_job(
-            b,
-            sample_name=sample_name,
-            project_name=project_name,
-            reference=reference,
-            cram_fpath=cram_path,
-            crai_fpath=crai_path,
-            depends_on=depends_on,
-            out_gvcf_path=hc_gvcf_path,
-            overwrite=overwrite,
-        )
-        hc_jobs.append(hc_j)
-
-    if depends_on:
-        hc_jobs[0].depends_on(*depends_on)
 
     postproc_j = postproc_gvcf(
         b=b,
@@ -120,7 +67,7 @@ def produce_gvcf(
         gvcf_path=hc_gvcf_path,
         output_path=output_path,
         overwrite=overwrite,
-        depends_on=[hc_j],
+        depends_on=depends_on + [last_j],
     )
     last_j = postproc_j
 
@@ -131,18 +78,95 @@ def produce_gvcf(
             output_path=output_path,
             sample_names=[sample_name],
             project_name=project_name,
-            first_j=hc_jobs[0],
+            first_j=first_j,
             last_j=last_j,
             depends_on=depends_on,
         )
     return last_j
 
 
-def hc_job(
+def haplotype_caller(
     b: hb.Batch,
     sample_name: str,
     project_name: str,
-    reference: hb.ResourceGroup,
+    tmp_bucket: str,
+    cram_path: str,
+    crai_path: Optional[str] = None,
+    number_of_intervals: int = 1,
+    intervals: Optional[hb.ResourceGroup] = None,
+    overwrite: bool = True,
+    depends_on: Optional[List[Job]] = None,
+    dragen_mode: bool = False,
+) -> Tuple[Job, Job, str]:
+    """
+    Run haplotype caller in parallel sharded by intervals. 
+    Returns the first and the last job object, and path to the output GVCF file.
+    """
+    hc_gvcf_path = join(tmp_bucket, 'haplotypecaller', f'{sample_name}.g.vcf.gz')
+    if utils.can_reuse(hc_gvcf_path, overwrite):
+        first_j = last_j = b.new_job('HaplotypeCaller [reuse]', dict(
+            sample=sample_name, project=project_name))
+        return first_j, last_j, hc_gvcf_path
+
+    hc_jobs = []
+    if number_of_intervals > 1:
+        if intervals is None:
+            intervals = split_intervals.get_intervals(
+                b=b,
+                scatter_count=number_of_intervals,
+                out_bucket=join(tmp_bucket, 'intervals'),
+            )
+
+        # Splitting variant calling by intervals
+        for idx in range(number_of_intervals):
+            hc_jobs.append(
+                _haplotype_caller_one(
+                    b,
+                    sample_name=sample_name,
+                    project_name=project_name,
+                    cram_fpath=cram_path,
+                    crai_fpath=crai_path,
+                    interval=intervals[f'interval_{idx}'],
+                    interval_idx=idx,
+                    number_of_intervals=number_of_intervals,
+                    depends_on=depends_on,
+                    dragen_mode=dragen_mode,
+                    overwrite=overwrite,
+                )
+            )
+        last_j = merge_gvcfs_job(
+            b=b,
+            sample_name=sample_name,
+            project_name=project_name,
+            gvcfs=[j.output_gvcf for j in hc_jobs],
+            out_gvcf_path=hc_gvcf_path,
+            overwrite=overwrite,
+        )
+    else:
+        hc_j = _haplotype_caller_one(
+            b,
+            sample_name=sample_name,
+            project_name=project_name,
+            cram_fpath=cram_path,
+            crai_fpath=crai_path,
+            depends_on=depends_on,
+            out_gvcf_path=hc_gvcf_path,
+            overwrite=overwrite,
+        )
+        hc_jobs.append(hc_j)
+        last_j = hc_j
+
+    first_j = hc_jobs[0]
+    if depends_on:
+        first_j.depends_on(*depends_on)
+    
+    return first_j, last_j, hc_gvcf_path
+
+
+def _haplotype_caller_one(
+    b: hb.Batch,
+    sample_name: str,
+    project_name: str,
     cram_fpath: str,
     crai_fpath: Optional[str] = None,
     interval: Optional[hb.ResourceFile] = None,
@@ -154,7 +178,7 @@ def hc_job(
     dragen_mode: bool = False,
 ) -> Job:
     """
-    Run HaplotypeCaller on an input BAM or CRAM, and output GVCF
+    Add one HaplotypeCaller job on an interval
     """
     job_name = 'HaplotypeCaller'
     if interval_idx is not None:
@@ -166,10 +190,15 @@ def hc_job(
         return j
 
     j.image(resources.GATK_IMAGE)
-    j.cpu(2)
-    java_mem = 7
-    j.memory('standard')  # ~ 4G/core ~ 7.5G
-    j.storage('60G')
+
+    # Enough storage to localize CRAMs (can't pass GCS URL to CRAM to gatk directly
+    # because we will hit GCP egress bandwidth limit:
+    # https://batch.hail.populationgenomics.org.au/batches/7493/jobs/2)
+    # 45 should be enough to fit a CRAM (30G), GVCF (1G), and ref data (5G),
+    # and we can squeeze 4 jobs on a 32-core machine (185G/4=46.25G) or
+    # 5 jobs on a 16-core machine (265G/4=53G)
+    job_res = STANDARD.set_resources(j, storage_gb=45)
+
     j.declare_resource_group(
         output_gvcf={
             'g.vcf.gz': '{root}-' + sample_name + '.g.vcf.gz',
@@ -179,12 +208,36 @@ def hc_job(
     if depends_on:
         j.depends_on(*depends_on)
 
+    ref_fasta = resources.REF_FASTA
+    ref_fai = resources.REF_FASTA + '.fai'
+    ref_dict = (
+        ref_fasta.replace('.fasta', '').replace('.fna', '').replace('.fa', '') + '.dict'
+    )
+    
+    # cram = b.read_input_group(**{
+    #     'cram': cram_fpath,
+    #     'cram.crai': crai_fpath or (cram_fpath + '.crai'),
+    # })
+
     cmd = f"""\
-    gatk --java-options "-Xms{java_mem}g -XX:GCTimeLimit=50 -XX:GCHeapFreeLimit=10" \\
+    CRAM=/io/batch/{sample_name}.cram
+    CRAI=/io/batch/{sample_name}.cram.crai
+
+    # Retrying copying to avoid google bandwidth limits
+    retry_gs_cp {cram_fpath} $CRAM
+    retry_gs_cp {crai_fpath or (cram_fpath + '.crai')} $CRAI
+
+    # Copying reference data as well to avoid crazy logging costs 
+    # for region requests
+    retry_gs_cp {ref_fasta} /io/batch/{basename(ref_fasta)}
+    retry_gs_cp {ref_fai}   /io/batch/{basename(ref_fai)}
+    retry_gs_cp {ref_dict}  /io/batch/{basename(ref_dict)}
+
+    gatk --java-options "-Xms{job_res.get_java_mem_mb()}g -XX:GCTimeLimit=50 -XX:GCHeapFreeLimit=10" \\
     HaplotypeCaller \\
-    -R {reference.base} \\
-    -I {cram_fpath} \\
-    --read-index {crai_fpath or (cram_fpath + '.crai')} \\
+    -R /io/batch/{basename(ref_fasta)} \\
+    -I $CRAM \\
+    --read-index $CRAI \\
     {f"-L {interval} " if interval is not None else ""} \\
     --disable-spanning-event-genotyping \\
     {"--dragen-mode " if dragen_mode else ""} \\
@@ -193,7 +246,7 @@ def hc_job(
     -GQB 20 \\
     -ERC GVCF
     """
-    j.command(wrap_command(cmd, monitor_space=True))
+    j.command(wrap_command(cmd, monitor_space=True, setup_gcp=True, define_retry_function=True))
     if out_gvcf_path:
         b.write_output(j.output_gvcf, out_gvcf_path.replace('.g.vcf.gz', ''))
     return j
@@ -263,9 +316,13 @@ def postproc_gvcf(
 
     j = b.new_job(f'ReblockGVCF', dict(sample=sample_name, project=project_name))
     j.image(resources.GATK_IMAGE)
-    mem_gb = 8
-    j.memory(f'{mem_gb}G')
-    j.storage(f'50G')
+
+    # Enough to fit a pre-reblocked GVCF, which can be as big as 10G,
+    # the reblocked result (1G), and ref data (5G). 
+    # We need at least 2 CPU, so on 16-core instance it would be 8 jobs,
+    # meaning we have more than enough disk (265/8=33.125G). 
+    job_res = STANDARD.set_resources(j, storage_gb=20)
+
     j.declare_resource_group(
         output_gvcf={
             'g.vcf.gz': '{root}.g.vcf.gz',
@@ -279,48 +336,23 @@ def postproc_gvcf(
         ref_fasta.replace('.fasta', '').replace('.fna', '').replace('.fa', '') + '.dict'
     )
 
-    j.command(f"""
-    export GOOGLE_APPLICATION_CREDENTIALS=/gsa-key/key.json
-    gcloud -q auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
-
-    function fail {{
-      echo $1 >&2
-      exit 1
-    }}
-
-    function retry {{
-      local n=1
-      local max=10
-      local delay=30
-      while true; do
-        "$@" && break || {{
-          if [[ $n -lt $max ]]; then
-            ((n++))
-            echo "Command failed. Attempt $n/$max:"
-            sleep $delay;
-          else
-            fail "The command has failed after $n attempts."
-          fi
-        }}
-      done
-    }}
-
+    cmd = f"""\
     GVCF=/io/batch/{sample_name}.g.vcf.gz
     GVCF_NODP=/io/batch/{sample_name}-nodp.g.vcf.gz
     REBLOCKED=/io/batch/{sample_name}-reblocked.g.vcf.gz
 
     # Retrying copying to avoid google bandwidth limits
-    retry gsutil cp {gvcf_path} $GVCF
-    retry gsutil cp {resources.NOALT_REGIONS} /io/batch/noalt-regions.bed
+    retry_gs_cp {gvcf_path} $GVCF
+    retry_gs_cp {resources.NOALT_REGIONS} /io/batch/noalt-regions.bed
 
     # Copying reference data as well to avoid crazy logging costs 
     # for region requests
-    retry gsutil cp {ref_fasta} /io/batch/{basename(ref_fasta)}
-    retry gsutil cp {ref_fai}   /io/batch/{basename(ref_fai)}
-    retry gsutil cp {ref_dict}  /io/batch/{basename(ref_dict)}
+    retry_gs_cp {ref_fasta} /io/batch/{basename(ref_fasta)}
+    retry_gs_cp {ref_fai}   /io/batch/{basename(ref_fai)}
+    retry_gs_cp {ref_dict}  /io/batch/{basename(ref_dict)}
 
     # Reindexing just to make sure the index is not corrupted
-    bcftools index --tbi /io/batch/{sample_name}.g.vcf.gz
+    bcftools index --tbi $GVCF
 
     # Remove INFO/DP field, which contradicts the FORMAT/DP, in the way that
     # it has _all_ reads, not just variant-calling-usable reads. If we keep INFO/DP,
@@ -335,7 +367,7 @@ def postproc_gvcf(
     | bcftools view -Oz -o $GVCF_NODP
     tabix -p vcf $GVCF_NODP
 
-    gatk --java-options "-Xms{mem_gb - 1}g" \\
+    gatk --java-options "-Xms{job_res.get_java_mem_mb()}g" \\
     ReblockGVCF \\
     --reference /io/batch/{basename(ref_fasta)} \\
     -V $GVCF_NODP \\
@@ -343,14 +375,18 @@ def postproc_gvcf(
     -O $REBLOCKED \\
     --create-output-variant-index true
 
-    EXISTING_SN=$(bcftools view $REBLOCKED | awk '/^#CHROM/ {{ print $NF; exit }}')    
+    EXISTING_SN=$(bcftools query -l $GVCF)
+
     bcftools view $REBLOCKED -T /io/batch/noalt-regions.bed \\
     | bcftools annotate -x INFO/DS \\
     | bcftools reheader -s <(echo "$EXISTING_SN {sample_name}") \\
     | bcftools view -Oz -o {j.output_gvcf['g.vcf.gz']}
 
     tabix -p vcf {j.output_gvcf['g.vcf.gz']}
-    """)
+    """
+    j.command(wrap_command(
+        cmd, setup_gcp=True, monitor_space=True, define_retry_function=True
+    ))
     if output_path:
         b.write_output(j.output_gvcf, output_path.replace('.g.vcf.gz', ''))
     if depends_on:
