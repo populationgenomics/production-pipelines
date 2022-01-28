419ee7dc8fde858c4e94a22daecb1641dc6afd9a
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-01-29 02:29:49 +1100

    Helper functions to set job resources

--- cpg_pipes/benchmark.py
+++ cpg_pipes/benchmark.py
@@ -6,24 +6,33 @@ TOY_INPUTS_BUCKET = f'{BENCHMARK_BUCKET}/inputs/test'
 RESULTS_BUCKET = f'{BENCHMARK_BUCKET}/fastqc'
 
 
-na12878fq = AlignmentInput(
-    fqs1=[f'{BENCHMARK_BUCKET}/inputs/NA12878/ERR194147_1.fastq.gz'],
-    fqs2=[f'{BENCHMARK_BUCKET}/inputs/NA12878/ERR194147_2.fastq.gz'],
-)
-
+# 40k reads:
 tiny_fq = AlignmentInput(
     # This set is 50MB each:
     fqs1=[f'{BENCHMARK_BUCKET}/inputs/toy/2-699835.L001.R1.n40000.fastq.gz'],
     fqs2=[f'{BENCHMARK_BUCKET}/inputs/toy/2-699835.L001.R2.n40000.fastq.gz'],
 )
 
+# ~300k reads:
 tiny_cram = AlignmentInput(
     bam_or_cram_path=f'{BENCHMARK_BUCKET}/inputs/toy/NA12878-chr21-tiny.cram',
 )
 
-giab_cram_inputs = {
+# WGS:
+giab_crams = {
     sn: AlignmentInput(
         bam_or_cram_path=f'gs://cpg-reference/validation/giab/cram/{sn}.cram'
     )
     for sn in ['NA12878', 'NA12891', 'NA12892']
 }
+na12878fq = AlignmentInput(
+    fqs1=[f'{BENCHMARK_BUCKET}/inputs/NA12878/ERR194147_1.fastq.gz'],
+    fqs2=[f'{BENCHMARK_BUCKET}/inputs/NA12878/ERR194147_2.fastq.gz'],
+)
+perth_neuro_fq = AlignmentInput(
+    fqs1=[f'{BENCHMARK_BUCKET}/inputs/PERTHNEURO_FQ/HNFWKCCXY_3_181017_FD07777491_Homo-sapiens__R_170503_GINRAV_DNA_M002_R1.fastq.gz'],
+    fqs2=[f'{BENCHMARK_BUCKET}/inputs/PERTHNEURO_FQ/HNFWKCCXY_3_181017_FD07777491_Homo-sapiens__R_170503_GINRAV_DNA_M002_R2.fastq.gz'],
+)
+perth_neuro_cram = AlignmentInput(
+    bam_or_cram_path=f'{BENCHMARK_BUCKET}/inputs/PERTHNEURO_CRAM/CPG13045.cram',
+)
--- cpg_pipes/hailbatch.py
+++ cpg_pipes/hailbatch.py
@@ -3,13 +3,15 @@ Utility functions to interact with Hail Batch Jobs and Resources
 """
 
 import logging
+import math
 import os
 from dataclasses import dataclass
-from typing import Optional, Union, List, Tuple, Dict
+from typing import Optional, Union, List, Tuple, Dict, cast
 import hailtop.batch as hb
+from hailtop.batch import ResourceGroup
 from hailtop.batch.job import Job
 
-from cpg_pipes import utils
+from cpg_pipes import utils, resources
 
 logger = logging.getLogger(__file__)
 logging.basicConfig(format='%(levelname)s (%(name)s %(lineno)s): %(message)s')
@@ -17,40 +19,48 @@ logger.setLevel(logging.INFO)
 
 
 @dataclass
-class BamOrCramAlignmentInput:
+class AlignmentInput:
     """
-    Represents inputs for an alignment job of CRAM or BAM files
-    """
-    bam_or_cram_path: Optional[Union[str, hb.ResourceGroup]] = None
-    index_path: Optional[str] = None
-
-    def as_cram_input_group(self, b) -> hb.ResourceGroup:
-        """
-        Makes a ResourceGroup of bam/cram with accompanying index
-        """
-        assert self.bam_or_cram_path
-        if isinstance(self.bam_or_cram_path, str):
-            return b.read_input_group(
-                base=self.bam_or_cram_path,
-                index=self.index_path or self.bam_or_cram_path + '.crai'
-            )
-        else:
-            return self.bam_or_cram_path
-
-
-@dataclass
-class FqAlignmentInput:
-    """
-    Represents inputs for an alignment job of fastq files
+    Represents inputs for an alignment job, which can be a set of fastq files,
+    or a CRAM or a BAM file with an index.
     """
     fqs1: Optional[List[Union[str, hb.ResourceFile]]] = None
     fqs2: Optional[List[Union[str, hb.ResourceFile]]] = None
+    bam_or_cram_path: Optional[Union[str, hb.ResourceGroup]] = None
+    index_path: Optional[str] = None
+    
+    def is_fastq(self) -> bool:
+        """
+        Checks that it's a fastq pair, and both in pair are of the same type and length
+        """
+        if self.fqs1 or self.fqs2:
+            assert self.fqs1 and self.fqs2, self
+            if any(isinstance(fq, str) for fq in [self.fqs1, self.fqs2]):
+                assert all(isinstance(fq, str) for fq in [self.fqs1, self.fqs2]), self
+            elif any(isinstance(fq, hb.ResourceFile) for fq in [self.fqs1, self.fqs2]):
+                assert all(isinstance(fq, hb.ResourceFile) for fq in [self.fqs1, self.fqs2]), self
+            else:
+                assert len(self.fqs1) == len(self.fqs2), self
+            return True
+        assert self.bam_or_cram_path, self
+        return False
+
+    def is_bam_or_cram(self) -> bool:
+        """
+        Checks that it's a BAM or a CRAM file
+        """
+        if self.bam_or_cram_path:
+            return True
+        assert self.fqs1 and self.fqs2, self
+        return False
 
     def as_fq_inputs(self, b) -> Tuple[List[hb.Resource], List[hb.Resource]]:
         """
         Makes a pair of lists of ResourceFile objects for fqs1 and fqs2
         """
-        assert self.fqs1 is not None and self.fqs2 is not None 
+        assert self.is_fastq()
+        self.fqs1 = cast(List, self.fqs1)
+        self.fqs2 = cast(List, self.fqs2)
         if isinstance(self.fqs1[0], hb.Resource):
             files1 = self.fqs1
             files2 = self.fqs2
@@ -59,10 +69,27 @@ class FqAlignmentInput:
             files2 = [b.read_input(f1) for f1 in self.fqs2]
         return files1, files2
 
-
-@dataclass
-class AlignmentInput(BamOrCramAlignmentInput, FqAlignmentInput):
-    pass
+    def as_cram_input_group(self, b) -> hb.ResourceGroup:
+        """
+        Makes a ResourceGroup of bam/cram with accompanying index
+        """
+        assert self.is_bam_or_cram()
+        self.bam_or_cram_path = cast(str, self.bam_or_cram_path)
+        index_path = self.index_path
+        if not index_path:
+            if self.bam_or_cram_path.endswith('.bam'):
+                index_path = self.bam_or_cram_path + '.bai'
+            else:
+                assert self.bam_or_cram_path.endswith('.cram'), self.bam_or_cram_path
+                index_path = self.bam_or_cram_path + '.crai'
+                
+        if isinstance(self.bam_or_cram_path, str):
+            return b.read_input_group(
+                base=self.bam_or_cram_path,
+                index=index_path
+            )
+        else:
+            return self.bam_or_cram_path
 
 
 @dataclass
@@ -192,9 +219,9 @@ def get_hail_bucket(tmp_bucket, keep_scratch) -> str:
 
 def setup_batch(
     title: str, 
-    keep_scratch: bool,
-    tmp_bucket: str,
-    analysis_project_name: str,
+    analysis_project_name: str = 'fewgenomes',
+    tmp_bucket: str = 'gs://cpg-fewgenomes-test-tmp/hail',
+    keep_scratch: bool = True,
     billing_project: Optional[str] = None,
     hail_bucket: Optional[str] = None,
 ) -> Batch:
@@ -222,30 +249,83 @@ def setup_batch(
     return b
 
 
+def fasta_ref_resource(b) -> ResourceGroup:
+    """
+    Returns fasta reference resource group
+    """
+    return b.read_input_group(**resources.REF_D)
+
+
+GCLOUD_CMD = """\
+export GOOGLE_APPLICATION_CREDENTIALS=/gsa-key/key.json
+gcloud -q auth activate-service-account \
+--key-file=$GOOGLE_APPLICATION_CREDENTIALS
+"""
+
+RETRY_CMD = """\
+function fail {
+  echo $1 >&2
+  exit 1
+}
+
+function retry {
+  local n_attempts=10
+  local delay=30
+  local n=1
+  while true; do
+    "$@" && break || {
+      if [[ $n -lt $n_attempts ]]; then
+        ((n++))
+        echo "Command failed. Attempt $n/$n_attempts:"
+        sleep $delay;
+      else
+        fail "The command has failed after $n attempts."
+      fi
+    }
+  done
+}
+
+function retry_gs_cp {
+  src=$1
+
+  if [ -n "$2" ]; then
+    dst=$2
+  else
+    dst=/io/batch/${basename $src}
+  fi
+  
+  retry gsutil -o GSUtil:check_hashes=never cp $src $dst
+}
+"""
+
+
 def wrap_command(
     command: str,
     monitor_space: bool = False,
     setup_gcp: bool = False,
-    dedent: bool = True
+    define_retry_function: bool = False,
+    dedent: bool = True,
 ) -> str:
     """
     Wraps a command for submission
     If job_resource is defined, monitors output space.
     If output_bucket_path_to_check is defined, checks if this file(s) exists,
     and if it does, skips running the rest of the job.
-    """
-    gcp_cmd = ''
-    if setup_gcp:
-        gcp_cmd = """\
-        export GOOGLE_APPLICATION_CREDENTIALS=/gsa-key/key.json
-        gcloud -q auth activate-service-account \
-        --key-file=$GOOGLE_APPLICATION_CREDENTIALS
-        """
     
+    :param command: command to wrap
+    :param monitor_space: add a background process that checks the instance disk 
+        space every 5 minutes and prints it to the screen
+    :param setup_gcp: login to GCP
+    :param define_retry_function: when set, adds bash functions `retry` that attempts 
+        to redo a command with a pause of default 30 seconds (useful to pull inputs 
+        and get around GoogleEgressBandwidth Quota or other google quotas)
+    :param dedent: remove all common leading intendation from the command
+    """
     cmd = f"""\
     set -o pipefail
     set -ex
-    {gcp_cmd}
+    {GCLOUD_CMD if setup_gcp else ''}
+    {RETRY_CMD if define_retry_function else ''}
     
     {f'(while true; do {monitor_space_command()}; sleep 600; done) &'
     if monitor_space else ''}
@@ -254,7 +334,7 @@ def wrap_command(
     
     {monitor_space_command() if monitor_space else ''}
     """
-    
+
     if dedent:
         # remove any leading spaces and tabs
         cmd = '\n'.join(line.strip() for line in cmd.split('\n'))
@@ -268,3 +348,227 @@ def monitor_space_command():
     Make command that monitors the instance storage space and memory
     """
     return f'df -h; du -sh /io; du -sh /io/batch'
+
+
+@dataclass(init=False)
+class MachineType:
+    """
+    Hail Batch machine type on GCP
+    """
+    MIN_NCPU: int = 2
+    THREADS_ON_CPU = 2  # hyperthreading
+
+    def __init__(
+        self,
+        name: str,
+        ncpu: int,
+        mem_gb_per_core: float,
+        price_per_hour: float,
+    ):
+        self.name = name
+        self.ncpu = ncpu
+        self.mem_gb_per_core = mem_gb_per_core
+        self.price_per_hour = price_per_hour
+    
+    def calc_instance_disk_gb(self) -> int:
+        """
+        The maximum available storage on an instance is calculated 
+        in `batch/batch/utils.py/unreserved_worker_data_disk_size_gib()`
+        as the disk size (375G) minus reserved image size (30G) minus
+        reserved storage per core (5G*ncpu = 120G for a 32-core instance),
+        """
+        disk_size_gb = 375
+        reserved_gb = 30
+        reserved_gb_per_core = 5
+        return disk_size_gb - reserved_gb - reserved_gb_per_core * self.ncpu
+
+    def set_resources(
+        self, 
+        j: Job,
+        ncpu: Optional[int] = None, 
+        nthreads: Optional[int] = None, 
+        mem_gb: Optional[float] = None,
+        storage_gb: Optional[float] = None, 
+        attach_disk_storage_gb: Optional[float] = None,
+    ) -> 'JobResource':
+        """
+        Set resources to a Job object. If any of other parameters are set,
+        they will be used as a bound to request a fraction of an instance.
+        """
+        return self.request_resources(
+            ncpu=ncpu,
+            nthreads=nthreads,
+            mem_gb=mem_gb,
+            storage_gb=storage_gb,
+            attach_disk_storage_gb=attach_disk_storage_gb,
+        ).set_to_job(j)
+
+    def request_resources(
+        self,
+        ncpu: Optional[int] = None, 
+        nthreads: Optional[int] = None, 
+        mem_gb: Optional[float] = None,
+        storage_gb: Optional[float] = None, 
+        attach_disk_storage_gb: Optional[float] = None,
+    ) -> 'JobResource':
+        """
+        Request resources from the machine, satisfying all provided requirements.
+        If not requirements are provided, the minimal amount of cores 
+        (self.MIN_NCPU) will be used.
+        """
+        # determining the biggest limit to satisfy, measured in the number of CPUs:
+        min_ncpu = max(filter(None, [
+            self.adjust_ncpu(ncpu or self.MIN_NCPU),
+            self.nthreads_to_ncpu(nthreads) if nthreads else None,
+            self.mem_gb_to_ncpu(mem_gb) if mem_gb else None,
+            self.storage_gb_to_ncpu(storage_gb) if storage_gb else None,
+        ]))
+        return JobResource(
+            machine_type=self, 
+            ncpu=min_ncpu, 
+            attach_disk_storage_gb=attach_disk_storage_gb
+        )
+
+    def mem_gb_to_ncpu(self, mem_gb: float) -> int:
+        """
+        Converts memory requirement to the number of CPU requirement.
+        """
+        ncpu = int(math.ceil(mem_gb / self.mem_gb_per_core))
+        return self.adjust_ncpu(ncpu)
+
+    def storage_gb_to_ncpu(self, storage_gb: float) -> int:
+        """
+        Converts storage requirement to the number of CPU requirement.
+
+        We want to avoid attaching disks (attaching a disk to an existing instance
+        might fail with `mkfs.ext4 ...` error, see:
+        https://batch.hail.populationgenomics.org.au/batches/7488/jobs/12
+        So this function will calculate the number of CPU to request so your jobs 
+        can be packed to fit the default instance's available storage 
+        (caluclated with self.calc_instance_disk_gb()).
+        """
+
+        # Hail Batch actually requests 5% lower number than the 
+        # requested one (e.g. "req_storage: 46.25G, actual_storage: 44.0 GiB"),
+        # so we will ask for a bigger number.        
+        adjusted_storage_gb = storage_gb * 1.05
+        njobs_fit_on_machine = self.calc_instance_disk_gb() / adjusted_storage_gb
+        return self.adjust_ncpu(int(math.ceil(self.ncpu / njobs_fit_on_machine)))
+
+    def nthreads_to_ncpu(self, nthreads: int) -> int:
+        return self.adjust_ncpu(math.ceil(nthreads / 2))
+
+    def adjust_ncpu(self, ncpu: int) -> int:
+        """
+        Adjust request number of CPU to a correct requestable number:
+        the nearest power of 2, not less than the minimal number of cores allowed.
+        """
+        if ncpu > self.ncpu:
+            ValueError(
+                f'Requesting more cores than available on {self.name} machine: '
+                f'{ncpu}>{self.ncpu}'
+            )
+
+        if ncpu < MachineType.MIN_NCPU:
+            logger.warning(
+                f'align: ncpu is adjusted: {ncpu} -> {MachineType.MIN_NCPU}, '
+                f'to the minimal amount to request from an instance.'
+            )
+            ncpu = MachineType.MIN_NCPU
+
+        # round to the nearest power of 2 (15 -> 16, 16 -> 16, 17 -> 32)
+        return int(pow(2, math.ceil(math.log2(ncpu))))
+    
+
+# Default Hail Batch machine. 
+# 
+# Bigger default number of cores (32 vs 16) would allow for more threads for alignment,
+# however would mean a smaller available storage (5G per core is reserved)
+STANDARD = MachineType(
+    'standard',
+    ncpu=16,  # 32
+    mem_gb_per_core=3.75,  # Total 60G
+    price_per_hour=1.0787,  # 32-core machine would cost 2.1574
+)
+
+# ~1.7 times memory per core than standard-32, but only 16 cores.
+# Total 104G - smaller than standard-32, but less expensive, so useful for 
+# memory consuming tools that don't benefit from multiple threads 
+HIGHMEM = MachineType(
+    'highmem', 
+    ncpu=16,
+    mem_gb_per_core=6.5,  
+    price_per_hour=1.3431,
+)
+
+
+@dataclass(init=False)
+class JobResource:
+    """
+    Represents a fraction of a Hail Batch instance.
+    """
+    def __init__(
+        self,
+        machine_type: MachineType,
+        ncpu: Optional[int] = None,
+        attach_disk_storage_gb: Optional[float] = None,
+    ):
+        """
+        :param machine_type: Hail Batch machine pool type
+        :param ncpu: number of CPU request. Will be used to calculate the fraction of
+            the machine to take. If not set, all machine's CPUs will be used.
+        :param attach_disk_storage_gb: if set to > MachineType.max_default_storage_gb, 
+            a larger disc will be attached by Hail Batch. 
+        """
+        self.machine_type = machine_type
+
+        self.fraction_of_full: float = 1.0
+        if ncpu is not None:
+            self.fraction_of_full = ncpu / self.machine_type.ncpu
+
+        self.attach_disk_storage_gb = None
+        if attach_disk_storage_gb is not None:
+            if self.fraction_of_full < 1:
+                raise ValueError(
+                    f'Storage can be overridden only when the entire machine is used, '
+                    f'not a fraction ({self.fraction_of_full}). '
+                    f'override_storage_gb={attach_disk_storage_gb}'
+                )
+            self.attach_disk_storage_gb = attach_disk_storage_gb
+
+    def get_mem_gb(self) -> float:
+        return self.get_ncpu() * self.machine_type.mem_gb_per_core
+
+    def get_java_mem_mb(self) -> int:
+        """
+        Calculate memory to pass to the `java -Xms` option. 
+        Subtracts 1G to start a java VM, and converts to MB as the option doesn't
+        support fractions of GB.
+        """
+        return int(math.floor(self.get_mem_gb() - 1))
+
+    def get_ncpu(self) -> int:
+        return int(self.machine_type.ncpu * self.fraction_of_full)
+    
+    def get_nthreads(self) -> int:
+        return self.get_ncpu() * MachineType.THREADS_ON_CPU
+
+    def get_storage_gb(self) -> float:
+        """
+        Calculate storage in GB
+        """
+        if self.attach_disk_storage_gb:
+            return self.attach_disk_storage_gb
+        else:
+            return self.machine_type.calc_instance_disk_gb() * self.fraction_of_full
+
+    def set_to_job(self, j: Job) -> 'JobResource':
+        """
+        Set the resources to a Job object. Return self to allow chaining, e.g.:
+        >>> nthreads = STANDARD.request_resources(nthreads=4).set_to_job(j).get_nthreads()
+        """
+        j.cpu(self.get_ncpu())
+        j.memory(f'{self.get_mem_gb()}G')
+        j.storage(f'{self.get_storage_gb()}G')
+        # returning self to allow command chaining.
+        return self
