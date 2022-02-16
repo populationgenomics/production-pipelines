dec3b831105d428c0bf59842dabccd052da97ee3
vivbak <vivian.bakiris@populationgenomics.org.au>
2022-02-16 22:12:30 +1100

    add linters

--- scripts/docker_sync.py
+++ scripts/docker_sync.py
@@ -1,4 +1,4 @@
-# Syncing Docker Images
+""" Syncing Docker Images"""
 
 import subprocess
 
@@ -11,25 +11,28 @@ def main():
     """ Move each docker image specified in json """
 
     subprocess.run(
-        ["gcloud", "auth", "configure-docker", "australia-southeast1-docker.pkg.dev"]
+        ['gcloud', 'auth', 'configure-docker', 'australia-southeast1-docker.pkg.dev'],
+        check=True,
     )
     # Eventually  create an optional command line input to parse any json
+    github_url = 'https://raw.githubusercontent.com/'
+    path_to_docker_json = 'populationgenomics/gatk-sv/main/input_values/dockers.json'
 
-    docker_json_url = "https://raw.githubusercontent.com/populationgenomics/gatk-sv/main/input_values/dockers.json"
+    docker_json_url = github_url + path_to_docker_json
 
     response = urlopen(docker_json_url)
     docker_json = json.loads(response.read())
-    docker_json.pop("name")
+    docker_json.pop('name')
 
     for key in docker_json:
-        initial_path = "docker://" + docker_json[key]
-        docker_image = docker_json[key].split("/")[-1]
+        initial_path = 'docker://' + docker_json[key]
+        docker_image = docker_json[key].split('/')[-1]
         destination_path = (
-            "docker://australia-southeast1-docker.pkg.dev/cpg-common/images/sv/"
+            'docker://australia-southeast1-docker.pkg.dev/cpg-common/images/sv/'
             + docker_image
         )
-        subprocess.run(["skopeo", "copy", initial_path, destination_path])
+        subprocess.run(['skopeo', 'copy', initial_path, destination_path], check='True')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
