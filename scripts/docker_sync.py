032f17f50f9ff3e31d4749c5e4a9bb79864e7bd1
vivbak <vivian.bakiris@populationgenomics.org.au>
2022-02-16 22:28:57 +1100

    Add optional docker json url

--- scripts/docker_sync.py
+++ scripts/docker_sync.py
@@ -1,24 +1,27 @@
-""" Syncing Docker Images"""
+""" A simple script to copy docker images specified in a json file to CPG's AR"""
 
 import subprocess
-
-from urllib.request import urlopen
-
 import json
+from urllib.request import urlopen
+import click
 
 
-def main():
-    """ Move each docker image specified in json """
+@click.command()
+@click.option('--docker_json_url')
+def main(docker_json_url):
+    """ Copies each docker image """
+
+    if not docker_json_url:
+        github_url = 'https://raw.githubusercontent.com/'
+        path_to_docker_json = (
+            'populationgenomics/gatk-sv/main/input_values/dockers.json'
+        )
+        docker_json_url = github_url + path_to_docker_json
 
     subprocess.run(
         ['gcloud', 'auth', 'configure-docker', 'australia-southeast1-docker.pkg.dev'],
         check=True,
     )
-    # Eventually  create an optional command line input to parse any json
-    github_url = 'https://raw.githubusercontent.com/'
-    path_to_docker_json = 'populationgenomics/gatk-sv/main/input_values/dockers.json'
-
-    docker_json_url = github_url + path_to_docker_json
 
     response = urlopen(docker_json_url)
     docker_json = json.loads(response.read())
@@ -35,4 +38,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    main()  # pylint: disable=E1120
