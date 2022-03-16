3b5e8921196de3944072a254c944da13caa4744b
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-03-16 13:17:20 +1100

    Bump version: 0.2.11 → 0.3.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import setuptools
 
 setuptools.setup(
     name='cpg-pipes',
-    version='0.2.11',
+    version='0.3.0',
     description='Hail Batch bioinformatics pipelines',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
