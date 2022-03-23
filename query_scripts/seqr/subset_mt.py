1052bdff550c90ff57b20f6930078a21c03bd317
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-03-23 19:54:06 +1100

    Update seqr query scripts

--- query_scripts/seqr/subset_mt.py
+++ query_scripts/seqr/subset_mt.py
@@ -9,7 +9,9 @@ Subsets matrix table to a list of samples and annotate with SeqrGenotypesSchema.
 import logging
 import click
 import hail as hl
-from lib.model.seqr_mt_schema import SeqrGenotypesSchema
+from lib.model.base_mt_schema import row_annotation
+from lib.model.seqr_mt_schema import BaseMTSchema
+from cpg_utils.hail import init_query_service
 
 logger = logging.getLogger(__file__)
 
@@ -38,15 +40,67 @@ def main(
     """
     Entry point
     """
-    hl.init(default_reference='GRCh38')
+    init_query_service()
 
     mt = hl.read_matrix_table(mt_path)
+
     logger.info('Subsetting to the requested set of samples')
     if subset_tsv_path:
         mt = _subset_samples_and_variants(mt, subset_tsv_path)
 
+    # tmp_path = 'gs://cpg-circa-main-tmp/unittest/test_seqr_loader_annotate_dataset/' \
+    #            'dataset-tmp-allchroms.mt'
+    # # if not hl.hadoop_exists(tmp_path + '/_SUCCESS'):
+    # #     mt = hl.filter_intervals(mt, [
+    # #         hl.parse_locus_interval(f'chr{i}:1234567-1235567')
+    # #         for i in range(1, 22+1)
+    # #     ])
+    # #     mt.write(tmp_path, overwrite=True)
+    # mt = hl.read_matrix_table(tmp_path)
+    # mt.count()
+    # mt.rows().show()
+    # mt.entries().show()
+    # 
     logger.info('Annotating genotypes')
     mt = _compute_genotype_annotated_vcf(mt)
+
+    # is_called = hl.is_defined(mt.GT)
+    # _genotype_fields = {
+    #     'num_alt': hl.if_else(is_called, mt.GT.n_alt_alleles(), -1),
+    #     'gq': hl.if_else(is_called, mt.GQ, hl.missing(hl.tint)),
+    #     'ab': hl.bind(
+    #         lambda total: hl.if_else(
+    #             (is_called) & (total != 0) & (hl.len(mt.AD) > 1),
+    #             hl.float(mt.AD[1] / total),
+    #             hl.missing(hl.tfloat),
+    #         ),
+    #         hl.sum(mt.AD),
+    #     ),
+    #     'dp': hl.if_else(
+    #         is_called, hl.int(hl.min(mt.DP, 32000)), hl.missing(hl.tfloat)
+    #     ),
+    #     'sample_id': mt.s,
+    # }
+    # mt = mt.annotate_rows(
+    #     genotypes=hl.agg.collect(hl.struct(**_genotype_fields))
+    # )
+    # 
+    # logger.info('Annotating gq')
+    # def _genotype_filter_samples(filter):
+    #     return hl.set(mt.genotypes.filter(filter).map(lambda g: g.sample_id))
+    # 
+    # start = 0
+    # end = 95
+    # step = 95
+    # mt = mt.annotate_rows(samples_no_call=hl.struct(
+    #     **{
+    #         ('%i_to_%i' % (i, i + step)):
+    #             _genotype_filter_samples(
+    #                 lambda g: ((g.gq >= i) & (g.gq < i + step))
+    #             )
+    #         for i in range(start, end, step)
+    #     }
+    # ))
 
     # Fixing NaN values in AS_MQ
     mt.annotate_rows(
@@ -55,6 +109,32 @@ def main(
                 ~hl.is_nan(mt.info.AS_MQ),  # pylint: disable=invalid-unary-operand-type
                 mt.info.AS_MQ,
                 0,
+            ),
+        )
+    )
+
+    # AS_MQ and InbreedingCoeff can be NaN, need to fix that to avoid ES loader 
+    # failures like: 
+    # `is.hail.relocated.org.elasticsearch.hadoop.rest.EsHadoopRemoteException: 
+    # mapper_parsing_exception: failed to parse field [info_AS_MQ] of type [double]`
+    # in: https://batch.hail.populationgenomics.org.au/batches/6621/jobs/6
+    mt = mt.annotate_rows(
+        info=mt.info.annotate(
+            AS_MQ=hl.if_else(
+                hl.is_nan(mt.info.AS_MQ), 
+                0.0, 
+                mt.info.AS_MQ
+            ),
+            InbreedingCoeff=hl.if_else(
+                hl.is_nan(mt.info.InbreedingCoeff), 
+                0.0, 
+                mt.info.InbreedingCoeff
+            ),
+            # https://batch.hail.populationgenomics.org.au/batches/6973/jobs/12
+            AS_InbreedingCoeff=hl.if_else(
+                hl.is_nan(mt.info.AS_InbreedingCoeff), 
+                0.0, 
+                mt.info.AS_InbreedingCoeff
             ),
         )
     )
@@ -97,6 +177,81 @@ def _subset_samples_and_variants(mt, subset_tsv_path: str) -> hl.MatrixTable:
         f'out of {count_before} samples in vds'
     )
     return mt
+
+
+class SeqrGenotypesSchema(BaseMTSchema):
+    @row_annotation()
+    def genotypes(self):
+        return hl.agg.collect(hl.struct(**self._genotype_fields()))
+
+    @row_annotation(fn_require=genotypes)
+    def samples_no_call(self):
+        return self._genotype_filter_samples(lambda g: g.num_alt == -1)
+
+    @row_annotation(fn_require=genotypes)
+    def samples_num_alt(self, start=1, end=3, step=1):
+        return hl.struct(
+            **{
+                ('%i' % i): 
+                self._genotype_filter_samples(lambda g: g.num_alt == i)
+                for i in range(start, end, step)
+            }
+        )
+
+    # @row_annotation(fn_require=genotypes)
+    # def samples_gq(self, start=0, end=95, step=5):
+    #     # struct of x_to_y to a set of samples in range of x and y for gq.
+    #     return hl.struct(
+    #         **{
+    #             ('%i_to_%i' % (i, i + step)): 
+    #             self._genotype_filter_samples(
+    #                 lambda g: ((g.gq >= i) & (g.gq < i + step))
+    #             )
+    #             for i in range(start, end, step)
+    #         }
+    #     )
+
+    @row_annotation(fn_require=genotypes)
+    def samples_ab(self, start=0, end=45, step=5):
+        # struct of x_to_y to a set of samples in range of x and y for ab.
+        return hl.struct(
+            **{
+                '%i_to_%i'
+                % (i, i + step): self._genotype_filter_samples(
+                    lambda g: (
+                        (g.num_alt == 1)
+                        & ((g.ab * 100) >= i)
+                        & ((g.ab * 100) < i + step)
+                    )
+                )
+                for i in range(start, end, step)
+            }
+        )
+
+    def _genotype_filter_samples(self, filter):
+        # Filter on the genotypes.
+        return hl.set(self.mt.genotypes.filter(filter).map(lambda g: g.sample_id))
+
+    def _genotype_fields(self):
+        # Convert the mt genotype entries into num_alt, gq, ab, dp, and sample_id.
+        is_called = hl.is_defined(self.mt.GT)
+        return {
+            'num_alt': hl.if_else(is_called, self.mt.GT.n_alt_alleles(), -1),
+            'gq': hl.if_else(is_called, self.mt.GQ, hl.null(hl.tint)),
+            'ab': hl.bind(
+                lambda total: hl.if_else(
+                    (is_called) & (total != 0) & (hl.len(self.mt.AD) > 1),
+                    hl.float(self.mt.AD[1] / total),
+                    hl.null(hl.tfloat),
+                ),
+                hl.sum(self.mt.AD),
+            ),
+            'dp': hl.if_else(
+                is_called, hl.int(hl.min(self.mt.DP, 32000)), hl.null(hl.tfloat)
+            ),
+            'sample_id': self.mt.s,
+        }
+
 
 
 def _compute_genotype_annotated_vcf(
