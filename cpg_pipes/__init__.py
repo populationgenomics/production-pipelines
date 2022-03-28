e97c98688f357b0ece816c3357716e5fd591dcdb
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-03-28 12:28:21 +1100

    Add cpg_pipes/__init__.py to fix linting

---
+++ cpg_pipes/__init__.py
@@ -0,0 +1,35 @@
+import coloredlogs
+import importlib.metadata
+
+from .providers.storage import (
+    Path,
+    to_path,
+    Namespace,
+)
+
+
+coloredlogs.install(
+    level='DEBUG', 
+    fmt='%(asctime)s %(levelname)s (%(name)s %(lineno)s): %(message)s'
+)
+
+
+def get_package_name() -> str:
+    """
+    Get name of the package.
+    """
+    return __name__.split('.', 1)[0]
+
+
+def get_package_path() -> Path:
+    """
+    Get local install path of the package.
+    """
+    return to_path(__file__).parent.absolute()
+
+
+def get_version() -> str:
+    """
+    Get package version.
+    """
+    return importlib.metadata.version(get_package_name())
