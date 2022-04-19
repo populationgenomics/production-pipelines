84cccc31d179f735229223d0ec7bbd7fcb293c37
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-04-19 20:50:00 +1000

    Update from initial

--- pipelines/gatk_sv/gatk_sv.py
+++ pipelines/gatk_sv/gatk_sv.py
@@ -17,28 +17,24 @@ import json
 import logging
 import os
 from os.path import join, dirname
-from typing import Dict, List, Optional
+from typing import Any
 
 import click
 from analysis_runner.cromwell import (
     run_cromwell_workflow_from_repo_and_get_outputs,
     CromwellOutputType,
 )
+from hailtop.batch import Batch
 
-from cpg_pipes import images
-from cpg_pipes.hb.batch import job_name
+from cpg_pipes import images, Path
+from cpg_pipes.hb.batch import make_job_name
 from cpg_pipes.hb.command import wrap_command
-from cpg_pipes.pipeline.cli_opts import pipeline_click_options
-from cpg_pipes.pipeline.cohort import Cohort
-from cpg_pipes.pipeline.pipeline import Pipeline, skipped, stage
-from cpg_pipes.pipeline.project import Project
-from cpg_pipes.pipeline.sample import Sample
-from cpg_pipes.pipeline.stage import StageInput, StageOutput, CohortStage, SampleStage, \
-    ProjectStage
+from cpg_pipes.pipeline import stage, SampleStage, StageOutput, DatasetStage, \
+    CohortStage, pipeline_click_options, create_pipeline
+from cpg_pipes.pipeline.pipeline import ExpectedResultT, StageInput
+from cpg_pipes.targets import Sample, Dataset, Cohort
 
 logger = logging.getLogger(__file__)
-logging.basicConfig(format='%(levelname)s (%(name)s %(lineno)s): %(message)s')
-logger.setLevel(logging.INFO)
 
 
 GATK_SV_COMMIT = 'c9e88f056fa154a01e2fcd7f8f0703342537a06e'
@@ -47,59 +43,57 @@ SV_CALLERS = ['manta', 'wham']
 ACCESS_LEVEL = os.environ['ACCESS_LEVEL']
 
 
-def get_dockers(keys: List[str]) -> Dict[str, object]:
+def get_dockers(keys: list[str]) -> dict[str, str]:
     """parse the WDL inputs with docker images"""
     with open(join(dirname(__file__), 'dockers.json')) as f:
         return {k: v for k, v in json.load(f).items() if k in keys}
 
 
-def get_references(keys: List[str]) -> Dict[str, object]:
+def get_references(keys: list[str]) -> dict[str, str]:
     """parse the WDL inputs with reference files"""
     with open(join(dirname(__file__), 'references.json')) as f:
         return {k: v for k, v in json.load(f).items() if k in keys}
 
 
 def add_gatksv_job(
-    pipeline,
+    batch: Batch,
+    dataset_name: str,
     wfl_name: str,
-    input_dict: Dict[str, object], 
-    expected_out_dict: Dict[str, str],
-    project_name: Optional[str] = None,
-    sample_id: Optional[str] = None,
+    input_dict: dict[str, Any], 
+    expected_out_dict: dict[str, Path],
+    sample_id: str | None = None,
 ):
     """
     Generic function to add a job that would run one GATK-SV workflow.
     """
     # Where Cromwell writes the output. 
     # Will be different from paths in expected_out_dict:
-    output_suffix = f'gatk_sv/output/{wfl_name}'
-    if project_name: 
-        output_suffix = join(output_suffix, project_name)
+    output_prefix = f'gatk_sv/output/{wfl_name}/{dataset_name}'
     if sample_id: 
-        output_suffix = join(output_suffix, sample_id)
+        output_prefix = join(output_prefix, sample_id)
 
     outputs_to_collect = dict()
     for key in expected_out_dict.keys():
         outputs_to_collect[key] = CromwellOutputType.single_path(f'{wfl_name}.{key}')
     
-    job_prefix = job_name(wfl_name, sample=sample_id, project=project_name)
+    job_prefix = make_job_name(wfl_name, sample=sample_id, dataset=dataset_name)
     output_dict = run_cromwell_workflow_from_repo_and_get_outputs(
-        b=pipeline.b,
+        b=batch,
         job_prefix=job_prefix,
-        dataset=pipeline.analysis_project.stack,
+        dataset=dataset_name,
         access_level=ACCESS_LEVEL,
         repo='gatk-sv',
         commit=GATK_SV_COMMIT,
         cwd='wdl',
         workflow=f'{wfl_name}.wdl',
         libs=['.'],
-        output_suffix=output_suffix,
+        output_prefix=output_prefix,
         input_dict={f'{wfl_name}.{k}': v for k, v in input_dict.items()},
         outputs_to_collect=outputs_to_collect,
         driver_image=images.DRIVER_IMAGE,
     )
 
-    copy_j = pipeline.b.new_job(f'{job_prefix}: copy outputs')
+    copy_j = batch.new_job(f'{job_prefix}: copy outputs')
     copy_j.image(images.DRIVER_IMAGE)
     cmds = []
     for key, resource in output_dict.items():
@@ -110,14 +104,16 @@ def add_gatksv_job(
     return output_dict, copy_j
 
 
-@skipped(assume_results_exist=True)
 @stage
 class GatherSampleEvidence(SampleStage):
     """
     https://github.com/broadinstitute/gatk-sv#gathersampleevidence    
     """
-    def expected_result(self, sample: Sample) -> Dict[str, str]:
-        d = dict()
+    def expected_outputs(self, sample: Sample) -> dict[str, Path]:
+        """
+        Expectes to produce coverage counts, and VCF for each variant caller
+        """
+        d: dict[str, Path] = dict()
         fname_by_key = {
             'coverage_counts': 'coverage_counts.tsv.gz'
         }
@@ -131,18 +127,19 @@ class GatherSampleEvidence(SampleStage):
             fname_by_key[f'pesr_{k}_index'] = f'pesr_{k}.txt.gz.tbi'
 
         for key, fname in fname_by_key.items():
-            stage = self.name.lower()
-            d[key] = join(
-                sample.project.get_bucket(), 'gatk_sv', stage, sample.id + '-' + fname
-            )
+            stage_name = self.name.lower()
+            d[key] = sample.dataset.get_bucket() / 'gatk_sv' / stage_name / sample.id + '-' + fname
         return d
 
     def queue_jobs(self, sample: Sample, inputs: StageInput) -> StageOutput:
-        cram_path = f'{sample.project.get_bucket()}/cram/{sample.id}.cram'
+        """
+        Queue jobs
+        """
+        cram_path = sample.dataset.get_bucket() / 'cram' / f'{sample.id}.cram'
 
-        input_dict: Dict[str, object] = {
-            'bam_or_cram_file': cram_path,
-            'bam_or_cram_index': cram_path + '.crai',
+        input_dict: dict[str, str] = {
+            'bam_or_cram_file': str(cram_path),
+            'bam_or_cram_index': str(cram_path) + '.crai',
             'sample_id': sample.id,
             # 'revise_base_cram_to_bam': True,
         }
@@ -174,28 +171,30 @@ class GatherSampleEvidence(SampleStage):
             # 'melt_standard_vcf_header',
         ]))
 
-        expected_d = self.expected_result(sample)
+        expected_d = self.expected_outputs(sample)
 
         output_dict, j = add_gatksv_job(
-            self.pipe,
+            batch=self.b,
+            dataset_name=sample.dataset.name,
             wfl_name=self.name,
             input_dict=input_dict,
             expected_out_dict=expected_d,
             sample_id=sample.id,
-            project_name=sample.project.name,
         )
 
         return self.make_outputs(sample, data=output_dict, jobs=[j])
 
 
-@skipped(assume_results_exist=True)
-@stage(requires_stages=GatherSampleEvidence)
-class EvidenceQC(ProjectStage):
+@stage(required_stages=GatherSampleEvidence)
+class EvidenceQC(DatasetStage):
     """
     # https://github.com/broadinstitute/gatk-sv#evidenceqc
     """
-    def expected_result(self, project: Project) -> Dict[str, str]:
-        d = dict()
+    def expected_outputs(self, dataset: Dataset) -> dict[str, Path]:
+        """
+        Expected to return a bunch of batch-level summary files.
+        """
+        d: dict[str, Path] = dict()
         fname_by_key = {
             'ploidy_matrix':       'ploidy_matrix.bed.gz',
             'ploidy_plots':        'ploidy_plots.tar.gz',
@@ -211,17 +210,19 @@ class EvidenceQC(ProjectStage):
                 fname_by_key[f'{caller}_qc_{k}'] = f'{caller}_QC.outlier.{k}'
 
         for key, fname in fname_by_key.items():
-            d[key] = join(project.get_bucket(), 'gatk_sv', self.name.lower(), fname)
+            d[key] = dataset.get_bucket() / 'gatk_sv' / self.name.lower() / fname
         return d
 
-    def queue_jobs(self, project: Project, inputs: StageInput) -> StageOutput:
-
+    def queue_jobs(self, dataset: Dataset, inputs: StageInput) -> StageOutput:
+        """
+        Queue jobs
+        """
         d = inputs.as_dict_by_target(GatherSampleEvidence)
 
-        sids = [s.id for s in project.get_samples()]
+        sids = [s.id for s in dataset.get_samples()]
 
         input_dict = {
-            'batch': project.name,
+            'batch': dataset.name,
             'samples': sids,
             'run_vcf_qc': True,  # generates <caller>_qc_low/<caller>_qc_high
             'counts': [d[sid]['coverage_counts'] for sid in sids],
@@ -241,41 +242,46 @@ class EvidenceQC(ProjectStage):
             'wgd_scoring_mask',
         ]))
 
-        expected_d = self.expected_result(project)
+        expected_d = self.expected_outputs(dataset)
 
         output_dict, j = add_gatksv_job(
-            self.pipe,
+            batch=self.b,
+            dataset_name=dataset.name,
             wfl_name=self.name,
             input_dict=input_dict,
             expected_out_dict=expected_d,
-            project_name=project.name,
         )
 
-        return self.make_outputs(project, data=output_dict, jobs=[j])
+        return self.make_outputs(dataset, data=output_dict, jobs=[j])
 
 
-@stage(requires_stages=[GatherSampleEvidence])
+@stage(required_stages=[GatherSampleEvidence])
 class TrainGCNV(CohortStage):
     """
     # https://github.com/populationgenomics/gatk-sv/blob/main/wdl/TrainGCNV.wdl
     """
-    def expected_result(self, cohort: Cohort) -> Dict[str, str]:
+    def expected_outputs(self, cohort: Cohort) -> dict[str, Path]:
+        """
+        Expected to return 2 tarballs
+        """
         d = dict()
         fname_by_key = {
             'cohort_contig_ploidy_model_tar': 'cohort_contig_ploidy_model.tar',
             'cohort_contig_ploidy_calls_tar': 'cohort_contig_ploidy_calls.tar',
         }
         for key, fname in fname_by_key.items():
-            d[key] = join(self.pipe.analysis_bucket, 'gatk_sv', self.name.lower(), fname)
+            d[key] = cohort.analysis_dataset.get_bucket() / 'gatk_sv' / self.name.lower() / fname
         return d
 
     def queue_jobs(self, cohort: Cohort, inputs: StageInput) -> StageOutput:
-
+        """
+        Queue jobs
+        """
         d = inputs.as_dict_by_target(GatherSampleEvidence)
         
-        sids = cohort.get_all_sample_ids()
+        sids = cohort.get_sample_ids()
         input_dict = {
-            'cohort': cohort.unique_id,
+            'cohort': cohort.target_id,
             'samples': sids,
             'count_files': [d[sid]['coverage_counts'] for sid in sids],
             'ref_copy_number_autosomal_contigs': 2,
@@ -299,10 +305,11 @@ class TrainGCNV(CohortStage):
             'contig_ploidy_priors',
         ]))
 
-        expected_d = self.expected_result(cohort)
+        expected_d = self.expected_outputs(cohort)
 
         output_dict, j = add_gatksv_job(
-            self.pipe,
+            batch=self.b,
+            dataset_name=cohort.analysis_dataset.name,
             wfl_name=self.name,
             input_dict=input_dict,
             expected_out_dict=expected_d,
@@ -314,23 +321,16 @@ class TrainGCNV(CohortStage):
 @click.command()
 @pipeline_click_options
 def main(
-    input_projects: List[str],
-    output_version: str,
     **kwargs,
-):  # pylint: disable=missing-function-docstring
-    assert input_projects
-    title = (
-        f'GATK-SV: samples from: {", ".join(input_projects)}'
-        f', version {output_version}'
-    )
-    pipeline = Pipeline(
+): 
+    """
+    GATK-SV workflow.
+    """
+    pipeline = create_pipeline(
         name='gatk_sv',
-        title=title,
-        input_projects=input_projects,
-        output_version=output_version,
         **kwargs
     )
-    pipeline.submit_batch()
+    pipeline.run()
 
 
 if __name__ == '__main__':
