2328afb51fb90b8e22f7f21795a8748d9d5f26ae
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-05-13 15:19:06 +1000

    Run copy_seqr_refdata.py on dataproc

---
+++ cpg_pipes/dataproc_scripts/copy_seqr_refdata.py
@@ -0,0 +1,31 @@
+"""
+Copy seqr reference data to gs://cpg-reference.
+"""
+import click
+import hail as hl
+import logging
+logger = logging.getLogger(__file__)
+logging.basicConfig(format='%(levelname)s (%(name)s %(lineno)s): %(message)s')
+logger.setLevel(logging.INFO)
+
+
+@click.command()
+@click.argument('version')
+def main(version):
+    """On dataproc: copy seqr reference data"""
+    hl.init(default_reference='GRCh38')
+    ht = hl.read_table(
+        'gs://seqr-reference-data/GRCh38/all_reference_data/v2'
+        '/combined_reference_data_grch38-2.0.4.ht'
+    )
+    assert version
+    out_bucket = f'gs://cpg-reference/seqr/{version}'
+    ht.write(
+        f'{out_bucket}/combined_reference_data_grch38-2.0.4.ht'
+    )
+    ht = hl.read_table('gs://seqr-reference-data/GRCh38/clinvar/clinvar.GRCh38.ht')
+    ht.write(f'{out_bucket}/clinvar.GRCh38.ht')
+
+
+if __name__ == '__main__':
+    main()
--- pipelines/building_refdata/copy_seqr_refdata.py
+++ pipelines/building_refdata/copy_seqr_refdata.py
@@ -1,64 +1,33 @@
 """
-Copy seqr reference data to gs://cpg-reference
-
-Would first clean up existing data.
+Copy seqr reference data to gs://cpg-reference.
 """
 import os
-from os.path import basename
 
+import click
+from analysis_runner import dataproc
 from cpg_pipes import to_path
 from cpg_pipes.hb.batch import setup_batch
-from cpg_pipes.hb.command import wrap_command, python_command, GCLOUD_CMD
 
-src_seqr_bucket = 'gs://cpg-seqr-reference-data'
-src_referen_path = f'{src_seqr_bucket}/GRCh38/all_reference_data/v2/combined_reference_data_grch38-2.0.3.ht'
-src_clinvar_path = f'{src_seqr_bucket}/GRCh38/clinvar/clinvar.GRCh38.ht'
-
-dst_seqr_bucket = 'gs://cpg-reference/seqr'
+VERSION = 'v0'
 
 
+@click.command()
 def main():
+    """Copy seqr reference data under ."""
     b = setup_batch(
         description='Copy seqr reference data',
         billing_project=os.environ['HAIL_BILLING_PROJECT'],
         hail_bucket=to_path('gs://cpg-reference/hail-tmp'),
     )
-    j = b.new_job('Copy reference data')
-    j.image(os.environ['CPG_DRIVER_IMAGE'])
-    python_cmd = f"""
-    import logging
-    logger = logging.getLogger(__file__)
-    logging.basicConfig(format='%(levelname)s (%(name)s %(lineno)s): %(message)s')
-    logger.setLevel(logging.INFO)
-    
-    import asyncio
-    import hail as hl
-    asyncio.get_event_loop().run_until_complete(
-        hl.init_batch(
-            default_reference='GRCh38',
-            billing_project='{os.environ["HAIL_BILLING_PROJECT"]}',
-            remote_tmpdir='{to_path("gs://cpg-reference/hail-tmp")}',
-        )
+    dataproc.hail_dataproc_job(
+        b,
+        f'cpg_pipes/dataproc_scripts/copy_seqr_refdata.py {VERSION}',
+        max_age='8h',
+        num_secondary_workers=50,
+        job_name='Copy Seqr reference data',
     )
-    ht = hl.read_table('{src_referen_path}')
-    ht.write('{dst_seqr_bucket}/{basename(src_referen_path)}', overwrite=True)
-
-    ht = hl.read_table('{src_clinvar_path}')
-    ht.write('{dst_seqr_bucket}/{basename(src_clinvar_path)}', overwrite=True)
-    """
-
-    cmd = f"""
-    set -o pipefail
-    set -ex
-    {GCLOUD_CMD}
-
-    cat << EOT >> script.py
-    {python_cmd}
-    EOT
-    python3 script.py
-    """
-    j.command(wrap_command(cmd, monitor_space=True))
     b.run(wait=False)
 
 
-main()
+if __name__ == '__main__':
+    main()
