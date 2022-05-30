ced6ecbf8427a6b5f5eb2892ba02266de1f020b2
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-05-30 14:22:27 +1000

    Update

--- cpg_pipes/providers/images.py
+++ cpg_pipes/providers/images.py
@@ -5,12 +5,12 @@ import os
 
 
 AVAILABLE = {
-    'gatk': 'gatk:4.2.3.0',
+    'gatk': 'gatk:4.2.6.1',
     'bcftools': 'bcftools:1.10.2--h4f4756c_2',
     'sm-api': 'sm-api:4.0.0',
     'bwa': 'bwa:v0',
     'bwamem2': 'bwamem2:v0',
-    'dragmap': 'dragmap:1.2.1',
+    'dragmap': 'dragmap:1.3.0',
     'samtools': 'picard_samtools:v0',
     'picard': 'picard_samtools:v0',
     'picard_samtools': 'picard_samtools:v0',
@@ -20,6 +20,7 @@ AVAILABLE = {
     'verify-bam-id': 'verify-bam-id:1.0.1',
     'multiqc': 'multiqc:v1.12',
     'fastqc': 'fastqc:v0.11.9_cv8',
+    'hail': 'australia-southeast1-docker.pkg.dev/analysis-runner/images/driver:7d00c4871b2e96f50bae208e4184c3c4789a2fa4-hail-83056327f288917537531475ba475287b413db1c',
 }
 
 
@@ -40,10 +41,3 @@ class Images:
                 f'Image {name} is not available. Known images: {AVAILABLE}'
             )
         return os.path.join(self._prefix, AVAILABLE[name])
-
-    @staticmethod
-    def driver_image() -> str:
-        """
-        Hail "driver" image.
-        """
-        return os.environ['CPG_DRIVER_IMAGE']
