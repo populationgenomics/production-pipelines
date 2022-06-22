cf9d4ec27064276f3f5dc69ffa76bfc63b0b17c1
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-06-22 13:20:03 +1000

    Add options to populate_sm_analysis.py

--- scripts/populate_sm_analysis.py
+++ scripts/populate_sm_analysis.py
@@ -30,39 +30,73 @@ input_provider.populate_cohort(
 
 status = CpgStatusReporter(smdb)
 
-for sample in cohort.get_samples():
-    if (path := sample.get_cram_path().path).exist():
+POPULATE_SAMPLES = True
+POPULATE_JOINT_CALL = False
+POPULATE_ES_INDEX = False
+
+
+if POPULATE_SAMPLES:
+    for sample in cohort.get_samples():
+        if (path := sample.get_cram_path().path).exists():
+            status.create_analysis(
+                str(path),
+                analysis_type='cram',
+                analysis_status='completed',
+                target=sample,
+                meta=sample.get_job_attrs() | dict(
+                    size=path.stat().st_size,
+                    sequencing_type=cohort.sequencing_type.value,
+                ),
+            )
+        if (path := sample.get_gvcf_path().path).exists():
+            status.create_analysis(
+                str(path),
+                analysis_type='gvcf',
+                analysis_status='completed',
+                target=sample,
+                meta=sample.get_job_attrs() | dict(
+                    size=path.stat().st_size,
+                    sequencing_type=cohort.sequencing_type.value,
+                ),
+            )
+
+if POPULATE_JOINT_CALL:
+    h = cohort.alignment_inputs_hash()
+    path = cohort.analysis_dataset.prefix() / 'mt' / f'{h}.mt'
+    if exists(path):
         status.create_analysis(
             str(path),
-            analysis_type='cram',
+            analysis_type='joint-calling',
             analysis_status='completed',
-            target=sample,
-            meta=sample.get_job_attrs() | dict(
-                size=path.stat().st_size,
-                sequencing_type=cohort.sequencing_type.value,
-            ),
-        )
-    if (path := sample.get_gvcf_path().path).exist():
-        status.create_analysis(
-            str(path),
-            analysis_type='gvcf',
-            analysis_status='completed',
-            target=sample,
-            meta=sample.get_job_attrs() | dict(
-                size=path.stat().st_size,
+            target=cohort,
+            meta=cohort.get_job_attrs() | dict(
                 sequencing_type=cohort.sequencing_type.value,
             ),
         )
 
-h = cohort.alignment_inputs_hash()
-path = cohort.analysis_dataset.prefix() / 'mt' / f'{h}.mt'
-if exists(path):
-    status.create_analysis(
-        str(path),
-        analysis_type='joint-calling',
-        analysis_status='completed',
-        target=cohort,
-        meta=cohort.get_job_attrs() | dict(
-            sequencing_type=cohort.sequencing_type.value,
-        ),
-    )
+if POPULATE_ES_INDEX:
+    for name in [
+        'acute-care-genome-2022_0620_1843_l4h8u',
+        'ravenscroft-arch-genome-2022_0618_1137_4qfyn',
+        'circa-genome-2022_0618_1137_4qfyn',
+        'ohmr3-mendelian-genome-2022_0618_1137_4qfyn',
+        # 'validation-genome-2022_0618_1137_4qfyn',
+        'mito-disease-genome-2022_0618_1137_4qfyn',
+        'perth-neuro-genome-2022_0618_1137_4qfyn',
+        'ohmr4-epilepsy-genome-2022_0618_1137_4qfyn',
+        'hereditary-neuro-genome-2022_0618_1137_4qfyn',
+        'ravenscroft-rdstudy-genome-2022_0618_1137_4qfyn',
+        'heartkids-genome-2022_0618_1137_4qfyn',
+    ]:
+        ds_name = name.split('-genome-')[0]
+        print(f'Adding {ds_name}')
+        dataset = cohort.create_dataset(ds_name)
+        status.create_analysis(
+            str(name),
+            analysis_type='es-index',
+            analysis_status='completed',
+            target=dataset,
+            meta=dataset.get_job_attrs() | dict(
+                sequencing_type=cohort.sequencing_type.value,
+            ),
+        )
