3dda98732a60f6a5418506aee9eb859b9a0e2795
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-06-24 14:44:24 +1000

    Fix annotating sample type

--- cpg_pipes/dataproc_scripts/seqr/mt_to_es.py
+++ cpg_pipes/dataproc_scripts/seqr/mt_to_es.py
@@ -90,9 +90,6 @@ def main(
     rg38.add_liftover(liftover_path, rg37)
     mt = mt.annotate_rows(rg37_locus=hl.liftover(mt.locus, 'GRCh37'))
 
-    # TODO: remote this. Fixing sequencing type
-    mt = mt.annotate_globals(sampleType='WGS')
-        
     logger.info('Getting rows and exporting to the ES')
     row_table = elasticsearch_row(mt)
     es_shards = _mt_num_shards(mt)
