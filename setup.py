3c3f13d109a90dfc5240b6602a62a037ad3de0c0
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-07-29 05:02:04 +1000

    Updates from initial

--- setup.py
+++ setup.py
@@ -7,13 +7,14 @@ import setuptools
 
 setuptools.setup(
     name='cpg-pipes',
-    version='0.3.0',
+    version='0.3.5',
     description='Hail Batch bioinformatics pipelines',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/populationgenomics/production-pipelines',
     license='MIT',
-    packages=['cpg_pipes'],
+    packages=['cpg_pipes']
+    + ['cpg_pipes.' + p for p in sorted(setuptools.find_packages('./cpg_pipes'))],
     package_data={'cpg_pipes': ['filter_cutoffs.yaml']},
     include_package_data=True,
     zip_safe=False,
@@ -23,11 +24,11 @@ setuptools.setup(
         if fname.endswith('.py')
     ],
     install_requires=[
-        'click',
-        'click-config-file',
+        'cpg-utils',
         'pandas',
         'hail>=0.2.91',
         'cpg-gnomad',  # github.com/populationgenomics/gnomad_methods
+        'peddy',
         'google-cloud-storage',
         'google-cloud-secret-manager',
         'fsspec',
@@ -36,6 +37,7 @@ setuptools.setup(
         'cloudpathlib[all]',
         'coloredlogs',
         'types-PyYAML',  # https://mypy.readthedocs.io/en/stable/getting_started.html#library-stubs-and-typeshed
+        'python-slugify',
     ],
     keywords='bioinformatics',
     classifiers=[
