7eccc6181b2b87f86e432f13b5482ec8bf055086
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-08-29 18:31:27 +1000

    Add haplotype_caller and genotype stage

--- jobs/picard.py
+++ jobs/picard.py
@@ -14,6 +14,110 @@ from cpg_utils.workflows.filetypes import CramPath
 from cpg_utils.workflows.utils import can_reuse, exists
 
 
+def get_intervals(
+    b: hb.Batch,
+    scatter_count: int,
+    intervals_path: Path | str | None = None,
+    job_attrs: dict[str, str] | None = None,
+    output_prefix: Path | None = None,
+) -> tuple[Job | None, list[hb.ResourceFile | None]]:
+    """
+    Add a job that split genome into intervals to parallelize variant calling.
+
+    As input interval file, takes intervals_path if provided, otherwise checks refs
+    for the intervals of provided sequencing_type.
+
+    This job calls picard's IntervalListTools to scatter the input interval list
+    into scatter_count sub-interval lists, inspired by this WARP task :
+    https://github.com/broadinstitute/warp/blob/bc90b0db0138747685b459c83ce52c8576ce03cd/tasks/broad/Utilities.wdl
+
+    Note that we use the mode INTERVAL_SUBDIVISION instead of
+    BALANCING_WITHOUT_INTERVAL_SUBDIVISION_WITH_OVERFLOW. Modes other than
+    INTERVAL_SUBDIVISION produce an unpredicted number of intervals. WDL can handle
+    that, but Hail Batch is not dynamic and have to expect certain number of output
+    files.
+    """
+    assert scatter_count > 0
+
+    sequencing_type = get_config()['workflow']['sequencing_type']
+
+    intervals_path = intervals_path or get_config()['workflow'].get('intervals_path')
+    if not intervals_path:
+        # Did we cache split intervals for this sequencing_type?
+        cache_bucket = (
+            reference_path('intervals_prefix')
+            / sequencing_type
+            / f'{scatter_count}intervals'
+        )
+        if exists(cache_bucket / '1.interval_list'):
+            return None, [
+                b.read_input(str(cache_bucket / f'{idx + 1}.interval_list'))
+                for idx in range(scatter_count)
+            ]
+        # Taking intervals file for the sequencing_type.
+        intervals_path = reference_path(
+            f'broad/{sequencing_type}_calling_interval_lists',
+        )
+
+    if scatter_count == 1:
+        return None, [b.read_input(str(intervals_path))]
+
+    job_attrs = (job_attrs or {}) | dict(tool='picard_IntervalListTools')
+    job_name = f'Make {scatter_count} intervals for {sequencing_type}'
+
+    if output_prefix and exists(output_prefix / '1.interval_list'):
+        job_attrs['reuse'] = 'true'
+        return b.new_job(job_name, job_attrs), [
+            b.read_input(str(output_prefix / f'{idx + 1}.interval_list'))
+            for idx in range(scatter_count)
+        ]
+
+    j = b.new_job(job_name, job_attrs)
+    j.image(image_path('picard'))
+    STANDARD.set_resources(j, storage_gb=16, mem_gb=2)
+
+    break_bands_at_multiples_of = {
+        'genome': 100000,
+        'exome': 0,
+    }.get(sequencing_type, 0)
+
+    cmd = f"""
+    mkdir $BATCH_TMPDIR/out
+    
+    picard -Xms1000m -Xmx1500m \
+    IntervalListTools \
+    SCATTER_COUNT={scatter_count} \
+    SUBDIVISION_MODE=INTERVAL_SUBDIVISION \
+    UNIQUE=true \
+    SORT=true \
+    BREAK_BANDS_AT_MULTIPLES_OF={break_bands_at_multiples_of} \
+    INPUT={b.read_input(str(intervals_path))} \
+    OUTPUT=$BATCH_TMPDIR/out
+    ls $BATCH_TMPDIR/out
+    ls $BATCH_TMPDIR/out/*
+    """
+    for idx in range(scatter_count):
+        name = f'temp_{str(idx + 1).zfill(4)}_of_{scatter_count}'
+        cmd += f"""
+        ln $BATCH_TMPDIR/out/{name}/scattered.interval_list {j[f'{idx + 1}.interval_list']}
+        """
+
+    j.command(command(cmd))
+    if output_prefix:
+        for idx in range(scatter_count):
+            b.write_output(
+                j[f'{idx + 1}.interval_list'],
+                str(output_prefix / f'{idx + 1}.interval_list'),
+            )
+
+    intervals: list[hb.ResourceFile | None] = []
+    for idx in range(scatter_count):
+        interval = j[f'{idx + 1}.interval_list']
+        assert isinstance(interval, hb.ResourceFile)
+        intervals.append(interval)
+    return j, intervals
+
+
 def markdup(
     b: hb.Batch,
     sorted_bam: hb.ResourceFile,
---
+++ stages/genotype.py
@@ -0,0 +1,56 @@
+"""
+Stage that generates a GVCF file.
+"""
+import logging
+
+import hailtop.batch as hb
+from cpg_utils import to_path, Path
+from cpg_utils.config import get_config
+from cpg_utils.workflows.filetypes import GvcfPath
+from cpg_utils.workflows.workflow import (
+    Sample,
+    stage,
+    StageInput,
+    StageOutput,
+    SampleStage,
+)
+
+from jobs import haplotype_caller
+from .align import Align
+
+
+hc_interval_lists: list[hb.Resource | None] | None = None
+
+
+@stage(required_stages=Align, analysis_type='gvcf')
+class Genotype(SampleStage):
+    """
+    Use HaplotypeCaller to genotype individual samples (i.e. CRAM -> GVCF).
+    """
+
+    def expected_outputs(self, sample: Sample) -> dict:
+        """
+        Generate a GVCF and corresponding TBI index, as well as QC.
+        """
+        qc_prefix = sample.dataset.prefix() / 'qc' / sample.id
+        return {
+            'gvcf': sample.make_gvcf_path().path,
+        }
+
+    def queue_jobs(self, sample: Sample, inputs: StageInput) -> StageOutput | None:
+        """
+        Use function from the jobs module
+        """
+        jobs = []
+        gvcf_path = self.expected_outputs(sample)['gvcf']
+        gvcf_jobs = haplotype_caller.produce_gvcf(
+            b=self.b,
+            output_path=gvcf_path,
+            sample_name=sample.id,
+            cram_path=sample.make_cram_path(),
+            tmp_prefix=self.tmp_prefix / sample.id,
+            overwrite=not get_config()['workflow'].get('check_intermediates'),
+            job_attrs=self.get_job_attrs(sample),
+        )
+        jobs.extend(gvcf_jobs)
+        return self.make_outputs(sample, data=self.expected_outputs(sample), jobs=jobs)
