63f37488ba3afb4a7972d91710c0f3589f1dd4ad
Vlad Savelyev <vladislav.savelyev@populationgenomics.org.au>
2022-08-30 16:15:21 +1000

    Add CRAM QC (#84)
    * Add alignment
    
    * Add CRAM QC
    
    * Remove MultiQC
    
    * Picard resources: request ncpu=2 instad of mem=7g
    
    * Fix merge

--- stages/align.py
+++ stages/align.py
@@ -17,6 +17,79 @@ from cpg_utils.workflows.workflow import (
 
 from jobs import align
 from jobs.align import Aligner, MarkDupTool, MissingAlignmentInputException
+from jobs.verifybamid import verifybamid
+from jobs.picard import picard_wgs_metrics, picard_collect_metrics
+
+
+@dataclasses.dataclass
+class QcOut:
+    """QC output file"""
+
+    suf: str
+    multiqc_key: str
+
+
+@dataclasses.dataclass
+class Qc:
+    """QC function definition, and corresponding outputs"""
+
+    func: Optional[Callable]
+    outs: dict[str, QcOut | None]
+
+
+def qc_functions() -> list[Qc]:
+    """
+    QC functions and their outputs for MultiQC aggregation
+    """
+    qcs = [
+        Qc(
+            func=None,
+            outs={
+                'markduplicates_metrics': QcOut(
+                    '.markduplicates-metrics', 'picard/markdups'
+                )
+            },
+        ),
+        Qc(
+            func=verifybamid,
+            outs={'verify_bamid': QcOut('.verify-bamid.selfSM', 'verifybamid/selfsm')},
+        ),
+        Qc(
+            func=picard_collect_metrics,
+            outs={
+                'alignment_summary_metrics': QcOut(
+                    '.alignment_summary_metrics', 'picard/alignment_metrics'
+                ),
+                'base_distribution_by_cycle_metrics': QcOut(
+                    '.base_distribution_by_cycle_metrics',
+                    'picard/basedistributionbycycle',
+                ),
+                'insert_size_metrics': QcOut(
+                    '.insert_size_metrics', 'picard/insertsize'
+                ),
+                'quality_by_cycle_metrics': QcOut(
+                    '.quality_by_cycle_metrics', 'picard/quality_by_cycle'
+                ),
+                'quality_yield_metrics': QcOut(
+                    '.quality_yield_metrics', 'picard/quality_yield_metrics'
+                ),
+            },
+        ),
+    ]
+
+    sequencing_type = get_config()['workflow']['sequencing_type']
+    if sequencing_type == 'genome':
+        qcs.append(
+            Qc(
+                func=picard_wgs_metrics,
+                outs={
+                    'picard_wgs_metrics': QcOut(
+                        '.picard-wgs-metrics', 'picard/wgs_metrics'
+                    )
+                },
+            )
+        )
+    return qcs
 
 
 @stage(analysis_type='cram')
@@ -29,9 +102,16 @@ class Align(SampleStage):
         """
         Stage is expected to generate a CRAM file and a corresponding index.
         """
+        qc_outs: dict[str, Path] = dict()
+        for qc in qc_functions():
+            for key, out in qc.outs.items():
+                if out:
+                    qc_outs[key] = (
+                        sample.dataset.prefix() / 'qc' / key / f'{sample.id}{out.suf}'
+                    )
         return {
             'cram': sample.make_cram_path().path,
-        }
+        } | qc_outs
 
     def queue_jobs(self, sample: Sample, inputs: StageInput) -> StageOutput | None:
         """
@@ -45,6 +125,9 @@ class Align(SampleStage):
                 b=self.b,
                 sample=sample,
                 output_path=sample.make_cram_path(),
+                out_markdup_metrics_path=self.expected_outputs(sample)[
+                    'markduplicates_metrics'
+                ],
                 job_attrs=self.get_job_attrs(sample),
                 overwrite=not get_config()['workflow'].get('check_intermediates'),
                 aligner=Aligner.DRAGMAP,
@@ -61,5 +144,23 @@ class Align(SampleStage):
                 )
         else:
             jobs.extend(align_jobs)
+
+        for qc in qc_functions():
+            out_path_kwargs = {
+                f'out_{key}_path': self.expected_outputs(sample)[key]
+                for key in qc.outs.keys()
+            }
+            if qc.func:
+                j = qc.func(  # type: ignore
+                    self.b,
+                    sample.make_cram_path(),
+                    job_attrs=self.get_job_attrs(sample),
+                    overwrite=not get_config()['workflow'].get('check_intermediates'),
+                    **out_path_kwargs,
+                )
+                if j:
+                    if align_jobs:
+                        j.depends_on(*align_jobs)
+                    jobs.append(j)
 
         return self.make_outputs(sample, data=self.expected_outputs(sample), jobs=jobs)
