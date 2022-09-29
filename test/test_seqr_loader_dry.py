bbe69726f70deaff04ffd2e4ab90e134e6ada1a2
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-09-29 23:39:57 +1000

    Test seqr loader dry: rename, small refactor

--- test/test_seqr_loader_dry.py
+++ test/test_seqr_loader_dry.py
@@ -1,11 +1,13 @@
 """
 Test Hail Query functions.
 """
+
 import toml
 from cpg_utils import to_path, Path
 from cpg_utils.config import update_dict
 from cpg_utils.workflows.batch import get_batch
 from cpg_utils.workflows.filetypes import BamPath, FastqPair, FastqPairs
+from cpg_utils.workflows.inputs import get_cohort
 from cpg_utils.workflows.targets import Cohort
 from cpg_utils.workflows.workflow import get_workflow
 from cpg_utils.config import set_config_paths
@@ -43,7 +45,7 @@ def _set_config(dir_path: Path, extra_conf: dict | None = None):
     set_config_paths([str(config_path)])
 
 
-def test_workflow_dry(mocker: MockFixture, tmpdir: str):
+def test_seqr_loader_dry(mocker: MockFixture, tmpdir: str):
     """
     Run entire seqr-loader in a dry mode.
     """
@@ -54,35 +56,34 @@ def test_workflow_dry(mocker: MockFixture, tmpdir: str):
         },
     )
 
-    cohort = Cohort()
-    ds = cohort.create_dataset('test-input-dataset')
-    ds.add_sample(
-        'CPG01',
-        'SAMPLE1',
-        alignment_input_by_seq_type={
-            'genome': BamPath('gs://test-input-dataset-upload/sample1.bam')
-        },
-    )
-    ds.add_sample(
-        'CPG02',
-        'SAMPLE2',
-        alignment_input_by_seq_type={
-            'genome': FastqPairs(
-                [
-                    FastqPair(
-                        'gs://test-input-dataset-upload/sample2_L1_R1.fq.gz',
-                        'gs://test-input-dataset-upload/sample2_L1_R2.fq.gz',
-                    ),
-                    FastqPair(
-                        'gs://test-input-dataset-upload/sample2_L2_R1.fq.gz',
-                        'gs://test-input-dataset-upload/sample2_L2_R2.fq.gz',
-                    ),
-                ]
-            )
-        },
-    )
-
-    def mock_get_cohort(*args, **kwargs) -> Cohort:
+    def mock_create_cohort(*args, **kwargs) -> Cohort:
+        cohort = Cohort()
+        ds = cohort.create_dataset('test-input-dataset')
+        ds.add_sample(
+            'CPG01',
+            'SAMPLE1',
+            alignment_input_by_seq_type={
+                'genome': BamPath('gs://test-input-dataset-upload/sample1.bam')
+            },
+        )
+        ds.add_sample(
+            'CPG02',
+            'SAMPLE2',
+            alignment_input_by_seq_type={
+                'genome': FastqPairs(
+                    [
+                        FastqPair(
+                            'gs://test-input-dataset-upload/sample2_L1_R1.fq.gz',
+                            'gs://test-input-dataset-upload/sample2_L1_R2.fq.gz',
+                        ),
+                        FastqPair(
+                            'gs://test-input-dataset-upload/sample2_L2_R1.fq.gz',
+                            'gs://test-input-dataset-upload/sample2_L2_R2.fq.gz',
+                        ),
+                    ]
+                )
+            },
+        )
         return cohort
 
     def mock_exists(*args, **kwargs) -> bool:
@@ -91,7 +92,7 @@ def test_workflow_dry(mocker: MockFixture, tmpdir: str):
     def do_nothing(*args, **kwargs):
         return None
 
-    mocker.patch('cpg_utils.workflows.inputs.create_cohort', mock_get_cohort)
+    mocker.patch('cpg_utils.workflows.inputs.create_cohort', mock_create_cohort)
     # functions like get_intervals checks file existence
     mocker.patch('cloudpathlib.cloudpath.CloudPath.exists', mock_exists)
     # cloudfuse (used in Vep) doesn't work with LocalBackend
@@ -105,16 +106,16 @@ def test_workflow_dry(mocker: MockFixture, tmpdir: str):
 
     assert (
         get_batch().job_by_tool['gatk HaplotypeCaller']['job_n']
-        == len(cohort.get_samples()) * 50
+        == len(get_cohort().get_samples()) * 50
     )
     assert get_batch().job_by_tool['picard MergeVcfs']['job_n'] == len(
-        cohort.get_samples()
+        get_cohort().get_samples()
     )
     assert get_batch().job_by_tool['gatk ReblockGVCF']['job_n'] == len(
-        cohort.get_samples()
+        get_cohort().get_samples()
     )
     assert (
         get_batch().job_by_tool['picard CollectVariantCallingMetrics']['job_n']
-        == len(cohort.get_samples()) + 1
+        == len(get_cohort().get_samples()) + 1
     )
     assert get_batch().job_by_tool['gatk GenomicsDBImport']['job_n'] == 50
