b7581fa3fbc35870657a29452747272e7afc9cc5
Vlad Savelyev <vladislav.savelyev@populationgenomics.org.au>
2022-10-05 21:49:16 +1100

    GATK-SV: Add GatherBatchEvidence (#118)
    * GATK-SV GatherSampleEvidence and EvidenceQC fixes:
    - EvidenceQC requires evidence files to be named in a specific way,
    - Using the sha with disabled localisaion,
    - Adding SD files,
    - Enable Scramble,
    - Pass CRAM fasta for CRAM->BAM to work properly,
    - Pass saved outputs instead of reasource files
    
    * Fix docs
    
    * Fix docs
    
    * Add ref_panels with SD
    
    * Add GatherBatchEvidence
    
    * Add bwa_ref_fasta to acute-care config
    
    * GatherBatchEvidence: add ref panel samples to PED (#122)
    
    * GatherBatchEvidence seems to expect ref panel samples in input PED file
    
    * Fix
    
    * Add ref_panel_bincov_matrix
    
    * Fix outputs
    
    * Add callers vcfs to GatherBatchEvidence
    
    * Check samples with no cram
    
    * Add acute-care-main config
    
    * Typo

--- gatk_sv/gatk_sv.py
+++ gatk_sv/gatk_sv.py
@@ -1,7 +1,7 @@
 """
 Driver for computing structural variants from GATK-SV from WGS data.
 """
-
+import logging
 import os
 from os.path import join
 from typing import Any
@@ -19,6 +19,7 @@ from cpg_utils import Path
 from cpg_utils.config import set_config_paths, get_config
 from cpg_utils.hail_batch import command, reference_path, image_path
 from cpg_utils.workflows.batch import make_job_name, Batch
+from cpg_utils.workflows.inputs import get_cohort
 from cpg_utils.workflows.utils import exists
 from cpg_utils.workflows.workflow import (
     stage,
@@ -45,41 +46,6 @@ def get_images(keys: list[str]) -> dict[str, str]:
     Dict of WDL inputs with docker image paths.
     """
     return {k: image_path(k) for k in get_config()['images'].keys() if k in keys}
-
-
-def get_gcnv_models() -> dict[str, str | list[str]]:
-    """
-    Dict of WDL inputs with gCNV models
-    """
-    res: dict[str, str | list[str]] = {
-        'ref_panel_samples': get_config()['sv_ref_panel']['ref_panel_samples'],
-        'contig_ploidy_model_tar': str(
-            reference_path('broad/sv/ref_panel/contig_ploidy_model_tar')
-        ),
-        'gcnv_model_tars': [
-            str(reference_path('broad/sv/ref_panel/model_tar_tmpl')).format(shard=i)
-            for i in range(get_config()['sv_ref_panel']['model_tar_cnt'])
-        ],
-        'ref_panel_PE_files': [
-            str(reference_path('broad/sv/ref_panel/ref_panel_PE_file_tmpl')).format(
-                sample=s
-            )
-            for s in get_config()['sv_ref_panel']['ref_panel_samples']
-        ],
-        'ref_panel_SR_files': [
-            str(reference_path('broad/sv/ref_panel/ref_panel_SR_file_tmpl')).format(
-                sample=s
-            )
-            for s in get_config()['sv_ref_panel']['ref_panel_samples']
-        ],
-        'ref_panel_SD_files': [
-            str(reference_path('broad/sv/ref_panel/ref_panel_SD_file_tmpl')).format(
-                sample=s
-            )
-            for s in get_config()['sv_ref_panel']['ref_panel_samples']
-        ],
-    }
-    return res
 
 
 def get_references(keys: list[str | dict[str, str]]) -> dict[str, str | list[str]]:
@@ -201,12 +167,11 @@ class GatherSampleEvidence(SampleStage):
 
     def queue_jobs(self, sample: Sample, inputs: StageInput) -> StageOutput:
         """Add jobs to batch"""
-        cram_path = sample.make_cram_path()
-        assert exists(cram_path.path), cram_path
+        assert sample.cram, sample
 
         input_dict: dict[str, Any] = {
-            'bam_or_cram_file': str(cram_path),
-            'bam_or_cram_index': str(cram_path) + '.crai',
+            'bam_or_cram_file': str(sample.cram),
+            'bam_or_cram_index': str(sample.cram) + '.crai',
             'sample_id': sample.id,
             # This option forces CRAM localisation, otherwise it would be passed to
             # samtools as a URL (in CramToBam.wdl) and it would fail to read it as
@@ -331,6 +296,165 @@ class EvidenceQC(DatasetStage):
         return self.make_outputs(dataset, data=expected_d, jobs=[j])
 
 
+@stage(required_stages=[GatherSampleEvidence, EvidenceQC])
+class GatherBatchEvidence(DatasetStage):
+    """
+    https://github.com/broadinstitute/gatk-sv#gather-batch-evidence
+    https://github.com/broadinstitute/gatk-sv/blob/master/wdl/GatherBatchEvidence.wdl
+    """
+
+    def expected_outputs(self, dataset: Dataset) -> dict[str, Path]:
+        d: dict[str, Path] = dict()
+        ending_by_key = {
+            'cnmops_dup': '.DUP.header.bed.gz',
+            'cnmops_dup_index': '.DUP.header.bed.gz.tbi',
+            'cnmops_del': '.DEL.header.bed.gz',
+            'cnmops_del_index': '.DEL.header.bed.gz.tbi',
+            'cnmops_large_del': '.DEL.large.bed.gz',
+            'cnmops_large_del_index': '.DEL.large.bed.gz.tb',
+            'cnmops_large_dup': '.DUP.large.bed.gz',
+            'cnmops_large_dup_index': '.DUP.large.bed.gz.tbi',
+            'merged_SR': '.sr.txt.gz',
+            'merged_SR_index': '.sr.txt.gz.tbi',
+            'merged_PE': '.pe.txt.gz',
+            'merged_PE_index': '.pe.txt.gz.tbi',
+            'merged_BAF': '.baf.txt.gz',
+            'merged_BAF_index': '.baf.txt.gz.tbi',
+            'merged_bincov': '.RD.txt.gz',
+            'merged_bincov_index': '.RD.txt.gz.tbi',
+            'SR_stats': '.SR.QC_matrix.txt',
+            'PE_stats': '.PE.QC_matrix.txt',
+            'BAF_stats': '.BAF.QC_matrix.txt',
+            'RD_stats': '.RD.QC_matrix.txt',
+            'median_cov': '_medianCov.transposed.bed',
+            'merged_dels': '.DEL.bed.gz',
+            'merged_dups': '.DUP.bed.gz',
+            'Matrix_QC_plot': '.00_matrix_FC_QC.png',
+        }
+        for caller in SV_CALLERS:
+            ending_by_key[f'std_{caller}_vcf_tar'] = f'.{caller}.tar.gz'
+
+        for key, ending in ending_by_key.items():
+            fname = f'{dataset.name}{ending}'
+            d[key] = dataset.prefix() / 'gatk_sv' / self.name.lower() / fname
+        return d
+
+    def queue_jobs(self, dataset: Dataset, inputs: StageInput) -> StageOutput | None:
+        """Add jobs to Batch"""
+        sids = dataset.get_sample_ids()
+
+        # PED file must include reference panel samples as well, so concatenating
+        # the `dataset` PED file with the reference panel PED file:
+        combined_ped_path = (
+            self.tmp_prefix
+            / 'ped_with_ref_panel'
+            / f'{dataset.alignment_inputs_hash()}.ped'
+        )
+        with combined_ped_path.open('w') as out:
+            with dataset.write_ped_file().open() as f:
+                out.write(f.read())
+            # The ref panel PED doesn't have a header, so can safely concatenate:
+            with reference_path('broad/sv/ref_panel/ped_file').open() as f:
+                out.write(f.read())
+
+        input_by_sid = inputs.as_dict_by_target(stage=GatherSampleEvidence)
+
+        input_dict: dict[str, Any] = {
+            'batch': dataset.name,
+            'samples': sids,
+            'ped_file': str(combined_ped_path),
+            'counts': [str(input_by_sid[sid]['coverage_counts']) for sid in sids],
+            'SR_files': [str(input_by_sid[sid]['sr']) for sid in sids],
+            'PE_files': [str(input_by_sid[sid]['pe']) for sid in sids],
+            'SD_files': [str(input_by_sid[sid]['sd']) for sid in sids],
+        }
+        for caller in SV_CALLERS:
+            input_dict[f'{caller}_vcfs'] = [
+                str(input_by_sid[sid][f'{caller}_vcf']) for sid in sids
+            ]
+
+        input_dict |= {
+            'ref_copy_number_autosomal_contigs': 2,
+            'allosomal_contigs': ['chrX', 'chrY'],
+            'gcnv_qs_cutoff': 30,
+            'min_svsize': 50,
+            'run_matrix_qc': True,
+            'matrix_qc_distance': 1000000,
+        }
+        input_dict |= get_references(
+            [
+                'genome_file',
+                'primary_contigs_fai',
+                {'sd_locs_vcf': 'dbsnp_vcf'},
+                {'cnmops_chrom_file': 'autosome_file'},
+                'cnmops_exclude_list',
+                {'cnmops_allo_file': 'allosome_file'},
+                'cytoband',
+                'mei_bed',
+            ]
+        )
+        input_dict |= {
+            'ref_dict': str(reference_path(f'broad/ref_fasta').with_suffix('.dict')),
+        }
+
+        # reference panel gCNV models
+        input_dict |= {
+            'ref_panel_samples': get_config()['sv_ref_panel']['ref_panel_samples'],
+            'ref_panel_bincov_matrix': str(
+                reference_path('broad/sv/ref_panel/ref_panel_bincov_matrix')
+            ),
+            'contig_ploidy_model_tar': str(
+                reference_path('broad/sv/ref_panel/contig_ploidy_model_tar')
+            ),
+            'gcnv_model_tars': [
+                str(reference_path('broad/sv/ref_panel/model_tar_tmpl')).format(shard=i)
+                for i in range(get_config()['sv_ref_panel']['model_tar_cnt'])
+            ],
+            'ref_panel_PE_files': [
+                str(reference_path('broad/sv/ref_panel/ref_panel_PE_file_tmpl')).format(
+                    sample=s
+                )
+                for s in get_config()['sv_ref_panel']['ref_panel_samples']
+            ],
+            'ref_panel_SR_files': [
+                str(reference_path('broad/sv/ref_panel/ref_panel_SR_file_tmpl')).format(
+                    sample=s
+                )
+                for s in get_config()['sv_ref_panel']['ref_panel_samples']
+            ],
+            'ref_panel_SD_files': [
+                str(reference_path('broad/sv/ref_panel/ref_panel_SD_file_tmpl')).format(
+                    sample=s
+                )
+                for s in get_config()['sv_ref_panel']['ref_panel_samples']
+            ],
+        }
+
+        input_dict |= get_images(
+            [
+                'sv_base_mini_docker',
+                'sv_base_docker',
+                'sv_pipeline_docker',
+                'sv_pipeline_qc_docker',
+                'linux_docker',
+                'condense_counts_docker',
+                'gatk_docker',
+                'gcnv_gatk_docker',
+                'cnmops_docker',
+            ]
+        )
+
+        expected_d = self.expected_outputs(dataset)
+        j = add_gatk_sv_job(
+            batch=self.b,
+            dataset=dataset,
+            wfl_name=self.name,
+            input_dict=input_dict,
+            expected_out_dict=expected_d,
+        )
+        return self.make_outputs(dataset, data=expected_d, jobs=[j])
+
+
 @click.command()
 @click.argument('config_paths', nargs=-1)
 def main(config_paths: list[str]):
@@ -341,7 +465,16 @@ def main(config_paths: list[str]):
     if _cpg_config_path_env_var := os.environ.get('CPG_CONFIG_PATH'):
         config_paths = _cpg_config_path_env_var.split(',') + config_paths
     set_config_paths(list(config_paths))
-    run_workflow(stages=[EvidenceQC])
+    samples_with_no_cram = [s for s in get_cohort().get_samples() if not s.cram]
+    if samples_with_no_cram:
+        logging.warning(
+            f'Found {len(samples_with_no_cram)}/{len(get_cohort().get_samples())} '
+            'samples with no CRAM in Metamist. Skipping'
+        )
+        for s in samples_with_no_cram:
+            s.active = False
+
+    run_workflow(stages=[GatherBatchEvidence])
 
 
 if __name__ == '__main__':
