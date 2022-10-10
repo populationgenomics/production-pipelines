2150e03147e78a31791fdb42dc1a71c94630e780
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-10-11 00:51:05 +1100

    Make sure last_stages is respected in main.py

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ Entry point to run the workflow.
 """
 import os
 
-import coloredlogs
 import click
+import coloredlogs
 
 from cpg_utils import to_path
+from cpg_utils.config import set_config_paths, get_config
 from cpg_utils.workflows.workflow import run_workflow
-from cpg_utils.config import set_config_paths
 from stages.multiqc import GvcfMultiQC, CramMultiQC
 from stages.seqr_loader import MtToEs
 
@@ -32,7 +32,12 @@ def main(config_paths: list[str]):
         config_paths += _env_var.split(',') + list(config_paths)
     set_config_paths(list(config_paths))
 
-    run_workflow(stages=[MtToEs, CramMultiQC, GvcfMultiQC])
+    stages = [GvcfMultiQC, CramMultiQC, MtToEs]
+    if last_stages := get_config()['workflow'].get('last_stages'):
+        stages = [s for s in stages if s.__name__ in last_stages]
+    if only_stages := get_config()['workflow'].get('only_stages'):
+        stages = [s for s in stages if s.__name__ in only_stages]
+    run_workflow(stages=stages)
 
 
 if __name__ == '__main__':
