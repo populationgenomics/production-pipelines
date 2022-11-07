ede94573c5287206f8368e44a16ba33128431196
vivbak <vivian.bakiris@populationgenomics.org.au>
2022-11-07 21:49:27 +1100

    test alignment on workshop-nov-22 project

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from cpg_workflows.stages.gvcf_qc import GvcfMultiQC
 from cpg_workflows.stages.fastqc import FastQCMultiQC
 from cpg_workflows.stages.seqr_loader import MtToEs, AnnotateDataset
 from cpg_workflows.stages.gatk_sv import GatherBatchEvidence
+from cpg_workflows.stages.align import Align
 
 fmt = '%(asctime)s %(levelname)s (%(name)s %(lineno)s): %(message)s'
 coloredlogs.install(level='INFO', fmt=fmt)
@@ -27,6 +28,7 @@ WORKFLOWS: dict[str, list[StageDecorator]] = {
     'seqr_loader': [AnnotateDataset, MtToEs, GvcfMultiQC, CramMultiQC],
     'large_cohort': [LoadVqsr, Frequencies, GvcfMultiQC, CramMultiQC],
     'gatk_sv': [GatherBatchEvidence],
+    'workshop22': [Align]
 }
 
 
