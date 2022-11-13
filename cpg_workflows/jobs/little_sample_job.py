615b3d14e131f441488a0834d948322a3ee734ae
vivbak <vivian.bakiris@populationgenomics.org.au>
2022-11-13 18:18:30 +1100

    Clean up

--- cpg_workflows/jobs/little_sample_job.py
+++ cpg_workflows/jobs/little_sample_job.py
@@ -2,19 +2,12 @@
 Sample Jobs.
 """
 
-from os.path import basename
-
 import hailtop.batch as hb
 from hailtop.batch.job import Job
 
 from cpg_utils import Path
 from cpg_utils.hail_batch import command
 from cpg_utils.hail_batch import image_path
-from cpg_workflows.filetypes import (
-    BamPath,
-    FastqPath,
-)
-from cpg_workflows.resources import STANDARD
 
 
 def little_sample_job(
@@ -31,13 +24,10 @@ def little_sample_job(
     j = b.new_job('TESTING WORKSHOP 22', (job_attrs or {}) | {'tool': 'silly_sample'})
     j.image(image_path('cpg_workflows'))
 
-    # fname = basename(str(input_path))
-
     cmd = ''
     input_file: str | hb.ResourceFile = b.read_input(input_path)
 
     cmd += f"""\
-    echo {input_file}
     echo {input_file} > {j.out}
     """
     j.command(command(cmd))
--- cpg_workflows/stages/sample_stage.py
+++ cpg_workflows/stages/sample_stage.py
@@ -2,7 +2,6 @@
 A sample stage.
 """
 
-from cpg_utils import to_path
 from cpg_utils.config import get_config
 from cpg_workflows.workflow import (
     stage,
@@ -24,10 +23,12 @@ class TestSampleStage(SampleStage):
 
     def expected_outputs(self, sample: Sample):
         """
-        Generate some stuff!.
+        Generate some stuff!
         """
+        path = (sample.dataset.prefix() / 'WorkshopNov22' / f'{sample.participant_id}.fastq.gz')
+        print(path)
         return {
-            'new_file': f'gs://cpg-fewgenomes-test/vivbak/WorkshopNov22/{sample.participant_id}.fastq.gz',
+            'new_file': f'gs://cpg-fewgenomes-test/WorkshopNov22/{sample.participant_id}.fastq.gz',
         }
 
     def queue_jobs(self, sample: Sample, inputs: StageInput) -> StageOutput | None:
@@ -39,8 +40,8 @@ class TestSampleStage(SampleStage):
 
         jobs = little_sample_job.little_sample_job(
                 b=get_batch(),
-                output_path=f'gs://cpg-fewgenomes-test/vivbak/WorkshopNov22/{sample.participant_id}.fastq.gz',
-                input_path=f'gs://cpg-fewgenomes-test/vivbak/WorkshopNov22/BRCA1_R1.fastq.gz',
+                output_path=f'gs://cpg-fewgenomes-test/WorkshopNov22/{sample.participant_id}.fastq.gz',
+                input_path=f'gs://cpg-fewgenomes-test/WorkshopNov22/BRCA1_R1.fastq.gz',
         )
         return self.make_outputs(
             sample, data=self.expected_outputs(sample), jobs=jobs
