14f6e51fef7ebc0aabcecd0e1a4e2f4a5b8185dc
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-11-14 10:28:28 +1100

    Flag optional

--- cpg_workflows/dataproc_scripts/mt_to_es.py
+++ cpg_workflows/dataproc_scripts/mt_to_es.py
@@ -121,7 +121,7 @@ class HailElasticsearchClientV8(HailElasticsearchClient):
 def main(
     mt_path: str,
     es_index: str,
-    done_flag_path: str,
+    done_flag_path: str | None,
     liftover_path: str,
     password: str = None,
 ):
@@ -175,8 +175,9 @@ def main(
         write_null_values=True,
     )
     _cleanup(es, es_index, es_shards)
-    with to_path(done_flag_path).open('w') as f:
-        f.write('done')
+    if done_flag_path:
+        with to_path(done_flag_path).open('w') as f:
+            f.write('done')
 
 
 def elasticsearch_row(mt: hl.MatrixTable):
