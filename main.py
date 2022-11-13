a24911eb6f9d174e5efbef292b098d19cad45d2d
vivbak <vivian.bakiris@populationgenomics.org.au>
2022-11-13 22:03:52 +1100

    prep for workshop

--- main.py
+++ main.py
@@ -19,9 +19,6 @@ from cpg_workflows.stages.fastqc import FastQCMultiQC
 from cpg_workflows.stages.seqr_loader import MtToEs, AnnotateDataset
 from cpg_workflows.stages.gatk_sv import GatherBatchEvidence
 from cpg_workflows.stages.align import Align
-from cpg_workflows.stages.genotype import Genotype
-from cpg_workflows.stages.cram_qc import CramQC
-from cpg_workflows.stages.sample_stage import TestSampleStage
 
 fmt = '%(asctime)s %(levelname)s (%(name)s %(lineno)s): %(message)s'
 coloredlogs.install(level='INFO', fmt=fmt)
@@ -32,7 +29,7 @@ WORKFLOWS: dict[str, list[StageDecorator]] = {
     'seqr_loader': [AnnotateDataset, MtToEs, GvcfMultiQC, CramMultiQC],
     'large_cohort': [LoadVqsr, Frequencies, GvcfMultiQC, CramMultiQC],
     'gatk_sv': [GatherBatchEvidence],
-    'workshop22': [TestSampleStage]
+    'workshop22': [Align]
     
 }
 
