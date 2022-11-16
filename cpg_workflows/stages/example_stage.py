58e492c1f1ba6a5eb26d8bf9bcb3d87a3f0220b0
Katie de Lange <katie.delange@populationgenomics.org.au>
2022-11-16 14:49:35 +1100

    Create an example stage and workflow

---
+++ cpg_workflows/stages/example_stage.py
@@ -0,0 +1,63 @@
+"""
+Example sample stage to copy and rename a FASTQ file.
+"""
+from cpg_workflows.jobs import example_job
+
+from cpg_workflows.workflow import (
+    Sample,
+    stage,
+    StageInput,
+    StageOutput,
+    SampleStage,
+)
+from .. import get_batch
+
+@stage(
+    required_stages=Align,
+    analysis_type='gvcf',
+    analysis_key='gvcf',
+)
+class ExampleStage(SampleStage):
+    """
+    Copy a FASTQ file and name it using it's external sample ID.
+    """
+
+    def expected_outputs(self, sample: Sample):
+        """
+        Expected output is a FASTQ file named based on the sample's external sample ID.
+        """
+        expected_output_path = (
+            sample.dataset.prefix() / 
+            'WorkshopNov22' / 
+            f'{sample.external_id}.fastq.gz'
+        )
+
+        return {
+            'new_file':expected_output_path
+        }
+
+    def queue_jobs(self, sample: Sample, inputs: StageInput) -> StageOutput | None:
+        """
+        Run the example job function.
+        """
+        expected_output_path = (
+            sample.dataset.prefix() / 
+            'WorkshopNov22' / 
+            f'{sample.external_id}.fastq.gz'
+        )
+
+        input_path = (sample.dataset.prefix() / 'WorkshopNov22' / 'BRCA1_R1.fastq.gz')
+
+        job_attrs = {
+            'sample': sample_id,
+            'external_id': sample.external_id
+        }
+
+        job = example_job.example_job(
+            b = get_batch(),
+            output_path: expected_output_path,
+            input_path: input_path,
+            job_attrs: job_attrs,
+        )
+
+        return self.make_outputs(sample, data = self.expected_outputs[sample], jobs=[job])
