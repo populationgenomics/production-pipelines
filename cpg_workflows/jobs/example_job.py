c63bd648cbdae961ce5ebd777552fa6fcaed53fe
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-11-17 10:54:41 +1100

    Add in files iwthout typos or syntax errors

---
+++ cpg_workflows/jobs/example_job.py
@@ -0,0 +1,30 @@
+""" 
+A job that won't do much
+"""
+
+import hailtop.batch as hb 
+from hailtop.batch.job import Job
+
+from cpg_utils import Path
+from cpg_utils.hail_batch import command
+from cpg_utils.hail_batch import image_path
+
+def example_job(
+    b: hb.Batch,
+    output_path: Path, 
+    input_path: Path, 
+    job_attrs: dict | None = None
+) -> Job :
+    j = b.new_job('Testing Workshop 22', (job_attrs or {}) | {'tool':'copying'})
+    j.image(image_path('cpg_workflows'))
+
+    input_file : str | hb.ResourceFile = b.read_input(str(input_path))
+
+    cmd = f"""\
+        echo {input_file} > {j.out}
+    """
+    j.command(command(cmd)) 
+
+    b.write_output(j.out, str(output_path))
+
+    return j 
---
+++ cpg_workflows/stages/example_stage.py
@@ -0,0 +1,50 @@
+"""
+An example stage that doesn't do much 
+"""
+
+from cpg_workflows.workflow import (
+    stage,
+    StageInput,
+    StageOutput,
+    SampleStage,
+    Sample
+)
+
+from cpg_workflows.jobs import example_job
+from .. import get_batch 
+
+@stage()
+class ExampleStage(SampleStage):
+    def expected_outputs(self, sample: Sample):
+
+        expected_output_path = (
+            sample.dataset.prefix() /
+            'WorkshopNov22'/
+            f'{sample.external_id}.fastq.gz'
+        )
+
+        return {
+            'new_file':expected_output_path
+        }
+
+    def queue_jobs(self, sample: Sample, inputs: StageInput) -> StageOutput | None :
+
+        expected_output_path = (
+            sample.dataset.prefix() /
+            'WorkshopNov22'/
+            f'{sample.external_id}.fastq.gz'
+        )
+
+        input_path = (sample.dataset.prefix() / 'WorkshopNov22' / 'BRCA1_R1.fastq.gz')
+
+        job_attrs = {
+            'sample': sample.id, 'external_id': sample.external_id
+        }
+
+        job = example_job.example_job(
+                b = get_batch(),
+                output_path = expected_output_path,
+                input_path = input_path,
+                job_attrs = job_attrs,
+        )
+        return self.make_outputs(sample, data=self.expected_outputs(sample), jobs=[job])
