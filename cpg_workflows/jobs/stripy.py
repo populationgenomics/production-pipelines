e2716b55bbfd8acd4aa058ad47c3c0f38ca34044
Cas Simons <cas.simons@populationgenomics.org.au>
2022-11-23 21:24:01 +1300

    make target_loci a configuration

--- cpg_workflows/jobs/stripy.py
+++ cpg_workflows/jobs/stripy.py
@@ -19,6 +19,7 @@ def stripy(
     b,
     sample: Sample,
     cram_path: CramPath,
+    target_loci: str,
     out_path: Path,
     log_path: Path,
     analysis_type: str = 'standard',
@@ -103,11 +104,7 @@ def stripy(
         --logflags {j.log_path} \\
         --config $BATCH_TMPDIR/config.json \\
         --analysis {analysis_type} \\
-        --locus AFF2,AR,ARX_1,ARX_2,ATN1,ATXN1,ATXN10,ATXN2,ATXN3,ATXN7,ATXN8OS,\
-BEAN1,C9ORF72,CACNA1A,CBL,CNBP,COMP,DAB1,DIP2B,DMD,DMPK,FMR1,FOXL2,FXN,GIPC1,GLS,\
-HOXA13_1,HOXA13_2,HOXA13_3,HOXD13,HTT,JPH3,LRP12,MARCHF6,NIPA1,NOP56,NOTCH2NLC,\
-NUTM2B-AS1,PABPN1,PHOX2B,PPP2R2B,PRDM12,RAPGEF2,RFC1,RUNX2,SAMD12,SOX3,STARD7,TBP,\
-TBX1,TCF4,TNRC6A,XYLT1,YEATS2,ZIC2,ZIC3
+        --locus {target_loci}
 
     ls $BATCH_TMPDIR/
   
