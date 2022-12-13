ecabb3fa1127dfefe839fb0c839fc5869165871d
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-12-13 16:01:04 +1100

    Add from fastq

---
+++ scripts/test_dragmap2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from cpg_workflows import get_batch
+from cpg_workflows.filetypes import CramPath, FastqPair, FastqPairs
+from cpg_workflows.jobs import align
+from cpg_workflows.targets import Dataset
+
+dataset = Dataset('test')
+sample = dataset.add_sample(
+    'CHM1_CHM13_2',
+    alignment_input_by_seq_type={
+        'genome': FastqPairs(
+            [
+                FastqPair(
+                    'gs://cpg-validation-test-upload/HCMVGDSX3_1_220405_FD07777372_Homo-sapiens_TCCGCCAATT-CAGCACGGAG_R_220405_CNTROL_DNA_M001_R1.fastq.gz',
+                    'gs://cpg-validation-test-upload/HCMVGDSX3_1_220405_FD07777372_Homo-sapiens_TCCGCCAATT-CAGCACGGAG_R_220405_CNTROL_DNA_M001_R2.fastq.gz',
+                )
+            ]
+        )
+    },
+)
+
+jobs = align.align(
+    b=get_batch('Test dragmap'),
+    sample=sample,
+    output_path=CramPath('gs://cpg-validation-test-tmp/test-dragmap/CHM1_CHM13_2.cram'),
+)
+get_batch().run(wait=False)
