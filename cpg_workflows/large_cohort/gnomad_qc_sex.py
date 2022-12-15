e024965b2d7ecc75d30d39ae1c0bfd7633d2f97b
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-12-15 11:31:52 +1100

    Use use_variant_dataset=True

--- cpg_workflows/large_cohort/gnomad_qc_sex.py
+++ cpg_workflows/large_cohort/gnomad_qc_sex.py
@@ -541,7 +541,7 @@ def annotate_sex(
                     hl.vds.filter_intervals(mtds, ref_keep_locus_intervals),
                     calling_intervals=included_intervals,
                     normalization_contig=normalization_contig,
-                    use_variant_dataset=False,
+                    use_variant_dataset=True,
                 )
             ploidy_ht = ploidy_ht.rename(
                 {
