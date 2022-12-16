10561506a075ae79add05c79b0656792cf0ca159
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2022-12-16 18:37:09 +1100

    Bump version: 1.6.0 → 1.6.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 setup(
     name='cpg-workflows',
     # This tag is automatically updated by bumpversion
-    version='1.6.0',
+    version='1.6.1',
     description='CPG workflows for Hail Batch',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
