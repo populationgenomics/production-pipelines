a8087b99a3fb750620b1cf598d5c2ae4f6f4a241
Cas Simons <cas.simons@populationgenomics.org.au>
2022-12-21 10:36:44 +1300

    Fix stripy output file names.

--- cpg_workflows/stages/stripy.py
+++ cpg_workflows/stages/stripy.py
@@ -30,11 +30,11 @@ class Stripy(SampleStage):
         return {
             'stripy_html': sample.dataset.web_prefix()
             / 'stripy'
-            / f'{sample._external_id}.stripy.html',
-            'stripy_json': sample.dataset.prefix()
+            / f'{sample.id}.stripy.html',
+            'stripy_json': sample.analysis_prefix.prefix()
             / 'stripy'
-            / f'{sample._external_id}.stripy.json',
-            'stripy_log': sample.dataset.prefix()
+            / f'{sample.id}.stripy.json',
+            'stripy_log': sample.analysis_prefix.prefix()
             / 'stripy'
             / f'{sample.id}.stripy.log.txt',
         }
