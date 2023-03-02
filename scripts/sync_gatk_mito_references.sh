5a52dadd67aa141a8819d8bfcccd0469e27d87fe
Cas Simons <cas.simons@populationgenomics.org.au>
2023-03-02 18:58:15 +1300

    url fix

--- scripts/sync_gatk_mito_references.sh
+++ scripts/sync_gatk_mito_references.sh
@@ -3,5 +3,5 @@
 # Sync the Broad reference resources into the corresponding CPG bucket.
 
 gsutil rsync -r \
-  gs://gatk-sv-resources-public/hg38/v0/chrM \
+  gs://gcp-public-data--broad-references/hg38/v0/chrM/ \
   gs://cpg-common-main/references/hg38/v0/chrM
