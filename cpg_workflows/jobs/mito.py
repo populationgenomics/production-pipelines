c2b2a25807b129a6e3debcae69110e687b0e1c15
Cas Simons <cas.simons@populationgenomics.org.au>
2023-03-02 22:02:18 +1300

    init

---
+++ cpg_workflows/jobs/mito.py
@@ -0,0 +1,93 @@
+"""
+Create Hail Batch jobs to run STRipy
+"""
+
+from hailtop.batch.job import Job
+from textwrap import dedent
+
+from cpg_utils import Path, to_path
+from cpg_utils.hail_batch import image_path, fasta_res_group
+from cpg_utils.hail_batch import command
+from cpg_workflows.resources import STANDARD
+from cpg_workflows.filetypes import CramPath
+from cpg_workflows.utils import can_reuse
+from cpg_workflows.targets import Sample
+
+
+def mito_realign(
+    b,
+    sample: Sample,
+    cram_path: CramPath,
+    mt_aligned_cram: str,
+    job_attrs: dict | None = None,
+    overwrite: bool = False,
+) -> Job | None:
+    """
+    Re-align reads to mito genome
+    """
+    if can_reuse(
+        [
+            out_path,
+        ],
+        overwrite,
+    ):
+        return None
+
+    job_attrs = job_attrs or {}
+    j = b.new_job('mito_realign', job_attrs)
+    j.image(image_path('bwamem2'))
+
+    reference = fasta_res_group(b)
+
+    mt_ref = b.read_input_group(
+        dict='gs://cpg-common-main/references/hg38/v0/chrM/Homo_sapiens_assembly38.chrM.dict',
+        fasta='gs://cpg-common-main/references/hg38/v0/chrM/Homo_sapiens_assembly38.chrM.fasta',
+        amb='gs://cpg-common-main/references/hg38/v0/chrM/Homo_sapiens_assembly38.chrM.fasta.amb',
+        ann='gs://cpg-common-main/references/hg38/v0/chrM/Homo_sapiens_assembly38.chrM.fasta.ann',
+        bwt='gs://cpg-common-main/references/hg38/v0/chrM/Homo_sapiens_assembly38.chrM.fasta.bwt',
+        fai='gs://cpg-common-main/references/hg38/v0/chrM/Homo_sapiens_assembly38.chrM.fasta.fai',
+        pac='gs://cpg-common-main/references/hg38/v0/chrM/Homo_sapiens_assembly38.chrM.fasta.pac',
+        sa='gs://cpg-common-main/references/hg38/v0/chrM/Homo_sapiens_assembly38.chrM.fasta.sa',
+    )
+
+    shifted_mt_ref = b.read_input_group(
+        dict='gs://cpg-common-main/references/hg38/v0/chrM/Homo_sapiens_assembly38.shifted_by_8000_bases.chrM.dict',
+        fasta='gs://cpg-common-main/references/hg38/v0/chrM/Homo_sapiens_assembly38.shifted_by_8000_bases.chrM.fasta',
+        amb='gs://cpg-common-main/references/hg38/v0/chrM/Homo_sapiens_assembly38.chrM.shifted_by_8000_bases.fasta.amb',
+        ann='gs://cpg-common-main/references/hg38/v0/chrM/Homo_sapiens_assembly38.chrM.shifted_by_8000_bases.fasta.ann',
+        bwt='gs://cpg-common-main/references/hg38/v0/chrM/Homo_sapiens_assembly38.chrM.shifted_by_8000_bases.fasta.bwt',
+        fai='gs://cpg-common-main/references/hg38/v0/chrM/Homo_sapiens_assembly38.chrM.shifted_by_8000_bases.fasta.fai',
+        pac='gs://cpg-common-main/references/hg38/v0/chrM/Homo_sapiens_assembly38.chrM.shifted_by_8000_bases.fasta.pac',
+        sa='gs://cpg-common-main/references/hg38/v0/chrM/Homo_sapiens_assembly38.chrM.shifted_by_8000_bases.fasta.sa',
+    )
+
+    # As we are only accessing a tiny chunk of the cram
+    # accessing the cram via cloudfuse is faster than localising the full cram
+    bucket = cram_path.path.drive
+    print(f'bucket = {bucket}')
+    bucket_mount_path = to_path('/bucket')
+    j.cloudfuse(bucket, str(bucket_mount_path), read_only=True)
+    mounted_cram_path = bucket_mount_path / '/'.join(cram_path.path.parts[2:])
+    assert cram_path.index_path  # keep mypy happy as index_path is optional
+    mounted_cram_index_path = bucket_mount_path / '/'.join(
+        cram_path.index_path.parts[2:]
+    )
+
+    res = STANDARD.request_resources(ncpu=4)
+    res.set_to_job(j)
+    nthreads = res.get_nthreads()
+
+    cmd = dedent(
+        f"""\
+        bazam -Xmx16g -Dsamjdk.reference_fasta={reference.base} \
+            -n{min(nthreads, 6)} {mounted_cram_path} -L chrM | \
+         bwa mem -p {mt_ref.fasta}  - | \
+         samtools view -bSu - | \
+         samtools sort -o out.bam
+        """
+    )
+
+    j.command(command(cmd))
+    b.write_output(j.mt_aligned_cram, str(mt_aligned_cram))
+
+    return j
---
+++ cpg_workflows/stages/mito.py
@@ -0,0 +1,81 @@
+"""
+Stage to call mito genome variants
+
+Reimplemented version of;
+https://github.com/broadinstitute/gatk/blob/master/scripts/mitochondria_m2_wdl/MitochondriaPipeline.wdl
+
+"""
+from typing import Any
+
+from cpg_utils import Path
+from cpg_utils.config import get_config
+from cpg_workflows import get_batch
+from cpg_workflows.filetypes import CramPath
+from cpg_workflows.jobs import mito
+from cpg_workflows.stages.align import Align
+from cpg_workflows.targets import Sample
+from cpg_workflows.utils import exists
+from cpg_workflows.workflow import (
+    stage,
+    StageInput,
+    StageOutput,
+    SampleStage,
+)
+
+
+@stage(
+    required_stages=Align,
+    # analysis_type='vcf',
+)
+class AlignAndGenotypeMito(SampleStage):
+    """
+    Re-align and call mitochondrial genome using bwa and mutect2
+    """
+
+    def expected_outputs(self, sample: Sample) -> dict[str, Path]:
+        # Listing all outputs from Broad WDL here commenting what we chose not to generate
+        return {
+    # 'subset_bam': sample.dataset.prefix() / f'{sample.id}.mito.foo',
+    # 'subset_bai': sample.dataset.prefix() / f'{sample.id}.mito.foo',
+    'mt_aligned_cram': sample.dataset.prefix() / f'{sample.id}.mito.cram',
+    # 'mt_aligned_bai': sample.dataset.prefix() / f'{sample.id}.mito.foo',
+    'out_vcf': sample.dataset.prefix() /  f'{sample.id}.mito.vcf',
+    # 'out_vcf_index': sample.dataset.prefix() / f'{sample.id}.mito.foo',
+    # 'split_vcf': sample.dataset.prefix() / f'{sample.id}.mito.foo',
+    # 'split_vcf_index': sample.dataset.prefix() / f'{sample.id}.mito.foo',
+    # 'input_vcf_for_haplochecker': sample.dataset.prefix() / f'{sample.id}.mito.foo',
+    # 'duplicate_metrics': sample.dataset.prefix() / f'{sample.id}.mito.foo',
+    # 'coverage_metrics': sample.dataset.prefix() / f'{sample.id}.mito.foo',
+    # 'theoretical_sensitivity_metrics': sample.dataset.prefix() / f'{sample.id}.mito.foo',
+    # 'contamination_metrics': sample.dataset.prefix() / f'{sample.id}.mito.foo',
+    # 'base_level_coverage_metrics': sample.dataset.prefix() / f'{sample.id}.mito.foo',
+    # 'mean_coverage': sample.dataset.prefix() / f'{sample.id}.mito.foo',
+    # 'median_coverage': sample.dataset.prefix() / f'{sample.id}.mito.foo',
+    # 'major_haplogroup': sample.dataset.prefix() / f'{sample.id}.mito.foo',
+    # 'contamination': sample.dataset.prefix() / f'{sample.id}.mito.foo',
+        }
+
+    def queue_jobs(self, sample: Sample, inputs: StageInput) -> StageOutput | None:
+        cram_path = inputs.as_path(sample, Align, 'cram')
+        crai_path = inputs.as_path(sample, Align, 'crai')
+
+        jobs = []
+        # Generate minibam
+        # cpg_workflows/jobs/align.py: extract_fastq
+        # sort_cmd
+        # finalise_alignment (mark dups)
+
+        # map twice
+        # collect metrics
+
+        j = mito.mito_realign(
+            b=get_batch(),
+            sample=sample,
+            cram_path=CramPath(cram_path, crai_path),
+            mt_aligned_cram=self.expected_outputs(sample)['mt_aligned_cram'],
+            job_attrs=self.get_job_attrs(sample),
+        )
+
+        jobs.append(j)
+
+        return self.make_outputs(sample, data=self.expected_outputs(sample), jobs=jobs)
