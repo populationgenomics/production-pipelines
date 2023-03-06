6bc4852963c428d553e11f52df54cf5bc580dfc8
Cas Simons <cas.simons@populationgenomics.org.au>
2023-03-06 20:56:20 +1300

    hc path

--- cpg_workflows/jobs/mito.py
+++ cpg_workflows/jobs/mito.py
@@ -403,7 +403,7 @@ def get_contamination(
 
     cmd = f"""
         PARENT_DIR="$(dirname "{vcf['vcf.gz']}")"
-        java -jar /haplocheckCLI/haplocheckCLI.jar "$PARENT_DIR"
+        java -jar /haplocheckCLI.jar "$PARENT_DIR"
 
         sed 's/\"//g' output > output-noquotes
 
