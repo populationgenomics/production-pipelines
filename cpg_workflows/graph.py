f6bbabfbd1b6f0a2a157b924b19f55bb47dc8095
Sabrina <9669990+violetbrina@users.noreply.github.com>
2023-03-10 16:07:03 +1100

    IN PROGRESS: open file on click

--- cpg_workflows/graph.py
+++ cpg_workflows/graph.py
@@ -5,6 +5,7 @@ Heavily inspired by:
 https://towardsdatascience.com/visualize-hierarchical-data-using-plotly-and-datapane-7e5abe2686e1
 """
 
+import os
 from copy import deepcopy
 from typing import Callable
 from itertools import groupby
@@ -16,12 +17,18 @@ import plotly.graph_objs as go
 
 class GraphPlot:
     def __init__(self, G: nx.DiGraph, **kwargs):
+        # Data
         self.G = deepcopy(G)
+
+        script_location = os.path.dirname(os.path.abspath(__file__))
+        self.stages_folder = os.path.abspath(f'{script_location}/stages/')
+        self.links = {n: self._get_node_file_link(n) for n in G.nodes}
 
         # Text and sizes
         self.title = 'Workflow Graph'
         self.title_fontsize = 24
         self.node_text_position = 'top center'
+        self.node_text_fontsize = 14
         self.node_size = 50
         self.node_border_weight = 5
         self.edge_weight = 1
@@ -35,7 +42,7 @@ class GraphPlot:
         # Colors
         self.colorscale = 'Blugrn'
         self.node_color_key = self.partite_key
-        self.grey_color = '#999'
+        self.grey_color = 'rgba(153, 153, 153, 0.5)'
         self.dark_emphasis_color = '#0c1f27'
         self.arrow_opacity = 0.8
 
@@ -58,7 +65,7 @@ class GraphPlot:
     def __add__(self, other) -> go.Figure:
         assert type(self) == type(other)
 
-        fig = make_subplots(rows=1, cols=2)
+        fig = make_subplots(rows=1, cols=2, column_titles=[self.title, other.title])
 
         for trace in self.create_traces():
             fig.add_trace(trace, row=1, col=1)
@@ -66,8 +73,11 @@ class GraphPlot:
         for trace in other.create_traces():
             fig.add_trace(trace, row=1, col=2)
 
+        # Set the overall layout using this objects layout
+        # Make sure the updates are to both subplot axes
         layout = self._get_layout()
         fig.update_layout(layout)
+        fig.update_layout(title='')
         fig.update_yaxes(layout.yaxis)
         fig.update_xaxes(layout.xaxis)
 
@@ -92,6 +102,7 @@ class GraphPlot:
                 showticklabels=False,
                 autorange='reversed',
             ),
+            coloraxis=dict(reversescale=False),
         )
 
     def display_graph(self):
@@ -110,7 +121,7 @@ class GraphPlot:
         node_name, node_hovertext, node_color = self._get_node_data()
 
         # Begin plotting
-        edge_x, edge_y = self._get_edge_positions(self._edge_filter)
+        edge_x, edge_y = self._get_edge_positions(self._non_skipped_edge)
         edge_trace_dark = go.Scatter(
             x=edge_x,
             y=edge_y,
@@ -119,7 +130,9 @@ class GraphPlot:
             mode='lines',
             opacity=self.arrow_opacity,
         )
-        edge_x, edge_y = self._get_edge_positions(lambda x: not self._edge_filter(x))
+        edge_x, edge_y = self._get_edge_positions(
+            lambda x: not self._non_skipped_edge(x)
+        )
         edge_trace_grey = go.Scatter(
             x=edge_x,
             y=edge_y,
@@ -129,16 +142,18 @@ class GraphPlot:
             opacity=self.arrow_opacity,
         )
 
+        layer_min, layer_max = self._get_layer_range()
         node_trace = go.Scatter(
             x=node_x,
             y=node_y,
             mode='markers+text',
             textposition=self.node_text_position,
+            textfont_size=self.node_text_fontsize,
             hoverinfo='text',
             marker=dict(
                 showscale=True,
                 colorscale=self.colorscale,
-                reversescale=True,
+                reversescale=False,
                 color=[],
                 size=self.node_size,
                 line=dict(
@@ -149,12 +164,16 @@ class GraphPlot:
                     title='Workflow Depth',
                     xanchor='left',
                     titleside='right',
+                    tickmode='array',
+                    tickvals=list(range(-layer_min, -layer_max - 1, -1)),
+                    ticktext=list(range(layer_min, layer_max + 1, 1)),
                 ),
             ),
         )
         node_trace.text = node_name
         node_trace.hovertext = node_hovertext
         node_trace.marker.color = node_color
+        node_trace.marker.colorbar
 
         return [edge_trace_dark, edge_trace_grey, node_trace]
 
@@ -191,7 +210,7 @@ class GraphPlot:
     def _get_node_data(self):
         node_color = list(
             map(
-                lambda n: self._get_node_color(n[0], n[1][self.node_color_key])[0],
+                lambda n: self._get_node_color(n[0], -n[1][self.node_color_key])[0],
                 self.G.nodes.items(),
             )
         )
@@ -242,14 +261,21 @@ class GraphPlot:
         else:
             return self.dark_emphasis_color
 
-    def _edge_filter(self, edge):
+    def _non_skipped_edge(self, edge) -> bool:
         return (
             not self.G.nodes[edge[0]]['skipped']
             and not self.G.nodes[edge[1]]['skipped']
         )
 
+    def _non_skipped_node(self, node: str) -> bool:
+        return not self.G.nodes[node]['skipped']
+
     def _get_edge_color(self, edge):
-        return self.dark_emphasis_color if self._edge_filter(edge) else self.grey_color
+        return (
+            self.dark_emphasis_color
+            if self._non_skipped_edge(edge)
+            else self.grey_color
+        )
 
     def _get_node_colors(self):
         return [self._get_node_color(n)[0] for n in self.G.nodes]
@@ -301,24 +327,23 @@ class GraphPlot:
                 self.G.nodes[node['name']]['pos'] = group_pos[idx]
 
     def get_annotations(self, xref='x', yref='y'):
+        def pts(edge, key):
+            pts = {
+                'p1.x': self.G.nodes[edge[0]]['pos'][0],
+                'p1.y': self.G.nodes[edge[0]]['pos'][1],
+                'p2.x': self.G.nodes[edge[1]]['pos'][0],
+                'p2.y': self.G.nodes[edge[1]]['pos'][1],
+            }
+            return pts.get(key)
+
         return [
             go.layout.Annotation(
-                ax=(self.G.nodes[edge[0]]['pos'][0] + self.G.nodes[edge[1]]['pos'][0])
-                / 2,
-                ay=(self.G.nodes[edge[0]]['pos'][1] + self.G.nodes[edge[1]]['pos'][1])
-                / 2,
+                ax=(pts(edge, 'p1.x') * 7 + pts(edge, 'p2.x') * 1) / 8,
+                ay=(pts(edge, 'p1.y') * 7 + pts(edge, 'p2.y') * 1) / 8,
+                x=(pts(edge, 'p1.x') * 5 + pts(edge, 'p2.x') * 3) / 8,
+                y=(pts(edge, 'p1.y') * 5 + pts(edge, 'p2.y') * 3) / 8,
                 axref=xref,
                 ayref=yref,
-                x=(
-                    self.G.nodes[edge[1]]['pos'][0] * 3
-                    + self.G.nodes[edge[0]]['pos'][0]
-                )
-                / 4,
-                y=(
-                    self.G.nodes[edge[1]]['pos'][1] * 3
-                    + self.G.nodes[edge[0]]['pos'][1]
-                )
-                / 4,
                 xref=xref,
                 yref=yref,
                 showarrow=True,
@@ -357,3 +382,18 @@ class GraphPlot:
             )
         )
         return sorted_group + skipped_group
+
+    def _get_layer_range(self):
+        nodes = set(filter(self._non_skipped_node, self.G.nodes))
+        layer = nx.get_node_attributes(self.G, self.partite_key)
+        layer = set([val for node, val in layer.items() if node in nodes])
+        return min(layer), max(layer)
+
+    def _get_node_file_link(self, node: str):
+        def normalize(string: str):
+            return string.lower().replace('-', '').replace('_', '').replace('.py', '')
+
+        files = os.scandir(self.stages_folder)
+        files = {normalize(os.path.basename(f)): f for f in files}
+        print(files)
+        
