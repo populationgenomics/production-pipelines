39bba519a1e36a89b9947718690faefa4df6ebb0
Cas Simons <cas.simons@populationgenomics.org.au>
2023-03-12 23:53:14 +1300

    dumb

--- cpg_workflows/jobs/mito.py
+++ cpg_workflows/jobs/mito.py
@@ -647,53 +647,7 @@ def get_contamination(
     return j
 
 
-def parse_contamination_worker(
-    haplocheck_report: str, verifybamid_report: str = "", out_path: str = ""
-):
-    """
-    Process haplocheckCLI and verifyBamIDoutputs to get contamination level as a
-    single float
 
-    Based on logic here:
-    https://github.com/broadinstitute/gatk/blob/227bbca4d6cf41dbc61f605ff4a4b49fc3dbc337/scripts/mitochondria_m2_wdl/AlignAndCall.wdl#LL523-L524
-
-    """
-    cleaned_lines = []
-    with open(haplocheck_report) as haplocheck:
-        # Split line on tabs and strip double quotes
-        for line in haplocheck:
-            cleaned_lines.append([x.strip('"') for x in line.strip().split('\t')])
-    # print(cleaned_lines)
-    # Reformat into a nice dict
-    assert len(cleaned_lines) == 2, "haplocheck report is unexpected format"
-    assert len(cleaned_lines[0]) == 17, "haplocheck report is unexpected format"
-    report = dict(zip(cleaned_lines[0], cleaned_lines[1]))
-
-    # Determine final contamination level
-    if report['Contamination'] == 'YES':
-        if float(report['MeanHetLevelMajor']) == 0:
-            max_contamination = float(report['MeanHetLevelMinor'])
-        else:
-            max_contamination = 1.0 - float(report['MeanHetLevelMajor'])
-    else:
-        max_contamination = 0.0
-
-    # If verifybamid_report is provided, chose the higher of the two
-    if verifybamid_report:
-        with open(verifybamid_report) as verifybamid:
-            lines = [line.split('\t') for line in verifybamid.readlines()]
-            assert len(lines) == 2
-            report = dict(zip(lines[0], lines[1]))
-
-        verifybamid_estimate = float(report['FREEMIX'])
-
-        if verifybamid_estimate > max_contamination:
-            max_contamination = verifybamid_estimate
-
-    if out_path:
-        with open(out_path, 'w') as out:
-            print(max_contamination, file=out)
-    return max_contamination
 
 
 def parse_contamination(
@@ -711,6 +665,54 @@ def parse_contamination(
 
     res = STANDARD.request_resources(ncpu=2)
     res.set_to_job(j)
+
+    def parse_contamination_worker(
+        haplocheck_report: str, verifybamid_report: str = "", out_path: str = ""
+    ):
+        """
+        Process haplocheckCLI and verifyBamIDoutputs to get contamination level as a
+        single float
+
+        Based on logic here:
+        https://github.com/broadinstitute/gatk/blob/227bbca4d6cf41dbc61f605ff4a4b49fc3dbc337/scripts/mitochondria_m2_wdl/AlignAndCall.wdl#LL523-L524
+
+        """
+        cleaned_lines = []
+        with open(haplocheck_report) as haplocheck:
+            # Split line on tabs and strip double quotes
+            for line in haplocheck:
+                cleaned_lines.append([x.strip('"') for x in line.strip().split('\t')])
+        # print(cleaned_lines)
+        # Reformat into a nice dict
+        assert len(cleaned_lines) == 2, "haplocheck report is unexpected format"
+        assert len(cleaned_lines[0]) == 17, "haplocheck report is unexpected format"
+        report = dict(zip(cleaned_lines[0], cleaned_lines[1]))
+
+        # Determine final contamination level
+        if report['Contamination'] == 'YES':
+            if float(report['MeanHetLevelMajor']) == 0:
+                max_contamination = float(report['MeanHetLevelMinor'])
+            else:
+                max_contamination = 1.0 - float(report['MeanHetLevelMajor'])
+        else:
+            max_contamination = 0.0
+
+        # If verifybamid_report is provided, chose the higher of the two
+        if verifybamid_report:
+            with open(verifybamid_report) as verifybamid:
+                lines = [line.split('\t') for line in verifybamid.readlines()]
+                assert len(lines) == 2
+                report = dict(zip(lines[0], lines[1]))
+
+            verifybamid_estimate = float(report['FREEMIX'])
+
+            if verifybamid_estimate > max_contamination:
+                max_contamination = verifybamid_estimate
+
+        if out_path:
+            with open(out_path, 'w') as out:
+                print(max_contamination, file=out)
+        return max_contamination
 
     contamination_level = j.call(parse_contamination_worker, haplocheck_output)
     # contamination_level = j.call(myfunc.get_final_contamination, 'foo')
