8f55ec40b5fffd4e1e93a4f6cf3f8fb243e8fe19
Cas Simons <cas.simons@populationgenomics.org.au>
2023-04-22 09:48:41 +1200

    Add ValidateFastq to pre_alignment pipeline.

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ from cpg_workflows.workflow import run_workflow, StageDecorator
 from cpg_workflows.stages.large_cohort import LoadVqsr, Frequencies, AncestryPlots
 from cpg_workflows.stages.cram_qc import CramMultiQC
 from cpg_workflows.stages.gvcf_qc import GvcfMultiQC
-from cpg_workflows.stages.fastqc import FastQCMultiQC
+from cpg_workflows.stages.fastqc import FastQCMultiQC, ValidateFastq
 from cpg_workflows.stages.seqr_loader import MtToEs, AnnotateDataset
 from cpg_workflows.stages.gatk_sv import ClusterBatch
 from cpg_workflows.stages.stripy import Stripy
 
 
 WORKFLOWS: dict[str, list[StageDecorator]] = {
-    'pre_alignment': [FastQCMultiQC],
+    'pre_alignment': [FastQCMultiQC, ValidateFastq],
     'seqr_loader': [AnnotateDataset, MtToEs, GvcfMultiQC, CramMultiQC, Stripy],
     'large_cohort': [LoadVqsr, Frequencies, AncestryPlots, GvcfMultiQC, CramMultiQC],
     'gatk_sv': [ClusterBatch],
