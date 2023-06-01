da3cb97db1f36a61f8d9dcf607edfdfb33fce469
MattWellie <mattwellie@gmail.com>
2023-06-01 16:27:32 +1000

    adds happy runner step

--- cpg_workflows/jobs/validation.py
+++ cpg_workflows/jobs/validation.py
@@ -6,9 +6,9 @@ jobs relating to the validation steps of the pipeline
 from hailtop.batch.job import Job
 from hailtop.batch import Batch
 
-from cpg_utils import Path
+from cpg_utils import to_path, Path
 from cpg_utils.config import get_config
-from cpg_utils.hail_batch import image_path, query_command
+from cpg_utils.hail_batch import fasta_res_group, image_path, query_command
 
 
 def validation_mt_to_vcf_job(
@@ -53,3 +53,103 @@ def validation_mt_to_vcf_job(
         vcf_j.depends_on(*depends_on)
 
     return vcf_j
+
+
+def run_happy_on_vcf(
+    b: Batch,
+    vcf_path: str,
+    sample_ext_id: str,
+    out_prefix: str,
+    job_attrs: dict | None = None,
+    depends_on: list[Job] | None = None
+):
+    """
+    Run hap.py on the for this single-sample VCF
+    Use Truth data from config
+
+    Args:
+        b (): the batch to create a new job in
+        vcf_path (str): path to the single-sample VCF
+        sample_ext_id (str): external ID to find reference data
+        out_prefix (str): where to export happy outputs
+        job_attrs ():
+        depends_on ():
+
+    Returns:
+        This Job or None
+    """
+
+    happy_j = b.new_job(
+        f'Run Happy on {sample_ext_id} VCF', (job_attrs or {}) | {'tool': 'hap.py'}
+    )
+    happy_j.image(image_path('happy')).memory('100Gi').storage('100Gi').cpu(4)
+    if depends_on:
+        happy_j.depends_on(*depends_on)
+
+    # region: read input data into batch
+    vcf_input = b.read_input_group(vcf=vcf_path, index=f'{vcf_path}.tbi')
+
+    ref_data = get_config()['references'][sample_ext_id]
+    assert all(key in ref_data for key in ['vcf', 'index', 'bed'])
+
+    # read in sample-specific truth data from config
+    truth_input = b.read_input_group(
+        vcf=ref_data['vcf'], index=ref_data['index'], bed=ref_data['bed']
+    )
+
+    happy_j.declare_resource_group(
+        output={
+            'happy_extended.csv': '{root}/output.extended.csv',
+            'happy.vcf.gz': '{root}/output.vcf.gz',
+            'happy.vcf.gz.tbi': '{root}/output.vcf.gz.tbi',
+            'happy_roc.all.csv.gz': '{root}/output.roc.all.csv.gz',
+            'happy_metrics.json.gz': '{root}/output.metrics.json.gz',
+            'happy_runinfo.json': '{root}/output.runinfo.json',
+            'summary.csv': '{root}/output.summary.csv',
+        },
+    )
+
+    # read in all the SDF genome indices used by hap.py
+    ref_genome_sdf = get_config()['references']['refgenome_sdf']
+    sdf = b.read_input_group(
+        **{file.name: file.as_uri() for file in to_path(ref_genome_sdf).glob('*')},
+    )
+    # endregion
+
+    # includes the creation of the output temp file, which hap.py won't do
+    command = (
+        f'mkdir {happy_j.output} && '
+        f'hap.py {truth_input["vcf"]} {vcf_input["vcf"]} '
+        f'-r {fasta_res_group(b)["base"]} -R {truth_input["bed"]} '
+        f'-o {happy_j.output}/output --leftshift '
+        f'--threads 4 --preprocess-truth '
+        f'--engine-vcfeval-path=/opt/hap.py/libexec/rtg-tools-install/rtg '
+        f'--engine-vcfeval-template {sdf} --engine=vcfeval '
+    )
+
+    # region: stratification BED files
+    if stratification := get_config()['stratification']:
+        strat_folder = to_path(stratification)
+        assert (
+            strat_folder.exists()
+        ), f'{stratification} does not exist, or was not accessible'
+
+        definitions = strat_folder / 'definition.tsv'
+        assert (
+            definitions.exists()
+        ), f'the region file {str(definitions)} does not exist'
+
+        strat_bed_files = list(strat_folder.glob('*.bed*'))
+        assert len(strat_bed_files) > 0, 'No bed files in the stratified BED folder'
+
+        # create a dictionary to pass to input generation
+        strat_dict = {'definition.tsv': str(definitions)}
+        strat_dict.update({file.name: str(file) for file in strat_bed_files})
+        batch_beds = b.read_input_group(**strat_dict)
+        command += f'--stratification {batch_beds["definition.tsv"]}'
+    # endregion
+
+    happy_j.command(command)
+
+    b.write_output(happy_j.output, out_prefix)
+    return happy_j
--- cpg_workflows/stages/happy_validation.py
+++ cpg_workflows/stages/happy_validation.py
@@ -23,7 +23,7 @@ from cpg_workflows.workflow import (
     get_workflow,
 )
 from cpg_workflows.stages.seqr_loader import AnnotateDataset, _sg_vcf_meta
-from cpg_workflows.jobs.validation import validation_mt_to_vcf_job
+from cpg_workflows.jobs.validation import validation_mt_to_vcf_job, run_happy_on_vcf
 from .. import get_batch
 
 
@@ -57,6 +57,10 @@ class ValidationMtToVcf(SampleStage):
         if sample.dataset.name != 'validation':
             return None
 
+        # only keep the samples with reference data
+        if sample.external_id not in get_config()['references']:
+            return None
+
         # get the input mt for this dataset
         mt_path = inputs.as_path(target=sample.dataset, stage=AnnotateDataset, key='mt')
         exp_outputs = self.expected_outputs(sample)
@@ -71,6 +75,8 @@ class ValidationMtToVcf(SampleStage):
         )
 
         return self.make_outputs(sample, data=exp_outputs, jobs=job)
+
+
 @stage(
     required_stages=ValidationMtToVcf,
     analysis_type='custom',
@@ -80,20 +86,20 @@ class ValidationMtToVcf(SampleStage):
 class ValidationHappyOnVcf(SampleStage):
 
     def expected_outputs(self, sample: Sample):
-
+        output_prefix = (
+            sample.dataset.prefix() /
+            'validation' /
+            get_workflow().output_version /
+            sample.id
+        )
         return {
-            'vcf': (
-                sample.dataset.prefix()
-                / 'validation'
-                / get_workflow().output_version
-                / f'{sample.id}.vcf.bgz'
-            ),
-            'index': (
-                sample.dataset.prefix()
-                / 'validation'
-                / get_workflow().output_version
-                / f'{sample.id}.vcf.bgz.tbi'
-            )
+            'vcf': output_prefix / 'happy.vcf.bgz',
+            'index': output_prefix / 'happy.vcf.bgz.tbi',
+            'happy_csv': output_prefix / 'happy_extended.csv',
+            'happy_roc': output_prefix / 'happy_roc.all.csv.gz',
+            'happy_metrics': output_prefix / 'happy_metrics.json.gz',
+            'happy_runinfo': output_prefix / 'happy_runinfo.json',
+            'happy_summary': output_prefix / 'summary.csv',
         }
 
     def queue_jobs(self, sample: Sample, inputs: StageInput) -> StageOutput | None:
@@ -101,17 +107,29 @@ class ValidationHappyOnVcf(SampleStage):
         if sample.dataset.name != 'validation':
             return None
 
-        # get the input mt for this dataset
-        mt_path = inputs.as_path(target=sample.dataset, stage=AnnotateDataset, key='mt')
-        exp_outputs = self.expected_outputs(sample)
+        # only keep the samples with reference data
+        if sample.external_id not in get_config()['references']:
+            return None
 
-        job = validation_mt_to_vcf_job(
+        # get the input vcf for this sequence group
+        input_vcf = inputs.as_path(target=sample, stage=ValidationMtToVcf, key='vcf')
+
+        # set the prefix to write outputs to
+        output_prefix = (
+            sample.dataset.prefix() /
+            'validation' /
+            get_workflow().output_version /
+            sample.id
+        )
+
+        exp_outputs = self.expected_outputs(sample)
+        job = run_happy_on_vcf(
             b=get_batch(),
-            mt_path=mt_path,
-            sample_id=sample.id,
-            out_vcf_path=exp_outputs['vcf'],
+            vcf_path=str(input_vcf),
+            sample_ext_id=sample.external_id,
+            out_prefix=str(output_prefix),
             job_attrs=self.get_job_attrs(sample),
-            depends_on=inputs.get_jobs(sample),
+            depends_on=inputs.get_jobs(sample)
         )
 
         return self.make_outputs(sample, data=exp_outputs, jobs=job)
