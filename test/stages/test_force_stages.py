b1a69c946e98ab039c9fbdc803de2ced5815a815
daniaki <7043686+daniaki@users.noreply.github.com>
2023-06-02 10:34:21 +1000

    Inline test configs; use `set_config`, `tmp_path`

--- test/stages/test_force_stages.py
+++ test/stages/test_force_stages.py
@@ -2,22 +2,47 @@
 Test building stages DAG.
 """
 
-import toml
 from pytest_mock import MockFixture
-from . import TOML, run_workflow
+from . import run_workflow
+from .. import set_config
 
 
-def test_force_stages(mocker: MockFixture):
+def test_force_stages(mocker: MockFixture, tmp_path):
     """
     A -> B -> C, all results exist
     force_stages = [B2]
     Should run: B
     """
-    conf = toml.loads(TOML)
-    conf['workflow']['force_stages'] = ['B']
-    mocker.patch('cpg_utils.config.get_config', lambda: conf)
-    mocker.patch('cpg_workflows.utils.exists_not_cached', lambda *args: True)
+    conf = f"""
+    [workflow]
+    dataset_gcp_project = 'fewgenomes'
+    access_level = 'test'
+    dataset = 'fewgenomes'
+    sequencing_type = 'genome'
 
+    check_inputs = false
+    check_intermediates = false
+
+    # Skip stages with outputs that already exist
+    check_expected_outputs = true 
+
+    # Except force stage B to re-run
+    force_stages = ['B']
+
+    [storage.default]
+    default = '{tmp_path}'
+    [storage.fewgenomes]
+    default = '{tmp_path}'
+
+    [hail]
+    billing_project = 'fewgenomes'
+    delete_scratch_on_exit = false
+    backend = 'local'
+    dry_run = true
+    """
+
+    mocker.patch('cpg_workflows.utils.exists_not_cached', lambda *args: True)
+    set_config(conf, tmp_path / 'config.toml')
     run_workflow(mocker)
 
     from cpg_workflows.workflow import get_batch
