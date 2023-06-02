bb14542f76b4b5c126d39853199b077e3b7235d5
daniaki <7043686+daniaki@users.noreply.github.com>
2023-06-02 10:27:02 +1000

    Code cleanup; add fixture to reset global vars

--- test/conftest.py
+++ test/conftest.py
@@ -1,38 +1,24 @@
-import os
-import string
-from random import choices
-import time
-from cpg_utils import to_path
+import cpg_utils.config
+import pytest
+
+import cpg_workflows.batch
+import cpg_workflows.inputs
+import cpg_workflows.metamist
+import cpg_workflows.stages.gatk_sv
+import cpg_workflows.workflow
 
 
-def results_prefix():
-    """
-    Output directory for the test results.
-    """
-    path = (
-        to_path(__file__).parent
-        / 'results'
-        / os.getenv(
-            'TEST_TIMESTAMP',
-            # Generate a timestamp string. Don't import `timestamp` from `cpg_utils`,
-            # as it would break mocking in some tests.
-            time.strftime('%Y_%m%d_%H%M')
-            + '_'
-            + ''.join(choices(string.ascii_uppercase + string.digits, k=5)),
-        )
-    ).absolute()
-    path.mkdir(parents=True, exist_ok=True)
-    return path
+@pytest.fixture(autouse=True, scope='function')
+def pre_and_post_test():
+    yield
 
+    # Reset config paths to defaults
+    cpg_utils.config.set_config_paths([])
 
-def update_dict(d1: dict, d2: dict) -> None:
-    """
-    Helper functon to merge one dict into another. We don't import `update_dict` from
-    `cpg_utils`, as it would break mocking in some tests.
-    """
-    for k, v2 in d2.items():
-        v1 = d1.get(k)
-        if isinstance(v1, dict) and isinstance(v2, dict):
-            update_dict(v1, v2)
-        else:
-            d1[k] = v2
+    # Clear pre-existing state before running a new workflow
+    setattr(cpg_utils.config, '_config', None)
+    setattr(cpg_workflows.batch, '_batch', None)
+    setattr(cpg_workflows.workflow, '_workflow', None)
+    setattr(cpg_workflows.inputs, '_cohort', None)
+    setattr(cpg_workflows.stages.gatk_sv, '_FASTA', None)
+    setattr(cpg_workflows.metamist, '_metamist', None)
