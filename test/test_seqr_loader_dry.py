1e9b8650e7dba5d7b1176b28be72f3ef55d8f757
MattWellie <mattwellie@gmail.com>
2023-06-08 09:20:19 +1000

    corrected lambda return in mock

--- test/test_seqr_loader_dry.py
+++ test/test_seqr_loader_dry.py
@@ -177,9 +177,6 @@ def test_seqr_loader_dry(mocker: MockFixture, tmp_path):
 
     mocker.patch('cpg_workflows.inputs.create_cohort', _mock_cohort)
 
-    def mock_exists(*args, **kwargs) -> bool:
-        return False
-
     def do_nothing(*args, **kwargs):
         return None
 
@@ -190,7 +187,7 @@ def test_seqr_loader_dry(mocker: MockFixture, tmp_path):
     # functions like get_intervals checks file existence
     mocker.patch('cpg_workflows.workflow.list_all_parent_dirs', lambda *args: {})
     mocker.patch('cpg_workflows.workflow.list_of_all_dir_contents', lambda *args: {})
-    mocker.patch('cpg_workflows.workflow.exists_on_pre_collected', lambda *args: False)
+    mocker.patch('cpg_workflows.workflow.exists_on_pre_collected', lambda *args: None)
     # cloudfuse (used in Vep) doesn't work with LocalBackend
     mocker.patch('hailtop.batch.job.Job.cloudfuse', do_nothing)
     # always_run (used in MtToEs -> hail_dataproc_job) doesn't work with LocalBackend
