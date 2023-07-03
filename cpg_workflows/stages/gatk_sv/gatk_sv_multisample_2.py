0e525a7fc61d2b617736f424449795de810cb2bf
MattWellie <mattwellie@gmail.com>
2023-07-03 15:00:42 +1000

    Update to valid file extensions

--- cpg_workflows/stages/gatk_sv/gatk_sv_multisample_2.py
+++ cpg_workflows/stages/gatk_sv/gatk_sv_multisample_2.py
@@ -87,11 +87,11 @@ class MakeCohortVcf(CohortStage):
             for batch_name in batch_names
         ]
         bincov_files = [
-            batch_prefix / batch_name / 'GatherBatchEvidence' / 'RD.txt.gz'
+            batch_prefix / batch_name / 'GatherBatchEvidence' / f'{self.name}.RD.txt.gz'
             for batch_name in batch_names
         ]
         disc_files = [
-            batch_prefix / batch_name / 'GatherBatchEvidence' / 'pe.txt.gz'
+            batch_prefix / batch_name / 'GatherBatchEvidence' / f'{self.name}.pe.txt.gz'
             for batch_name in batch_names
         ]
         median_cov_files = [
