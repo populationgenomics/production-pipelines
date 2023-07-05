09476d5849ec84e2d13da37cf81913f68de9a71d
mgeaghan <michael.p.geaghan@gmail.com>
2023-07-05 17:06:05 +1000

    add trim stage

---
+++ cpg_workflows/jobs/trim.py
@@ -0,0 +1,131 @@
+"""
+Trim raw FASTQ reads using cutadapt
+"""
+
+import hailtop.batch as hb
+from hailtop.batch.job import Job
+from cpg_utils.hail_batch import command
+from cpg_utils.config import get_config
+from cpg_workflows.utils import can_reuse
+from cpg_workflows.resources import STANDARD
+from cpg_workflows.filetypes import (
+    FastqPair,
+    FastqPairs,
+)
+from cpg_workflows.workflow import (
+    SequencingGroup,
+)
+
+class MissingFastqInputException(Exception):
+    """Raise if alignment input is missing"""
+    pass
+
+
+class InvalidSequencingTypeException(Exception):
+    """Raise if alignment type is not 'rna'"""
+    pass
+
+
+class Cutadapt:
+    """
+    Construct a cutadapt command for trimming FASTQs.
+    """
+
+    def __init__(
+        self,
+        input_fastq_pair: FastqPair,
+        output_fastq_pair: FastqPair,
+        adapterR13p: str | None = None,
+        adapterR23p: str | None = None,
+        adapterR15p: str | None = None,
+        adapterR25p: str | None = None,
+        quality_trim: int | None = None,  # TODO: determine default
+        two_colour: bool = True,
+        min_length: int | None = None,
+        polyA: bool = False,
+        poly: str | None = None,
+        # TODO: add more parameters
+    ):
+        self.command = ['cutadapt', '-o', str(output_fastq_pair.r1), '-p', str(output_fastq_pair.r2)]
+        adapter_args = []
+        if adapterR13p:
+            adapter_args.extend(['-a', adapterR13p])
+        if adapterR23p:
+            adapter_args.extend(['-A', adapterR23p])
+        if adapterR15p:
+            adapter_args.extend(['-g', adapterR15p])
+        if adapterR25p:
+            adapter_args.extend(['-G', adapterR25p])
+        self.command.extend(adapter_args)
+        if quality_trim:
+            if two_colour:
+                self.command.append(f'--nextseq-trim={quality_trim}')
+            else:
+                self.command.append(f'-q {quality_trim}')
+        if min_length:
+            self.command.append(f'--minimum-length={min_length}')
+        if polyA:
+            self.command.append('--poly-a')
+        self.command.extend([str(input_fastq_pair.r1), str(input_fastq_pair.r2)])
+
+    def __str__(self) -> str:
+        return ' '.join(self.command)
+    
+    def __repr__(self) -> str:
+        return str(self)
+
+
+def trim(
+    b: hb.Batch,
+    sequencing_group: SequencingGroup,
+    job_attrs: dict | None = None,
+    input_fq_pair: FastqPair | None = None,
+    output_fq_pair: FastqPair | None = None,
+    extra_label: str | None = None,
+    overwrite: bool = False,
+    requested_nthreads: int | None = None,
+) -> Job:
+    """
+    Takes an input FastqPair object, and creates a job to trim the FASTQs using cutadapt.
+    """
+    # Don't run if all output files exist and can be reused
+    if (
+        output_fq_pair and
+        can_reuse(output_fq_pair.r1, overwrite) and
+        can_reuse(output_fq_pair.r2, overwrite)
+    ):
+        return []
+    
+    base_job_name = 'TrimFastqs'
+    if extra_label:
+        base_job_name += f' {extra_label}'
+    
+    # if number of threads is not requested, using whole instance
+    requested_nthreads = requested_nthreads or STANDARD.max_threads()
+    
+    if not get_config()['workflow']['sequencing_type'] == 'rna':
+        raise InvalidSequencingTypeException(
+            f"Invalid sequencing type '{get_config()['workflow']['sequencing_type']}'" +
+            f" for job type '{base_job_name}'; sequencing type must be 'rna'"
+        )
+    
+    trim_tool = 'cutadapt'
+
+    trim_j_name = base_job_name
+    trim_j_attrs = (job_attrs or {}) | dict(label=base_job_name, tool=trim_tool)
+    trim_j = b.new_job(trim_j_name, trim_j_attrs)
+    trim_cmd = Cutadapt(  # TODO: add more arguments
+        input_fastq_pair=input_fq_pair,
+        output_fastq_pair=FastqPair(
+            r1=trim_j.output_r1,
+            r2=trim_j.output_r2,
+        ),
+    )
+    trim_j.command(command(str(trim_cmd), monitor_space=True))
+
+    # Write output to file
+    if output_fq_pair:
+        b.write_output(trim_j.output_r1, str(output_fq_pair.r1))
+        b.write_output(trim_j.output_r2, str(output_fq_pair.r2))
+
+    return trim_j
---
+++ cpg_workflows/stages/trim.py
@@ -0,0 +1,140 @@
+"""
+Trim raw FASTQ reads using cutadapt
+"""
+
+import logging
+from cpg_utils import Path
+from cpg_utils.config import get_config
+from cpg_workflows import get_batch
+from cpg_workflows.workflow import (
+    stage,
+    StageInput,
+    StageOutput,
+    SequencingGroup,
+    SequencingGroupStage,
+)
+from cpg_workflows.filetypes import (
+    FastqPair,
+    FastqPairs,
+)
+from cpg_workflows.jobs import trim
+from os.path import basename
+import re
+from dataclasses import dataclass
+
+def get_trim_inputs(sequencing_group: SequencingGroup) -> FastqPairs:
+    """
+    Get the input FASTQ file pairs for trimming
+    """
+    sequencing_type = get_config()['workflow']['sequencing_type']
+    alignment_input = sequencing_group.alignment_input_by_seq_type.get(sequencing_type)
+    if (
+        not alignment_input or
+        (get_config()['workflow'].get('check_inputs', True) and not alignment_input.exists()) or
+        not isinstance(alignment_input, (FastqPair, FastqPairs))
+    ):
+        return None
+    if isinstance(alignment_input, FastqPair):
+        alignment_input = FastqPairs([alignment_input])
+    return alignment_input
+
+
+@dataclass
+class InOutFastqPair:
+    """
+    Represents a single set of input and output paired FASTQ files
+    """
+
+    id: str
+    input_pair: FastqPair
+    output_pair: FastqPair
+
+
+def get_input_output_pairs(sequencing_group: SequencingGroup) -> list[InOutFastqPair]:
+    """
+    Get the input FASTQ pairs, determine the trimmed output FASTQ file pair paths and
+    output a list of InOutFastqPair objects
+    """
+    inputs = get_trim_inputs(sequencing_group)
+    if not inputs or not isinstance(inputs, FastqPairs):
+        return None
+    prefix = sequencing_group.dataset.prefix() / 'trim'
+    trim_suffix = '.trimmed.fastq.gz'
+    input_output_pairs = []
+    i = 1
+    for pair in inputs:
+        input_r1_bn = re.sub(re.escape('.fastq.gz'), '', basename(pair.r1))
+        input_r2_bn = re.sub(re.escape('.fastq.gz'), '', basename(pair.r2))
+        output_r1 = prefix / f'{input_r1_bn}{trim_suffix}'
+        output_r2 = prefix / f'{input_r2_bn}{trim_suffix}'
+        input_output_pairs.append(InOutFastqPair(
+            str(i),  # ID
+            pair,  # input FASTQ pair
+            FastqPair(output_r1, output_r2),  # output FASTQ pair
+        ))
+        i += 1
+    return input_output_pairs
+
+    
+def get_output_dict(sequencing_group: SequencingGroup) -> dict[str, Path]:
+    """
+    Return a flat dictionary of output files
+    """
+    input_output_pairs = get_input_output_pairs(sequencing_group)
+    if not input_output_pairs:
+        return {}
+    out_dict = {}
+    for io_pair in input_output_pairs:
+        r1_id = f'{io_pair.id}_R1'
+        r2_id = f'{io_pair.id}_R2'
+        out_dict.update({
+            f'{r1_id}': io_pair.output_pair.r1,
+            f'{r2_id}': io_pair.output_pair.r2,
+        })
+    return out_dict
+
+
+@stage
+class Trim(SequencingGroupStage):
+    """
+    Trim raw FASTQ reads using cutadapt
+    """
+
+    def expected_outputs(self, sequencing_group: SequencingGroup) -> dict[str, Path]:
+        """
+        Expect a series of trimmed FASTQ files
+        """
+        return get_output_dict(sequencing_group)
+    
+    def queue_jobs(self, sequencing_group: SequencingGroup, inputs: StageInput) -> StageOutput | None:
+        """
+        Queue a cutadapt job for each FASTQ file
+        """
+        input_output_pairs = get_input_output_pairs(sequencing_group)
+        jobs = []
+        for io_pair in input_output_pairs:
+            try:
+                j = trim.trim(
+                    b=get_batch(),
+                    sequencing_group=sequencing_group,
+                    input_fq_pair=io_pair.input_pair,
+                    output_fq_pair=io_pair.output_pair,
+                    job_attrs=self.get_job_attrs(sequencing_group),
+                    overwrite=sequencing_group.forced,
+                    extra_label=f'fastq_pair_{io_pair.id}'
+                )
+                jobs.append(j)
+            except trim.MissingFastqInputException:
+                if get_config()['workflow'].get('skip_samples_with_missing_input'):
+                    logging.error(f'No FASTQ inputs, skipping sample {sequencing_group}')
+                    sequencing_group.active = False
+                    return self.make_outputs(sequencing_group, skipped=True)  # return empty output
+                else:
+                    return self.make_outputs(
+                        target=sequencing_group, error_msg=f'No FASTQ input found'
+                    )
+        return self.make_outputs(
+            sequencing_group,
+            data=self.expected_outputs(sequencing_group),
+            jobs=jobs,
+        )
