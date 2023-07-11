9fa35da87018abcec86a36f292ac2c976353f65a
mgeaghan <michael.p.geaghan@gmail.com>
2023-07-11 16:41:48 +1000

    update trim test

--- test/stages/test_trim.py
+++ test/stages/test_trim.py
@@ -175,13 +175,8 @@ def test_rare_rna(mocker: MockFixture, tmp_path):
         for s in sample_list
         if isinstance(s.alignment_input_by_seq_type.get('rna'), FastqPairs)
     ]
-    n_trim_jobs_list.extend([
-        1
-        for s in sample_list
-        if isinstance(s.alignment_input_by_seq_type.get('rna'), FastqPair)
-    ])
     n_trim_jobs = sum(n_trim_jobs_list)
-    assert len(trim_job) == n_trim_jobs
+    assert trim_job['job_n'] == n_trim_jobs
 
     output_path = dataset_path('cmd.txt')
     output_path_parent = os.path.dirname(output_path)
