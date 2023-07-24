f2d5c005adec43a323a7d1f9aedf11fce6647ef2
Daniel Esposito <7043686+daniaki@users.noreply.github.com>
2023-07-24 22:37:52 +1000

    Update function signature

--- test/factories/batch.py
+++ test/factories/batch.py
@@ -5,9 +5,9 @@ from hailtop.batch import LocalBackend
 from cpg_workflows.batch import Batch
 
 
-def create_local_batch(tmp_path: Path | str) -> Batch:
+def create_local_batch(tmp_dir: Path | str, name: str = 'local-test') -> Batch:
     """
     FIXME: __init__ looks in config for hail `dry_run` so this function needs to run
     after config is set up via `set_config_paths` to avoid a `KeyError`.
     """
-    return Batch(name='test-align_job', backend=LocalBackend(tmp_dir=str(tmp_path)))
+    return Batch(name=name, backend=LocalBackend(tmp_dir=str(tmp_dir)))
