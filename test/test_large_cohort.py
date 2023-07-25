5241767605af309ce24d8bf6ca4643d71e25f706
Daniel Esposito <7043686+daniaki@users.noreply.github.com>
2023-07-25 13:01:05 +1000

    Refactored test to use `PipelineConfig` as example

--- test/test_large_cohort.py
+++ test/test_large_cohort.py
@@ -5,68 +5,32 @@ Test large-cohort workflow.
 from os.path import exists
 from pathlib import Path
 
+from cpg_utils import Path as CPGPath
 from cpg_utils import to_path
+from cpg_utils.hail_batch import start_query_context
 from pytest_mock import MockFixture
 
+from cpg_workflows.filetypes import GvcfPath
+from cpg_workflows.large_cohort import (
+    ancestry_pca,
+    ancestry_plots,
+    combiner,
+    dense_subset,
+    relatedness,
+    sample_qc,
+    site_only_vcf,
+)
+from cpg_workflows.targets import Cohort
+
 from . import set_config
+from .factories.config import HailConfig, PipelineConfig, StorageConfig, WorkflowConfig
+from .factories.types import SequencingType
+
 
 ref_prefix = to_path(__file__).parent / 'data/large_cohort/reference'
 gnomad_prefix = ref_prefix / 'gnomad/v0'
 broad_prefix = ref_prefix / 'hg38/v0'
 
-TOML = """
-[workflow]
-dataset_gcp_project = "thousand-genomes"
-dataset = "thousand-genomes"
-access_level = "test"
-sequencing_type = "genome"
-check_intermediates = true
-
-[large_cohort]
-n_pcs = 3
-
-[hail]
-billing_project = "thousand-genomes"
-query_backend = "spark_local"
-
-[combiner]
-intervals = ["chr20:start-end", "chrX:start-end", "chrY:start-end"]
-
-[references]
-genome_build = "GRCh38"
-
-[storage.default]
-default = "{directory}"
-web = "{directory}-web"
-analysis = "{directory}-analysis"
-tmp = "{directory}-test-tmp"
-web_url = "https://test-web.populationgenomics.org.au/fewgenomes"
-
-[storage.thousand-genomes]
-default = "{directory}"
-web = "{directory}-web"
-analysis = "{directory}-analysis"
-tmp = "{directory}-test-tmp"
-web_url = "https://test-web.populationgenomics.org.au/fewgenomes"
-
-[large_cohort.sample_qc_cutoffs]
-min_n_snps = 2500
-
-[references.gnomad]
-tel_and_cent_ht = "{gnomad_prefix}/telomeres_and_centromeres/hg38.telomeresAndMergedCentromeres.ht"
-lcr_intervals_ht = "{gnomad_prefix}/lcr_intervals/LCRFromHengHg38.ht"
-seg_dup_intervals_ht = "{gnomad_prefix}/seg_dup_intervals/GRCh38_segdups.ht"
-clinvar_ht = "{gnomad_prefix}/clinvar/clinvar_20190923.ht"
-hapmap_ht = "{gnomad_prefix}/hapmap/hapmap_3.3.hg38.ht"
-kgp_omni_ht = "{gnomad_prefix}/kgp/1000G_omni2.5.hg38.ht"
-kgp_hc_ht = "{gnomad_prefix}/kgp/1000G_phase1.snps.high_confidence.hg38.ht"
-mills_ht = "{gnomad_prefix}/mills/Mills_and_1000G_gold_standard.indels.hg38.ht"
-predetermined_qc_variants = "{gnomad_prefix}/sample_qc/pre_ld_pruning_qc_variants.ht"
-
-[references.broad]
-genome_calling_interval_lists = "{broad_prefix}/wgs_calling_regions.hg38.interval_list"
-protein_coding_gtf = "{broad_prefix}/sv-resources/resources/v1/MANE.GRCh38.v0.95.select_ensembl_genomic.gtf"
-"""
 
 DEFAULT_CONFIG = Path(
     to_path(__file__).parent.parent / 'cpg_workflows' / 'defaults.toml'
@@ -76,12 +40,91 @@ LARGE_COHORT_CONFIG = Path(
 )
 
 
-def _mock_cohort():
-    from cpg_workflows.filetypes import GvcfPath
-    from cpg_workflows.targets import Cohort
+def create_config(
+    tmp_path: Path,
+    seq_type: SequencingType,
+    gnomad_prefix: CPGPath,
+    broad_prefix: CPGPath,
+) -> PipelineConfig:
+    return PipelineConfig(
+        workflow=WorkflowConfig(
+            dataset='large-cohort-test',
+            access_level='test',
+            sequencing_type=seq_type,
+            check_intermediates=True,
+        ),
+        hail=HailConfig(query_backend='spark_local'),
+        storage={
+            'default': StorageConfig(
+                default=tmp_path,
+                web=tmp_path / 'web',
+                analysis=tmp_path / 'analysis',
+                tmp=tmp_path / 'test-tmp',
+            ),
+            'large-cohort-test': StorageConfig(
+                default=tmp_path,
+                web=tmp_path / 'web',
+                analysis=tmp_path / 'analysis',
+                tmp=tmp_path / 'test-tmp',
+            ),
+        },
+        references={
+            'genome_build': 'GRCh38',
+            'gnomad': {
+                'tel_and_cent_ht': (
+                    gnomad_prefix
+                    / 'telomeres_and_centromeres'
+                    / 'hg38.telomeresAndMergedCentromeres.ht'
+                ),
+                'lcr_intervals_ht': (
+                    gnomad_prefix / 'lcr_intervals' / 'LCRFromHengHg38.ht'
+                ),
+                'seg_dup_intervals_ht': (
+                    gnomad_prefix / 'seg_dup_intervals' / 'GRCh38_segdups.ht'
+                ),
+                'clinvar_ht': (gnomad_prefix / 'clinvar' / 'clinvar_20190923.ht'),
+                'hapmap_ht': (gnomad_prefix / 'hapmap' / 'hapmap_3.3.hg38.ht'),
+                'kgp_omni_ht': (gnomad_prefix / 'kgp' / '1000G_omni2.5.hg38.ht'),
+                'kgp_hc_ht': (
+                    gnomad_prefix / 'kgp' / '1000G_phase1.snps.high_confidence.hg38.ht'
+                ),
+                'mills_ht': (
+                    gnomad_prefix
+                    / 'mills'
+                    / 'Mills_and_1000G_gold_standard.indels.hg38.ht'
+                ),
+                'predetermined_qc_variants': (
+                    gnomad_prefix / 'sample_qc' / 'pre_ld_pruning_qc_variants.ht'
+                ),
+            },
+            'broad': {
+                'genome_calling_interval_lists': (
+                    broad_prefix / 'wgs_calling_regions.hg38.interval_list'
+                ),
+                'protein_coding_gtf': (
+                    broad_prefix
+                    / 'sv-resources'
+                    / 'resources'
+                    / 'v1'
+                    / 'MANE.GRCh38.v0.95.select_ensembl_genomic.gtf'
+                ),
+            },
+        },
+        large_cohort={
+            'n_pcs': 3,
+            'sample_qc_cutoffs': {
+                'min_n_snps': 2500,
+            },
+            'combiner': {
+                'intervals': ['chr20:start-end', 'chrX:start-end', 'chrY:start-end']
+            },
+        },
+    )
 
+
+def _mock_cohort(dataset_id: str):
     cohort = Cohort()
-    dataset = cohort.create_dataset('thousand-genomes')
+    dataset = cohort.create_dataset(dataset_id)
     gvcf_root = to_path(__file__).parent / 'data' / 'large_cohort' / 'gvcf'
     found_gvcf_paths = list(gvcf_root.glob('*.g.vcf.gz'))
     assert len(found_gvcf_paths) > 0, gvcf_root
@@ -95,98 +138,86 @@ def _mock_cohort():
     return cohort
 
 
-def test_large_cohort(mocker: MockFixture, tmp_path):
-    """
-    Run entire workflow in a local mode.
-    """
-    conf = TOML.format(
-        directory=str(tmp_path),
-        gnomad_prefix=gnomad_prefix,
-        broad_prefix=broad_prefix,
-    )
-    set_config(
-        conf,
-        tmp_path / 'config.toml',
-        merge_with=[DEFAULT_CONFIG, LARGE_COHORT_CONFIG],
-    )
+class TestAllLargeCohortMethods:
+    def test_with_sample_data(self, mocker: MockFixture, tmp_path: Path):
+        """
+        Run entire workflow in a local mode.
+        """
+        conf = create_config(tmp_path, 'genome', gnomad_prefix, broad_prefix)
+        set_config(
+            conf,
+            tmp_path / 'config.toml',
+            merge_with=[DEFAULT_CONFIG, LARGE_COHORT_CONFIG],
+        )
 
-    mocker.patch('cpg_workflows.inputs.create_cohort', _mock_cohort)
+        mocker.patch(
+            'cpg_workflows.inputs.create_cohort',
+            lambda: _mock_cohort(conf.workflow.dataset),
+        )
 
-    from cpg_utils.hail_batch import start_query_context
+        start_query_context()
 
-    from cpg_workflows.large_cohort import (
-        ancestry_pca,
-        ancestry_plots,
-        combiner,
-        dense_subset,
-        relatedness,
-        sample_qc,
-        site_only_vcf,
-    )
+        res_pref = tmp_path
+        vds_path = res_pref / 'v01.vds'
+        combiner.run(out_vds_path=vds_path, tmp_prefix=res_pref / 'tmp')
 
-    start_query_context()
+        sample_qc_ht_path = res_pref / 'sample_qc.ht'
+        sample_qc.run(
+            vds_path=vds_path,
+            out_sample_qc_ht_path=sample_qc_ht_path,
+            tmp_prefix=res_pref / 'tmp',
+        )
 
-    res_pref = tmp_path
-    vds_path = res_pref / 'v01.vds'
-    combiner.run(out_vds_path=vds_path, tmp_prefix=res_pref / 'tmp')
+        dense_mt_path = res_pref / 'dense.mt'
+        dense_subset.run(
+            vds_path=vds_path,
+            out_dense_mt_path=dense_mt_path,
+        )
 
-    sample_qc_ht_path = res_pref / 'sample_qc.ht'
-    sample_qc.run(
-        vds_path=vds_path,
-        out_sample_qc_ht_path=sample_qc_ht_path,
-        tmp_prefix=res_pref / 'tmp',
-    )
+        relateds_to_drop_ht_path = res_pref / 'relateds_to_drop.ht'
+        relatedness.run(
+            dense_mt_path=dense_mt_path,
+            sample_qc_ht_path=sample_qc_ht_path,
+            out_relatedness_ht_path=res_pref / 'relatedness.ht',
+            out_relateds_to_drop_ht_path=relateds_to_drop_ht_path,
+            tmp_prefix=res_pref / 'tmp',
+        )
 
-    dense_mt_path = res_pref / 'dense.mt'
-    dense_subset.run(
-        vds_path=vds_path,
-        out_dense_mt_path=dense_mt_path,
-    )
+        scores_ht_path = res_pref / 'scores.ht'
+        eigenvalues_ht_path = res_pref / 'eigenvalues.ht'
+        loadings_ht_path = res_pref / 'loadings.ht'
+        inferred_pop_ht_path = res_pref / 'inferred_pop.ht'
+        ancestry_sample_qc_ht_path = res_pref / 'ancestry_sample_qc.ht'
 
-    relateds_to_drop_ht_path = res_pref / 'relateds_to_drop.ht'
-    relatedness.run(
-        dense_mt_path=dense_mt_path,
-        sample_qc_ht_path=sample_qc_ht_path,
-        out_relatedness_ht_path=res_pref / 'relatedness.ht',
-        out_relateds_to_drop_ht_path=relateds_to_drop_ht_path,
-        tmp_prefix=res_pref / 'tmp',
-    )
+        ancestry_pca.run(
+            dense_mt_path=dense_mt_path,
+            sample_qc_ht_path=sample_qc_ht_path,
+            relateds_to_drop_ht_path=relateds_to_drop_ht_path,
+            tmp_prefix=res_pref / 'tmp',
+            out_scores_ht_path=scores_ht_path,
+            out_eigenvalues_ht_path=eigenvalues_ht_path,
+            out_loadings_ht_path=loadings_ht_path,
+            out_inferred_pop_ht_path=inferred_pop_ht_path,
+            out_sample_qc_ht_path=ancestry_sample_qc_ht_path,
+        )
+        ancestry_plots.run(
+            out_path_pattern=res_pref / 'plots' / '{scope}_pc{pci}_{pca_suffix}.{ext}',
+            sample_qc_ht_path=ancestry_sample_qc_ht_path,
+            scores_ht_path=scores_ht_path,
+            eigenvalues_ht_path=eigenvalues_ht_path,
+            loadings_ht_path=loadings_ht_path,
+            inferred_pop_ht_path=inferred_pop_ht_path,
+        )
 
-    scores_ht_path = res_pref / 'scores.ht'
-    eigenvalues_ht_path = res_pref / 'eigenvalues.ht'
-    loadings_ht_path = res_pref / 'loadings.ht'
-    inferred_pop_ht_path = res_pref / 'inferred_pop.ht'
-    ancestry_sample_qc_ht_path = res_pref / 'ancestry_sample_qc.ht'
+        siteonly_vcf_path = res_pref / 'siteonly.vcf.bgz'
+        site_only_vcf.run(
+            vds_path=vds_path,
+            sample_qc_ht_path=sample_qc_ht_path,
+            relateds_to_drop_ht_path=relateds_to_drop_ht_path,
+            out_vcf_path=siteonly_vcf_path,
+            tmp_prefix=res_pref / 'tmp',
+        )
 
-    ancestry_pca.run(
-        dense_mt_path=dense_mt_path,
-        sample_qc_ht_path=sample_qc_ht_path,
-        relateds_to_drop_ht_path=relateds_to_drop_ht_path,
-        tmp_prefix=res_pref / 'tmp',
-        out_scores_ht_path=scores_ht_path,
-        out_eigenvalues_ht_path=eigenvalues_ht_path,
-        out_loadings_ht_path=loadings_ht_path,
-        out_inferred_pop_ht_path=inferred_pop_ht_path,
-        out_sample_qc_ht_path=ancestry_sample_qc_ht_path,
-    )
-    ancestry_plots.run(
-        out_path_pattern=res_pref / 'plots' / '{scope}_pc{pci}_{pca_suffix}.{ext}',
-        sample_qc_ht_path=ancestry_sample_qc_ht_path,
-        scores_ht_path=scores_ht_path,
-        eigenvalues_ht_path=eigenvalues_ht_path,
-        loadings_ht_path=loadings_ht_path,
-        inferred_pop_ht_path=inferred_pop_ht_path,
-    )
-
-    siteonly_vcf_path = res_pref / 'siteonly.vcf.bgz'
-    site_only_vcf.run(
-        vds_path=vds_path,
-        sample_qc_ht_path=sample_qc_ht_path,
-        relateds_to_drop_ht_path=relateds_to_drop_ht_path,
-        out_vcf_path=siteonly_vcf_path,
-        tmp_prefix=res_pref / 'tmp',
-    )
-
-    assert exists(vds_path)
-    assert exists(res_pref / 'plots' / 'dataset_pc1_hgdp_1kg_sites.html')
-    assert exists(siteonly_vcf_path)
+        assert exists(vds_path)
+        assert exists(res_pref / 'plots' / 'dataset_pc1_hgdp_1kg_sites.html')
+        assert exists(siteonly_vcf_path)
