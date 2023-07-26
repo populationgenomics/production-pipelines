7ff7ad87b86fde156069627adb745e15c72547eb
vivbak <vivian.bakiris@populationgenomics.org.au>
2023-07-26 15:17:11 +1000

    Add other test, fix path

--- test/jobs/test_genotype.py
+++ test/jobs/test_genotype.py
@@ -38,6 +38,33 @@ def default_config() -> PipelineConfig:
     )
 
 
+def custom_scatter_config() -> PipelineConfig:
+    return PipelineConfig(
+        workflow=WorkflowConfig(
+            dataset='genotype-test',
+            access_level='test',
+            sequencing_type='genome',
+            check_inputs=False,
+            reblock_gq_bands=[20, 30, 50],
+            scatter_count_genotype=10,
+        ),
+        images={
+            'picard': 'picard:2.27.4',
+            'gatk': 'gatk:4.2.6.1',
+        },
+        other={
+            'resource_overrides': {},
+            'references': {
+                'broad': {
+                    'ref_fasta': 'hg38_reference.fa',
+                    'genome_calling_interval_lists': 'wgs_calling_regions.hg38.interval_list',
+                    'noalt_bed': 'primary_contigs_plus_mito.bed.gz',
+                }
+            },
+        },
+    )
+
+
 class TestGenotyping:
     def test_genotype_jobs_with_default_scatter_count(self, tmp_path: Path):
         # ---- Test setup
@@ -50,7 +77,7 @@ class TestGenotyping:
             dataset=dataset_id,
             sequencing_type=config.workflow.sequencing_type,
             alignment_input=create_fastq_pairs_input(location=tmp_path, n=1),
-            cram=CramPath(tmp_path / 'test.cram'),
+            cram=CramPath(tmp_path / 'test_genotype.cram'),
         )
 
         assert sg.cram
@@ -82,6 +109,7 @@ class TestGenotyping:
             cmd = get_command_str(job)
             if job.name == make_intervals_job_name:
                 # TODO: Validate input and output
+                print(cmd)
                 assert re.search(r'picard', cmd)
                 assert re.search(r'IntervalListTools', cmd)
                 assert re.search(r'SCATTER_COUNT=50', cmd)
@@ -95,6 +123,7 @@ class TestGenotyping:
 
             elif job.name == postproc_job_name:
                 # TODO Validate input and output
+                print(cmd)
                 assert re.search(r'gatk', cmd)
                 assert re.search(r'ReblockGVCF', cmd)
             else:
@@ -107,3 +136,38 @@ class TestGenotyping:
                 assert match
                 haplotype_output_paths.append(match.group(1))
                 print(cmd)
+
+    def test_genotype_jobs_with_custom_scatter_count(self, tmp_path: Path):
+        config = custom_scatter_config()
+        set_config(config, tmp_path / 'config.toml')
+        dataset_id = config.workflow.dataset
+        batch = create_local_batch(tmp_path)
+        sg = create_sequencing_group(
+            dataset=dataset_id,
+            sequencing_type=config.workflow.sequencing_type,
+            alignment_input=create_fastq_pairs_input(location=tmp_path, n=1),
+            cram=CramPath(tmp_path / 'test_genotype2.cram'),
+        )
+
+        assert sg.cram
+        # ---- The job that we want to test
+        genotype_jobs = genotype(
+            b=batch,
+            sequencing_group_name=sg.id,
+            cram_path=sg.cram,
+            output_path=sg.gvcf,
+            tmp_prefix=tmp_path,
+        )
+
+        # ---- Assertions
+        expected_scatter_count = 10
+        merge_job_name = f'Merge {expected_scatter_count} GVCFs'
+        postproc_job_name = 'Postproc GVCF'
+        make_intervals_job_name = f'Make {expected_scatter_count} intervals for genome'
+        assert (
+            len(genotype_jobs) == expected_scatter_count + 3
+        )  # +3 for making intervals, merging and postprocessing
+        job_names = [job.name for job in genotype_jobs]
+        assert make_intervals_job_name in job_names
+        assert merge_job_name in job_names
+        assert postproc_job_name in job_names
