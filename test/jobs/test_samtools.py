df5acef476e5e40987e361b4818d3ec753eec482
Michael Harper <michael.harper@populationgenomics.org.au>
2023-08-07 13:45:04 +1000

    Update test_samtools.py

--- test/jobs/test_samtools.py
+++ test/jobs/test_samtools.py
@@ -29,19 +29,21 @@ def default_config() -> PipelineConfig:
         images={
             'samtools': 'test_image',
         },
-        other={
-            'references': {
-                'broad': {
-                    'ref_fasta': 'hg38_reference.fa',
-                    'dragmap_prefix': 'gs://a-cpg-bucket/dragen_reference/',
-                }
-            },
+        references={
+            'broad': {
+                'ref_fasta': 'hg38_reference.fa',
+                'dragmap_prefix': 'gs://a-cpg-bucket/dragen_reference/',
+            }
         },
     )
 
 
-def setup_test(tmp_path: Path):
+def setup_test(tmp_path: Path, ref_fasta: str | None = None):
     config = default_config()
+
+    if ref_fasta != None:
+        config.workflow.ref_fasta = ref_fasta
+
     set_config(config, tmp_path / 'config.toml')
 
     cram_pth = create_cram_input(
@@ -54,44 +56,27 @@ def setup_test(tmp_path: Path):
 
 
 class TestSamtoolsRun:
-    def test_samtools_stats_creates_a_job(self, tmp_path: Path):
-        # ---- Test setup
-        config, cram_pth, batch = setup_test(tmp_path)
-
-        # ---- The job we want to test
-        j = samtools_stats(
-            b=batch,
-            cram_path=cram_pth,
-            out_samtools_stats_path=tmp_path,
-            job_attrs=None,
-            overwrite=True,
-        )
-
-        # ---- Assertions
-        assert (
-            j is not None
-        ), 'The samtools_stats function did not create a job. Check if overwrite=False in samtools_stats call'
-
     def test_creates_one_job(self, tmp_path: Path):
         # ---- Test setup
-        config, cram_pth, batch = setup_test(tmp_path)
+        _, cram_pth, batch = setup_test(tmp_path)
 
         # ---- The job we want to test
         j = samtools_stats(
             b=batch,
             cram_path=cram_pth,
-            out_samtools_stats_path=tmp_path,
+            out_samtools_stats_path=(tmp_path / 'output_file'),
             job_attrs=None,
-            overwrite=True,
+            overwrite=False,
         )
         # ---- Assertions
         assert (
             len(batch.select_jobs('samtools stats')) == 1
         ), "Unexpected number of 'samtools stats' jobs in batch list, should be just 1 job"
 
-    def test_will_return_None_if_path_already_exists(self, tmp_path: Path):
+    def test_will_return_none_if_path_already_exists(self, tmp_path: Path):
+        # Giving output file that DOES exist
         # ---- Test setup
-        config, cram_pth, batch = setup_test(tmp_path)
+        _, cram_pth, batch = setup_test(tmp_path)
 
         output = tmp_path / 'output_stats_file'
         output.touch()
@@ -126,43 +111,24 @@ class TestSamtoolsRun:
         # --- Assertions
         assert j is not None
 
-    def test_pass_file_that_doesnt_exist(self, tmp_path: Path):
-        # can_reuse() executes all(exists()) which checks whether all files in the path exist
-        # if any of the files in the paths do not exist can_reuse() returns False and a job is created
-
-        # ---- Test setup
-        config, cram_pth, batch = setup_test(tmp_path)
-
-        # ---- The job we want to test
-        j = samtools_stats(
-            b=batch,
-            cram_path=cram_pth,
-            out_samtools_stats_path=(tmp_path / 'output_stats_file'),  # doesn't exist
-            job_attrs=None,
-            overwrite=False,
-        )
-
-        # ---- Assertions
-        assert j is not None
-
     def test_sets_job_attrs_or_sets_default_attrs_if_not_supplied(self, tmp_path: Path):
         # ---- Test setup
-        config, cram_pth, batch = setup_test(tmp_path)
+        _, cram_pth, batch = setup_test(tmp_path)
 
         # ---- The jobs we want to test
         j_default_attrs = samtools_stats(
             b=batch,
             cram_path=cram_pth,
-            out_samtools_stats_path=tmp_path,
+            out_samtools_stats_path=(tmp_path / 'output_file'),
             job_attrs=None,
-            overwrite=True,
+            overwrite=False,
         )
         j_supplied_attrs = samtools_stats(
             b=batch,
             cram_path=cram_pth,
-            out_samtools_stats_path=tmp_path,
+            out_samtools_stats_path=(tmp_path / 'second_output_file'),
             job_attrs={'test_tool': 'test_samtools'},
-            overwrite=True,
+            overwrite=False,
         )
 
         assert j_default_attrs.attributes == {'tool': 'samtools'}
@@ -179,19 +145,18 @@ class TestSamtoolsRun:
         j = samtools_stats(
             b=batch,
             cram_path=cram_pth,
-            out_samtools_stats_path=tmp_path,
+            out_samtools_stats_path=(tmp_path / 'output_file'),
             job_attrs=None,
             overwrite=True,
         )
         assert j._image == config.images['samtools']
 
-    # TODO: reference is '__RESOURCE_FILE__0' how do i check?
     def test_uses_reference_in_workflow_config_section_if_set(self, tmp_path: Path):
         # Not sure how to check what reference file is being used. \
         # The resource group inside the job 'j' is not obvious in its naming
 
         # ---- Test setup
-        config, cram_pth, batch = setup_test(tmp_path)
+        config, cram_pth, batch = setup_test(tmp_path, ref_fasta='test_workflow_ref.fa')
 
         # ---- The jobs we want to test
         j = samtools_stats(
@@ -201,13 +166,28 @@ class TestSamtoolsRun:
             job_attrs=None,
             overwrite=True,
         )
-        pass
+        cmd = get_command_str(j)
+        ref_file = config.workflow.ref_fasta
+        assert re.search(fr"--reference \${{BATCH_TMPDIR}}/inputs/\w+/{ref_file}", cmd)
 
-    # TODO: same as above
-    def test_uses_broad_reference_as_default_if_reference_not_set_in_workflow_config_section():
-        pass
+    def test_uses_broad_reference_as_default_if_reference_not_set_in_workflow_config_section(
+        self, tmp_path: Path
+    ):
+        # ---- Test setup
+        config, cram_pth, batch = setup_test(tmp_path)
 
-    # TODO: what is meant by this test?
+        # ---- The jobs we want to test
+        j = samtools_stats(
+            b=batch,
+            cram_path=cram_pth,
+            out_samtools_stats_path=(tmp_path / 'output_file'),
+            job_attrs=None,
+        )
+
+        cmd = get_command_str(j)
+        ref_file = config.references['broad']['ref_fasta']
+        assert re.search(fr"--reference \${{BATCH_TMPDIR}}/inputs/\w+/{ref_file}", cmd)
+
     def test_uses_fail_safe_copy_on_cram_path_and_index_in_bash_command(
         self, tmp_path: Path
     ):
@@ -218,70 +198,44 @@ class TestSamtoolsRun:
         j = samtools_stats(
             b=batch,
             cram_path=cram_pth,
-            out_samtools_stats_path=tmp_path,
+            out_samtools_stats_path=(tmp_path / 'output_file'),
             job_attrs=None,
-            overwrite=True,
         )
-        print()
 
-    # Can't test this properly as no output is being generated
+        cmd = get_command_str(j)
+        assert re.search(fr"retry_gs_cp .*{cram_pth.path}", cmd)
+        assert re.search(fr"retry_gs_cp .*{cram_pth.index_path}", cmd)
+
     def test_samtools_writes_to_resource_file_named_output_stats(self, tmp_path: Path):
         # ---- Test setup
-        config, cram_pth, batch = setup_test(tmp_path)
+        _, cram_pth, batch = setup_test(tmp_path)
 
         # ---- The jobs we want to test
         j = samtools_stats(
             b=batch,
             cram_path=cram_pth,
-            out_samtools_stats_path=tmp_path,
+            out_samtools_stats_path=(tmp_path / 'output_file'),
             job_attrs=None,
-            overwrite=True,
         )
         # ---- Assertions
         cmd = get_command_str(j)
-        match = re.search(r'\$CRAM > (\S+)', cmd)
-        # Retrieve the j.output_stats from the regex match
-        actual_output_stats = match.group(1)
-        assert (
-            actual_output_stats == j.output_stats
-        ), f'Expected: {j.output_stats}, Got: {actual_output_stats}'
+        assert re.search(r'\$CRAM > \${BATCH_TMPDIR}/.*/output_stats', cmd)
 
     # TODO: What's the difference between this test and testing whether output exists?
-    def test_batch_writes_samtools_stats_file_to_output_path():
-        pass
-
-    # TODO: Check if implemented spy object properly
-    def test_check_batch_command_executed(self, mocker: MockFixture, tmp_path: Path):
+    def test_batch_writes_samtools_stats_file_to_output_path(
+        self, mocker: MockFixture, tmp_path: Path
+    ):
         # ---- Test setup
-        config, cram_pth, batch = setup_test(tmp_path)
+        _, cram_pth, batch = setup_test(tmp_path)
 
         # ---- The jobs we want to test
+        spy = mocker.spy(batch, 'write_output')
+        out_path = tmp_path / 'output_file'
         j = samtools_stats(
             b=batch,
             cram_path=cram_pth,
-            out_samtools_stats_path=tmp_path,
+            out_samtools_stats_path=out_path,
             job_attrs=None,
-            overwrite=True,
         )
-        # Set up spy object to track running of batch jobs
-        spy_batch_run = mocker.spy(Batch, 'run')
 
-        # ---- Assertions
-        spy_batch_run.assert_called_once
-
-    def test_check_output_files_exist(self, tmp_path: Path):
-        # ---- Test setup
-        config, cram_pth, batch = setup_test(tmp_path)
-
-        # ---- The jobs we want to test
-        j = samtools_stats(
-            b=batch,
-            cram_path=cram_pth,
-            out_samtools_stats_path=tmp_path,
-            job_attrs=None,
-            overwrite=True,
-        )
-        # ---- Assertions
-        assert (
-            tmp_path / f'{j.output_stats}'
-        ).exists(), f'No output file named {j.output_stats}'
+        spy.assert_called_with(j.output_stats, str(out_path))
