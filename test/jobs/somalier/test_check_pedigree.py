71c6408227158091c05fb5b86a7fd24d439ca76f
Michael Harper <michael.harper@populationgenomics.org.au>
2023-08-29 11:54:17 +1000

    Removed comments

--- test/jobs/somalier/test_check_pedigree.py
+++ test/jobs/somalier/test_check_pedigree.py
@@ -55,11 +55,9 @@ def setup_pedigree_test(tmp_path: Path, config: PipelineConfig | None = None):
 
 
 class TestSomalierCheckPedigree:
-    # Test _check_pedigree
     def test_creates_one_relate_job(self, tmp_path: Path):
-        # ---- Test setup
         _, batch, somalier_path_by_sgid, dataset = setup_pedigree_test(tmp_path)
-        # ---- The job that we want to test
+
         _ = pedigree(
             dataset=dataset,
             b=batch,
@@ -71,17 +69,14 @@ class TestSomalierCheckPedigree:
             label=None,
         )
 
-        # ---- Assertions
         relate_jobs = batch.select_jobs('Pedigree check')
         assert len(relate_jobs) == 1
 
     def test_if_label_provided_adds_label_to_default_job_title_(self, tmp_path: Path):
-        # ---- Test setup
         _, batch, somalier_path_by_sgid, dataset = setup_pedigree_test(tmp_path)
 
         label = 'test-label'
 
-        # ---- The job that we want to test
         pedigree_jobs = pedigree(
             dataset=dataset,
             b=batch,
@@ -94,7 +89,6 @@ class TestSomalierCheckPedigree:
         )
         pedigree_check_j = pedigree_jobs[1]
 
-        # ---- Assertions
         job_name = f'Pedigree check [{label}]'
         assert pedigree_check_j.name == job_name
 
@@ -111,10 +105,8 @@ class TestSomalierCheckPedigree:
     def test_if_job_attrs_supplied_job_attrs_set_or_sets_default_attrs_if_not_supplied(
         self, tmp_path: Path, job_attrs, expected_attrs
     ):
-        # ---- Test setup
         _, batch, somalier_path_by_sgid, dataset = setup_pedigree_test(tmp_path)
 
-        # ---- The job that we want to test
         pedigree_jobs = pedigree(
             dataset=dataset,
             b=batch,
@@ -127,15 +119,12 @@ class TestSomalierCheckPedigree:
         )
         pedigree_check_j = pedigree_jobs[1]
 
-        # ---- Assertions
         assert pedigree_check_j is not None
         assert pedigree_check_j.attributes == expected_attrs
 
     def test_uses_image_specified_in_config(self, tmp_path: Path):
-        # ---- Test setup
         config, batch, somalier_path_by_sgid, dataset = setup_pedigree_test(tmp_path)
 
-        # ---- The job that we want to test
         pedigree_jobs = pedigree(
             dataset=dataset,
             b=batch,
@@ -147,15 +136,12 @@ class TestSomalierCheckPedigree:
         )
         check_pedigree_j = pedigree_jobs[1]
 
-        # ---- Assertions
         assert check_pedigree_j is not None
         assert check_pedigree_j._image == config.images['cpg_workflows']
 
     def test_script_path_correctness(self, tmp_path: Path):
-        # ---- Test setup
         _, batch, somalier_path_by_sgid, dataset = setup_pedigree_test(tmp_path)
 
-        # ---- The job that we want to test
         pedigree_jobs = pedigree(
             dataset=dataset,
             b=batch,
@@ -166,7 +152,6 @@ class TestSomalierCheckPedigree:
             out_html_path=(tmp_path / 'out_html'),
         )
         check_pedigree_j = pedigree_jobs[1]
-        # ---- Assertions
         cmd = get_command_str(check_pedigree_j)
         script_name = 'check_pedigree.py'
         assert re.search(fr'python3 {script_name}', cmd)
@@ -174,10 +159,8 @@ class TestSomalierCheckPedigree:
     def test_sed_commands_for_samples_pairs_and_expected_ped_files(
         self, tmp_path: Path
     ):
-        # ---- Test setup
         _, batch, somalier_path_by_sgid, dataset = setup_pedigree_test(tmp_path)
 
-        # ---- The job that we want to test
         pedigree_jobs = pedigree(
             dataset=dataset,
             b=batch,
@@ -189,7 +172,6 @@ class TestSomalierCheckPedigree:
         )
         check_pedigree_j = pedigree_jobs[1]
 
-        # ---- Assertions
         cmd = get_command_str(check_pedigree_j)
         sequencing_group_id = dataset.get_sequencing_groups()[0].id
         rich_id = dataset.rich_id_map()[sequencing_group_id]
@@ -209,14 +191,12 @@ class TestSomalierCheckPedigree:
         )
 
     def test_sed_commands_not_present_if_no_rich_id_map_provided(self, tmp_path: Path):
-        # ---- Test setup
         config, batch, somalier_path_by_sgid, _ = setup_pedigree_test(tmp_path)
 
         dataset_id = config.workflow.dataset
         dataset = create_dataset(name=dataset_id)
         dataset.add_sequencing_group(id='CPG000001')
-        # ---- The job that we want to test
-        # TODO: check if this can be put in setup_pedigree_test()
+
         pedigree_jobs = pedigree(
             dataset=dataset,
             b=batch,
@@ -228,7 +208,6 @@ class TestSomalierCheckPedigree:
         )
         check_pedigree_j = pedigree_jobs[1]
 
-        # ---- Assertions
         cmd = get_command_str(check_pedigree_j)
 
         assert 'sed -iBAK' not in cmd
@@ -237,10 +216,8 @@ class TestSomalierCheckPedigree:
     def test_check_pedigree_uses_output_files_from_relate(
         self, mocker: MockFixture, tmp_path: Path
     ):
-        # ---- Test setup
         _, batch, somalier_path_by_sgid, dataset = setup_pedigree_test(tmp_path)
 
-        # ---- The job that we want to test
         pedigree_jobs = pedigree(
             dataset=dataset,
             b=batch,
@@ -263,15 +240,12 @@ class TestSomalierCheckPedigree:
             matched_sample = out_sample_path_match.group(0)
             matched_pairs = out_pairs_path_match.group(0)
 
-        # ---- Assertions
         assert f'--somalier-samples {matched_sample}' in check_pedigree_j_cmd
         assert f'--somalier-pairs {matched_pairs}' in check_pedigree_j_cmd
 
     def test_flags_called_correctly(self, tmp_path: Path):
-        # ---- Test setup
         _, batch, somalier_path_by_sgid, dataset = setup_pedigree_test(tmp_path)
 
-        # ---- The job that we want to test
         pedigree_jobs = pedigree(
             dataset=dataset,
             b=batch,
@@ -283,7 +257,6 @@ class TestSomalierCheckPedigree:
         )
         check_pedigree_j = pedigree_jobs[1]
 
-        # ---- Assertions
         cmd = get_command_str(check_pedigree_j)
         expected_ped = 'test_ped.ped'
         somalier_html_url = 'test_html_url'
@@ -305,10 +278,8 @@ class TestSomalierCheckPedigree:
         assert re.search('--send-to-slack', cmd)
 
     def test_if_send_to_slack_false_flag(self, tmp_path: Path):
-        # ---- Test setup
         _, batch, somalier_path_by_sgid, dataset = setup_pedigree_test(tmp_path)
 
-        # ---- The job that we want to test
         pedigree_jobs = pedigree(
             dataset=dataset,
             b=batch,
@@ -321,16 +292,13 @@ class TestSomalierCheckPedigree:
         )
         check_pedigree_j = pedigree_jobs[1]
 
-        # ---- Assertions
         cmd = get_command_str(check_pedigree_j)
         assert re.search('--no-send-to-slack', cmd)
 
     @pytest.mark.parametrize('out_html_url', ['test_html_url', None])
     def test_if_out_html_url_flag_is_set(self, tmp_path: Path, out_html_url: str):
-        # ---- Test setup
         _, batch, somalier_path_by_sgid, dataset = setup_pedigree_test(tmp_path)
 
-        # ---- The job that we want to test
         pedigree_jobs = pedigree(
             dataset=dataset,
             b=batch,
@@ -344,7 +312,6 @@ class TestSomalierCheckPedigree:
         )
         check_pedigree_j = pedigree_jobs[1]
 
-        # ---- Assertions
         cmd = get_command_str(check_pedigree_j)
         pattern = r'python3 check_pedigree\.py.*?--html-url ([^\n]+)'
         regex = re.compile(pattern, re.DOTALL)
@@ -355,10 +322,8 @@ class TestSomalierCheckPedigree:
             assert re.search(fr'--html-url {out_html_url}', cmd)
 
     def test_job_output_created(self, tmp_path: Path):
-        # ---- Test setup
         _, batch, somalier_path_by_sgid, dataset = setup_pedigree_test(tmp_path)
 
-        # ---- The job that we want to test
         pedigree_jobs = pedigree(
             dataset=dataset,
             b=batch,
@@ -370,7 +335,6 @@ class TestSomalierCheckPedigree:
         )
         check_pedigree_j = pedigree_jobs[1]
 
-        # ---- Assertions
         cmd = get_command_str(check_pedigree_j)
         job_output = 'output'
         assert re.search(
@@ -381,10 +345,8 @@ class TestSomalierCheckPedigree:
     def test_if_output_path_provided_writes_outputs_to_final_destination(
         self, mocker: MockFixture, tmp_path: Path
     ):
-        # ---- Test setup
         _, batch, somalier_path_by_sgid, dataset = setup_pedigree_test(tmp_path)
 
-        # ---- The job that we want to test
         spy = mocker.spy(batch, 'write_output')
 
         pedigree_jobs = pedigree(
@@ -399,7 +361,6 @@ class TestSomalierCheckPedigree:
         )
         check_pedigree_j = pedigree_jobs[1]
 
-        # ---- Assertions
         out_checks_path = tmp_path / 'out_path'
         assert check_pedigree_j is not None
         spy.assert_has_calls(
@@ -409,10 +370,8 @@ class TestSomalierCheckPedigree:
     def test_if_output_path_not_provided_does_not_write_outputs_to_final_destination(
         self, mocker: MockFixture, tmp_path: Path
     ):
-        # ---- Test setup
         _, batch, somalier_path_by_sgid, dataset = setup_pedigree_test(tmp_path)
 
-        # ---- The job that we want to test
         spy = mocker.spy(batch, 'write_output')
 
         pedigree_jobs = pedigree(
@@ -427,7 +386,6 @@ class TestSomalierCheckPedigree:
         )
         check_pedigree_j = pedigree_jobs[1]
 
-        # ---- Assertions
         out_checks_path = tmp_path / 'out_path'
         assert check_pedigree_j is not None
         with pytest.raises(AssertionError):
