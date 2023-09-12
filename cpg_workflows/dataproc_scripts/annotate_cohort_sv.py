754c03ace63eaaeb5f16eb7a11568eb14cda0533
MattWellie <mattwellie@gmail.com>
2023-09-12 12:38:42 +1000

    clean up click commands

--- cpg_workflows/dataproc_scripts/annotate_cohort_sv.py
+++ cpg_workflows/dataproc_scripts/annotate_cohort_sv.py
@@ -12,16 +12,14 @@ from cpg_workflows.query_modules.seqr_loader_sv import annotate_cohort_sv
 
 
 @click.command()
-@click.option('--vcf-path', 'vcf_path', required=True)
-@click.option('--out-mt-path', 'out_mt_path', required=True)
-@click.option('--checkpoint-prefix', 'checkpoint_prefix')
+@click.option('--vcf-path', required=True)
+@click.option('--out-mt-path', required=True)
+@click.option('--checkpoint-prefix')
 def main(vcf_path: str, out_mt_path: str, checkpoint_prefix: str | None = None):
     hl.init(default_reference=genome_build())
 
     annotate_cohort_sv(
-        vcf_path=vcf_path,
-        out_mt_path=out_mt_path,
-        checkpoint_prefix=checkpoint_prefix,
+        vcf_path=vcf_path, out_mt_path=out_mt_path, checkpoint_prefix=checkpoint_prefix
     )
 
 
