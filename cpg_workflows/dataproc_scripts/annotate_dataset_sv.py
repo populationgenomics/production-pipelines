bda2b2f583f0bfee7f39f7d64496f1b0079d17f1
MattWellie <mattwellie@gmail.com>
2023-09-12 11:51:41 +1000

    gpg signature failed

--- cpg_workflows/dataproc_scripts/annotate_dataset_sv.py
+++ cpg_workflows/dataproc_scripts/annotate_dataset_sv.py
@@ -34,13 +34,11 @@ def main(mt_path: str, sgids: str, out_mt_path: str, checkpoint_prefix: str):
     with to_path(sgids).open() as f:
         sgid_list = f.read().strip().split(',')
 
-    subset_mt_path = to_path(checkpoint_prefix) / 'cohort-subset.mt'
+    subset_mt = str(to_path(checkpoint_prefix) / 'cohort-subset.mt')
 
-    subset_mt_to_sgids(
-        mt_path=mt_path, sgid_list=sgid_list, out_mt_path=str(subset_mt_path)
-    )
+    subset_mt_to_sgids(mt_path=mt_path, sgid_list=sgid_list, out_mt_path=subset_mt)
 
-    annotate_dataset_sv(mt_path=str(subset_mt_path), out_mt_path=out_mt_path)
+    annotate_dataset_sv(mt_path=subset_mt, out_mt_path=out_mt_path)
 
 
 if __name__ == '__main__':
