45e6a1b9d8d6e71def2d09313bd33ec2fc921649
Matt Welland <mattwellie@gmail.com>
2023-09-20 12:28:58 +1000

    Implement SV -> ES Index (#462)
    * Reintroduce Filtering stages (#466)
    
    * silence library loggers
    
    * local backend tries to resolve gs:// config path(s)

--- cpg_workflows/stages/gatk_sv/gatk_sv_common.py
+++ cpg_workflows/stages/gatk_sv/gatk_sv_common.py
@@ -175,7 +175,7 @@ def add_gatk_sv_jobs(
         outputs_to_collect=outputs_to_collect,
         driver_image=driver_image,
         copy_outputs_to_gcp=copy_outputs,
-        labels=labels
+        labels=labels,
     )
 
     copy_j = batch.new_job(f'{job_prefix}: copy outputs')
