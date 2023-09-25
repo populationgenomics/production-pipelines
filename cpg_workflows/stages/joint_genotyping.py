35349448907b388ee859492fa041da3ea6c721c4
Cas Simons <cas.simons@populationgenomics.org.au>
2023-09-26 12:14:43 +1300

    add combiner jc option

--- cpg_workflows/stages/joint_genotyping.py
+++ cpg_workflows/stages/joint_genotyping.py
@@ -24,17 +24,20 @@ from ..resources import joint_calling_scatter_count
 class JointGenotyping(CohortStage):
     """
     Joint-calling of GVCFs together.
+
+    Optionally uses traditional joint calling, or the faster vcf combiner approach.
     """
 
     def expected_outputs(self, cohort: Cohort) -> dict:
         """
-        Generate a pVCF and a site-only VCF.
+        Generate a site-only VCF and a hail mt.
         """
         return {
             # writing into perm location for late debugging
             # convert to str to avoid checking existence
             'tmp_prefix': str(self.prefix / 'tmp'),
-            'vcf': to_path(self.prefix / 'full.vcf.gz'),
+            # 'vcf': to_path(self.prefix / 'full.vcf.gz'),
+            'mt': to_path(self.prefix / 'full.mt'),
             'siteonly': to_path(self.prefix / 'siteonly.vcf.gz'),
             'siteonly_part_pattern': str(
                 self.prefix / 'siteonly_parts' / 'part{idx}.vcf.gz'
@@ -67,28 +70,55 @@ class JointGenotyping(CohortStage):
         vcf_path = self.expected_outputs(cohort)['vcf']
         siteonly_vcf_path = self.expected_outputs(cohort)['siteonly']
         scatter_count = joint_calling_scatter_count(len(cohort.get_sequencing_groups()))
-        out_siteonly_vcf_part_paths = [
-            to_path(
-                self.expected_outputs(cohort)['siteonly_part_pattern'].format(idx=idx)
-            )
-            for idx in range(scatter_count)
-        ]
 
-        jc_jobs = joint_genotyping.make_joint_genotyping_jobs(
-            b=get_batch(),
-            out_vcf_path=vcf_path,
-            out_siteonly_vcf_path=siteonly_vcf_path,
-            tmp_bucket=to_path(self.expected_outputs(cohort)['tmp_prefix']),
-            gvcf_by_sgid=gvcf_by_sgid,
-            tool=joint_genotyping.JointGenotyperTool.GnarlyGenotyper
-            if get_config()['workflow'].get('use_gnarly', False)
-            else joint_genotyping.JointGenotyperTool.GenotypeGVCFs,
-            intervals_path=get_config()['workflow'].get('intervals_path'),
-            job_attrs=self.get_job_attrs(),
-            scatter_count=scatter_count,
-            out_siteonly_vcf_part_paths=out_siteonly_vcf_part_paths,
-        )
-        jobs.extend(jc_jobs)
+        if get_config()['workflow'].get('use_vcf_combiner', False):
+            from cpg_workflows.large_cohort.dataproc_utils import dataproc_job
+            from cpg_workflows.large_cohort.combiner import run
+
+            combine_job = dataproc_job(
+                    job_name=self.__class__.__name__,
+                    function=run,
+                    function_path_args=dict(
+                        out_vds_path=self.expected_outputs(cohort),
+                        tmp_prefix=self.tmp_prefix,
+                    ),
+                    autoscaling_policy=(
+                        get_config()['hail']
+                        .get('dataproc', {})
+                        .get('combiner_autoscaling_policy')
+                    ),
+                    depends_on=inputs.get_jobs(cohort),
+                )
+            jobs.append(combine_job)
+
+        else:
+            # Run joint genotyping using GenotypeGVCFs or GnarlyGenotyper.
+            out_siteonly_vcf_part_paths = [
+                to_path(
+                    self.expected_outputs(cohort)['siteonly_part_pattern'].format(
+                        idx=idx
+                    )
+                )
+                for idx in range(scatter_count)
+            ]
+
+            jc_jobs = joint_genotyping.make_joint_genotyping_jobs(
+                b=get_batch(),
+                out_vcf_path=vcf_path,
+                out_siteonly_vcf_path=siteonly_vcf_path,
+                tmp_bucket=to_path(self.expected_outputs(cohort)['tmp_prefix']),
+                gvcf_by_sgid=gvcf_by_sgid,
+                tool=joint_genotyping.JointGenotyperTool.GnarlyGenotyper
+                if get_config()['workflow'].get('use_gnarly', False)
+                else joint_genotyping.JointGenotyperTool.GenotypeGVCFs,
+                intervals_path=get_config()['workflow'].get('intervals_path'),
+                job_attrs=self.get_job_attrs(),
+                scatter_count=scatter_count,
+                out_siteonly_vcf_part_paths=out_siteonly_vcf_part_paths,
+            )
+            jobs.extend(jc_jobs)
+
         for job in jobs:
             assert job
+
         return self.make_outputs(cohort, data=self.expected_outputs(cohort), jobs=jobs)
