68c788405dddc2dbc1760a4612307f171a353d17
MattWellie <mattwellie@gmail.com>
2023-10-06 12:36:10 +1100

    extend script to MT generation

--- scripts/vep_backend/vep_batch_backend.py
+++ scripts/vep_backend/vep_batch_backend.py
@@ -7,9 +7,10 @@ Run VEP in parallel using batch backend
 import click
 
 from cpg_utils import to_path
-from cpg_utils.hail_batch import output_path
+from cpg_utils.hail_batch import output_path, image_path, query_command
 from cpg_utils.config import get_config
 from cpg_workflows.batch import get_batch
+from cpg_workflows.query_modules import seqr_loader
 from cpg_workflows.jobs.vep import add_vep_jobs
 
 
@@ -20,7 +21,8 @@ from cpg_workflows.jobs.vep import add_vep_jobs
     required=True,
     help='Path to where finished VEP-annotated VCF will be output',
 )
-def main(vcf_path: str, output_ht: str):
+@click.option('--to-mt', is_flag=True, help='Complete transition to MT')
+def main(vcf_path: str, output_ht: str, to_mt: bool = False):
     """
     Run VEP in parallel using Picard tools intervals as partitions.
     Input: the full path to a VCF file, along with a tabix (.tbi) file,
@@ -29,13 +31,31 @@ def main(vcf_path: str, output_ht: str):
     vep_image = get_config()['images']['vep']
     scatter_count = get_config()['vep']['scatter_count']
     b = get_batch(f'Run VEP with Batch Backend, image {vep_image}, scatter count {scatter_count}')
-    add_vep_jobs(
+    vep_ht = output_path(output_ht)
+    vep_jobs = add_vep_jobs(
         b=b,
         input_siteonly_vcf_path=to_path(vcf_path),
         tmp_prefix=to_path(output_path('vcf_fragments/', 'tmp')),
-        out_path=to_path(output_path(output_ht)),
+        out_path=to_path(vep_ht),
         scatter_count=scatter_count,
     )
+    if to_mt:
+        assert vep_ht.endswith('.ht')
+        vep_mt = vep_ht[:-len('.ht')] +'.mt'
+        j = b.new_job(f'annotate cohort', {'tool': 'hail query'})
+        j.image(image_path('cpg_workflows'))
+        j.command(
+            query_command(
+                seqr_loader,
+                seqr_loader.annotate_cohort.__name__,
+                str(vcf_path),
+                vep_mt,
+                vep_ht,
+                setup_gcp=True,
+            )
+        )
+        if vep_jobs:
+            j.depends_on(*vep_jobs)
     b.run(wait=False)
 
 
