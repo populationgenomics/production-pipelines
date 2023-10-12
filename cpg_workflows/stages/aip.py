6e4783a4b6d10a8a209bb0f063c081516ccb6e02
MattWellie <mattwellie@gmail.com>
2023-10-13 09:05:21 +1100

    changes

--- cpg_workflows/stages/aip.py
+++ cpg_workflows/stages/aip.py
@@ -24,7 +24,11 @@ from os.path import join
 from datetime import datetime
 from cpg_utils import Path
 from cpg_utils.config import get_config
-from cpg_utils.hail_batch import authenticate_cloud_credentials_in_job, copy_common_env, image_path
+from cpg_utils.hail_batch import (
+    authenticate_cloud_credentials_in_job,
+    copy_common_env,
+    image_path,
+)
 from metamist.graphql import gql, query
 
 from cpg_workflows.batch import get_batch
@@ -35,17 +39,14 @@ from cpg_workflows.workflow import (
     StageOutput,
     StageInput,
 )
-from cpg_workflows.resources import (
-    HIGHMEM,
-    STANDARD
-)
+from cpg_workflows.resources import HIGHMEM, STANDARD
 from cpg_workflows.jobs.aip.hpo_panel_match import main as panel_match_main
 
 
-DATED_FOLDER = join('reanalysis', get_config()['workflow'].get(
-    'fake_date',
-    datetime.now().strftime('%Y-%m-%d')
-))
+DATED_FOLDER = join(
+    'reanalysis',
+    get_config()['workflow'].get('fake_date', datetime.now().strftime('%Y-%m-%d')),
+)
 
 
 @stage
@@ -64,13 +65,12 @@ class GeneratePanelData(DatasetStage):
         py_job = get_batch().new_python_job('create_panel_data')
         py_job.image(get_config()['workflow']['driver_image'])
 
+        # only needs a really tiny image
+        STANDARD.set_resources(py_job, ncpu=1, mem_gb=1.0)
         expected_d = self.expected_outputs(dataset)
         hpo_file = get_batch().read_input(get_config()['workflow']['obo_file'])
         py_job.call(
-            panel_match_main,
-            dataset.name,
-            hpo_file,
-            str(expected_d['hpo_panels'])
+            panel_match_main, dataset.name, hpo_file, str(expected_d['hpo_panels'])
         )
 
         return self.make_outputs(dataset, data=expected_d, jobs=py_job)
@@ -94,12 +94,14 @@ class QueryPanelapp(DatasetStage):
         authenticate_cloud_credentials_in_job(job)
         copy_common_env(job)
 
-        hpo_panel_json = inputs.as_str(target=dataset, stage=GeneratePanelData, key='hpo_panels')
+        hpo_panel_json = inputs.as_str(
+            target=dataset, stage=GeneratePanelData, key='hpo_panels'
+        )
         expected_out = self.expected_outputs(dataset)
         job.command(
             f'python3 reanalysis/query_panelapp.py '
             f'--panels {hpo_panel_json} '
-            f'--out_json {expected_out["panel_data"]}'
+            f'--out_json {str(expected_out["panel_data"])}'
         )
 
         return self.make_outputs(dataset, data=expected_out, jobs=job)
@@ -108,10 +110,8 @@ class QueryPanelapp(DatasetStage):
 def query_for_latest_mt(dataset: str) -> str:
     """
     query for the latest MT for a dataset
-
     Args:
         dataset (str):
-
     Returns:
         str, the path to the latest MT
     """
@@ -124,16 +124,15 @@ def query_for_latest_mt(dataset: str) -> str:
                 }
             }
         }
-    """)
+    """
+    )
     result = query(my_query, variables={'dataset': dataset})
     seq_type_exome = get_config()['workflow'].get('sequencing_type') == 'exome'
     mt_path = ''
     for analysis in result['project']['analyses']:
-        if (analysis['output'].endswith('.mt') and
-                (
-                        (seq_type_exome and '/exome/' in analysis['output']) or
-                        (not seq_type_exome and '/exome/' not in analysis['output'])
-                )
+        if analysis['output'].endswith('.mt') and (
+            (seq_type_exome and '/exome/' in analysis['output'])
+            or (not seq_type_exome and '/exome/' not in analysis['output'])
         ):
             mt_path = analysis['output']
 
@@ -149,32 +148,83 @@ class RunHailFiltering(DatasetStage):
     """
 
     def expected_outputs(self, dataset: Dataset) -> dict[str, Path]:
-        return {'labelled_vcf': dataset.prefix() / DATED_FOLDER / 'hail_labelled.vcf.bgz',
-                'pedigree': dataset.prefix() / DATED_FOLDER / 'pedigree.ped'}
+        return {
+            'labelled_vcf': dataset.prefix() / DATED_FOLDER / 'hail_labelled.vcf.bgz',
+            'pedigree': dataset.prefix() / DATED_FOLDER / 'pedigree.ped',
+        }
 
     def queue_jobs(self, dataset: Dataset, inputs: StageInput) -> StageOutput:
 
         # either do as you're told, or find the latest
         # this could potentially follow on from the AnnotateDataset stage
-        input_mt = get_config()['workflow'].get('matrix_table', query_for_latest_mt(dataset.name))
+        input_mt = get_config()['workflow'].get(
+            'matrix_table', query_for_latest_mt(dataset.name)
+        )
         job = get_batch().new_job('run hail labelling')
         job.image(image_path('aip'))
-        job.memory('32Gi')
+        HIGHMEM.set_resources(job, mem_gb=16.0)
 
         # auth and copy env
         authenticate_cloud_credentials_in_job(job)
         copy_common_env(job)
 
-        panelapp_json = inputs.as_str(target=dataset, stage=QueryPanelapp, key='panel_data')
+        panelapp_json = inputs.as_str(
+            target=dataset, stage=QueryPanelapp, key='panel_data'
+        )
         expected_out = self.expected_outputs(dataset)
         pedigree = dataset.write_ped_file(out_path=expected_out['pedigree'])
-        local_ped = get_batch().read_input(pedigree)  # peddy can't read cloud paths
+        local_ped = get_batch().read_input(
+            str(pedigree)
+        )  # peddy can't read cloud paths
         job.command(
             f'python3 reanalysis/hail_filter_and_label.py '
             f'--mt {input_mt} '
             f'--panelapp {panelapp_json} '
             f'--pedigree {local_ped} '
-            f'--vcf_out {expected_out["labelled_vcf"]}'
+            f'--vcf_out {str(expected_out["labelled_vcf"])}'
         )
 
+        return self.make_outputs(dataset, data=expected_out, jobs=job)
+
+
+def _aip_summary_meta(
+    output_path: str,  # pylint: disable=W0613:unused-argument
+) -> dict[str, str]:
+    """
+    Add meta.type to custom analysis object
+    """
+    return {'type': 'aip_output_json'}
+
+@stage(
+    required_stages=RunHailFiltering,
+    analysis_type='custom',
+    analysis_keys=['summary_json'],
+    update_analysis_meta=_aip_summary_meta
+)
+class ValidateMOI(DatasetStage):
+    """
+    labelled_vcf: str,
+    out_json: str,
+    panelapp: str,
+    pedigree: str,
+    input_path: str,
+    participant_panels: str | None = None,
+    """
+
+    def expected_outputs(self, dataset: Dataset) -> dict[str, Path]:
+        return {'summary_json': dataset.prefix() / DATED_FOLDER / 'summary_output.json'}
+    def queue_jobs(self, dataset: Dataset, inputs: StageInput) -> StageOutput:
+        job = get_batch().new_job(f'AIP summary for {dataset.name}')
+
+
+        expected_out = self.expected_outputs(dataset)
+        return self.make_outputs(dataset, data=expected_out, jobs=job)
+
+@stage(required_stages=[ValidateMOI])
+class CreateAIPHTML(DatasetStage):
+
+    def expected_outputs(self, dataset: Dataset) -> dict[str, Path]:
+        return {'panel_data': dataset.prefix() / DATED_FOLDER / 'panelapp_data.json'}
+    def queue_jobs(self, dataset: Dataset):
+        expected_out = self.expected_outputs(dataset)
         return self.make_outputs(dataset, data=expected_out, jobs=job)
