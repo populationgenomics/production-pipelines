854eb5b7f36cf2c71e69845146f5c122bd4d8f6c
cassimons <cassimons@gmail.com>
2023-11-01 09:56:55 +1300

    Set thread count to core count (#499)
    * Set thread count to core count
    
    * Improve comment.
    
    ---------
    
    Co-authored-by: Cas Simons <cas.simons@populationgenomics.org.au>

--- cpg_workflows/jobs/align.py
+++ cpg_workflows/jobs/align.py
@@ -475,7 +475,7 @@ def _align_one(
         {prepare_fastq_cmd}
         dragen-os -r {dragmap_index} {input_params} \\
             --RGID {sequencing_group_name} --RGSM {sequencing_group_name} \\
-            --num-threads {nthreads - 3}
+            --num-threads {nthreads - 1}
         """
 
     else:
