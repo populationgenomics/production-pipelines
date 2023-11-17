8a19dde032d2cee06a051fd8449849d67a2966fc
John Marshall <john.marshall@populationgenomics.org.au>
2023-11-17 16:48:18 +1300

    Part 2: Add Hail Python job that does the reheadering

--- scripts/fix_sq_headers.py
+++ scripts/fix_sq_headers.py
@@ -2,7 +2,11 @@
 """
 Usage: fix_sq_headers list|update
 
-Uses the usual config entries (workflow.dataset/.access_level)
+Uses the usual config entries (workflow.dataset/.access_level) and:
+
+  images.samtools      Path of an image containing samtools
+  references.expected  Name (from KNOWN_REFS) of the (bad) refset expected
+  references.new       Dict file containing the replacement @SQ headers
 """
 
 import hashlib
@@ -10,7 +14,10 @@ import os
 import subprocess as sb
 import sys
 
-from cpg_utils.hail_batch import get_config
+from hailtop.batch.resource import JobResourceFile
+
+from cpg_utils import Path, to_path
+from cpg_utils.hail_batch import get_batch, get_config, image_path
 from metamist.graphql import query, gql
 
 FIND_CRAMS = gql("""
@@ -35,6 +42,14 @@ KNOWN_REFS = {
     '49f7a7583b832aed0104b1901f9cf5fd': 'Homo_sapiens_assembly38_masked',
     '760a471d32c9a356ffb37f632bebfea3': 'Homo_sapiens_assembly38[unmasked]',
     }
+
+
+def path_exists(path: Path):
+    try:
+        return path.exists()
+    except Exception:
+        # GSPath.exists() raises google.api_core.exceptions.NotFound if the bucket is missing
+        return False
 
 
 def set_htslib_token():
@@ -69,6 +84,35 @@ def classify_SQ(path: str):
     return KNOWN_REFS.get(hashed, hashed)
 
 
+def do_reheader(newref, incram, outcram, outcrai):
+    translate = {}
+    with open(newref) as refp:
+        for line in refp:
+            if line.startswith('@SQ'):
+                field = parse_one_SQ(line)
+                translate[field['SN']] = (line.strip(), field['LN'])
+
+    head_cmd = ['samtools', 'head', incram]
+    headers = sb.run(head_cmd, check=True, capture_output=True, text=True).stdout
+
+    newhdr_file = 'tmp.hdr'
+    with open(newhdr_file, 'w') as newhdr:
+        for line in headers.splitlines():
+            if line.startswith('@SQ'):
+                field = parse_one_SQ(line)
+                new_line, new_len = translate[field['SN']]
+                if field['LN'] != new_len:
+                    raise ValueError(f'{incram}:{field["SN"]}: new length {new_len} differs '
+                                     f'from existing {field["LN"]}')
+                print(new_line, file=newhdr)
+            else:
+                print(line, file=newhdr)
+
+    sb.run(['samtools', 'reheader', '--no-PG', '--in-place', newhdr_file, incram], check=True)
+    os.rename(incram, outcram)
+    sb.run(['samtools', 'index', '-o', outcrai, outcram], check=True)
+
+
 if __name__ == '__main__':
     config = get_config(True)
 
@@ -98,6 +142,52 @@ if __name__ == '__main__':
         for path in cram_list:
             ref = classify_SQ(path) or '(file missing)'
             print(f'{path}: {ref}')
+
+    elif command == 'update':
+        expected_refset = config['references']['expected']
+        new_reference_path = config['references']['new']
+
+        set_htslib_token()
+        reheader_list = []
+        for fname in cram_list:
+            path = to_path(fname)
+            reheadered_path = path.parent / 'reheadered' / path.name
+
+            if not path_exists(path):
+                print(f'{fname}: file does not exist')
+            elif path_exists(reheadered_path):
+                print(f'{reheadered_path}: reheadered file already exists')
+            elif (ref := classify_SQ(fname)) != expected_refset:
+                print(f'{fname}: unexpected original reference headers: {ref}')
+            else:
+                reheader_list.append((path, reheadered_path, path.stat().st_size))
+
+        if len(reheader_list) == 0:
+            sys.exit('No CRAMs to be reheadered!')
+
+        print(f'CRAM files to be reheadered: {len(reheader_list)}')
+
+        b = get_batch(f'Reheader {dataset} to {new_reference_path}')
+        new_reference = b.read_input(new_reference_path)
+
+        for path, newpath, filesize in reheader_list:
+            j = b.new_python_job(f'Reheader {path}')
+            j.image(image_path('samtools'))
+            j.storage(filesize * 1.2)
+
+            j.call(do_reheader, new_reference, b.read_input(str(path)), j.out_cram, j.out_crai)
+
+            assert isinstance(j.out_cram, JobResourceFile)
+            assert isinstance(j.out_crai, JobResourceFile)
+            j.out_cram.add_extension('.cram')
+            j.out_crai.add_extension('.cram.crai')
+
+            b.write_output(j.out_cram, str(newpath))
+            b.write_output(j.out_crai, str(newpath.with_suffix('.cram.crai')))
+
+            print(f'Added reheader job for {path}')
+
+        b.run(wait=False)
 
     else:
         sys.exit(__doc__)
