06aea81fec7383c43bccf57b2fd8fa05618521d9
John Marshall <john.marshall@populationgenomics.org.au>
2023-12-11 14:03:20 +1300

    Ensure populated CramPaths have both path and index_path (#542)
    The resulting self.index_path will only be set if the
    CramPath() constructor also specifies an index_path.
    
    Use exists() to benefit from whole-directory existence caching.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 setup(
     name='cpg-workflows',
     # This tag is automatically updated by bumpversion
-    version='1.18.12',
+    version='1.18.13',
     description='CPG workflows for Hail Batch',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
