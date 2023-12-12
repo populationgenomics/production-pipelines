b99d01e4429352fb98f942a566402e40aa623a28
MattWellie <mattwellie@gmail.com>
2023-12-13 08:27:53 +1100

    require fixed cpg-utils

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     license='MIT',
     packages=find_packages(),
     install_requires=[
-        'cpg-utils>=4.18.2',
+        'cpg-utils>=4.18.3',
         'cyvcf2==0.30.18',
         'analysis-runner>=2.41.2',
         'hail!=0.2.120',  # Temporarily work around hail-is/hail#13337
