b44a744616dcfb7f945c94c5d1f3745a92958edd
MattWellie <mattwellie@gmail.com>
2023-12-18 10:57:23 +1100

    Shift common annotation method

--- cpg_workflows/stages/gatk_sv/gatk_sv_multisample_2.py
+++ cpg_workflows/stages/gatk_sv/gatk_sv_multisample_2.py
@@ -24,6 +24,7 @@ from cpg_workflows.stages.gatk_sv.gatk_sv_common import (
     get_images,
     get_references,
     make_combined_ped,
+    queue_annotate_sv_jobs
 )
 from cpg_workflows.stages.seqr_loader import es_password
 from cpg_workflows.workflow import (
@@ -547,46 +548,20 @@ class AnnotateVcf(CohortStage):
         configure and queue jobs for SV annotation
         passing the VCF Index has become implicit, which may be a problem for us
         """
-        input_dict: dict[str, Any] = {
-            'vcf': inputs.as_dict(cohort, FilterGenotypes)['filtered_vcf'],
-            'prefix': cohort.name,
-            'ped_file': make_combined_ped(cohort, self.prefix),
-            'sv_per_shard': 5000,
-            'population': get_config()['references']['gatk_sv'].get(
-                'external_af_population'
-            ),
-            'ref_prefix': get_config()['references']['gatk_sv'].get(
-                'external_af_ref_bed_prefix'
-            ),
-            'use_hail': False,
+        expected_out = self.expected_outputs(cohort)
+        billing_labels = {
+            'stage': self.name.lower(),
+            AR_GUID_NAME: try_get_ar_guid(),
         }
-
-        input_dict |= get_references(
-            [
-                'noncoding_bed',
-                'protein_coding_gtf',
-                {'ref_bed': 'external_af_ref_bed'},
-                {'contig_list': 'primary_contigs_list'},
-            ]
-        )
-
-        # images!
-        input_dict |= get_images(
-            ['sv_pipeline_docker', 'sv_base_mini_docker', 'gatk_docker']
-        )
-        expected_d = self.expected_outputs(cohort)
-
-        billing_labels = {'stage': self.name.lower(), AR_GUID_NAME: try_get_ar_guid()}
-
-        jobs = add_gatk_sv_jobs(
+        job_or_none = queue_annotate_sv_jobs(
             batch=get_batch(),
-            dataset=cohort.analysis_dataset,
-            wfl_name=self.name,
-            input_dict=input_dict,
-            expected_out_dict=expected_d,
+            cohort=cohort,
+            cohort_prefix=self.prefix,
+            input_vcf=inputs.as_dict(cohort, MakeCohortVcf)['vcf'],
+            outputs=expected_out,
             labels=billing_labels,
         )
-        return self.make_outputs(cohort, data=expected_d, jobs=jobs)
+        return self.make_outputs(cohort, data=expected_out, jobs=job_or_none)
 
 
 @stage(required_stages=AnnotateVcf)
--- cpg_workflows/stages/gcnv.py
+++ cpg_workflows/stages/gcnv.py
@@ -3,10 +3,13 @@ Stages that implement GATK-gCNV.
 """
 
 from cpg_utils import Path
-from cpg_utils.config import get_config
+from cpg_utils.config import get_config, try_get_ar_guid, AR_GUID_NAME
 from cpg_utils.hail_batch import get_batch
-from cpg_workflows.stages.gatk_sv.gatk_sv_common import get_images
 from cpg_workflows.jobs import gcnv
+from cpg_workflows.stages.gatk_sv.gatk_sv_common import (
+    get_images,
+    queue_annotate_sv_jobs
+)
 from cpg_workflows.targets import SequencingGroup, Cohort
 from cpg_workflows.workflow import (
     stage,
@@ -15,6 +18,15 @@ from cpg_workflows.workflow import (
     StageInput,
     StageOutput
 )
+
+
+def _gcnv_annotated_meta(
+    output_path: str,  # pylint: disable=W0613:unused-argument
+) -> dict[str, str]:
+    """
+    Callable, adds custom analysis object meta attribute
+    """
+    return {'type': 'gCNV-annotated'}
 
 
 @stage
@@ -219,3 +231,58 @@ class FastCombineGCNVs(CohortStage):
             output_path=outputs['combined_calls'],
         )
         return self.make_outputs(cohort, data=outputs, jobs=job_or_none)
+
+@stage(
+    required_stages=FastCombineGCNVs,
+    analysis_type='sv',
+    analysis_keys=['annotated_vcf'],
+    update_analysis_meta=_gcnv_annotated_meta,
+)
+class AnnotateCNV(CohortStage):
+    """
+    Smaller, direct annotation using SvAnnotate
+    Add annotations, such as the inferred function and allele frequencies of variants,
+    to final VCF.
+
+    This is a full clone of the GATK-SV pipeline Cromwell stage, but use on a slightly
+    different output. Trying to work out the best way to handle this through inheritance
+
+    Annotations methods include:
+    * Functional annotation - annotate SVs with inferred functional consequence on
+      protein-coding regions, regulatory regions such as UTR and promoters, and other
+      non-coding elements.
+    * Allele frequency annotation - annotate SVs with their allele frequencies across
+      all samples, and samples of specific sex, as well as specific subpopulations.
+    * Allele Frequency annotation with external callset - annotate SVs with the allele
+      frequencies of their overlapping SVs in another callset, e.g. gnomad SV callset.
+    """
+
+    def expected_outputs(self, cohort: Cohort) -> dict:
+        return {
+            'annotated_vcf': self.prefix / 'unfiltered_annotated.vcf.bgz',
+            'annotated_vcf_index': self.prefix / 'unfiltered_annotated.vcf.bgz.tbi',
+        }
+
+    def queue_jobs(self, cohort: Cohort, inputs: StageInput) -> StageOutput | None:
+        """
+        configure and queue jobs for SV annotation
+        passing the VCF Index has become implicit, which may be a problem for us
+        """
+        expected_out = self.expected_outputs(cohort)
+
+        billing_labels = {
+            'stage': self.name.lower(),
+            AR_GUID_NAME: try_get_ar_guid(),
+        }
+
+        job_or_none = queue_annotate_sv_jobs(
+            batch=get_batch(),
+            cohort=cohort,
+            cohort_prefix=self.prefix,
+            input_vcf=inputs.as_dict(cohort, FastCombineGCNVs)[
+                'combined_calls'
+            ],
+            outputs=expected_out,
+            labels=billing_labels,
+        )
+        return self.make_outputs(cohort, data=expected_out, jobs=job_or_none)
