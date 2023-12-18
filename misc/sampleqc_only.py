465e8c723a94b2c43db06af08129838d468491a4
Michael Harper <michael.harper@populationgenomics.org.au>
2023-12-18 17:38:42 +1100

    adding the script to just run SampleQC stage

---
+++ misc/sampleqc_only.py
@@ -0,0 +1,16 @@
+from cpg_utils.hail_batch import get_batch, output_path
+
+from cpg_workflows.large_cohort.dataproc_utils import dataproc_job
+from cpg_workflows.large_cohort.sample_qc import run
+
+dataproc_job(
+    job_name='SampleQC',
+    function=run,
+    function_path_args=dict(
+        vds_path='gs://cpg-bioheart-test/vds/3-1-3.vds',
+        out_sample_qc_ht_path=output_path('sample-qc-out'),
+        tmp_prefix=output_path('2023-12-18-Harper', 'tmp'),
+    ),
+)
+
+get_batch().run(wait=False)
