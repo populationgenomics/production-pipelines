8626c334f977ad000ae2f73061c427cdd0778b96
Michael Harper <michael.harper@populationgenomics.org.au>
2024-01-08 09:43:00 +1100

    adding python file that runs fastqe on batch

---
+++ scripts/metamist_docker_tutorial.py
@@ -0,0 +1,112 @@
+from argparse import ArgumentParser
+
+from cpg_utils.hail_batch import get_batch, image_path, output_path
+from metamist.graphql import gql, query
+
+# PROJECT = get_config()["workflow"]["dataset"]
+PROJECT = "bioheart-test"
+
+SG_ASSAY_QUERY = gql(
+    """
+    query ($project: String!, $sgids: [String!]) {
+        sequencingGroups(
+            project: {eq: $project},
+            id: {in_: $sgids}
+        ) {
+            id
+            assays {
+                id
+                meta
+            }
+        }
+    }
+    """
+)
+
+
+def get_assays(project: str, sgids: list) -> list[str]:
+    """
+    Queries the specified project for sequencing groups and assays, and returns a dictionary mapping sequencing group IDs to read locations.
+
+    Args:
+        project (str): The name of the project to query.
+
+    Returns:
+        dict: A dictionary where each key is a sequencing group ID and each value is a list of read locations for that sequencing group.
+    """
+    sg_assay_map = {}
+
+    # Use the query template above to query the sequencing groups and assays
+    query_response = query(SG_ASSAY_QUERY, {"project": project, "sgids": sgids})
+    for sg in query_response['sequencingGroups']:
+        sg_id = sg['id']
+        for assay in sg['assays']:
+            assay_meta = assay['meta']
+            reads = assay_meta.get('reads')
+            if reads is None:
+                continue
+            else:
+                read_locations = [read['location'] for read in reads]
+            sg_assay_map[sg_id] = read_locations
+
+    return sg_assay_map
+
+
+def main(project: str, sgids: list[str]):
+    # region: metadata queries
+    # This section is all executed prior to the workflow being scheduled,
+    # so we have access to all variables
+
+    # Metamist query for files
+    file_dict = get_assays(project)
+    # endregion
+
+    # region: Batch time
+    b = get_batch('Schedule some worker tasks')
+    for sg, files in file_dict.items():
+        # check that we have 2 files (assuming FQ, rather than BAM)
+        assert len(files) == 2
+
+        # Create a job for each sample
+        j = b.new_job(f'Job for {sg}')
+
+        # Set the docker image to use in this job
+        # this pulls the image path from the portion of the config
+        # populated by the images repository
+        j.image(image_path('fastqe'))
+
+        # read data into the batch tmp resource location
+        file_1 = b.read_input(files[0])
+        file_2 = b.read_input(files[1])
+
+        # Set the command to run
+        # batch.read_input will create a new path like /io/batch/75264c/CPGAAAA_1.fq.gz
+        # accessible from inside the job container, and unique to this batch/job
+        j.command(
+            f'echo "Hello world, I am a job for {sg}!, using {file_1} and {file_2}"'
+            f'I\'m also creating an output file at {j.output}'
+            f'echo "Some outputs" > {j.output}'
+        )
+
+        # read the output out into GCP
+        # The helper method output_path() will create a new path based on the current project,
+        # test/main, and the output prefix provided to analysis_runner
+        # -o my_output
+        # --dataset my-dataset
+        # --access_level test
+        # output_path('this_file.txt')
+        # -> gs://cpg-my-dataset-test/my_output/this_file.txt
+        b.write_output(j.output, output_path(f'/{sg}.txt'))
+    # endregion
+
+
+if __name__ == '__main__':
+    # optional direct input of samples
+    parser = ArgumentParser(description='Hail Batch FastQE')
+    parser.add_argument('-p', nargs='+', help='Project name', required=True)
+    parser.add_argument('-sg', nargs='+', help='Sequencing group IDs', required=True)
+    args, fails = parser.parse_known_args()
+
+    if fails:
+        raise ValueError(f'Failed to parse arguments: {fails}')
+    main(project=args.p, sgids=args.sg)
