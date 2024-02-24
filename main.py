f5ffde8e455883881699905819374f785f944c61
MattWellie <mattwellie@gmail.com>
2024-02-24 10:12:03 +1000

    add new stage to main

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ from cpg_workflows.stages.gatk_sv.gatk_sv_multisample_1 import (
 )
 from cpg_workflows.stages.gatk_sv.gatk_sv_multisample_2 import AnnotateVcf, AnnotateDatasetSv, MtToEsSv
 from cpg_workflows.stages.gatk_sv.gatk_sv_single_sample import CreateSampleBatches
-from cpg_workflows.stages.gcnv import GermlineCNVCalls, GCNVJointSegmentation
+from cpg_workflows.stages.gcnv import GermlineCNVCalls, GCNVJointSegmentation, RecalculateClusteredQuality
     # AnnotateCNV,
     # AnnotateCNVVcfWithStrvctvre,
     # AnnotateGCNVCohortForSeqr,
@@ -63,7 +63,7 @@ WORKFLOWS: dict[str, list[StageDecorator]] = {
     ],  # stage to run between FilterBatch & GenotypeBatch
     'gatk_sv_multisample_2': [AnnotateVcf, AnnotateDatasetSv, MtToEsSv],
     'rare_disease_rnaseq': [Outrider, Fraser],
-    'gcnv': [GermlineCNVCalls, GCNVJointSegmentation],
+    'gcnv': [GermlineCNVCalls, GCNVJointSegmentation, RecalculateClusteredQuality],
 }
 
 
