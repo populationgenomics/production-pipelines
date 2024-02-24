ea61976f1fc75a5b7d2f57898fea627fa6657664
MattWellie <mattwellie@gmail.com>
2024-02-24 15:00:07 +1000

    corrected stage dependencies

--- cpg_workflows/stages/gcnv.py
+++ cpg_workflows/stages/gcnv.py
@@ -242,7 +242,7 @@ class GCNVJointSegmentation(CohortStage):
         return self.make_outputs(cohort, data=expected_out, jobs=jobs)
 
 
-@stage(required_stages=[GCNVJointSegmentation, GermlineCNVCalls])
+@stage(required_stages=[GCNVJointSegmentation, GermlineCNVCalls, DeterminePloidy])
 class RecalculateClusteredQuality(SequencingGroupStage):
     """
     following joint segmentation, we need to post-process the clustered breakpoints
