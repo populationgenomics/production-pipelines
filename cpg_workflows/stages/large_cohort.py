ed5d6cf7495480be946c54c5e25d61ea036e1954
Michael Harper <michael.harper@populationgenomics.org.au>
2024-03-13 09:30:44 +1100

    Changing gnomad_methods submodule to use different normal_ploidy_cutoff and aneuploidy_cutoff, as well as changing to test-tmp directory for this output

--- cpg_workflows/stages/large_cohort.py
+++ cpg_workflows/stages/large_cohort.py
@@ -51,7 +51,7 @@ class Combiner(CohortStage):
 @stage(required_stages=[Combiner])
 class SampleQC(CohortStage):
     def expected_outputs(self, cohort: Cohort) -> Path:
-        return get_workflow().prefix / 'sample_qc.ht'
+        return self.tmp_prefix / 'Xvar_Yref_norm_aneu_changed_sample_qc.ht'
 
     def queue_jobs(self, cohort: Cohort, inputs: StageInput) -> StageOutput | None:
         from cpg_workflows.large_cohort import sample_qc
