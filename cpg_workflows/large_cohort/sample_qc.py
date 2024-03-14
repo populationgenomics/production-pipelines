848ed336629ed2f4845d97e6c263a6922f8b7f52
Michael Harper <michael.harper@populationgenomics.org.au>
2024-03-14 11:01:31 +1100

    Back outside of dataproc to test building a new image for debugging process while outside of dataproc

--- cpg_workflows/large_cohort/sample_qc.py
+++ cpg_workflows/large_cohort/sample_qc.py
@@ -15,14 +15,14 @@ from cpg_workflows.inputs import get_cohort
 from cpg_workflows.utils import can_reuse
 
 
-def run(vds_path: Path, out_sample_qc_ht_path: Path, tmp_prefix: Path):
+def run(vds_path: str, out_sample_qc_ht_path: str, tmp_prefix: str):
 
     if can_reuse(out_sample_qc_ht_path, overwrite=True):
         return []
 
     ht = initialise_sample_table()
 
-    vds = hl.vds.read_vds(str(vds_path))
+    vds = hl.vds.read_vds(vds_path)
 
     # Remove centromeres and telomeres:
     tel_cent_ht = hl.read_table(str(reference_path('gnomad/tel_and_cent_ht')))
@@ -30,7 +30,7 @@ def run(vds_path: Path, out_sample_qc_ht_path: Path, tmp_prefix: Path):
         vds = hl.vds.filter_intervals(vds, tel_cent_ht, keep=False)
 
     # Run Hail sample-QC stats:
-    sqc_ht_path = tmp_prefix / 'sample_qc.ht'
+    sqc_ht_path = to_path(tmp_prefix) / 'sample_qc.ht'
     if can_reuse(sqc_ht_path, overwrite=True):
         sqc_ht = hl.read_table(str(sqc_ht_path))
     else:
@@ -45,12 +45,12 @@ def run(vds_path: Path, out_sample_qc_ht_path: Path, tmp_prefix: Path):
     ht.describe()
 
     logging.info('Run sex imputation')
-    sex_ht = impute_sex(vds, ht, tmp_prefix)
+    sex_ht = impute_sex(vds, ht, to_path(tmp_prefix))
     ht = ht.annotate(**sex_ht[ht.s])
 
     logging.info('Adding soft filters')
     ht = add_soft_filters(ht)
-    ht.checkpoint(str(out_sample_qc_ht_path), overwrite=True)
+    ht.checkpoint(out_sample_qc_ht_path, overwrite=True)
 
 
 def initialise_sample_table() -> hl.Table:
