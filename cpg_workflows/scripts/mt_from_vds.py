b25ca73f844669e6adb03b038cfc94865c92c4ce
MattWellie <mattwellie@gmail.com>
2024-03-19 22:01:00 +1000

    better script

--- cpg_workflows/scripts/mt_from_vds.py
+++ cpg_workflows/scripts/mt_from_vds.py
@@ -11,17 +11,17 @@ import hail as hl
 from cpg_utils.hail_batch import init_batch
 from cpg_workflows.utils import get_logger
 
+if __name__ == '__main__':
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument('--vds', help='Input VDS')
+    parser.add_argument('--mt', help='Output MT')
+    args = parser.parse_args()
 
-parser = ArgumentParser(description=__doc__)
-parser.add_argument('--vds', help='Input VDS')
-parser.add_argument('--mt', help='Output MT')
-args = parser.parse_args()
+    get_logger(__file__).info(f'Creating VDS {args.out} from {len(args.gvcfs)} gVCFs')
 
-get_logger(__file__).info(f'Creating VDS {args.out} from {len(args.gvcfs)} gVCFs')
+    init_batch()
 
-init_batch()
-
-vds = hl.vds.read_vds(args.vds)
-vds = hl.vds.split_multi(vds)
-mt = hl.vds.to_dense_mt(vds)
-mt.write(args.mt)
+    vds = hl.vds.read_vds(args.vds)
+    vds = hl.vds.split_multi(vds)
+    mt = hl.vds.to_dense_mt(vds)
+    mt.write(args.mt)
