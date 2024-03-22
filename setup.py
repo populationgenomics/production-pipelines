1b51dcbcbccd7ebd45178c8ac70ca03d83218624
Michael Franklin <illusional@users.noreply.github.com>
2024-03-22 16:49:11 +1100

    Use modified cpg-utils version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     license='MIT',
     packages=find_packages(),
     install_requires=[
-        'cpg-utils>=4.18.3',
+        'git+https://github.com/populationgenomics/cpg-utils.git@refactor-repo',
         'cyvcf2==0.30.18',
         'analysis-runner>=2.43.3',
         'hail!=0.2.120',  # Temporarily work around hail-is/hail#13337
