871f045fadfed811a5889f687e8913bfa1f4d867
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-03-27 19:32:59 +1100

    Update str_pca_dataproc_runner.py

--- str_pca_dataproc_runner.py
+++ str_pca_dataproc_runner.py
@@ -6,14 +6,11 @@ Script to submit a dataproc job
 
 """
 from cpg_utils.hail_batch import get_batch
-from cpg_workflows.large_cohort.dataproc_utils import dataproc_job
-from str_pca_dataproc_hail_script import pca_runner
 
 def main():
-    script = (
-        f'dataproc_helper/str_pca_dataproc_hail_script.py '
-        f'--file-path=gs://cpg-bioheart-test/str/associatr/mt_filtered/v1/str.mt'
-    )
+
+    from cpg_workflows.large_cohort.dataproc_utils import dataproc_job
+    from str_pca_dataproc_hail_script import pca_runner
     dataproc_job(
                 function = pca_runner,
                 function_path_args=dict(file_path='gs://cpg-bioheart-test/str/associatr/mt_filtered/v1/str.mt'),
