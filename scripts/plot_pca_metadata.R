e1869649252387d604557ef524a1ec2563760674
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2024-04-02 13:29:13 +1100

    Assign dataframe to variable in correct place

--- scripts/plot_pca_metadata.R
+++ scripts/plot_pca_metadata.R
@@ -42,8 +42,8 @@ plot.pca <- function(scores_df, metadata_df, variable_name){
         pca_axis1=paste0("PC",i)
         pca_axis2=paste0("PC",i+1)
         df <- data.frame(PC1 = scores_df[,pca_axis1], PC2 = scores_df[,pca_axis2], covariate = metadata_df[,variable_name])
-        df %>% 
-        p <- ggplot(aes(x=PC1, y=PC2)) + geom_point(aes(fill=covariate), alpha=0.6, shape=21, size=3) + 
+        p <- df %>% 
+        ggplot(aes(x=PC1, y=PC2)) + geom_point(aes(fill=covariate), alpha=0.6, shape=21, size=3) + 
         theme_bw() + ggtitle(variable_name) + theme(legend.title=element_blank()) +
         xlab(pca_axis1) + ylab(pca_axis2)
         # Save plot
