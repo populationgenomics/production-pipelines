bfe776f9b494f83958a1a66b214cdea23341b1de
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-04-08 15:17:07 +1000

    Update str_pca_dataproc_hail_script.py

--- cpg_workflows/large_cohort/str_pca_dataproc_hail_script.py
+++ cpg_workflows/large_cohort/str_pca_dataproc_hail_script.py
@@ -11,16 +11,16 @@ def pca_runner(file_path):
     mt = hl.read_matrix_table(str(file_path))
 
     # drop rows with missing sum_length
-    #missing_condition = hl.is_missing(mt.sum_length)
-    #mt =mt.annotate_rows(missing_count=hl.agg.count_where(missing_condition))
-    #mt = mt.filter_rows(mt.missing_count == 0)
+    missing_condition = hl.is_missing(mt.sum_length)
+    mt =mt.annotate_rows(missing_count=hl.agg.count_where(missing_condition))
+    mt = mt.filter_rows(mt.missing_count == 0)
 
     #  replace missing sum_length with the mean for that locus (PCA doesn't accept missing values) and normalise
     mt = mt.annotate_entries(
         sum_length_normalised=hl.or_else((mt.sum_length - mt.mean_sum_length) / mt.stdev_sum_length, 0.0))
 
     # run PCA
-    eigenvalues, scores, loadings = hl.pca(mt.sum_length_normalised, k=10, compute_loadings=True)
+    eigenvalues, scores, loadings = hl.pca(mt.sum_length_normalised, k=2, compute_loadings=True)
 
     scores_output_path = 'gs://cpg-bioheart-test/str/qc/iterative_pca/option_7/scores.tsv.bgz'
     scores.export(str(scores_output_path))
