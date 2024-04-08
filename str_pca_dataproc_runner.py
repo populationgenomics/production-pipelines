8f45c69e8d2aae3986474f44cb48da4c8672b85d
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-04-08 17:57:23 +1000

    redo opt6

--- str_pca_dataproc_runner.py
+++ str_pca_dataproc_runner.py
@@ -13,9 +13,9 @@ def main():
     from cpg_workflows.large_cohort.dataproc_utils import dataproc_job
     dataproc_job(
                 function = pca_runner,
-                function_path_args=dict(file_path ='gs://cpg-bioheart-test/str/qc/iterative_pca_input/str_pc_option7_annotated.mt'),
+                function_path_args=dict(file_path ='gs://cpg-bioheart-test/str/associatr/mt_filtered/v1/str.mt'),
                 job_name='STR-PCA',
-                num_workers= 20)
+                num_workers= 5)
 
     get_batch(name='STR PCA Data proc job').run(wait=False)
 
