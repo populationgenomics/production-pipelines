9600374feaa264747c1b278e55d0e49420f4beef
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-04-09 13:44:28 +1000

    opt11

--- cpg_workflows/large_cohort/str_pca_dataproc_hail_script.py
+++ cpg_workflows/large_cohort/str_pca_dataproc_hail_script.py
@@ -44,12 +44,12 @@ def pca_runner(file_path):
     mt = mt.filter_cols(hl.literal(remove_samples).contains(mt.s), keep=False)
 
     # remove putative variants driving the batch effect
-    #table_variants = hl.import_table('gs://cpg-bioheart-test/str/filtered_variants_opt9.csv')
-    #table_variants = table_variants.annotate(locus = hl.parse_locus(table_variants['locus']))
-    #table_variants = table_variants.key_by('locus')
+    table_variants = hl.import_table('gs://cpg-bioheart-test/str/filtered_variants_opt11.csv')
+    table_variants = table_variants.annotate(locus = hl.parse_locus(table_variants['locus']))
+    table_variants = table_variants.key_by('locus')
 
-    #mt = mt.annotate_rows(not_batch_effect = hl.is_defined(table_variants[mt.locus]))
-    #mt = mt.filter_rows(mt.not_batch_effect == True)
+    mt = mt.annotate_rows(not_batch_effect = hl.is_defined(table_variants[mt.locus]))
+    mt = mt.filter_rows(mt.not_batch_effect == True)
 
 
     # drop chrX
@@ -92,15 +92,15 @@ def pca_runner(file_path):
     # run PCA
     eigenvalues, scores, loadings = hl.pca(mt.sum_length_normalised, k=10, compute_loadings=True)
 
-    scores_output_path = 'gs://cpg-bioheart-test/str/qc/iterative_pca/option_10/scores.tsv.bgz'
+    scores_output_path = 'gs://cpg-bioheart-test/str/qc/iterative_pca/option_11/scores.tsv.bgz'
     scores.export(str(scores_output_path))
 
-    loadings_output_path = 'gs://cpg-bioheart-test/str/qc/iterative_pca/option_10/loadings.tsv.bgz'
+    loadings_output_path = 'gs://cpg-bioheart-test/str/qc/iterative_pca/option_11/loadings.tsv.bgz'
     loadings.export(str(loadings_output_path))
 
     # Convert the list to a regular Python list
     eigenvalues_list = hl.eval(eigenvalues)
     # write the eigenvalues to a file
-    with to_path('gs://cpg-bioheart-test/str/qc/iterative_pca/option_10/eigenvalues.txt').open('w') as f:
+    with to_path('gs://cpg-bioheart-test/str/qc/iterative_pca/option_11/eigenvalues.txt').open('w') as f:
         for item in eigenvalues_list:
             f.write(f'{item}\n')
