6e554677f3f41ca8e98870178cbcaadc49b6618a
John Marshall <john.marshall@populationgenomics.org.au>
2024-04-10 18:05:56 +1200

    Optionally allow column inconsistencies when unioning background datasets (#685)
    For data generated at different times, the tables being unioned
    in add_background() may have slightly differing sets of columns.
    For example, we've encountered a case where one table has an
    `autosomal_mean_dp` column but other(s) do not. Use unify=True
    to allow the code to continue despite these minor extra columns,
    but require this to be turned on in the config only when necessary.
    
    Bump version: 1.22.7 → 1.22.8

--- cpg_workflows/large_cohort/ancestry_pca.py
+++ cpg_workflows/large_cohort/ancestry_pca.py
@@ -23,6 +23,7 @@ def add_background(
     Add background dataset samples to the dense MT and sample QC HT.
     """
     sites_table = get_config()['references']['ancestry']['sites_table']
+    allow_missing_columns = get_config()['large_cohort']['pca_background'].get('allow_missing_columns', False)
     qc_variants_ht = hl.read_table(sites_table)
     dense_mt = dense_mt.select_cols().select_rows().select_entries('GT', 'GQ', 'DP', 'AD')
     for dataset in get_config()['large_cohort']['pca_background']['datasets']:
@@ -44,7 +45,7 @@ def add_background(
             for path in dataset_dict['metadata_table']:
                 sample_qc_background = hl.read_table(path)
                 metadata_tables.append(sample_qc_background)
-            metadata_tables = hl.Table.union(*metadata_tables)
+            metadata_tables = hl.Table.union(*metadata_tables, unify=allow_missing_columns)
             background_mt = background_mt.annotate_cols(**metadata_tables[background_mt.col_key])
         else:
             raise ValueError('Background dataset path must be either .mt or .vds')
