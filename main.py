a59e54475f81b49e18997856d8c5be67c0b073dc
John Marshall <john.marshall@populationgenomics.org.au>
2024-04-12 10:46:08 +1200

    Delay cpg_workflows.stages.* imports until after main() starts
    Some of these stages quite reasonably define top-level constants based
    on config entries accessed via get_config(). And this means that config
    files set via main.py's --config option aren't accessible, because the
    config has already been read during the imports, before main() has run.
    
    Fix this by introducing get_workflows() which delays these stage imports
    until the function is called, and finalise the config path before calling
    get_workflows() to initialise WORKFLOWS.

--- main.py
+++ main.py
@@ -11,54 +11,57 @@ import coloredlogs
 from cpg_utils import to_path
 from cpg_utils.config import set_config_paths
 from cpg_workflows import defaults_config_path
-from cpg_workflows.stages.aip import CreateAIPHTML, GenerateSeqrFile, ValidateMOI
-from cpg_workflows.stages.cram_qc import CramMultiQC
-from cpg_workflows.stages.exomiser import ExomiserSeqrTSV, RunExomiser
-from cpg_workflows.stages.fastqc import FastQCMultiQC
-from cpg_workflows.stages.fraser import Fraser
-from cpg_workflows.stages.gatk_sv.gatk_sv_multisample_1 import (
-    FilterBatch,
-    GenotypeBatch,
-    MergeBatchSites,
-)
-from cpg_workflows.stages.gatk_sv.gatk_sv_multisample_2 import MtToEsSv
-from cpg_workflows.stages.gatk_sv.gatk_sv_single_sample import CreateSampleBatches
-from cpg_workflows.stages.gcnv import AnnotateCohortgCNV, AnnotateDatasetCNV, MtToEsCNV
-from cpg_workflows.stages.gvcf_qc import GvcfMultiQC
-from cpg_workflows.stages.happy_validation import (
-    ValidationHappyOnVcf,
-    ValidationMtToVcf,
-    ValidationParseHappy,
-)
-from cpg_workflows.stages.large_cohort import AncestryPlots, Frequencies, LoadVqsr
-from cpg_workflows.stages.mito import MitoReport
-from cpg_workflows.stages.outrider import Outrider
-from cpg_workflows.stages.seqr_loader import AnnotateDataset, DatasetVCF, MtToEs
-from cpg_workflows.stages.stripy import Stripy
 from cpg_workflows.workflow import StageDecorator, run_workflow
 
-WORKFLOWS: dict[str, list[StageDecorator]] = {
-    'aip': [ValidateMOI, CreateAIPHTML, GenerateSeqrFile],
-    'exomiser': [RunExomiser, ExomiserSeqrTSV],
-    'pre_alignment': [FastQCMultiQC],
-    'seqr_loader': [
-        DatasetVCF,
-        AnnotateDataset,
-        MtToEs,
-        GvcfMultiQC,
-        CramMultiQC,
-        Stripy,
-        MitoReport,
-    ],
-    'validation': [ValidationMtToVcf, ValidationHappyOnVcf, ValidationParseHappy],
-    'large_cohort': [LoadVqsr, Frequencies, AncestryPlots, GvcfMultiQC, CramMultiQC],
-    'gatk_sv_singlesample': [CreateSampleBatches],
-    'gatk_sv_multisample_1': [FilterBatch, GenotypeBatch],
-    'gatk_sv_sandwich': [MergeBatchSites],  # stage to run between FilterBatch & GenotypeBatch
-    'gatk_sv_multisample_2': [MtToEsSv],
-    'rare_disease_rnaseq': [Outrider, Fraser],
-    'gcnv': [AnnotateCohortgCNV, AnnotateDatasetCNV, MtToEsCNV],
-}
+
+def get_workflows() -> dict[str, list[StageDecorator]]:
+    from cpg_workflows.stages.aip import CreateAIPHTML, GenerateSeqrFile, ValidateMOI
+    from cpg_workflows.stages.cram_qc import CramMultiQC
+    from cpg_workflows.stages.exomiser import ExomiserSeqrTSV, RunExomiser
+    from cpg_workflows.stages.fastqc import FastQCMultiQC
+    from cpg_workflows.stages.fraser import Fraser
+    from cpg_workflows.stages.gatk_sv.gatk_sv_multisample_1 import (
+        FilterBatch,
+        GenotypeBatch,
+        MergeBatchSites,
+    )
+    from cpg_workflows.stages.gatk_sv.gatk_sv_multisample_2 import MtToEsSv
+    from cpg_workflows.stages.gatk_sv.gatk_sv_single_sample import CreateSampleBatches
+    from cpg_workflows.stages.gcnv import AnnotateCohortgCNV, AnnotateDatasetCNV, MtToEsCNV
+    from cpg_workflows.stages.gvcf_qc import GvcfMultiQC
+    from cpg_workflows.stages.happy_validation import (
+        ValidationHappyOnVcf,
+        ValidationMtToVcf,
+        ValidationParseHappy,
+    )
+    from cpg_workflows.stages.large_cohort import AncestryPlots, Frequencies, LoadVqsr
+    from cpg_workflows.stages.mito import MitoReport
+    from cpg_workflows.stages.outrider import Outrider
+    from cpg_workflows.stages.seqr_loader import AnnotateDataset, DatasetVCF, MtToEs
+    from cpg_workflows.stages.stripy import Stripy
+
+    return {
+        'aip': [ValidateMOI, CreateAIPHTML, GenerateSeqrFile],
+        'exomiser': [RunExomiser, ExomiserSeqrTSV],
+        'pre_alignment': [FastQCMultiQC],
+        'seqr_loader': [
+            DatasetVCF,
+            AnnotateDataset,
+            MtToEs,
+            GvcfMultiQC,
+            CramMultiQC,
+            Stripy,
+            MitoReport,
+        ],
+        'validation': [ValidationMtToVcf, ValidationHappyOnVcf, ValidationParseHappy],
+        'large_cohort': [LoadVqsr, Frequencies, AncestryPlots, GvcfMultiQC, CramMultiQC],
+        'gatk_sv_singlesample': [CreateSampleBatches],
+        'gatk_sv_multisample_1': [FilterBatch, GenotypeBatch],
+        'gatk_sv_sandwich': [MergeBatchSites],  # stage to run between FilterBatch & GenotypeBatch
+        'gatk_sv_multisample_2': [MtToEsSv],
+        'rare_disease_rnaseq': [Outrider, Fraser],
+        'gcnv': [AnnotateCohortgCNV, AnnotateDatasetCNV, MtToEsCNV],
+    }
 
 
 @click.command(no_args_is_help=True)
@@ -109,6 +112,20 @@ def main(
     fmt = '%(asctime)s %(levelname)s (%(name)s %(lineno)s): %(message)s'
     coloredlogs.install(level='DEBUG' if verbose else 'INFO', fmt=fmt)
 
+    wfl_conf_path = to_path(__file__).parent / f'configs/defaults/{workflow}.toml'
+    assert wfl_conf_path.exists(), wfl_conf_path
+
+    for path in config_paths:
+        assert to_path(path).exists(), path
+
+    config_paths = os.environ['CPG_CONFIG_PATH'].split(',') + list(config_paths)
+    # Assuming the defaults is already loaded in __init__.py:
+    assert to_path(config_paths[0]) == defaults_config_path
+    # Inserting after the "defaults" config, but before user configs:
+    set_config_paths(config_paths[:1] + [str(wfl_conf_path)] + config_paths[1:])
+
+    WORKFLOWS = get_workflows()
+
     if not workflow and not list_workflows:
         click.echo('You must specify WORKFLOW as a first positional command line argument.')
     if not workflow or list_workflows or workflow == 'list':
@@ -124,18 +141,6 @@ def main(
         )
         click.echo(f'{", ".join(s.__name__ for s in WORKFLOWS[workflow])}')
         return
-
-    wfl_conf_path = to_path(__file__).parent / f'configs/defaults/{workflow}.toml'
-    assert wfl_conf_path.exists(), wfl_conf_path
-
-    for path in config_paths:
-        assert to_path(path).exists(), path
-
-    config_paths = os.environ['CPG_CONFIG_PATH'].split(',') + list(config_paths)
-    # Assuming the defaults is already loaded in __init__.py:
-    assert to_path(config_paths[0]) == defaults_config_path
-    # Inserting after the "defaults" config, but before user configs:
-    set_config_paths(config_paths[:1] + [str(wfl_conf_path)] + config_paths[1:])
 
     run_workflow(stages=WORKFLOWS[workflow], dry_run=dry_run)
 
