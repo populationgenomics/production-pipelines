82dcbc1ffe30d524ce046bba4857158829b8b446
Michael Harper <michael.harper@populationgenomics.org.au>
2024-05-01 14:59:04 +1000

    Changing back to v3 parameters and filtering to only exome sites prior to ld-pruning

--- cpg_workflows/large_cohort/sites_table_job.py
+++ cpg_workflows/large_cohort/sites_table_job.py
@@ -9,6 +9,8 @@ from cpg_utils.config import output_path
 from cpg_utils.hail_batch import get_batch, init_batch, reference_path
 from gnomad.sample_qc.pipeline import get_qc_mt
 
+NUM_ROWS_BEFORE_LD_PRUNE = 200000
+
 
 @click.option(
     '--vds-path',
@@ -22,79 +24,65 @@ def main(vds_path):
 
     vds = hl.vds.read_vds(str(vds_path))
 
-    # Pre-filtering
-    print('Filtering centromeres and telomeres')
-    tel_cent_ht = hl.read_table(str(reference_path('gnomad/tel_and_cent_ht')))
-    if tel_cent_ht.count() > 0:
-        vds = hl.vds.filter_intervals(vds, tel_cent_ht, keep=False)
-    print('Done filtering centromeres and telomeres')
-
-    print('Filtering to autosomes')
-    vds = hl.vds.filter_chromosomes(vds, keep=[f'chr{chrom}' for chrom in range(1, 23)])
-    print('Done filtering to autosomes')
-
-    print('Filtering low complexity regions and segmental duplications')
-    for name in ['lcr_intervals_ht', 'seg_dup_intervals_ht']:
-        interval_table = hl.read_table(str(reference_path(f'gnomad/{name}')))
-        if interval_table.count() > 0:
-            # remove all rows where the locus falls within a defined interval
-            tmp_variant_data = vds.variant_data.filter_rows(
-                hl.is_defined(interval_table[vds.variant_data.locus]),
-                keep=False,
-            )
-            vds = hl.vds.VariantDataset(reference_data=vds.reference_data, variant_data=tmp_variant_data)
-    print('Done filtering low complexity regions and segmental duplications')
-
     print('Splitting multi-allelic sites')
-    # print('Variant count before split:', vds.variant_data.count())
     vds = hl.vds.split_multi(vds, filter_changed_loci=True)
-    # print('Variant count after split:', vds.variant_data.count())
     print('Done splitting multi-allelic sites')
 
     print('Densifying VDS')
-    # print('Variant count before densify:', vds.variant_data.count())
-    mt = hl.vds.to_dense_mt(vds)
-    # print('Variant count after densify:', mt.count())
-    # mt.show(n_cols=10)
+    hgdp_1kg = hl.vds.to_dense_mt(vds)
     print('Done densifying VDS')
 
     # Run variant QC
     print('Running variant QC')
-    # Choosing variants based off of gnomAD v4 parameters
-    # Inbreeding coefficient > -0.8
-    # Bi-allelic, autosomal variants only
-    # Allele frequency above 0.01%
-    # Call rate above 95%
-    # HWE p-value above 1e-8
-    mt = hl.variant_qc(mt)
+    # choose variants based off of gnomAD v3 parameters
+    # Inbreeding coefficient > -0.25 (no excess of heterozygotes)
+    # Must be single nucleotide variants that are autosomal (i.e., no sex), and bi-allelic
+    # Have an allele frequency above 1% (note deviation from gnomAD, which is 0.1%)
+    # Have a call rate above 99%
+    hgdp_1kg = hl.variant_qc(hgdp_1kg)
     print('Done running variant QC')
 
     print('Generating sites table')
     print('Filtering using gnomAD v4 parameters')
-    mt = mt.annotate_rows(
+    hgdp_1kg = hgdp_1kg.annotate_rows(
         IB=hl.agg.inbreeding(
-            mt.GT,
-            mt.variant_qc.AF[1],
+            hgdp_1kg.GT,
+            hgdp_1kg.variant_qc.AF[1],
         ),
     )
-    mt = mt.filter_rows(
-        (hl.len(mt.alleles) == 2)
-        & (mt.locus.in_autosome())
-        & (mt.variant_qc.AF[1] > 0.0001)
-        & (mt.variant_qc.call_rate > 0.95)
-        & (mt.IB.f_stat > -0.8)
-        & (mt.variant_qc.p_value_hwe > 1e-8),
+    hgdp_1kg = hgdp_1kg.filter_rows(
+        (hl.len(hgdp_1kg.alleles) == 2)
+        & (hgdp_1kg.locus.in_autosome())
+        & (hgdp_1kg.variant_qc.AF[1] > 0.01)
+        & (hgdp_1kg.variant_qc.call_rate > 0.99)
+        & (hgdp_1kg.IB.f_stat > -0.8),
     )
-    print('Done filtering using gnomAD v4 parameters')
+    print('Done filtering using gnomAD v3 parameters')
 
+    print('Filtering to exome intervals')
+    exome_intervals = hl.import_locus_intervals(str(reference_path('broad/exome_calling_interval_lists')))
+    hgdp_1kg_exome = hgdp_1kg.filter_rows(hl.is_defined(exome_intervals[hgdp_1kg.locus]))
+    print('Done filtering to exome intervals')
+
+    # downsize input variants for ld_prune
+    # otherwise, persisting the pruned_variant_table will cause
+    # script to fail. See https://github.com/populationgenomics/ancestry/pull/79
     print('Writing sites table pre-LD pruning')
-    checkpoint_path = output_path('sites_table.mt', 'tmp')
-    mt = mt.checkpoint(checkpoint_path, overwrite=True)
+    checkpoint_path = output_path('hgdp1_1kg_exome_pre_pruning.mt', 'tmp')
+    hgdp_1kg_exome = hgdp_1kg_exome.checkpoint(checkpoint_path, overwrite=True)
     print('Done writing sites table pre-LD pruning')
 
+    nrows = hgdp_1kg_exome.count_rows()
+    print(f'hgdp_1kg_exome.count_rows() = {nrows}')
+    hgdp_1kg_exome = hgdp_1kg_exome.sample_rows(
+        NUM_ROWS_BEFORE_LD_PRUNE / nrows,
+        seed=12345,
+    )
+
+    # as per gnomAD, LD-prune variants with a cutoff of r2 = 0.1
     print('Pruning sites table')
     pruned_variant_table = hl.ld_prune(
-        mt.GT,
+        hgdp_1kg_exome.GT,
         r2=0.1,
         bp_window_size=500000,
     )
@@ -104,7 +92,8 @@ def main(vds_path):
     # repartition table after pruning
     pruned_variant_table = pruned_variant_table.repartition(100, shuffle=False)
     print('Done repartitioning sites table')
-    pruned_variant_table.checkpoint(output_path('final_sites_table.mt', 'default'), overwrite=True)
+    pruned_variant_table_path = output_path('pruned_variants_exome.ht')
+    pruned_variant_table.write(pruned_variant_table_path)
     print('Done writing sites table')
 
 
