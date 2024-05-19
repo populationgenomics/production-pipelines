ef14e45c355d882ebb8eb6ceea1efdc725db94bd
MattWellie <mattwellie@gmail.com>
2024-05-19 12:30:03 +1000

    test concordance

---
+++ cpg_workflows/scripts/compare_sv_callsets.py
@@ -0,0 +1,45 @@
+"""
+test script, see what happens when we compare two of our own callsets
+"""
+
+from dataclasses import dataclass
+
+from cpg_utils import to_path
+from cpg_workflows.stages.gatk_sv.gatk_sv_common import (
+    add_gatk_sv_jobs,
+    get_fasta,
+    get_images,
+    get_references,
+)
+
+
+@dataclass
+class Dataset:
+    name: str
+
+
+truth_vcf = 'gs://cpg-seqr-test/gatk_sv/d84fe17c11fba10df5749878e62065f1624b35_3461/FilterGenotypes/filtered.vcf.gz'
+eval_vcf = 'gs://cpg-seqr-test/gatk_sv/b00828c6cf20df7160f20b23537148dbd8b270_2700/FilterGenotypes/filtered.vcf.gz'
+input_dict: dict = {
+    'output_prefix': 'concordance_test',
+    'reference_dict': str(get_fasta().with_suffix('.dict')),
+    'eval_vcf': eval_vcf,
+    'truth_vcf': truth_vcf,
+}
+input_dict |= get_images(['gatk_docker', 'sv_base_mini_docker'])
+input_dict |= get_references([{'contig_list': 'primary_contigs_list'}])
+
+test_prefix = to_path('gs://cpg-seqr-test/gatk_sv/test_concordance')
+expected_outs = {
+    'concordance_vcf': test_prefix / 'sv_concordance.vcf.gz',
+    'concordance_vcf_index': test_prefix / 'sv_concordance.vcf.gz.tbi',
+}
+
+# # can't use this in a rogue script... sigh
+jobs = add_gatk_sv_jobs(
+    dataset=Dataset('test_concordance'),
+    wfl_name='SVConcordance',
+    input_dict=input_dict,
+    expected_out_dict=expected_outs
+)
+
