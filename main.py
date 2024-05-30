05f8d8fda1c6ed64c095ae196310572152808f41
vivbak <vivian.bakiris@populationgenomics.org.au>
2024-05-30 16:09:34 +1000

    Restore order. Revert main.py back to original

--- main.py
+++ main.py
@@ -11,28 +11,25 @@ import coloredlogs
 from cpg_utils import to_path
 from cpg_utils.config import set_config_paths
 from cpg_workflows import defaults_config_path
-
-# from cpg_workflows.stages.aip import CreateAIPHTML, GenerateSeqrFile, ValidateMOI
+from cpg_workflows.stages.aip import CreateAIPHTML, GenerateSeqrFile, ValidateMOI
 from cpg_workflows.stages.cram_qc import CramMultiQC
-
-# from cpg_workflows.stages.exomiser import ExomiserSeqrTSV, RunExomiser
-# from cpg_workflows.stages.fastqc import FastQCMultiQC
-# from cpg_workflows.stages.fraser import Fraser
-# from cpg_workflows.stages.gatk_sv.gatk_sv_multisample_1 import (
-#     FilterBatch,
-#     GenotypeBatch,
-#     MergeBatchSites,
-# )
-# from cpg_workflows.stages.gatk_sv.gatk_sv_multisample_2 import MtToEsSv
-# from cpg_workflows.stages.gatk_sv.gatk_sv_single_sample import CreateSampleBatches
-# from cpg_workflows.stages.gcnv import AnnotateCohortgCNV, AnnotateDatasetCNV, MtToEsCNV
+from cpg_workflows.stages.exomiser import ExomiserSeqrTSV, RunExomiser
+from cpg_workflows.stages.fastqc import FastQCMultiQC
+from cpg_workflows.stages.fraser import Fraser
+from cpg_workflows.stages.gatk_sv.gatk_sv_multisample_1 import (
+    FilterBatch,
+    GenotypeBatch,
+    MergeBatchSites,
+)
+from cpg_workflows.stages.gatk_sv.gatk_sv_multisample_2 import MtToEsSv
+from cpg_workflows.stages.gatk_sv.gatk_sv_single_sample import CreateSampleBatches
+from cpg_workflows.stages.gcnv import AnnotateCohortgCNV, AnnotateDatasetCNV, MtToEsCNV
 from cpg_workflows.stages.gvcf_qc import GvcfMultiQC
-
-# from cpg_workflows.stages.happy_validation import (
-#     ValidationHappyOnVcf,
-#     ValidationMtToVcf,
-#     ValidationParseHappy,
-# )
+from cpg_workflows.stages.happy_validation import (
+    ValidationHappyOnVcf,
+    ValidationMtToVcf,
+    ValidationParseHappy,
+)
 from cpg_workflows.stages.large_cohort import AncestryPlots, Frequencies, LoadVqsr
 from cpg_workflows.stages.mito import MitoReport
 from cpg_workflows.stages.outrider import Outrider
@@ -59,12 +56,12 @@ WORKFLOWS: dict[str, list[StageDecorator]] = {
     ],
     'validation': [ValidationMtToVcf, ValidationHappyOnVcf, ValidationParseHappy],
     'large_cohort': [LoadVqsr, Frequencies, AncestryPlots, GvcfMultiQC, CramMultiQC],
-    # 'gatk_sv_singlesample': [CreateSampleBatches],
-    # 'gatk_sv_multisample_1': [FilterBatch, GenotypeBatch],
-    # 'gatk_sv_sandwich': [MergeBatchSites],  # stage to run between FilterBatch & GenotypeBatch
-    # 'gatk_sv_multisample_2': [MtToEsSv],
-    # 'rare_disease_rnaseq': [Outrider, Fraser],
-    # 'gcnv': [AnnotateCohortgCNV, AnnotateDatasetCNV, MtToEsCNV],
+    'gatk_sv_singlesample': [CreateSampleBatches],
+    'gatk_sv_multisample_1': [FilterBatch, GenotypeBatch],
+    'gatk_sv_sandwich': [MergeBatchSites],  # stage to run between FilterBatch & GenotypeBatch
+    'gatk_sv_multisample_2': [MtToEsSv],
+    'rare_disease_rnaseq': [Outrider, Fraser],
+    'gcnv': [AnnotateCohortgCNV, AnnotateDatasetCNV, MtToEsCNV],
 }
 
 
