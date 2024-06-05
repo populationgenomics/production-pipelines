6fa42a4cab7cec67b085ea71c2c54cec41062195
Vivian Bakiris <79084890+vivbak@users.noreply.github.com>
2024-06-05 17:49:35 +1000

    Update cpg_workflows/targets.py
    Co-authored-by: Matt Welland <mattwellie@gmail.com>

--- cpg_workflows/targets.py
+++ cpg_workflows/targets.py
@@ -108,7 +108,7 @@ class MultiCohort(Target):
         # NOTE: For a cohort, we simply pull the dataset name from the config.
         input_cohorts = get_config()['workflow'].get('input_cohorts', [])
         if input_cohorts:
-            cohorts_string = '_'.join(input_cohorts)
+            cohorts_string = '_'.join(sorted(input_cohorts))
 
         self.name = cohorts_string or get_config()['workflow']['dataset']
 
