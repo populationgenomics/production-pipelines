a051e72df4267fb644b8e5e89500f2a809e93f4e
Michael Harper <michael.harper@populationgenomics.org.au>
2024-06-17 12:44:03 +1000

    removing required stage of Genotype from Combiner

--- cpg_workflows/stages/large_cohort.py
+++ cpg_workflows/stages/large_cohort.py
@@ -16,7 +16,6 @@ from cpg_workflows.workflow import (
 from .genotype import Genotype
 
 
-@stage(required_stages=[Genotype])
 class Combiner(CohortStage):
     def expected_outputs(self, cohort: Cohort) -> Path:
         if vds_version := get_config()['workflow'].get('vds_version'):
