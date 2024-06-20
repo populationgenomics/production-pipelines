cd4fe7b4e792ae24db817aeb2cc8861a9cbb7e81
Michael Harper <michael.harper@populationgenomics.org.au>
2024-06-20 14:13:46 +1000

    Indexing BAM

---
+++ scripts/index_bam.py
@@ -0,0 +1,25 @@
+import click
+
+import hail as hl
+
+from cpg_utils.hail_batch import command, get_batch, image_path
+
+
+@click.command()
+@click.option('--bam-path', required=True, type=str)
+@click.option('--output-path', required=True, type=str)
+def main(bam_path: str, output_path: str):
+
+    b = get_batch('Indexing bam')
+    j = b.new_job('Index BAM')
+    j.image(image_path('samtools'))
+    j.storage('10Gi')
+    bam_input = b.read_input(bam_path)
+    cmd = f"samtools index {bam_input} {j.out_bam_index}"
+
+    j.command(command(cmd))
+    b.write_output(j.out_bam_index, output_path)
+
+
+if __name__ == '__main__':
+    main()  # pylint: disable=no-value-for-parameter
