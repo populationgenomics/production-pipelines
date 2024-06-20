4abfd8c724dbfa62f60989574c3513dcc969b778
Michael Harper <michael.harper@populationgenomics.org.au>
2024-06-20 15:01:10 +1000

    subsampling vcf

---
+++ scripts/bcf_tools.py
@@ -0,0 +1,26 @@
+import click
+
+import hail as hl
+
+from cpg_utils.hail_batch import command, get_batch, image_path
+
+
+@click.command()
+@click.option('--gvcf-path', required=True, type=str)
+@click.option('--output-path', required=True, type=str)
+def main(gvcf_path: str, output_path: str):
+
+    b = get_batch('Subsample VCF')
+    j = b.new_job('Subsample VCF')
+    j.image(image_path('bcftools'))
+    j.storage('10Gi')
+    gvcf_input = b.read_input(gvcf_path)
+    cmd = f"bcftools view -r chr2:25227855-25342590 {gvcf_input} -Oz -o {j.out_vcf}"
+
+    j.command(command(cmd))
+    b.write_output(j.out_vcf, output_path)
+    b.run(wait=False)
+
+
+if __name__ == '__main__':
+    main()  # pylint: disable=no-value-for-parameter
