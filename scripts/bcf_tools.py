af074aa929cd88e54db4c2537e5123b81b4fa547
Michael Harper <michael.harper@populationgenomics.org.au>
2024-06-20 15:11:13 +1000

    subsampling vcf

--- scripts/bcf_tools.py
+++ scripts/bcf_tools.py
@@ -14,8 +14,9 @@ def main(gvcf_path: str, output_path: str):
     j = b.new_job('Subsample VCF')
     j.image(image_path('bcftools'))
     j.storage('10Gi')
-    gvcf_input = b.read_input(gvcf_path)
-    cmd = f"bcftools view -r chr2:25227855-25342590 {gvcf_input} -Oz -o {j.out_vcf}"
+    gvcf_input = b.read_input_group(gvcf=gvcf_path, gvcf_index=f'{gvcf_path}.tbi')
+
+    cmd = f"bcftools view -r chr2:25227855-25342590 {gvcf_input.gvcf} -Oz -o {j.out_vcf}"
 
     j.command(command(cmd))
     b.write_output(j.out_vcf, output_path)
