9e6a9cd12e153b6799d57a389644e32cbca92fa3
Michael Harper <michael.harper@populationgenomics.org.au>
2024-06-21 12:17:30 +1000

    checking inputs to Combiner

--- cpg_workflows/stages/large_cohort.py
+++ cpg_workflows/stages/large_cohort.py
@@ -35,8 +35,7 @@ class Combiner(CohortStage):
 
         logging.info(f'Inputs: {inputs._outputs_by_target_by_stage}')
         gvcf_by_sgid = {
-            sequencing_group.id: GvcfPath(inputs.as_path(target=sequencing_group, stage=Genotype, key='gvcf'))
-            for sequencing_group in cohort.get_sequencing_groups()
+            sequencing_group.id: sequencing_group.gvcf for sequencing_group in cohort.get_sequencing_groups()
         }
 
         not_found_gvcfs: list[str] = []
