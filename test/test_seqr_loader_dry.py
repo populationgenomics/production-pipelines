d43e44e508cc2286049383a07343422142c850cf
Michael Harper <michael.harper@populationgenomics.org.au>
2024-06-24 07:48:34 +1000

    Changed import

--- test/test_seqr_loader_dry.py
+++ test/test_seqr_loader_dry.py
@@ -197,7 +197,7 @@ def test_seqr_loader_dry(mocker: MockFixture, tmp_path):
 
     from cpg_utils.hail_batch import get_batch
     from cpg_workflows.inputs import get_cohort
-    from cpg_workflows.stages.alignment_pipeline.cram_qc import CramMultiQC
+    from cpg_workflows.stages.alignment.cram_qc import CramMultiQC
     from cpg_workflows.stages.genotyping.gvcf_qc import GvcfMultiQC
     from cpg_workflows.stages.joint_genotyping_qc import JointVcfQC
     from cpg_workflows.stages.seqr_loader import MtToEs
