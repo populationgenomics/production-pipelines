8ab2982a046c0aae925f9910f17372f35af7a354
Michael Harper <michael.harper@populationgenomics.org.au>
2024-06-26 13:51:17 +1000

    Removing CramMultiQC and GvcfMultiQC as last stage of alignemtn and genotype pipelines however can still use the stages at a later date. For example, when we create custom cohorts and do QC, we would want to be able to run MultiQC to generate a summary report across all the individual CRAM QC metrics for the samples in that custom cohort

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from cpg_utils import to_path
 from cpg_utils.config import set_config_paths
 from cpg_workflows import defaults_config_path
 from cpg_workflows.stages.aip import CreateAIPHTML, GenerateSeqrFile, ValidateMOI
-from cpg_workflows.stages.alignment.cram_qc import CramMultiQC
+from cpg_workflows.stages.alignment.cram_qc import CramMultiQC, CramQC
 from cpg_workflows.stages.exomiser import ExomiserSeqrTSV, RunExomiser
 from cpg_workflows.stages.fastqc import FastQCMultiQC
 from cpg_workflows.stages.fraser import Fraser
@@ -24,7 +24,7 @@ from cpg_workflows.stages.gatk_sv.gatk_sv_multisample_1 import (
 from cpg_workflows.stages.gatk_sv.gatk_sv_multisample_2 import MtToEsSv
 from cpg_workflows.stages.gatk_sv.gatk_sv_single_sample import CreateSampleBatches
 from cpg_workflows.stages.gcnv import AnnotateCohortgCNV, AnnotateDatasetCNV, MtToEsCNV
-from cpg_workflows.stages.genotyping.gvcf_qc import GvcfMultiQC
+from cpg_workflows.stages.genotyping.gvcf_qc import GvcfHappy, GvcfMultiQC, GvcfQC
 from cpg_workflows.stages.happy_validation import (
     ValidationHappyOnVcf,
     ValidationMtToVcf,
@@ -39,8 +39,8 @@ from cpg_workflows.stages.stripy import Stripy
 from cpg_workflows.workflow import StageDecorator, run_workflow
 
 WORKFLOWS: dict[str, list[StageDecorator]] = {
-    'alignment': [CramMultiQC],
-    'genotype': [GvcfMultiQC],
+    'alignment': [CramQC],
+    'genotype': [GvcfQC, GvcfHappy],
     'aip': [ValidateMOI, CreateAIPHTML, GenerateSeqrFile],
     'exomiser': [RunExomiser, ExomiserSeqrTSV],
     'pre_alignment': [FastQCMultiQC],
