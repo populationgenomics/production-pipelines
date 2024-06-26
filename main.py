791e51d74aff90fcb0dcdaf7e392b7c9906af87a
MattWellie <mattwellie@gmail.com>
2024-06-26 14:06:13 +1000

    LINT

--- main.py
+++ main.py
@@ -30,11 +30,11 @@ from cpg_workflows.stages.happy_validation import (
     ValidationParseHappy,
 )
 from cpg_workflows.stages.large_cohort import AncestryPlots, Frequencies, LoadVqsr
-from cpg_workflows.stages.seqr_loader_long_read.long_read_sv_annotation import AnnotateLongReadSVs, ReFormatPacBioSVs
 from cpg_workflows.stages.mito import MitoReport
 from cpg_workflows.stages.outrider import Outrider
 from cpg_workflows.stages.seqr_loader import AnnotateDataset, DatasetVCF, MtToEs
 from cpg_workflows.stages.seqr_loader_long_read.bam_to_cram import BamToCram
+from cpg_workflows.stages.seqr_loader_long_read.long_read_sv_annotation import AnnotateLongReadSVs, ReFormatPacBioSVs
 from cpg_workflows.stages.stripy import Stripy
 from cpg_workflows.stages.talos import CreateTalosHTML, GenerateSeqrFile, ValidateMOI
 from cpg_workflows.workflow import StageDecorator, run_workflow
