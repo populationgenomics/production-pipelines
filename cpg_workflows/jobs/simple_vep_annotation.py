164b1be1f9cb52dbd06e1c33403ee5f2031699e3
MattWellie <mattwellie@gmail.com>
2024-07-01 09:29:50 +1000

    Updates from review Co-authored-by: Cas Simons <cas.simons@populationgenomics.org.au>

--- cpg_workflows/jobs/simple_vep_annotation.py
+++ cpg_workflows/jobs/simple_vep_annotation.py
@@ -6,7 +6,7 @@ This is a VV simple VEP annotation process, annotating a VCF with a minimal numb
 This is to facilitate the migration of the clinvar re-summary process out of AIP/ClinvArbitration
 Minimal annotation set to improve runtimes
 
-There are potentially other uses for a VCF -> VCF annotation, but I'm not sure what they'll be
+Long-running/whole-genome VEP tasks could be improved by localising and unpacking the VEP cache inside the VM
 """
 import logging
 import os.path
@@ -18,7 +18,7 @@ from hailtop.batch.job import BashJob
 from cpg_utils import to_path
 from cpg_utils.config import image_path, output_path, reference_path
 from cpg_utils.hail_batch import get_batch
-from cpg_workflows.jobs.bcftools import naive_merge_vcfs
+from cpg_workflows.jobs.bcftools import naive_concat_vcfs
 
 CHROM_LIST: list[str] = [f'chr{x}' for x in list(range(1, 23))] + ['chrX', 'chrY', 'chrM']
 
@@ -29,12 +29,14 @@ CHROM_LIST: list[str] = [f'chr{x}' for x in list(range(1, 23))] + ['chrX', 'chrY
 def split_vcf_by_chromosome(
     localised_vcf: ResourceFile,
     output_dir: str | None = None,
+    storage: str | None = None,
 ) -> tuple[list[ResourceFile], BashJob]:
     """
     take a pre-localised file and split it into separate chromosomes
     Args:
         localised_vcf ():
         output_dir (str): if we want these persisted, where to put them
+        storage (str): can be set if required, default OS partition can hold ~5GB without additional attached storage
 
     Returns:
         the list of resource files, and the job that created them
@@ -49,7 +51,9 @@ def split_vcf_by_chromosome(
     # set some resources
     bcftools_job.image(image_path('bcftools')).cpu(1).memory('8G')
 
-    # todo storage? VCF * 2.5
+    # only attach further storage if requested
+    if storage:
+        bcftools_job.storage(storage)
 
     # existing fragments, instead of checking for each separately. This is an empty list if the 'dir' doesn't exist yet
     existing_fragments = [str(each_path) for each_path in to_path(output_dir).glob('*')] if output_dir else []
@@ -131,25 +135,22 @@ def annotate_localised_vcfs(
     annotation_jobs: list = []
 
     # next, annotate!
-    for chrom, vcf in zip(CHROM_LIST, vcf_list, strict=True):
+    for job_number, vcf in enumerate(vcf_list, start=1):
 
         if isinstance(output_dir, str):
             # the name for this chunk of annotation
-            result_path = os.path.join(output_dir, f'{chrom}.vcf.bgz')
+            result_path = os.path.join(output_dir, f'{job_number}.vcf.bgz')
             # do we already have it generated?
             if result_path in existing_outputs:
                 logging.info(f'{result_path} already exists')
                 vcf_fragment = get_batch().read_input_group(
-                    **{
-                        'vcf.bgz': result_path,
-                        'vcf.bgz.tbi': f'{result_path}.tbi',
-                    },
+                    **{'vcf.bgz': result_path, 'vcf.bgz.tbi': f'{result_path}.tbi'},
                 )['vcf.bgz']
                 ordered_annotated.append(vcf_fragment)
                 continue
 
         # annotate that fragment, making a VCF output
-        vep_job = get_batch().new_job(f'annotate {chrom} with VEP')
+        vep_job = get_batch().new_job(f'Annotate part {job_number} with VEP')
 
         # declare a resource group for this annotated VCF output
         vep_job.declare_resource_group(vcf={'vcf.bgz': '{root}.vcf.bgz', 'vcf.bgz.tbi': '{root}.vcf.bgz.tbi'})
@@ -172,7 +173,7 @@ def annotate_localised_vcfs(
         vep_job.command(f'tabix -p vcf {vep_job.vcf["vcf.bgz"]}')
 
         if isinstance(output_dir, str):
-            get_batch().write_output(vep_job.vcf, os.path.join(output_dir, chrom))
+            get_batch().write_output(vep_job.vcf, os.path.join(output_dir, str(job_number)))
 
         annotation_jobs.append(vep_job)
         # keep a list of the in-batch VCF paths
@@ -213,7 +214,7 @@ def split_and_annotate_vcf(vcf_in: str | ResourceFile, out_vcf: str) -> tuple[Re
     ordered_annotated, annotation_jobs = annotate_localised_vcfs(ordered_output_vcfs, output_dir=annotated_tmpdir)
 
     # now merge them all
-    merged_vcf_in_batch, merge_job = naive_merge_vcfs(ordered_annotated, output_file=out_vcf, vcfs_localised=True)
+    merged_vcf_in_batch, merge_job = naive_concat_vcfs(ordered_annotated, output_file=out_vcf, vcfs_localised=True)
 
     # collect all the jobs
     all_jobs = [bcftools_job, *annotation_jobs, merge_job]
