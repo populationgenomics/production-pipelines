38ab78e18085cdbcfa247cac480353d87e29fb8c
Michael Harper <michael.harper@populationgenomics.org.au>
2024-07-04 15:52:10 +1000

    fixes

--- scripts/subset_tob_cram.py
+++ scripts/subset_tob_cram.py
@@ -6,8 +6,8 @@ from cpg_utils.hail_batch import get_batch, get_config
 @click.command()
 @click.option('--input_cram_path', required=True, type=str)
 @click.option('--output_cram_path', required=True, type=str)
-@click.option('--chr_num', required=True, type=int)
-def main(input_cram_path: str, output_cram_path: str, chr_num: int):
+@click.option('--chr', required=True, type=str)
+def main(input_cram_path: str, output_cram_path: str, chr: str):
     """
     Subset a CRAM file to a single chromosome.
     """
@@ -21,7 +21,7 @@ def main(input_cram_path: str, output_cram_path: str, chr_num: int):
     )
 
     subset_cmd = f"""
-    samtools view -T {ref_path} -C -o {j.output_cram} {input_cram} chr{chr_num}
+    samtools view -T {ref_path} -C -o {j.output_cram} {input_cram} {chr}
     """
     j.command(subset_cmd)
     b.write_output(j.output_cram, output_cram_path)
