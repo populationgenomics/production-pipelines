1d61ea3590a0518a2ceb325c0b4edd990b98f849
Michael Harper <michael.harper@populationgenomics.org.au>
2024-07-09 09:43:33 +1000

    for testing fewgenomes, removing the output destination change

--- cpg_workflows/jobs/align.py
+++ cpg_workflows/jobs/align.py
@@ -621,15 +621,15 @@ def finalise_alignment(
     )
 
     if output_path:
-        extract_picard = get_config()['workflow'].get('extract_picard', False)
-        if extract_picard:
-            output_path = CramPath(
-                f'gs://cpg-tob-wgs-test/cram/picard_extracted_{sequencing_group.id}_chr21.cram',
-            )
-        else:
-            output_path = CramPath(
-                f'gs://cpg-tob-wgs-test/cram/samtools_extracted_{sequencing_group.id}_chr21.cram',
-            )
+        # extract_picard = get_config()['workflow'].get('extract_picard', False)
+        # if extract_picard:
+        #     output_path = CramPath(
+        #         f'gs://cpg-tob-wgs-test/cram/picard_extracted_{sequencing_group.id}_chr21.cram',
+        #     )
+        # else:
+        #     output_path = CramPath(
+        #         f'gs://cpg-tob-wgs-test/cram/samtools_extracted_{sequencing_group.id}_chr21.cram',
+        #     )
         if md_j is not None:
             b.write_output(md_j.output_cram, str(output_path.path.with_suffix('')))
             if out_markdup_metrics_path:
