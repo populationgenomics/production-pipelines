029dadd3a10b74690c82405fa231f82a8cd3a570
MattWellie <mattwellie@gmail.com>
2024-07-08 15:46:58 +1000

    moar docstring

--- cpg_workflows/jobs/seqr_loader_long_read.py
+++ cpg_workflows/jobs/seqr_loader_long_read.py
@@ -1,12 +1,18 @@
 def modify_sniffles_vcf(file_in: str, file_out: str, ext_id: str, int_id: str):
     """
-    to be run as a PythonJob - scrolls through the
+    to be run as a PythonJob - scrolls through the VCF and performs a few updates:
+
+    - replaces the External Sample ID with the internal CPG identifier
+    - replaces the ALT allele with a symbolic "<TYPE>", derived from the SVTYPE INFO field
+    - reduces the massive REF String to only the base at the variant position
+
+    rebuilds the VCF following those edits, and writes the compressed data back out
 
     Args:
         file_in (str): localised, VCF directly from Sniffles
         file_out (str): local batch output path, same VCF with INFO/ALT alterations
-        ext_id (str): external ID
-        int_id (str): required ID inside the reformatted VCF
+        ext_id (str): external ID to replace (if found)
+        int_id (str): CPG ID, required inside the reformatted VCF
     """
     import gzip
 
