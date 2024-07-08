617684422544fdb68299c86c6648268e64f7a2d7
MattWellie <mattwellie@gmail.com>
2024-07-09 09:57:16 +1000

    vqsr changes

--- cpg_workflows/stages/vqsr.py
+++ cpg_workflows/stages/vqsr.py
@@ -2,20 +2,12 @@
 Stage that performs AS-VQSR.
 """
 
-from cpg_utils import to_path
 from cpg_utils.config import get_config
+from cpg_utils.hail_batch import get_batch
 from cpg_workflows.jobs import vqsr
-from cpg_workflows.workflow import (
-    Cohort,
-    CohortStage,
-    StageInput,
-    StageOutput,
-    stage,
-)
-
-from .. import get_batch
-from ..resources import joint_calling_scatter_count
-from .joint_genotyping import JointGenotyping
+from cpg_workflows.resources import joint_calling_scatter_count
+from cpg_workflows.stages.joint_genotyping import JointGenotyping
+from cpg_workflows.workflow import Cohort, CohortStage, StageInput, StageOutput, stage
 
 
 @stage(required_stages=JointGenotyping)
@@ -28,28 +20,25 @@ class Vqsr(CohortStage):
         """
         Generate a site-only VCF.
         """
-        return {
-            # writing into perm location for late debugging
-            # convert to str to avoid checking existence
-            'tmp_prefix': str(self.prefix / 'tmp'),
-            'siteonly': self.prefix / 'siteonly.vcf.gz',
-        }
+        return {'siteonly': self.prefix / 'siteonly.vcf.gz'}
 
     def queue_jobs(self, cohort: Cohort, inputs: StageInput) -> StageOutput | None:
         """
-        Submit jobs.
+        This job takes the site-only VCF from the JointGenotyping stage with no prior
+        multi-allelic variant splitting required
+        VQSR itself splits out all variants
         """
         siteonly_vcf_path = inputs.as_path(stage=JointGenotyping, target=cohort, key='siteonly')
-
+        outputs = self.expected_outputs(cohort)
         jobs = vqsr.make_vqsr_jobs(
             b=get_batch(),
             input_siteonly_vcf_path=siteonly_vcf_path,
             scatter_count=joint_calling_scatter_count(len(cohort.get_sequencing_groups())),
             gvcf_count=len(cohort.get_sequencing_groups()),
-            out_path=self.expected_outputs(cohort)['siteonly'],
-            tmp_prefix=to_path(self.expected_outputs(cohort)['tmp_prefix']),
+            out_path=outputs['siteonly'],
+            tmp_prefix=self.prefix / 'tmp',
             use_as_annotations=get_config()['workflow'].get('use_as_vqsr', True),
             intervals_path=get_config()['workflow'].get('intervals_path'),
             job_attrs=self.get_job_attrs(),
         )
-        return self.make_outputs(cohort, data=self.expected_outputs(cohort), jobs=jobs)
+        return self.make_outputs(cohort, data=outputs, jobs=jobs)
