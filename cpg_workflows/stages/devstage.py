c161761a3ddb4c06db108790cab737c4400f203e
Michael Franklin <illusional@users.noreply.github.com>
2024-07-09 16:27:50 +1000

    Michael's suggestion

--- cpg_workflows/stages/devstage.py
+++ cpg_workflows/stages/devstage.py
@@ -4,8 +4,7 @@ Example Stages
 
 from dataclasses import dataclass
 
-from cpg_utils.config import get_config
-from cpg_utils.hail_batch import get_batch
+from cpg_utils.config import config_retrieve
 from cpg_workflows.jobs import example_job
 from cpg_workflows.targets import SequencingGroup
 from cpg_workflows.workflow import (
@@ -63,9 +62,17 @@ class ExampleStageConfig(StageConfig):
     An example stage config
     """
 
+    # put the images here, so you're not calling image_path later on
+    # everything you need for the stage (except stage input) should be in this config
+    image_samtools: str
+
     reference_version: str | None = None
     resource_override: str | None = None
     skip_stage: bool = False
+
+
+class ExampleStageOutputs(StageOutputs):
+    test_output: str
 
 
 @stage(analysis_type='custom', analysis_keys=['test_output'])
@@ -74,15 +81,30 @@ class ExampleStage(SequencingGroupStage):
     An example stage
     """
 
-    def setup_config_inputs(example_stage_config: dict) -> ExampleStageConfig:
-        return ExampleStageConfig(**example_stage_config)
+    def setup_config_inputs(self, global_config: dict) -> ExampleStageConfig:
+        return ExampleStageConfig(
+            reference_version=config_retrieve(['workflow', 'reference_version'], global_config),
+            images_samtools=config_retrieve(['workflow', 'images', 'samtools'], global_config),
+        )
 
-    def expected_outputs(self, sequencing_group: SequencingGroup):
-        return {'test_output': sequencing_group.dataset.prefix() / 'example' / f'{sequencing_group.id}_example.txt'}
+    def expected_outputs(self, sequencing_group: SequencingGroup) -> ExampleStageOutputs:
+        return ExampleStageOutputs(
+            test_output=sequencing_group.dataset.prefix() / 'example' / f'{sequencing_group.id}_example.txt',
+        )
 
-    def queue_jobs(self, sequencing_group: SequencingGroup, inputs: StageInput) -> StageOutput | None:
+    def queue_jobs(
+        self,
+        sequencing_group: SequencingGroup,
+        inputs: StageInput,
+        stage_config: ExampleStageConfig,
+    ) -> StageOutput | None:
         jobs = []
-        stage_config = self.setup_config_inputs(get_config('example_stage'))
-        j = example_job.example_job(batch=get_batch(), sequencing_group=sequencing_group, config=stage_config)
+
+        # no get_batch(), batch ref is provided as instance on class
+        j = example_job.example_job(
+            batch=self.batch,
+            sequencing_group=sequencing_group,
+            config=stage_config,
+        )
         jobs.append(j)
         return self.make_outputs(sequencing_group, data=self.expected_outputs(sequencing_group), jobs=jobs)
