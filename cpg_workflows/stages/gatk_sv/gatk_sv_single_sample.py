b0d11074ff920c84ac07745c73c8b0375734e24b
EddieLF <eddieformaini@gmail.com>
2024-07-24 13:52:47 +1000

    CreateSampleBatches save sgs to file and pass filepathto job

--- cpg_workflows/stages/gatk_sv/gatk_sv_single_sample.py
+++ cpg_workflows/stages/gatk_sv/gatk_sv_single_sample.py
@@ -2,11 +2,12 @@
 single-sample components of the GATK SV workflow
 """
 
+import json
 import logging
 from typing import Any
 
-from cpg_utils import Path
-from cpg_utils.config import AR_GUID_NAME, config_retrieve, try_get_ar_guid
+from cpg_utils import Path, to_path
+from cpg_utils.config import AR_GUID_NAME, config_retrieve, dataset_path, try_get_ar_guid
 from cpg_utils.hail_batch import get_batch
 from cpg_workflows.jobs import sample_batching
 from cpg_workflows.stages.gatk_sv.gatk_sv_common import (
@@ -235,10 +236,15 @@ class CreateSampleBatches(MultiCohortStage):
         if config_retrieve(['workflow', 'sequencing_type']) != 'genome':
             raise RuntimeError('This workflow is not intended for Exome data')
 
+        # Get the sequencing groups and write them to a json file in tmp
         sequencing_groups = {
             sequencing_group.id: sequencing_group.meta for sequencing_group in multicohort.get_sequencing_groups()
         }
+        sgs_tmp_json_path = to_path(self.tmp_prefix / 'sgs_meta.json')
+        with sgs_tmp_json_path.open('w') as f:
+            json.dump(sequencing_groups, f)
 
+        # Get the QC tables for each input cohort
         qc_tables = [inputs.as_dict(cohort, EvidenceQC)['qc_table'] for cohort in multicohort.get_cohorts()]
         # get those settings
         min_batch_size = config_retrieve(['workflow', 'min_batch_size'], 100)
@@ -254,7 +260,7 @@ class CreateSampleBatches(MultiCohortStage):
             sample_batching.partition_batches,
             qc_tables,
             sequencing_groups,
-            expected['batch_json'],
+            str(expected['batch_json']),
             min_batch_size,
             max_batch_size,
         )
