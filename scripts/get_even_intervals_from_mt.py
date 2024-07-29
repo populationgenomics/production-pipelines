4537f828eb2c474e9ef33386b0fdbc0d42cf40ea
EddieLF <eddieformaini@gmail.com>
2024-07-29 16:19:55 +1000

    Split by chrom

--- scripts/get_even_intervals_from_mt.py
+++ scripts/get_even_intervals_from_mt.py
@@ -10,28 +10,42 @@ import hail as hl
 import argparse
 
 
-def get_even_intervals_from_mt(mt, n_intervals, interval_list_file):
+def get_intervals_for_chr()
+
+def get_even_intervals_from_mt(mt: hl.MatrixTable, n_intervals: int, interval_list_file):
     """
     Get even intervals from a matrixtable.
     """
-    # Get the number of rows in the matrixtable
+    # Get the number of rows in the matrixtable        
     n_rows = mt.count_rows()
 
     # Get the number of rows in each interval
     interval_size = n_rows // n_intervals
 
-    # Get the contig and position of the start of each interval
+    for chrom in ['chr1',]:
+        get_intervals_for_chr(mt, chrom, n_intervals, interval_size, centromere_position, interval_list_file)
+
+    # Add the integer index of each row as a new row field.
+    mt = mt.add_row_index(name='row_idx')
+    mt = mt.rows().select('row_idx', 'locus')
+
+    # Iterate through the mt in intervals of interval_size, using the row_idx
     intervals = []
-    for i in range(n_intervals):
-        if i == 0:
-            start_locus = mt.locus.collect()[0]
-        else:
-            start_locus = mt.rows().filter(mt.locus.position == 1).collect()[i * interval_size]
-        intervals.append(start_locus)
+    for n in range(n_intervals):
+        # Get the start and end of the interval
+        start = n * interval_size
+        # stop = (n + 1) * interval_size - 1
+
+        intervals.append(
+            mt.filter_rows(mt.row_idx == start)['locus'],
+        )
+
+    # Evaluate the contig and position of each interval 
+    intervals = [interval.collect()[0] for interval in intervals]
 
     # Save the intervals to a file
-    with open(interval_list_file, 'w') as f:
-        for interval in intervals:
-            f.write(f"{interval.contig}\t{interval.position}\n")
+    # with open(interval_list_file, 'w') as f:
+    #     for interval in intervals:
+    #         f.write(f"{interval.contig}\t{interval.position}\n")
 
     return intervals
