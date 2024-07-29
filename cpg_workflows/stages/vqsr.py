1238c9831f7a34363d5291de652576197cab97e7
MattWellie <mattwellie@gmail.com>
2024-07-30 09:18:39 +1000

    vqsr to multicohorts

--- cpg_workflows/stages/vqsr.py
+++ cpg_workflows/stages/vqsr.py
@@ -2,54 +2,46 @@
 Stage that performs AS-VQSR.
 """
 
-from cpg_utils import to_path
+from cpg_utils import Path
 from cpg_utils.config import get_config
+from cpg_utils.hail_batch import get_batch
 from cpg_workflows.jobs import vqsr
-from cpg_workflows.workflow import (
-    Cohort,
-    CohortStage,
-    StageInput,
-    StageOutput,
-    stage,
-)
+from cpg_workflows.targets import MultiCohort
+from cpg_workflows.workflow import MultiCohortStage, StageInput, StageOutput, stage
 
-from .. import get_batch
 from ..resources import joint_calling_scatter_count
 from .joint_genotyping import JointGenotyping
 
 
 @stage(required_stages=JointGenotyping)
-class Vqsr(CohortStage):
+class Vqsr(MultiCohortStage):
     """
     Variant filtering of joint-called VCF.
     """
 
-    def expected_outputs(self, cohort: Cohort):
+    def expected_outputs(self, multicohort: MultiCohort) -> dict[str, Path]:
         """
         Generate a site-only VCF.
         """
-        return {
-            # writing into perm location for late debugging
-            # convert to str to avoid checking existence
-            'tmp_prefix': str(self.prefix / 'tmp'),
-            'siteonly': self.prefix / 'siteonly.vcf.gz',
-        }
+        return {'siteonly': self.prefix / 'siteonly.vcf.gz'}
 
-    def queue_jobs(self, cohort: Cohort, inputs: StageInput) -> StageOutput | None:
+    def queue_jobs(self, multicohort: MultiCohort, inputs: StageInput) -> StageOutput | None:
         """
         Submit jobs.
         """
-        siteonly_vcf_path = inputs.as_path(stage=JointGenotyping, target=cohort, key='siteonly')
+        siteonly_vcf_path = inputs.as_path(stage=JointGenotyping, target=multicohort, key='siteonly')
+        outputs = self.expected_outputs(multicohort)
+        number_of_sgids = len(multicohort.get_sequencing_groups())
 
         jobs = vqsr.make_vqsr_jobs(
             b=get_batch(),
             input_siteonly_vcf_path=siteonly_vcf_path,
-            scatter_count=joint_calling_scatter_count(len(cohort.get_sequencing_groups())),
-            gvcf_count=len(cohort.get_sequencing_groups()),
-            out_path=self.expected_outputs(cohort)['siteonly'],
-            tmp_prefix=to_path(self.expected_outputs(cohort)['tmp_prefix']),
+            scatter_count=joint_calling_scatter_count(number_of_sgids),
+            gvcf_count=number_of_sgids,
+            out_path=outputs['siteonly'],
+            tmp_prefix=self.prefix / 'tmp',
             use_as_annotations=get_config()['workflow'].get('use_as_vqsr', True),
             intervals_path=get_config()['workflow'].get('intervals_path'),
             job_attrs=self.get_job_attrs(),
         )
-        return self.make_outputs(cohort, data=self.expected_outputs(cohort), jobs=jobs)
+        return self.make_outputs(multicohort, data=outputs, jobs=jobs)
