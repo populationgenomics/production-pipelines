bbe528ddbd579c63723b4fc836212f71484fbec5
MattWellie <mattwellie@gmail.com>
2024-08-12 16:05:07 +0100

    subset wrapper

--- cpg_workflows/scripts/subset_partition_wrapper.py
+++ cpg_workflows/scripts/subset_partition_wrapper.py
@@ -1,4 +1,22 @@
 from cpg_utils.config import config_retrieve
-from cpg_utils.hail_batch import get_batch
+from cpg_utils.hail_batch import get_batch, query_command
+from cpg_workflows.query_modules import seqr_loader
 
-# create a new batch
+subset_j = get_batch().new_job('Subset cohort to dataset')
+subset_j.image(config_retrieve(['workflow', 'driver_image']))
+sequencing_group_ids = config_retrieve(['workflow', 'sgids'])
+mt_path = 'gs://cpg-seqr-test/seqr_loader/1d93d4819ca2d100c5108e4b0973915b5eb4a9_3905/AnnotateCohort/cohort.mt'
+subset_mt_path = 'gs://cpg-seqr-test/subset_cohort.mt'
+
+assert sequencing_group_ids
+subset_j.command(
+    query_command(
+        seqr_loader,
+        seqr_loader.subset_mt_to_samples.__name__,
+        mt_path,
+        sequencing_group_ids,
+        subset_mt_path,
+        setup_gcp=True,
+    ),
+)
+get_batch().run(wait=False)
