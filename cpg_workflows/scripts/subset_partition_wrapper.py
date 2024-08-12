d3ab089e0853a7b58e87ed418a211379760f0764
MattWellie <mattwellie@gmail.com>
2024-08-12 17:00:18 +0100

    chuck more memory at it

--- cpg_workflows/scripts/subset_partition_wrapper.py
+++ cpg_workflows/scripts/subset_partition_wrapper.py
@@ -4,6 +4,7 @@ from cpg_workflows.query_modules import seqr_loader
 
 subset_j = get_batch().new_job('Subset cohort to dataset')
 subset_j.image(config_retrieve(['workflow', 'driver_image']))
+subset_j.cpu(4).memory('highmem')
 sequencing_group_ids = config_retrieve(['workflow', 'sgids'])
 mt_path = 'gs://cpg-seqr-test/seqr_loader/1d93d4819ca2d100c5108e4b0973915b5eb4a9_3905/AnnotateCohort/cohort.mt'
 subset_mt_path = 'gs://cpg-seqr-test/subset_cohort.mt'
