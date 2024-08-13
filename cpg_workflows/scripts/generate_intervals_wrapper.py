c985d7316baaab87724b04d12a12c7b4bba1097d
MattWellie <mattwellie@gmail.com>
2024-08-13 18:16:09 +0100

    remove hard coding from the wrapper

--- cpg_workflows/scripts/generate_intervals_wrapper.py
+++ cpg_workflows/scripts/generate_intervals_wrapper.py
@@ -3,21 +3,32 @@ Generate new intervals from a MatrixTable based on data distribution
 A wrapper for the generate_new_intervals.py script
 """
 
+from argparse import ArgumentParser
+
 from cpg_utils.config import config_retrieve
 from cpg_utils.hail_batch import authenticate_cloud_credentials_in_job, get_batch
 
-# some hard coded paths
-save_bed = 'gs://cpg-seqr-test/seqr_loader/new_intervals.bed'
-input_mt = 'gs://cpg-seqr-test/seqr_loader/1d93d4819ca2d100c5108e4b0973915b5eb4a9_3905/AnnotateCohort/cohort.mt'
 
-job = get_batch().new_bash_job('generate_intervals')
+parser = ArgumentParser()
+parser.add_argument('--mt', help='MatrixTable to generate intervals from')
+parser.add_argument('--out', help='Output intervals file - path to write the resulting BED in GCP')
+parser.add_argument('--meres', help='.interval_list file with intervals to exclude (Centro/Telomeres)')
+parser.add_argument('--intervals', help='Number of intervals to generate', type=int)
+args = parser.parse_args()
+
+job = get_batch().new_bash_job(f'Generate {args.intervals} intervals from {args.mt}')
 authenticate_cloud_credentials_in_job(job)
 job.image(config_retrieve(['workflow', 'driver_image']))
 
-# read this file in locally
-meres = 'gs://cpg-common-test/references/hg38/v0/hg38.telomeresAndMergedCentromeres.interval_list'
-meres_in = get_batch().read_input(meres)
+# localise the centromere/telomere file in the batch
+meres_in = get_batch().read_input(args.meres)
 
-job.command(f'new_intervals_from_mt --mt {input_mt} --out {job.output} --meres_file {meres_in} --intervals 1500')
-get_batch().write_output(job.output, save_bed)
+job.command(
+    'new_intervals_from_mt '
+    f'--mt {args.mt} '
+    f'--out {job.output} '
+    f'--meres_file {meres_in} '
+    f'--intervals {args.intervals}',
+)
+get_batch().write_output(job.output, args.out)
 get_batch().run(wait=False)
