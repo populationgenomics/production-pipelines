0f807dbb8d921688e3543c9b5c5004b32a3b4224
Michael Harper <michael.harper@populationgenomics.org.au>
2024-08-21 15:32:39 +1000

    fixing batch

--- cpg_workflows/scripts/pca_differences_dp.py
+++ cpg_workflows/scripts/pca_differences_dp.py
@@ -1,9 +1,7 @@
 import logging
-import os
 from argparse import ArgumentParser
 
 import hail as hl
-import hailtop.batch as hb
 
 from cpg_utils.hail_batch import get_batch
 
@@ -29,16 +27,11 @@ def main(dense_mt_path: str, output_directory: str):
     from cpg_workflows.large_cohort.dataproc_utils import dataproc_job
     from cpg_workflows.scripts.run_pca import run
 
-    service_backend = hb.ServiceBackend(
-        billing_project=os.getenv('HAIL_BILLING_PROJECT'),
-        bucket=os.getenv('HAIL_BUCKET'),
-    )
-    batch = hb.Batch(name='run pca in dataproc', backend=service_backend)
-
     dense_mt: hl.MatrixTable = hl.read_matrix_table(dense_mt_path)
     dense_mt_subset = dense_mt.head(1000)
     n_pcs = dense_mt_subset.count_cols()
 
+    b = get_batch()
     j = dataproc_job(
         job_name='run_pca',
         function=run,
@@ -51,4 +44,5 @@ def main(dense_mt_path: str, output_directory: str):
 
 
 if '__main__' == __name__:
+    hl.init_batch()
     cli_main()
