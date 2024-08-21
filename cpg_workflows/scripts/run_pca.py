80cc5983b771b90d98058c2c68c4a757c155dc93
Michael Harper <michael.harper@populationgenomics.org.au>
2024-08-22 09:56:35 +1000

    re-refactoring

--- cpg_workflows/scripts/run_pca.py
+++ cpg_workflows/scripts/run_pca.py
@@ -1,13 +1,11 @@
 import hail as hl
 
 
-def run() -> tuple[hl.Table, hl.Table, hl.Table]:
-
-    output_directory = 'gs://cpg-bioheart-test/pca_differences'
-    hl.init(default_reference='GRCh38')
-    dense_mt = hl.read_matrix_table('gs://cpg-bioheart-test/large_cohort/tenk10k1-0/dense_subset.mt')
-    dense_mt_subset = dense_mt.head(1000)
-    n_pcs = dense_mt_subset.count_cols()
+def run(
+    dense_mt: hl.MatrixTable,
+    output_directory: str,
+    n_pcs: int = 10,
+) -> tuple[hl.Table, hl.Table, hl.Table]:
 
     pca_evals, pca_scores, pca_loadings = hl.hwe_normalized_pca(
         dense_mt.GT,
@@ -19,7 +17,3 @@ def run() -> tuple[hl.Table, hl.Table, hl.Table]:
         pca_scores.checkpoint(output_directory + '/scores.ht', overwrite=True),
         pca_loadings.checkpoint(output_directory + '/loadings.ht', overwrite=True),
     )
-
-
-if __name__ == '__main__':
-    run()
