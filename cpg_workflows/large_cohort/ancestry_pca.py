494160a6a8034cbc47bd6e8d9678e6bebf8cac27
Michael Harper <michael.harper@populationgenomics.org.au>
2024-08-22 14:52:14 +1000

    Removing some logging

--- cpg_workflows/large_cohort/ancestry_pca.py
+++ cpg_workflows/large_cohort/ancestry_pca.py
@@ -93,23 +93,11 @@ def add_background(
                 logging.info('No related samples to drop from background dataset')
 
             # save metadata info before merging dense and background datasets
-            logging.info(
-                f'background_mt n_cols is {background_mt.count_cols()} and n_rows {background_mt.count_rows()} after filtering and before selecting: {background_mt.show()}',
-            )
             ht = background_mt.cols()
             background_mt = background_mt.select_cols().select_rows().select_entries('GT', 'GQ', 'DP', 'AD')
             background_mt = background_mt.naive_coalesce(5000)
             # combine dense dataset with background population dataset
-            logging.info(
-                f'background_mt n_cols is {background_mt.count_cols()} and n_rows {background_mt.count_rows()} after selecting: {background_mt.show()}',
-            )
-            logging.info(
-                f'dense_mt n_cols is {dense_mt.count_cols()} and n_rows {dense_mt.count_rows()} before union_cols: {dense_mt.show()}',
-            )
             dense_mt = dense_mt.union_cols(background_mt)
-            logging.info(
-                f'dense_mt n_cols is {dense_mt.count_cols()} and n_rows {dense_mt.count_rows()} after union_cols: {dense_mt.show()}',
-            )
             sample_qc_ht = sample_qc_ht.union(ht, unify=allow_missing_columns)
         else:
             raise ValueError('Background dataset path must be either .mt or .vds')
