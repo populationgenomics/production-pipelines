2dee5d2af716b65fc08de84c09198ca37ce7b7d2
Michael Harper <michael.harper@populationgenomics.org.au>
2024-08-23 10:38:04 +1000

    Filtering to autosomes

--- cpg_workflows/scripts/run_pca.py
+++ cpg_workflows/scripts/run_pca.py
@@ -4,17 +4,20 @@ import pandas as pd
 
 import hail as hl
 
+from gnomad.utils.filtering import filter_to_autosomes
+
 
 def run() -> tuple[hl.Table, hl.Table, hl.Table]:
 
     output_directory = 'gs://cpg-bioheart-test/pca_differences'
     hl.init(default_reference='GRCh38')
-    dense_mt = hl.read_matrix_table('gs://cpg-bioheart-test/large_cohort/tenk10k1-0/dense_subset.mt')
-    dense_mt = dense_mt.head(1000)
-    dense_mt.checkpoint('gs://cpg-bioheart-test/pca_differences/dense_subset_checkpoint.mt', overwrite=True)
+    dense_mt = hl.read_matrix_table('gs://cpg-bioheart-test/pca_differences/dense_subset_checkpoint.mt')
     relateds_to_drop_path = 'gs://cpg-bioheart-test/large_cohort/tenk10k1-0/relateds_to_drop.ht'
     related_samples_to_drop = hl.read_table(relateds_to_drop_path)
     n_pcs = 5
+
+    logging.info('Filtering to autosomes')
+    dense_mt = filter_to_autosomes(dense_mt)
 
     logging.info(f'relateds_to_drop: {related_samples_to_drop.show()}')
     if related_samples_to_drop:
@@ -32,13 +35,13 @@ def run() -> tuple[hl.Table, hl.Table, hl.Table]:
 
     print("Checkpointing eigenvalues...")
     eigenvalues_ht = hl.Table.from_pandas(pd.DataFrame(pca_evals, columns=['f0']))
-    pca_evals_checkpoint = eigenvalues_ht.checkpoint(output_directory + '/eigenvalues2.ht', overwrite=True)
+    pca_evals_checkpoint = eigenvalues_ht.checkpoint(output_directory + '/eigenvalues.ht', overwrite=True)
 
     print("Checkpointing scores...")
-    pca_scores_checkpoint = pca_scores.checkpoint(output_directory + '/scores2.ht', overwrite=True)
+    pca_scores_checkpoint = pca_scores.checkpoint(output_directory + '/scores.ht', overwrite=True)
 
     print("Checkpointing loadings...")
-    pca_loadings_checkpoint = pca_loadings.checkpoint(output_directory + '/loadings2.ht', overwrite=True)
+    pca_loadings_checkpoint = pca_loadings.checkpoint(output_directory + '/loadings.ht', overwrite=True)
 
     return (
         pca_evals_checkpoint,
@@ -48,4 +51,5 @@ def run() -> tuple[hl.Table, hl.Table, hl.Table]:
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     run()
