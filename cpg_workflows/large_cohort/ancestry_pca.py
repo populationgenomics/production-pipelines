5efc162b98c77a0579ae1c5565963c4f19e3c2e4
Michael Harper <michael.harper@populationgenomics.org.au>
2024-09-05 14:19:44 +1000

    Adding logging of sample_qc_ht annotation before and after for debugging

--- cpg_workflows/large_cohort/ancestry_pca.py
+++ cpg_workflows/large_cohort/ancestry_pca.py
@@ -177,8 +177,14 @@ def run(
         n_pcs=n_pcs,
         out_ht_path=out_inferred_pop_ht_path,
     )
+    logging.info('Annotating inferred population labels to sample_qc with background added')
+    logging.info(f'sample_qc_ht.describe(): {sample_qc_ht.describe()}')
+    logging.info(f'sample_qc_ht.show(): {sample_qc_ht.show()}')
     sample_qc_ht.annotate(**pop_ht[sample_qc_ht.key])
     sample_qc_ht.checkpoint(str(out_sample_qc_ht_path), overwrite=True)
+    logging.info('After annotating inferred population labels to sample_qc with background added')
+    logging.info(f'sample_qc_ht.describe(): {sample_qc_ht.describe()}')
+    logging.info(f'sample_qc_ht.show(): {sample_qc_ht.show()}')
     return scores_ht, eigenvalues_ht, loadings_ht, sample_qc_ht
 
 
