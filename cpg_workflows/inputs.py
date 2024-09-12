populationgenomics/production-pipelines

commit 7844996dfacc4da4f13e0c2f6438ce4da6343f91

DatasetStages run once for each Dataset in the MultiCohort (#860)
* Fix: 852 - each MultiCohort has a direct connection to the Datasets it contains, each containing ALL SG IDs in the Dataset for this run. We no longer iterate over Cohorts to set up DatasetStages

* Bump version: 1.25.24 → 1.26.0

cpg_workflows/inputs.py
@@ -40,7 +40,7 @@ def get_multicohort() -> Cohort | MultiCohort:
         raise ValueError('Cannot use both custom_cohort_ids and input_datasets in the same workflow')
 
     # NOTE: When configuring sgs in the config is deprecated, this will be removed.
-    if config_retrieve(['workflow', 'input_cohorts'], []):
+    if custom_cohort_ids:
         if not isinstance(custom_cohort_ids, list):
             raise ValueError('input_cohorts must be a list')
         return actual_get_multicohort()
@@ -61,14 +61,31 @@ def create_multicohort() -> MultiCohort:
     for cohort_id in custom_cohort_ids:
         cohort = multicohort.create_cohort(cohort_id)
         sgs_by_dataset_for_cohort = datasets_by_cohort[cohort_id]
+        # TODO (mwelland): future optimisation following closure of #860
+        # TODO (mwelland): this should be done one Dataset at a time, not per Cohort
+        # TODO (mwelland): ensures the get-analyses-in-dataset query is only made once per Dataset
         _populate_cohort(cohort, sgs_by_dataset_for_cohort, read_pedigree=read_pedigree)
+
+    # now populate the datasets uniquely in the multicohort, each containing all sequencing groups in this dataset
+    # the MultiCohort has a dictionary of {dataset_name: Dataset} objects
+    # each of those Dataset objects links to all SequencingGroups in that Dataset, across all Cohorts
+    for cohort in multicohort.get_cohorts():
+        for dataset in cohort.get_datasets():
+            # is this dataset already in the multicohort, if so use it, otherwise add this one
+            mc_dataset = multicohort.add_dataset(dataset)
+            for sg in dataset.get_sequencing_groups():
+                # add each SG object to the MultiCohort level Dataset
+                mc_dataset.add_sequencing_group_object(sg)
 
     return multicohort
 
 
 def _populate_cohort(cohort: Cohort, sgs_by_dataset_for_cohort, read_pedigree: bool = True):
     """
-    Add datasets in the cohort. There exists only one cohort for the workflow run.
+    Add datasets in the cohort.
+    TODO (mwelland): future optimisation following closure of #860
+    TODO (mwelland): The Cohort object should not care about the Datasets it contains
+    TODO (mwelland): so we can lose a layer of the structure here
     """
     for dataset_name in sgs_by_dataset_for_cohort.keys():
         dataset = cohort.create_dataset(dataset_name)
@@ -80,6 +97,7 @@ def _populate_cohort(cohort: Cohort, sgs_by_dataset_for_cohort, read_pedigree: b
             # phenotypes are managed badly here, need a cleaner way to get them into the SG
             update_dict(metadata, {'phenotypes': entry['sample']['participant'].get('phenotypes', {})})
 
+            # create a SequencingGroup object from its component parts
             sequencing_group = dataset.add_sequencing_group(
                 id=str(entry['id']),
                 external_id=str(entry['sample']['externalId']),
@@ -95,6 +113,9 @@ def _populate_cohort(cohort: Cohort, sgs_by_dataset_for_cohort, read_pedigree: b
     if not cohort.get_datasets():
         raise MetamistError('No datasets populated')
 
+    # TODO (mwelland): future optimisation following closure of #860
+    # TODO (mwelland): this should be done one Dataset at a time, not per Dataset per Cohort
+    # TODO (mwelland): by querying per-dataset per-cohort we increase the number of identical requests
     _populate_analysis(cohort)
     if read_pedigree:
         _populate_pedigree(cohort)
@@ -119,11 +140,18 @@ def deprecated_create_cohort() -> MultiCohort:
         logging.warning('Using dataset will soon be deprecated. Use input_cohorts instead.')
 
     dataset_names = [d for d in dataset_names if d not in skip_datasets]
+
+    # create a MultiCohort object to hold the datasets & cohorts
     multi_cohort = MultiCohort()
+
+    # this is the deprecated entrypoint, so all SG IDs are in the same cohort
+    # maintains ability to run CohortStages and MultiCohortStages without
+    # explicitly requiring workflows to update to MultiCohort
     cohort = multi_cohort.create_cohort(analysis_dataset_name)
 
     for dataset_name in dataset_names:
         dataset = cohort.create_dataset(dataset_name)
+        mc_dataset = multi_cohort.add_dataset(dataset)
         sgs = get_metamist().get_sg_entries(dataset_name)
 
         for entry in sgs:
@@ -132,6 +160,7 @@ def deprecated_create_cohort() -> MultiCohort:
             # phenotypes are managed badly here, need a cleaner way to get them into the SG
             update_dict(metadata, {'phenotypes': entry['sample']['participant'].get('phenotypes', {})})
 
+            # create a SequencingGroup object from its component parts
             sequencing_group = dataset.add_sequencing_group(
                 id=str(entry['id']),
                 external_id=str(entry['sample']['externalId']),
@@ -144,6 +173,10 @@ def deprecated_create_cohort() -> MultiCohort:
 
             _populate_alignment_inputs(sequencing_group, entry)
 
+            # add the same SG Object directly to the MultiCohort level Dataset
+            # this object exists in both the MC.Cohort and MC.Dataset
+            mc_dataset.add_sequencing_group_object(sequencing_group)
+
     if not cohort.get_datasets():
         msg = 'No datasets populated'
         if 'skip_sgs' in config:
@@ -152,6 +185,9 @@ def deprecated_create_cohort() -> MultiCohort:
             msg += ' (after picking sequencing groups)'
         raise MetamistError(msg)
 
+    # TODO (mwelland): future optimisation following closure of #860
+    # TODO (mwelland): _populate_analysis should expect a Dataset, not a Cohort
+    # TODO (mwelland): ensures the get-analyses-in-dataset query is only made once per Dataset
     _populate_analysis(cohort)
     if config.get('read_pedigree', True):
         _populate_pedigree(cohort)

.bumpversion.cfg
@@ -1,5 +1,5 @@
 [bumpversion]
-current_version = 1.25.27
+current_version = 1.26.0
 commit = True
 tag = False
 

.github/workflows/docker.yaml
@@ -15,7 +15,7 @@ permissions:
   contents: read
 
 env:
-  VERSION: 1.25.27
+  VERSION: 1.26.0
 
 jobs:
   docker:

cpg_workflows/metamist.py
@@ -278,6 +278,8 @@ class Metamist:
             raise MetamistError('We only support one cohort at a time currently')
         sequencing_groups = entries['cohorts'][0]['sequencingGroups']
 
+        # TODO (mwelland): future optimisation following closure of #860
+        # TODO (mwelland): return all the SequencingGroups in the Cohort, no need for stratification
         return sort_sgs_by_project(sequencing_groups)
 
     def get_sg_entries(self, dataset_name: str) -> list[dict]:

cpg_workflows/targets.py
@@ -115,6 +115,7 @@ class MultiCohort(Target):
         assert self.name, 'Ensure cohorts or dataset is defined in the config file.'
 
         self._cohorts_by_name: dict[str, Cohort] = {}
+        self._datasets_by_name: dict[str, Dataset] = {}
         self.analysis_dataset = Dataset(name=get_config()['workflow']['dataset'])
 
     def __repr__(self):
@@ -155,25 +156,24 @@ class MultiCohort(Target):
         Gets list of all datasets.
         Include only "active" datasets (unless only_active is False)
         """
-        datasets = []
-        for cohort in self.get_cohorts(only_active):
-            datasets.extend(cohort.get_datasets(only_active))
-        return datasets
+        all_datasets = list(self._datasets_by_name.values())
+        if only_active:
+            all_datasets = [d for d in all_datasets if d.active and d.get_sequencing_groups()]
+        return all_datasets
 
     def get_sequencing_groups(self, only_active: bool = True) -> list['SequencingGroup']:
         """
-        Gets a flat list of all sequencing groups from all cohorts.
+        Gets a flat list of all sequencing groups from all datasets.
+        uses a dictionary to avoid duplicates (we could have the same sequencing group in multiple cohorts)
         Include only "active" sequencing groups (unless only_active is False)
         """
-        all_sequencing_groups = []
-        for cohort in self.get_cohorts(only_active):
-            all_sequencing_groups.extend(cohort.get_sequencing_groups(only_active))
-        return all_sequencing_groups
+        all_sequencing_groups: dict[str, 'SequencingGroup'] = {}
+        for dataset in self.get_datasets(only_active):
+            for sg in dataset.get_sequencing_groups(only_active):
+                all_sequencing_groups[sg.id] = sg
+        return list(all_sequencing_groups.values())
 
-    def create_cohort(
-        self,
-        name: str,
-    ):
+    def create_cohort(self, name: str):
         """
         Create a cohort and add it to the multi-cohort.
         """
@@ -184,6 +184,26 @@ class MultiCohort(Target):
         c = Cohort(name=name, multicohort=self)
         self._cohorts_by_name[c.name] = c
         return c
+
+    def add_dataset(self, d: 'Dataset') -> 'Dataset':
+        """
+        Add a Dataset to the MultiCohort
+        Args:
+            d: Dataset object
+        """
+        if d.name in self._datasets_by_name:
+            logging.debug(f'Dataset {d.name} already exists in the MultiCohort {self.name}')
+        else:
+            self._datasets_by_name[d.name] = d
+        return self._datasets_by_name[d.name]
+
+    def get_dataset_by_name(self, name: str, only_active: bool = True) -> Optional['Dataset']:
+        """
+        Get dataset by name.
+        Include only "active" datasets (unless only_active is False)
+        """
+        ds_by_name = {d.name: d for d in self.get_datasets(only_active)}
+        return ds_by_name.get(name)
 
     def get_job_attrs(self) -> dict:
         """
@@ -298,10 +318,7 @@ class Cohort(Target):
         self._datasets_by_name[dataset.name] = dataset
         return dataset
 
-    def create_dataset(
-        self,
-        name: str,
-    ) -> 'Dataset':
+    def create_dataset(self, name: str) -> 'Dataset':
         """
         Create a dataset and add it to the cohort.
         """
@@ -494,6 +511,17 @@ class Dataset(Target):
         )
         self._sequencing_group_by_id[id] = s
         return s
+
+    def add_sequencing_group_object(self, s: 'SequencingGroup'):
+        """
+        Add a sequencing group object to the dataset.
+        Args:
+            s: SequencingGroup object
+        """
+        if s.id in self._sequencing_group_by_id:
+            logging.debug(f'SequencingGroup {s.id} already exists in the dataset {self.name}')
+            return self._sequencing_group_by_id[s.id]
+        self._sequencing_group_by_id[s.id] = s
 
     def get_sequencing_groups(self, only_active: bool = True) -> list['SequencingGroup']:
         """

setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 setup(
     name='cpg-workflows',
     # This tag is automatically updated by bumpversion
-    version='1.25.27',
+    version='1.26.0',
     description='CPG workflows for Hail Batch',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',

test/stages/__init__.py
@@ -48,19 +48,26 @@ def mock_multidataset_cohort() -> Cohort:
 def mock_multicohort() -> MultiCohort:
     mc = MultiCohort()
 
-    c = mc.create_cohort('CohortA')
-    ds = c.create_dataset('projecta')
-    ds.add_sequencing_group('CPGXXXX', external_id='SAMPLE1')
-    ds.add_sequencing_group('CPGAAAA', external_id='SAMPLE2')
+    # Create a cohort with two datasets
+    cohort_a = mc.create_cohort('CohortA')
+    # Create a dataset in the cohort (legacy)
+    ds = cohort_a.create_dataset('projecta')
+    # Create a dataset in the multicohort (new)
+    dm1 = mc.add_dataset(ds)
+    # Add sequencing groups to the cohort.dataset AND multicohort.dataset
+    dm1.add_sequencing_group_object(ds.add_sequencing_group('CPGXXXX', external_id='SAMPLE1'))
+    dm1.add_sequencing_group_object(ds.add_sequencing_group('CPGAAAA', external_id='SAMPLE2'))
 
-    ds2 = c.create_dataset('projectc')
-    ds2.add_sequencing_group('CPGCCCC', external_id='SAMPLE3')
-    ds2.add_sequencing_group('CPGDDDD', external_id='SAMPLE4')
+    ds2 = cohort_a.create_dataset('projectc')
+    dm2 = mc.add_dataset(ds2)
+    dm2.add_sequencing_group_object(ds2.add_sequencing_group('CPGCCCC', external_id='SAMPLE3'))
+    dm2.add_sequencing_group_object(ds2.add_sequencing_group('CPGDDDD', external_id='SAMPLE4'))
 
-    d = mc.create_cohort('CohortB')
-    ds3 = d.create_dataset('projectb')
-    ds3.add_sequencing_group('CPGEEEEEE', external_id='SAMPLE5')
-    ds3.add_sequencing_group('CPGFFFFFF', external_id='SAMPLE6')
+    cohort_b = mc.create_cohort('CohortB')
+    ds3 = cohort_b.create_dataset('projectb')
+    dm3 = mc.add_dataset(ds3)
+    dm3.add_sequencing_group_object(ds3.add_sequencing_group('CPGEEEEEE', external_id='SAMPLE5'))
+    dm3.add_sequencing_group_object(ds3.add_sequencing_group('CPGFFFFFF', external_id='SAMPLE6'))
 
     return mc
 

test/test_cohort.py
@@ -835,15 +835,16 @@ def test_custom_cohort(mocker: MockFixture, tmp_path, monkeypatch):
 
     from cpg_workflows.inputs import get_multicohort
 
-    cohort = get_multicohort()
+    multicohort = get_multicohort()
 
-    assert cohort
-    assert isinstance(cohort, MultiCohort)
+    assert multicohort
+    assert isinstance(multicohort, MultiCohort)
 
     # Testing Cohort Information
-    assert len(cohort.get_sequencing_groups()) == 2
-    assert cohort.get_sequencing_group_ids() == ['CPGXXXX', 'CPGAAAA']
+    assert len(multicohort.get_sequencing_groups()) == 2
+    assert sorted(multicohort.get_sequencing_group_ids()) == ['CPGAAAA', 'CPGXXXX']
 
     # Test the projects they belong to
-    assert cohort.get_sequencing_groups()[0].dataset.name == 'projecta'
-    assert cohort.get_sequencing_groups()[1].dataset.name == 'projectb'
+    ds_map = {'CPGAAAA': 'projectb', 'CPGXXXX': 'projecta'}
+    for sg in multicohort.get_sequencing_groups():
+        assert sg.dataset.name == ds_map[sg.id]

commit d105f7031bf4fb4d52bafb50b842429160c3ce98

A quick fix for a bug introduced in #860 that comments out populating datasets with all sgs

cpg_workflows/inputs.py
@@ -66,16 +66,18 @@ def create_multicohort() -> MultiCohort:
         # TODO (mwelland): ensures the get-analyses-in-dataset query is only made once per Dataset
         _populate_cohort(cohort, sgs_by_dataset_for_cohort, read_pedigree=read_pedigree)
 
-    # now populate the datasets uniquely in the multicohort, each containing all sequencing groups in this dataset
-    # the MultiCohort has a dictionary of {dataset_name: Dataset} objects
-    # each of those Dataset objects links to all SequencingGroups in that Dataset, across all Cohorts
-    for cohort in multicohort.get_cohorts():
-        for dataset in cohort.get_datasets():
-            # is this dataset already in the multicohort, if so use it, otherwise add this one
-            mc_dataset = multicohort.add_dataset(dataset)
-            for sg in dataset.get_sequencing_groups():
-                # add each SG object to the MultiCohort level Dataset
-                mc_dataset.add_sequencing_group_object(sg)
+    # TODO: (vivbak) Remove this broken block of code to unblock. Temporary measure. Will fix asap.
+
+    # # now populate the datasets uniquely in the multicohort, each containing all sequencing groups in this dataset
+    # # the MultiCohort has a dictionary of {dataset_name: Dataset} objects
+    # # each of those Dataset objects links to all SequencingGroups in that Dataset, across all Cohorts
+    # for cohort in multicohort.get_cohorts():
+    #     for dataset in cohort.get_datasets():
+    #         # is this dataset already in the multicohort, if so use it, otherwise add this one
+    #         mc_dataset = multicohort.add_dataset(dataset)
+    #         for sg in dataset.get_sequencing_groups():
+    #             # add each SG object to the MultiCohort level Dataset
+    #             mc_dataset.add_sequencing_group_object(sg)
 
     return multicohort
 