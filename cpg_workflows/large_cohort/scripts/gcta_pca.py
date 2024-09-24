9eaf0755338d02aeac6ed43dae2a0ed311b96e4d
Michael Harper <michael.harper@populationgenomics.org.au>
2024-09-24 20:27:34 +1000

    Splitting up into two scripts, pca will run as a python job, editing entry point as well

--- cpg_workflows/large_cohort/scripts/gcta_pca.py
+++ cpg_workflows/large_cohort/scripts/gcta_pca.py
@@ -38,42 +38,6 @@ def cli_main():
     )
 
 
-def read_grm_bin(prefix, all_n=False, size=4):
-    def sum_i(i):
-        return sum(range(1, i + 1))
-
-    # File paths
-    bin_file_name = prefix.grm_bin
-    n_file_name = prefix.grm_N_bin
-    id_file_name = prefix.grm_id
-
-    logging.info(f"bin_file_name {bin_file_name}, n_file_name {n_file_name}, id_file_name {id_file_name}")
-    # Read ID file
-    id_data = np.loadtxt(id_file_name, dtype=str)
-    n = id_data.shape[0]
-
-    # Read GRM bin file
-    with open(bin_file_name, "rb") as bin_file:
-        grm = np.fromfile(bin_file, dtype=np.float32 if size == 4 else np.float64, count=n * (n + 1) // 2)
-
-    # Read N bin file
-    with open(n_file_name, "rb") as n_file:
-        if all_n:
-            N = np.fromfile(n_file, dtype=np.float32 if size == 4 else np.float64, count=n * (n + 1) // 2)
-        else:
-            N = struct.unpack('f' if size == 4 else 'd', n_file.read(size))[0]
-
-    # Compute indices for diagonal elements
-    i = np.array([sum_i(j) - 1 for j in range(1, n + 1)])
-
-    return {
-        "diag": grm[i],  # Diagonal elements of GRM
-        "off": np.delete(grm, i),  # Off-diagonal elements
-        "id": id_data,  # ID data
-        "N": N,  # N values
-    }
-
-
 def main(
     dense_mt_path: str,
     output_path: str,
@@ -110,43 +74,17 @@ def main(
     create_GRM_j.command(
         f'gcta --bfile {bfile} --make-grm --out {create_GRM_j.ofile}',
     )
+
+    # Create PCA job
+    py_PCA_j = b.new_python_job('Run PCA')
+    py_PCA_j.call(
+        'run_gcta_pca',
+        output_path=output_path,
+        version=version,
+        n_pcs=n_pcs,
+        relateds_to_drop=relateds_to_drop,
+    )
     b.write_output(create_GRM_j.ofile, f'{output_path}')
-
-    data_resource = b.read_input_group(
-        grm_bin=f'{output_path}.grm.bin',
-        grm_id=f'{output_path}.grm.id',
-        grm_N_bin=f'{output_path}.grm.N.bin',
-    )
-
-    # Run PCA using GCTA
-    run_PCA_j = b.new_job('Run PCA')
-    run_PCA_j.image(image_path('gcta'))
-    run_PCA_j.declare_resource_group(
-        ofile={
-            'eigenvec': '{root}.eigenvec',
-            'eigenval': '{root}.eigenval',
-        },
-    )
-    if relateds_to_drop := hl.read_table(relateds_to_drop):
-        sgids_to_remove = set(relateds_to_drop.s.collect())
-        remove_file = f'{version}.indi.list'
-        remove_flag = f'--remove ${{BATCH_TMPDIR}}/{remove_file}'
-        # write each sg id on new line to a file
-        grm_data = read_grm_bin(data_resource, all_n=True)
-        remove_contents = ''
-        for fam_id, sg_id in grm_data['id']:
-            if sg_id in sgids_to_remove:
-                remove_contents += f'{fam_id}\t{sg_id}\n'
-        collate_relateds_cmd = (
-            f'printf "{remove_contents}" >> ${{BATCH_TMPDIR}}/{remove_file} && cat ${{BATCH_TMPDIR}}/{remove_file}'
-        )
-        run_PCA_j.command(collate_relateds_cmd)
-
-    run_PCA_j.command(
-        f'gcta --grm {create_GRM_j.ofile} {remove_flag if relateds_to_drop else ""} --pca {n_pcs} --out {run_PCA_j.ofile}',
-    )
-
-    b.write_output(run_PCA_j.ofile, f'{output_path}')
     b.run()
 
 
