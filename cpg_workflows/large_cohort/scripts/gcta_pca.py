edfc3bf8593271f39a6207fca978fca00ec28998
Michael Harper <michael.harper@populationgenomics.org.au>
2024-09-24 21:25:46 +1000

    Changing location import

--- cpg_workflows/large_cohort/scripts/gcta_pca.py
+++ cpg_workflows/large_cohort/scripts/gcta_pca.py
@@ -12,7 +12,6 @@ import hail as hl
 
 from cpg_utils.config import get_config, image_path, reference_path
 from cpg_utils.hail_batch import get_batch, init_batch
-from cpg_workflows.large_cohort.scripts import run_gcta_pca
 
 
 def cli_main():
@@ -47,6 +46,7 @@ def main(
     relateds_to_drop: str,
     create_plink: bool | None = False,
 ):
+    from cpg_workflows.large_cohort.scripts import run_gcta_pca
 
     output_path = output_path + f'/{version}'
     dense_mt = hl.read_matrix_table(dense_mt_path)
