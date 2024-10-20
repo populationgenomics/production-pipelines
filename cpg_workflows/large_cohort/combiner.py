c3b06341ba814ddcefcad85693c6b8815f8eb891
Alex Stuckey <alex.stuckey@populationgenomics.org.au>
2024-10-21 12:02:05 +1300

    add can resue

--- cpg_workflows/large_cohort/combiner.py
+++ cpg_workflows/large_cohort/combiner.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING
 from hail.vds import new_combiner
 
 from cpg_utils.hail_batch import init_batch
+from cpg_workflows.utils import can_reuse, to_path
 
 if TYPE_CHECKING:  # TCH002 https://docs.astral.sh/ruff/rules/typing-only-third-party-import/
     from hail.vds.combiner.variant_dataset_combiner import VariantDatasetCombiner
@@ -20,22 +21,23 @@ def run_combiner(
     use_genome_default_intervals: bool = False
     use_exome_default_intervals: bool = False
 
-    init_batch()
+    if not can_reuse(to_path(output_vds_path)):
+        init_batch()
 
-    if sequencing_type == "genome":
-        use_genome_default_intervals = True
-    elif sequencing_type == "exome":
-        use_exome_default_intervals = True
+        if sequencing_type == "genome":
+            use_genome_default_intervals = True
+        elif sequencing_type == "exome":
+            use_exome_default_intervals = True
 
-    combiner: VariantDatasetCombiner = new_combiner(
-        output_path=output_vds_path,
-        gvcf_paths=gvcf_paths,
-        vds_paths=vds_paths,
-        reference_genome="GRCh38",
-        temp_path=tmp_prefix,
-        use_exome_default_intervals=use_exome_default_intervals,
-        use_genome_default_intervals=use_genome_default_intervals,
-        force=True,
-    )
+        combiner: VariantDatasetCombiner = new_combiner(
+            output_path=output_vds_path,
+            gvcf_paths=gvcf_paths,
+            vds_paths=vds_paths,
+            reference_genome="GRCh38",
+            temp_path=tmp_prefix,
+            use_exome_default_intervals=use_exome_default_intervals,
+            use_genome_default_intervals=use_genome_default_intervals,
+            force=True,
+        )
 
-    combiner.run()
+        combiner.run()
