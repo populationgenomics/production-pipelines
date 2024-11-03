d18b3d6da5d7f18538aef23017f87b7f1ba20950
MattWellie <mattwellie@gmail.com>
2024-11-04 08:08:42 +1000

    set a logging string from config

--- cpg_workflows/utils.py
+++ cpg_workflows/utils.py
@@ -21,7 +21,11 @@ from hailtop.batch import ResourceFile
 from cpg_utils import Path, to_path
 from cpg_utils.config import config_retrieve, get_config
 
-DEFAULT_LOG_FORMAT = '%(asctime)s - %(name)s - %(pathname)s: %(lineno)d - %(levelname)s - %(message)s'
+DEFAULT_LOG_FORMAT = config_retrieve(
+    'workflow',
+    'log_format',
+    '%(asctime)s - %(name)s - %(pathname)s: %(lineno)d - %(levelname)s - %(message)s',
+)
 LOGGERS: dict[str, logging.Logger] = {}
 
 
