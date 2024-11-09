7ef9268d9884aa8dc72997edb510f0a882b6c7b0
MattWellie <mattwellie@gmail.com>
2024-11-09 11:25:12 +1000

    take the seqr file directly

--- cpg_workflows/scripts/subset_mt_to_specific_variants.py
+++ cpg_workflows/scripts/subset_mt_to_specific_variants.py
@@ -1,47 +1,66 @@
 #!/usr/bin/env python
 
 """
-Aim here is to take a list of variants, and a MatrixTable, and subset the MatrixTable
-to only include the variants in the list
+Take a dump TSV from seqr containing a list of variants to extract, and a MatrixTable
+Creates a new subset MatrixTable to only include the variants listed in the file
 """
 
 from argparse import ArgumentParser
+from csv import DictReader
 
 import hail as hl
 
-from cpg_utils import Path
-from cpg_utils.config import config_retrieve
+from cpg_utils import to_path
 from cpg_utils.hail_batch import init_batch
 from cpg_workflows.utils import get_logger
 
 logger = get_logger(__file__)
 
 
+def get_variants_from_seqr(seqr_tsv: str) -> list[str]:
+    """
+    Read the seqr file, and return a list of variant IDs
+
+    Args:
+        seqr_tsv (str): path to the seqr file
+
+    Returns:
+        list[list[str | int]]: list of variant components
+    """
+
+    return_components = []
+    with to_path(seqr_tsv).open('r') as handle:
+        reader = DictReader(handle, delimiter='\t')
+        for row in reader:
+            return_components.append(f"chr{row['chrom']}:{row['pos']}:{row['ref']}:{row['alt']}")
+
+    return return_components
+
+
 def cli_main():
     parser = ArgumentParser()
-    parser.add_argument('--input', help='Path to MatrixTable')
+    parser.add_argument('--mt', help='Path to MatrixTable')
+    parser.add_argument('--seqr', help='Path to MatrixTable')
     parser.add_argument('--output', help='Path to write MatrixTable')
     args = parser.parse_args()
 
-    main(mt_path=args.input, out_path=args.output)
+    main(mt_path=args.mt, seqr_tsv=args.seqr, out_path=args.output)
 
 
-def main(mt_path: str, out_path: str):
+def main(mt_path: str, seqr_tsv: str, out_path: str):
     """
     Subset the MatrixTable to the provided list of variants
     Args:
         mt_path (str): cohort-level matrix table from VCF.
+        seqr_tsv (str): path to the seqr dump TSV
         out_path (str): where to write the results.
     """
-    hl.init(default_reference='GRCh38')
-    # init_batch()
+    init_batch()
 
-    # read the list of variants from config, a list of strings, e.g. ['1:100000:A:T', '1:100034:A:C']
-    variant_list = config_retrieve(['variant_list'])
+    # read the list of variants from the seqr dump TSV
+    variant_list = get_variants_from_seqr(seqr_tsv)
 
-    hl_var_list = hl.literal([
-        hl.parse_variant(variant, reference_genome='GRCh38') for variant in variant_list
-    ])
+    hl_var_list = hl.literal([hl.parse_variant(variant, reference_genome='GRCh38') for variant in variant_list])
 
     # read the MatrixTable
     mt = hl.read_matrix_table(mt_path)
