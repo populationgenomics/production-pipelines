7b988bc2293aebaad0b1fbce158386af4c9abdf8
Michael Harper <michael.harper@populationgenomics.org.au>
2024-11-26 11:55:27 +1100

    not using init_batch in gcta_pca.py

--- cpg_workflows/jobs/gcta_PCA.py
+++ cpg_workflows/jobs/gcta_PCA.py
@@ -76,8 +76,6 @@ def run_PCA(
     relateds_to_drop: str,
 ):
 
-    init_batch()
-    b = get_batch()
     # Create PCA job
     run_PCA_j = b.new_job('Run PCA')
     run_PCA_j.image(image_path('gcta'))
