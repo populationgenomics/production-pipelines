b3a15b580adb5f57deb5db902375f814ed609f64
MattWellie <mattwellie@gmail.com>
2024-11-28 15:07:05 +1000

    lint

--- cpg_workflows/scripts/densify_VDS_to_MT.py
+++ cpg_workflows/scripts/densify_VDS_to_MT.py
@@ -18,13 +18,12 @@ from argparse import ArgumentParser
 
 import hail as hl
 
-from gnomad.utils.sparse_mt import default_compute_info
-from gnomad.utils.vcf import adjust_vcf_incompatible_types
-
 from cpg_utils.config import config_retrieve
 from cpg_utils.hail_batch import init_batch
 from cpg_workflows.batch import override_jar_spec
 from cpg_workflows.utils import get_logger
+from gnomad.utils.sparse_mt import default_compute_info
+from gnomad.utils.vcf import adjust_vcf_incompatible_types
 
 
 def main(
