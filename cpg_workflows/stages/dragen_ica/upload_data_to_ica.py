b80e41ef4705bea88fc19ef34f5ea4d6208e5296
Alex Stuckey <alex.stuckey@populationgenomics.org.au>
2024-12-02 11:21:47 +1300

    draft attempt

--- cpg_workflows/stages/dragen_ica/upload_data_to_ica.py
+++ cpg_workflows/stages/dragen_ica/upload_data_to_ica.py
@@ -1,65 +1,100 @@
 import logging
+from io import BytesIO
 
 import icasdk
+import requests
+from google.cloud import storage
 from icasdk.apis.tags import project_data_api
 from icasdk.model.create_data import CreateData
 
+from cpg_utils.cloud import get_path_components_from_gcp_path
+from cpg_utils.config import get_gcp_project
 from cpg_workflows.filetypes import CramPath
 
 
-def clean_partial_upload():
-    pass
+def create_upload_url(
+    upload_api_instance: project_data_api.ProjectDataApi,
+    path_params: dict[str, str],
+    file_id: str,
+) -> str:
+    upload_url_path_params: dict[str, str] = path_params | {'dataId': file_id}
+    query_params = {}
+    try:
+        upload_api_response = upload_api_instance.create_upload_url_for_data(
+            path_params=upload_url_path_params,
+            query_params=query_params,
+        )
+        return upload_api_response.body['url']
+    except icasdk.ApiException as e:
+        raise icasdk.ApiException(f'Exception when calling ProjectDataApi -> create_upload_url_for_data: {e}') from e
 
 
 def check_object_already_exists(
-    configuration: icasdk.Configuration,
+    upload_api_instance: project_data_api.ProjectDataApi,
     path_params: dict[str, str],
     sg_name: str,
-    upload_folder: str,
+    folder_path: str,
 ) -> None:
-    with icasdk.ApiClient(configuration) as object_check_client:
-        object_check_instance = project_data_api.ProjectDataApi(object_check_client)
-        query_params = {
-            'filename': [sg_name],
-            'filenameMatchMode': 'EXACT',
-            'filePath': [f'/{upload_folder}/{sg_name}/'],
-            'filePathMatchMode': 'STARTS_WITH_CASE_INSENSITIVE',
-            'type': 'FILE',
-        }
-        try:
-            object_check_response = object_check_instance.get_project_data_list(
-                path_params=path_params,
-                query_params=query_params,
-            )
-            # No data under this filder / sg_name combo, so ok to proceed
-            if len(object_check_response.body['items']) == 0:
-                pass
-        except icasdk.ApiException as e:
-            raise icasdk.ApiException(f'Exception when calling ProjectDataApi -> get_project_data_list: {e}') from e
+    query_params = {
+        'filename': [sg_name],
+        'filenameMatchMode': 'EXACT',
+        'filePath': [f'/{folder_path}/{sg_name}'],
+        'filePathMatchMode': 'STARTS_WITH_CASE_INSENSITIVE',
+        'type': 'FILE',
+    }
+    try:
+        object_check_response = upload_api_instance.get_project_data_list(
+            path_params=path_params,
+            query_params=query_params,
+        )
+        # No data under this filder / sg_name combo, so ok to proceed
+        if len(object_check_response.body['items']) == 0:
+            return None
+        else:
+            if object_check_response.body['items'][0]['data']['details']['status'] == 'PARTIAL':
+                return object_check_response.body['items'][0]['data']['id']
+    except icasdk.ApiException as e:
+        raise icasdk.ApiException(f'Exception when calling ProjectDataApi -> get_project_data_list: {e}') from e
 
 
 def create_upload_file_id(
-    configuration: icasdk.Configuration,
+    upload_api_instance: project_data_api.ProjectDataApi,
     path_params: dict[str, str],
     sg_name: str,
     upload_folder: str,
 ) -> str:
-    check_object_already_exists(configuration, path_params, sg_name, upload_folder)
-    with icasdk.ApiClient(configuration) as upload_file_id_client:
-        upload_file_id_instance = project_data_api.ProjectDataApi(upload_file_id_client)
+    folder_path: str = f'{get_gcp_project()}/{upload_folder}/'
+    existing_file_id: str | None = None
+    existing_file_id = check_object_already_exists(upload_api_instance, path_params, sg_name, folder_path)
+    if not existing_file_id:
         body = CreateData(
             name=sg_name,
-            folderPath=f'{upload_folder}/',
+            folderPath=folder_path,
             dataType='FILE',
         )
         try:
-            upload_file_id_response = upload_file_id_instance.create_data_in_project(
+            upload_file_id_response = upload_api_instance.create_data_in_project(
                 path_params=path_params,
                 body=body,
             )
             return upload_file_id_response.body['data']['id']
         except icasdk.ApiException as e:
-            raise icasdk.ApiException(f'Exception when calling ProjectDataApi -> create_data_in_project: {e}') from e
+            raise icasdk.ApiException(
+                f'Exception when calling ProjectDataApi -> create_data_in_project: {e}',
+            ) from e
+    return existing_file_id
+
+
+def upload_data(upload_url: str, data_to_upload: str, bucket: str) -> None:
+    storage_client = storage.Client()
+
+    data_stream = BytesIO()
+    gcp_bucket = storage_client.bucket(bucket)
+    blob_to_upload = gcp_bucket.blob(data_to_upload)
+    blob_data: BytesIO = blob_to_upload.download_to_file(data_stream)
+    blob_data.seek(0)
+
+    res = requests.post(upload_url, data=blob_data)
 
 
 def run(sg_name: str, sg_path: CramPath, upload_folder: str, api_root: str, project_id: str, api_key: str):
@@ -68,5 +103,20 @@ def run(sg_name: str, sg_path: CramPath, upload_folder: str, api_root: str, proj
     configuration = icasdk.Configuration(host=api_root)
     configuration.api_key['ApiKeyAuth'] = api_key
     path_parameters: dict[str, str] = {'projectId': project_id}
-    upload_file_id: str = create_upload_file_id(configuration, path_parameters, sg_name, upload_folder)
-    logging.info(upload_file_id)
+
+    # Get the bucket, cram and index information
+    cram_path_components = get_path_components_from_gcp_path(str(sg_path.path))
+    bucket: str = f'{cram_path_components["bucket"]}'
+    cram: str = f'{cram_path_components["dataset"]}/{cram_path_components["subdir"]}/{cram_path_components["file"]}'
+    cram_index: str = (
+        f'{cram_path_components["dataset"]}/{cram_path_components["subdir"]}/{cram_path_components["file"]}.crai'
+    )
+
+    with icasdk.ApiClient(configuration) as upload_api_client:
+        for item in [sg_name, f'{sg_name}.crai']:
+            upload_api_instance = project_data_api.ProjectDataApi(upload_api_client)
+            upload_file_id: str = create_upload_file_id(upload_api_instance, path_parameters, item, upload_folder)
+            upload_url: str = create_upload_url(upload_api_instance, path_parameters, upload_file_id)
+            data_to_upload: str = cram if item.endswith('cram') else cram_index
+
+            upload_data(upload_url, data_to_upload, bucket)
