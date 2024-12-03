316530a89a9316b368d7b8bfdbcb1a403278a11f
MattWellie <mattwellie@gmail.com>
2024-12-03 16:06:57 +1000

    allow for re-use of the MT, if we only want to write new VCF shards

--- cpg_workflows/scripts/densify_VDS_to_MT.py
+++ cpg_workflows/scripts/densify_VDS_to_MT.py
@@ -21,7 +21,7 @@ import hail as hl
 from cpg_utils.config import config_retrieve
 from cpg_utils.hail_batch import init_batch
 from cpg_workflows.batch import override_jar_spec
-from cpg_workflows.utils import get_logger
+from cpg_workflows.utils import get_logger, can_reuse
 from gnomad.utils.sparse_mt import default_compute_info
 from gnomad.utils.vcf import adjust_vcf_incompatible_types
 
@@ -87,7 +87,10 @@ def main(
     get_logger().info('Splitting multiallelics, in a sparse way')
     mt = hl.experimental.sparse_split_multi(mt)
 
-    mt.write(dense_mt_out, overwrite=True)
+    # check here to see if we can reuse the dense MT
+    if not can_reuse(dense_mt_out):
+        get_logger().info(f'Writing fresh data into {dense_mt_out}')
+        mt.write(dense_mt_out, overwrite=True)
 
     if not (sites_only or separate_header):
         return
