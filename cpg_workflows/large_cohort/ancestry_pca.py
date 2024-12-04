6b0a5b0fc1a75b8e1d61119f7e02615a0e291ff7
katiedelange <katiedelange@gmail.com>
2024-12-04 13:05:22 +1300

    Override JAR spec for the ancestry PCA stage. (#1046)
    Co-authored-by: Katie de Lange <katie.delange@populationgenomics.org.au>

--- cpg_workflows/large_cohort/ancestry_pca.py
+++ cpg_workflows/large_cohort/ancestry_pca.py
@@ -9,7 +9,8 @@ from attr import dataclass
 import hail as hl
 
 from cpg_utils import Path, to_path
-from cpg_utils.config import get_config, reference_path
+from cpg_utils.config import config_retrieve, get_config, reference_path
+from cpg_workflows.batch import override_jar_spec
 from cpg_workflows.utils import can_reuse
 from gnomad.sample_qc.ancestry import assign_population_pcs, run_pca_with_relateds
 
@@ -130,6 +131,10 @@ def run(
         'pop': str
         'prob_<pop>': float64 (for each population label)
     """
+
+    if jar_spec := config_retrieve(['workflow', 'jar_spec_revision'], False):
+        override_jar_spec(jar_spec)
+
     min_pop_prob = get_config()['large_cohort']['min_pop_prob']
     n_pcs = get_config()['large_cohort']['n_pcs']
     dense_mt = hl.read_matrix_table(str(dense_mt_path))
