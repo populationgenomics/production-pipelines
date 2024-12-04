8d7a59834e41d350b44b900fc1e1f38ce1dfee45
Michael Harper <michael.harper@populationgenomics.org.au>
2024-12-04 14:58:22 +1100

    Using cpg_utils Batch method for jobs

--- scripts/gcta_standalone.py
+++ scripts/gcta_standalone.py
@@ -7,7 +7,7 @@ import hailtop.batch as hb
 
 from cpg_utils import Path, to_path
 from cpg_utils.config import image_path
-from cpg_utils.hail_batch import get_batch, init_batch
+from cpg_utils.hail_batch import Batch, get_batch, init_batch
 
 
 def make_plink(mt: hl.MatrixTable, plink_output_path: Path):
@@ -31,26 +31,27 @@ def make_plink_job(dense_mt: hl.MatrixTable, plink_output_path: Path) -> hb.batc
 @click.option('--version')
 def main(dense_mt_path: Path, plink_output_path: str, version: str):
     logging.basicConfig(level=logging.INFO)
-
+    b = Batch()
     # Make Plink files
     plink_output_path = to_path(plink_output_path) / version
     logging.info(f'plink_output_path: {plink_output_path}')
 
-    logging.info('Starting batch')
-    init_batch()
-    logging.info('Batch started')
+    # logging.info('Starting batch')
+    # init_batch()
+    # logging.info('Batch started')
     logging.info(f'Loading dense MT from {dense_mt_path}')
     dense_mt = hl.read_matrix_table(dense_mt_path)
     logging.info('Loaded dense MT')
-    plink_result = make_plink(dense_mt, plink_output_path)
+    plink_result = make_plink_job(dense_mt, plink_output_path)
     logging.info('Ran make_plink()')
     logging.info(f'plink_result: {plink_result}')
 
     # Create GRM
 
     # b = get_batch()
-    # logging.info('Writing output')
-    # b.write_output(plink_result, plink_output_path)
+    logging.info('Writing output')
+    b.write_output(plink_result, plink_output_path)
+    b.run()
 
 
 if __name__ == '__main__':
