e6f3e262d57e72e160aa3df916a95992813bc1a8
Michael Harper <michael.harper@populationgenomics.org.au>
2024-12-04 12:32:18 +1100

    Adding logging

--- scripts/gcta_standalone.py
+++ scripts/gcta_standalone.py
@@ -1,3 +1,5 @@
+import logging
+
 import click
 
 import hail as hl
@@ -13,8 +15,10 @@ def make_plink(mt: hl.MatrixTable, plink_output_path: Path):
 
 
 def make_plink_job(dense_mt: hl.MatrixTable, plink_output_path: Path) -> hb.batch._resource.PythonResult:
+    logging.info('Creating plink files')
     make_plink_job = get_batch().new_python_job('Make Plink Files', ({'tool': 'hail query'}))
     make_plink_job.image(image_path('cpg_workflows'))
+    logging.info('Created plink job')
     plink_result = make_plink_job.call(make_plink, dense_mt, plink_output_path)
 
     return plink_result
@@ -24,12 +28,20 @@ def make_plink_job(dense_mt: hl.MatrixTable, plink_output_path: Path) -> hb.batc
 @click.option('--dense-mt-path')
 @click.option('--plink-output-path')
 def main(dense_mt_path: Path, plink_output_path: Path):
+    logging.basicConfig(level=logging.INFO)
 
+    logging.info('Starting batch')
     init_batch()
+    logging.info('Batch started')
+    logging.info(f'Loading dense MT from {dense_mt_path}')
     dense_mt = hl.read_matrix_table(dense_mt_path)
+    logging.info('Loaded dense MT')
     plink_result = make_plink_job(dense_mt, plink_output_path)
+    logging.info('Ran make_plink_job()')
+    logging.info(f'plink_result: {plink_result}')
 
     b = get_batch()
+    logging.info('Writing output')
     b.write_output(plink_result, plink_output_path)
 
 
