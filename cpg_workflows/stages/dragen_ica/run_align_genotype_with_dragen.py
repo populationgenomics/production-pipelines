e935ce3f673c2e25c3efdcadc60c77fb76eff962
Alex Stuckey <alex.stuckey@populationgenomics.org.au>
2024-12-05 10:34:45 +1300

    first draft of dragen stage

---
+++ cpg_workflows/stages/dragen_ica/run_align_genotype_with_dragen.py
@@ -0,0 +1,100 @@
+from typing import Any
+
+import icasdk
+from icasdk.apis.tags import project_analysis_api
+from icasdk.model.analysis_data_input import AnalysisDataInput
+from icasdk.model.analysis_parameter_input import AnalysisParameterInput
+from icasdk.model.analysis_tag import AnalysisTag
+from icasdk.model.create_nextflow_analysis import CreateNextflowAnalysis
+from icasdk.model.nextflow_analysis_input import NextflowAnalysisInput
+
+
+def submit_dragen_run(
+    cram_id: str,
+    cram_index_id: str,
+    dragen_ht_id: str,
+    cram_reference_id: str,
+    dragen_pipeline_id: str,
+    user_tags: list[str],
+    technical_tags: list[str],
+    reference_tags: list[str],
+    user_reference: str,
+    project_id: dict[str, str],
+    api_instance: project_analysis_api.ProjectAnalysisApi,
+):
+    header_params: dict[Any, Any] = {}
+    body = CreateNextflowAnalysis(
+        userReference=user_reference,
+        pipelineId=dragen_pipeline_id,
+        tags=AnalysisTag(
+            technicalTags=technical_tags,
+            userTags=user_tags,
+            referenceTags=reference_tags,
+        ),
+        outputParentFolderId='',
+        analysisInput=NextflowAnalysisInput(
+            inputs=[
+                AnalysisDataInput(parameterCode='crams', dataIds=[cram_id]),
+                AnalysisDataInput(parameterCode='crais', dataIds=[cram_index_id]),
+                AnalysisDataInput(parameterCode='ref_tar', dataIds=[dragen_ht_id]),
+                AnalysisDataInput(parameterCode='cram_reference', dataIds=[cram_reference_id]),
+            ],
+            parameters=[
+                AnalysisParameterInput(
+                    code='enable_map_align',
+                    value='True',
+                ),
+                AnalysisParameterInput(code='enable_map_align_output', value='True'),
+                AnalysisParameterInput(code='output_format', value='CRAM'),
+                AnalysisParameterInput(code='enable_duplicate_marking', value='True'),
+                AnalysisParameterInput(code='enable_variant_caller', value='True'),
+                AnalysisParameterInput(code='vc_emit_reference_confidence', value='GVCF'),
+                AnalysisParameterInput(code='vc_enable_vcf_output', value='True'),
+                AnalysisParameterInput(code='enable_cnv', value='True'),
+                AnalysisParameterInput(code='enable_sv', value='True'),
+            ],
+        ),
+    )
+    try:
+        api_response = api_instance.create_nextflow_analysis(
+            path_params=project_id,
+            header_params=header_params,
+            body=body,
+        )
+        print(api_response)
+    except icasdk.ApiException as e:
+        raise icasdk.ApiException(f'Exception when calling ProjectAnalysisApi->create_nextflow_analysis: {e}') from e
+
+
+def run(
+    cram_id: str,
+    cram_index_id: str,
+    dragen_ht_id: str,
+    cram_reference_id: str,
+    dragen_pipeline_id: str,
+    user_tags: list[str],
+    technical_tags: list[str],
+    reference_tags: list[str],
+    user_reference: str,
+    api_root: str,
+    project_id: str,
+    api_key: str,
+) -> None:
+    configuration = icasdk.Configuration(host=api_root, api_key=api_key)
+
+    with icasdk.ApiClient(configuration=configuration) as api_client:
+        api_instance = project_analysis_api.ProjectAnalysisApi(api_client)
+        path_params: dict[str, str] = {'projectID': project_id}
+        submit_dragen_run(
+            cram_id=cram_id,
+            cram_index_id=cram_index_id,
+            dragen_ht_id=dragen_ht_id,
+            cram_reference_id=cram_reference_id,
+            dragen_pipeline_id=dragen_pipeline_id,
+            user_tags=user_tags,
+            technical_tags=technical_tags,
+            reference_tags=reference_tags,
+            user_reference=user_reference,
+            project_id=path_params,
+            api_instance=api_instance,
+        )
--- cpg_workflows/stages/realign_genotype_with_dragen.py
+++ cpg_workflows/stages/realign_genotype_with_dragen.py
@@ -10,7 +10,7 @@ import cpg_utils
 from cpg_utils.cloud import get_path_components_from_gcp_path
 from cpg_utils.config import config_retrieve, get_access_level, image_path
 from cpg_utils.hail_batch import get_batch
-from cpg_workflows.stages.dragen_ica import upload_data_to_ica
+from cpg_workflows.stages.dragen_ica import run_align_genotype_with_dragen, upload_data_to_ica
 from cpg_workflows.targets import SequencingGroup
 from cpg_workflows.workflow import SequencingGroupStage, StageInput, StageOutput, stage
 
@@ -49,7 +49,7 @@ coloredlogs.install(level=logging.INFO)
 
 @stage(analysis_type='ica_data_upload', analysis_keys=['cram', 'cram_index'])
 class UploadDataToIca(SequencingGroupStage):
-    from cpg_workflows.stages.dragen_ica import upload_data_to_ica
+    # from cpg_workflows.stages.dragen_ica import upload_data_to_ica
 
     def calculate_needed_storage(
         self,
@@ -65,12 +65,12 @@ class UploadDataToIca(SequencingGroupStage):
 
     def expected_outputs(self, sequencing_group: SequencingGroup) -> dict[str, cpg_utils.Path]:
         output_dict: dict[str, cpg_utils.Path] = {
-            'cram': cpg_utils.to_path(
+            'cram_id': cpg_utils.to_path(
                 f'gs://cpg-{sequencing_group.dataset.name.replace("-test", "")}-{get_access_level()}',
             )
             / GCP_FOLDER_FOR_ICA_UPLOAD
             / f'{sequencing_group.name}.cram_ica_file_id',
-            'cram_index': cpg_utils.to_path(
+            'cram_index_id': cpg_utils.to_path(
                 f'gs://cpg-{sequencing_group.dataset.name.replace("-test", "")}-{get_access_level()}',
             )
             / GCP_FOLDER_FOR_ICA_UPLOAD
@@ -85,8 +85,8 @@ class UploadDataToIca(SequencingGroupStage):
         bucket_name = cram_path_components['bucket']
 
         upload_job: PythonJob = get_batch().new_python_job(
-            'UploadDataToIca',
-            (self.get_job_attrs() or {}) | {'tool': 'ICA'},
+            name='UploadDataToIca',
+            attributes=(self.get_job_attrs() or {}) | {'tool': 'ICA'},
         )
         upload_job.image(image=image_path('cpg_workflows'))
 
@@ -114,8 +114,48 @@ class AlignGenotypeWithDragen(SequencingGroupStage):
     ) -> None:
         pass
 
+    def read_blob_contents(self, full_blob_path: str) -> str:
+        path_components: dict[str, str] = get_path_components_from_gcp_path(full_blob_path)
+        gcp_bucket: str = path_components['bucket']
+        blob_path: str = f'{path_components["suffix"]}{path_components["file"]}'
+        storage_client = storage.Client()
+        blob_client = storage.Blob(name=blob_path, bucket=storage_client.bucket(bucket_name=gcp_bucket))
+        return blob_client.download_as_text()
+
     def queue_jobs(self, sequencing_group: SequencingGroup, inputs: StageInput) -> StageOutput | None:
-        logging.info(f'{inputs}')
+        cram_id: str = self.read_blob_contents(inputs.as_str(sequencing_group, UploadDataToIca, 'cram_id'))
+        cram_index_id: str = self.read_blob_contents(inputs.as_str(sequencing_group, UploadDataToIca, 'cram_index_id'))
+
+        dragen_pipeline_id = config_retrieve(['ica.pipelines', 'dragen_3_7_8'])
+
+        dragen_ht_id: str = config_retrieve(['ica.reference_ids', 'dragen_ht_id'])
+        cram_reference_id: str = config_retrieve(['ica.reference_ids', 'cram_reference_id'])
+
+        user_tags: list[str] = config_retrieve(['ica.tags', 'user_tags'])
+        technical_tags: list[str] = config_retrieve(['ica.tags', 'technical_tags'])
+        reference_tags: list[str] = config_retrieve(['ica.tags', 'reference_tags'])
+        user_reference: str = config_retrieve(['ica.tags', 'user_reference'])
+
+        align_genotype_job = get_batch().new_python_job(
+            name='AlignGenotypeWithDragen',
+            attributes=(self.get_job_attrs() or {}) | {'tool': 'ICA'},
+        )
+        align_genotype_job.image(image=image_path('cpg_workflows'))
+        align_genotype_job.call(
+            run_align_genotype_with_dragen.run,
+            cram_id=cram_id,
+            cram_index_id=cram_index_id,
+            dragen_ht_id=dragen_ht_id,
+            cram_reference_id=cram_reference_id,
+            dragen_pipeline_id=dragen_pipeline_id,
+            user_tags=user_tags,
+            technical_tags=technical_tags,
+            reference_tags=reference_tags,
+            user_reference=user_reference,
+            api_root=ICA_REST_ENDPOINT,
+            project_id=ICA_PROJECT,
+            api_key=API_KEY,
+        )
 
 
 @stage(analysis_type='dragen_mlr', required_stages=[AlignGenotypeWithDragen])
