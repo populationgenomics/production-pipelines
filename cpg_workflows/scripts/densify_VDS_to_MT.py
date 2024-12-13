5bcd771829a1ba6f40ee1a1a0967cffabe59631a
Matt Welland <mattwellie@gmail.com>
2024-12-13 14:08:51 +1000

    RD Combiner: 1 (#1030)
    * Combine gVCFs and Densify to MT
    
    * RD Combiner: 2 - gcloud manifest condenser (#1031)
    
    * RD Combiner: 3 - initial VQSR training stages (#1032)
    
    * RD Combiner: 4 - Scattered per-fragment VQSR SNP tranche training job (#1033)
    
    * RD Combiner: 5 - Gather tranches into a single file(#1034)
    
    * RD Combiner: 6 - Run trained VQSR model on SNP fragments (#1035)
    
    * RD Combiner: 7 - Run trained VQSR model on Indels, genome-wide (#1036)
    
    * RD Combiner: 8 - Adds fragmented VEP annotation, building into a single Hail Table (#1037)
    
    * RD Combiner: 9 - AnnotateCohort as a standalone script (#1038)
    
    * RD Combiner: 10 -MT Subsetting script (#1039)
    
    * RD Combiner: 11 - AnnotateDataset as a standalone script (#1040)
    
    * RD Combiner: Optimisations (#1041)
    
    * remove align_and_genotype mini-workflow (#1064)
    
    * Bump version: 1.31.7 → 1.32.0

--- cpg_workflows/scripts/densify_VDS_to_MT.py
+++ cpg_workflows/scripts/densify_VDS_to_MT.py
@@ -4,6 +4,14 @@
 takes a path to a VDS, and an output path
 densifies the VDS into a MatrixTable
 Writes the MT to the output path
+
+Additional arguments:
+    --sites_only: optional, if used write a sites-only VCF directory to this location
+                  it's far more efficient for Hail to write out a VCF per-partition, rather than a single VCF
+                  this also prevents the need for writing out a single VCF, then fragmenting that to run VEP, VQSR
+    --separate_header: optional, if used write a sites-only VCF directory with a separate header to this location
+                  this makes the VCF write more efficient, and is easily rejoined into a single VCF using compose
+                  for situations where we need a single whole-genome/exome VCF (e.g. VQSR training)
 """
 
 from argparse import ArgumentParser
@@ -13,14 +21,16 @@ import hail as hl
 from cpg_utils.config import config_retrieve
 from cpg_utils.hail_batch import init_batch
 from cpg_workflows.batch import override_jar_spec
-from cpg_workflows.utils import get_logger
+from cpg_workflows.utils import can_reuse, get_logger
+from gnomad.utils.sparse_mt import default_compute_info
+from gnomad.utils.vcf import adjust_vcf_incompatible_types
 
 
 def main(
     vds_in: str,
     dense_mt_out: str,
-    partitions: int | None = None,
     sites_only: str | None = None,
+    separate_header: str | None = None,
 ) -> None:
     """
     Load a sparse VariantDataset
@@ -30,40 +40,74 @@ def main(
     Args:
         vds_in (str):
         dense_mt_out (str):
-        partitions (int): if specified, write data as this many partitions
         sites_only (str): optional, if used write a sites-only VCF directory to this location
+        separate_header (str): optional, if used write a sites-only VCF directory with a separate header to this location
     """
     init_batch()
 
     # if we need to manually specify a non-standard Hail QoB JAR file
-    if jar_spec := config_retrieve(['workflow', 'jar_spec_revision'], False):
+    if jar_spec := config_retrieve(['rd_combiner', 'densify', 'jar_spec_revision'], False):
         override_jar_spec(jar_spec)
 
     vds = hl.vds.read_vds(vds_in)
 
-    get_logger(__file__).info('Splitting multiallelics')
-    vds = hl.vds.split_multi(vds)
-
-    get_logger(__file__).info('Densifying data...')
+    get_logger().info('Densifying data...')
     mt = hl.vds.to_dense_mt(vds)
 
+    # taken from _filter_rows_and_add_tags in large_cohort/site_only_vcf.py
     # remove any monoallelic or non-ref-in-any-sample sites
-    mt = mt.filter_rows((hl.len(mt.alleles) > 1) & (hl.agg.any(mt.GT.is_non_ref())))
+    mt = mt.filter_rows((hl.len(mt.alleles) > 1) & (hl.agg.any(mt.LGT.is_non_ref())))
 
-    # naive coalesce just lumps adjacent partitions together, so this could create wildly unbalanced partitions
-    # if this is not used we'll retain the original partitions
-    if partitions:
-        mt = mt.naive_coalesce(partitions)
+    # annotate site-level DP to avoid name collision
+    mt = mt.annotate_rows(
+        site_dp=hl.agg.sum(mt.DP),
+        ANS=hl.agg.count_where(hl.is_defined(mt.LGT)) * 2,
+    )
 
-    # by default, drop the GVCF info
-    if "gvcf_info" in mt.entry:
-        mt = mt.drop('gvcf_info')
+    # content shared with large_cohort.site_only_vcf.py
+    info_ht = default_compute_info(mt, site_annotations=True, n_partitions=mt.n_partitions())
+    info_ht = info_ht.annotate(info=info_ht.info.annotate(DP=mt.rows()[info_ht.key].site_dp))
 
-    mt.write(dense_mt_out, overwrite=True)
+    info_ht = adjust_vcf_incompatible_types(
+        info_ht,
+        # with default INFO_VCF_AS_PIPE_DELIMITED_FIELDS, AS_VarDP will be converted
+        # into a pipe-delimited value e.g.: VarDP=|132.1|140.2
+        # which breaks VQSR parser (it doesn't recognise the delimiter and treats
+        # it as an array with a single string value "|132.1|140.2", leading to
+        # an IndexOutOfBound exception when trying to access value for second allele)
+        pipe_delimited_annotations=[],
+    )
 
+    # annotate this info back into the main MatrixTable
+    mt = mt.annotate_rows(info=info_ht[mt.row_key].info)
+
+    # unpack mt.info.info back into mt.info. Must be better syntax for this?
+    mt = mt.drop('gvcf_info')
+
+    get_logger().info('Splitting multiallelics, in a sparse way')
+    mt = hl.experimental.sparse_split_multi(mt)
+
+    # check here to see if we can reuse the dense MT
+    if not can_reuse(dense_mt_out):
+        get_logger().info(f'Writing fresh data into {dense_mt_out}')
+        mt.write(dense_mt_out, overwrite=True)
+
+    if not (sites_only or separate_header):
+        return
+
+    # read the dense MT and obtain the sites-only HT
+    mt = hl.read_matrix_table(dense_mt_out)
+    sites_only_ht = mt.rows()
+
+    # write a directory containing all the per-partition VCF fragments, each with a VCF header
     if sites_only:
-        mt = hl.read_matrix_table(dense_mt_out)
-        hl.export_vcf(mt, sites_only, tabix=True, parallel='header_per_shard')
+        get_logger().info('Writing sites-only VCF, header-per-shard')
+        hl.export_vcf(sites_only_ht, sites_only, tabix=True, parallel='header_per_shard')
+
+    # write a directory containing all the per-partition VCF fragments, with a separate VCF header file
+    if separate_header:
+        get_logger().info('Writing sites-only VCF, separate-header')
+        hl.export_vcf(sites_only_ht, separate_header, tabix=True, parallel='separate_header')
 
 
 def cli_main():
@@ -79,20 +123,19 @@ def cli_main():
         required=True,
     )
     parser.add_argument(
-        '--partitions',
-        help='Number of partitions to write the MT as',
-        type=int,
-    )
-    parser.add_argument(
         '--sites_only',
         help='Specify an output path for a sites-only VCF, or None',
+    )
+    parser.add_argument(
+        '--separate_header',
+        help='Specify an output path for a sites-only VCF, with a separate header file, or None',
     )
     args = parser.parse_args()
     main(
         vds_in=args.input,
         dense_mt_out=args.output,
-        partitions=args.partitions,
         sites_only=args.sites_only,
+        separate_header=args.separate_header,
     )
 
 
--- main.py
+++ main.py
@@ -24,7 +24,18 @@ from cpg_workflows.stages.happy_validation import ValidationHappyOnVcf, Validati
 from cpg_workflows.stages.large_cohort import AncestryPlots, Frequencies, LoadVqsr
 from cpg_workflows.stages.mito import MitoReport
 from cpg_workflows.stages.outrider import Outrider
-from cpg_workflows.stages.rd_combiner import DenseMTFromVDS, GVCFCombiner
+from cpg_workflows.stages.rd_combiner import (
+    AnnotateCohortSmallVariantsWithHailQuery,
+    AnnotateDatasetSmallVariantsWithHailQuery,
+    AnnotateFragmentedVcfWithVep,
+    ConcatenateVcfFragmentsWithGcloud,
+    CreateDenseMtFromVdsWithHail,
+    CreateVdsFromGvcfsWithHailCombiner,
+    GatherTrainedVqsrSnpTranches,
+    RunTrainedIndelVqsrOnCombinedVcf,
+    SubsetMatrixTableToDatasetUsingHailQuery,
+    TrainVqsrSnpTranches,
+)
 from cpg_workflows.stages.seqr_loader import AnnotateDataset, DatasetVCF, MtToEs
 from cpg_workflows.stages.seqr_loader_long_read.bam_to_cram import BamToCram
 from cpg_workflows.stages.seqr_loader_long_read.long_read_snps_indels_annotation import MtToEsLrSNPsIndels
@@ -40,7 +51,18 @@ WORKFLOWS: dict[str, list[StageDecorator]] = {
     'long_read_snps_indels_annotation': [MtToEsLrSNPsIndels],
     'long_read_sv_annotation': [MtToEsLrSv],
     'pre_alignment': [FastQCMultiQC],
-    'rd_combiner': [GVCFCombiner, DenseMTFromVDS],
+    'rd_combiner': [
+        CreateVdsFromGvcfsWithHailCombiner,
+        CreateDenseMtFromVdsWithHail,
+        ConcatenateVcfFragmentsWithGcloud,
+        GatherTrainedVqsrSnpTranches,
+        TrainVqsrSnpTranches,
+        RunTrainedIndelVqsrOnCombinedVcf,
+        AnnotateFragmentedVcfWithVep,
+        AnnotateCohortSmallVariantsWithHailQuery,
+        SubsetMatrixTableToDatasetUsingHailQuery,
+        AnnotateDatasetSmallVariantsWithHailQuery,
+    ],
     'seqr_loader': [
         DatasetVCF,
         AnnotateDataset,
