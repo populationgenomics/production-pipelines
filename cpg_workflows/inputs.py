3f682cf15ec37eb657fec78377e2db2a672f5b0e
Milo Hyben <milo.hyben@gmail.com>
2024-12-16 11:16:57 +1100

    Removing test suffix from dataset names for multicohorts.

--- cpg_workflows/inputs.py
+++ cpg_workflows/inputs.py
@@ -105,7 +105,7 @@ def _populate_cohort(cohort: Cohort, sgs_by_dataset_for_cohort, read_pedigree: b
     TODO (mwelland): so we can lose a layer of the structure here
     """
     for dataset_name in sgs_by_dataset_for_cohort.keys():
-        dataset = cohort.create_dataset(dataset_name)
+        dataset = cohort.create_dataset(dataset_name[:-5] if dataset_name.endswith('-test') else dataset_name)
         sgs = sgs_by_dataset_for_cohort[dataset_name]
 
         for entry in sgs:
