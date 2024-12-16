b91106946af526a0aae7a87982b19f561cd34665
Alex Stuckey <alex.stuckey@populationgenomics.org.au>
2024-12-16 13:25:53 +1300

    just test this bit

--- cpg_workflows/stages/realign_genotype_with_dragen.py
+++ cpg_workflows/stages/realign_genotype_with_dragen.py
@@ -156,186 +156,186 @@ class UploadDataToIca(SequencingGroupStage):
         return self.make_outputs(sequencing_group, self.expected_outputs(sequencing_group), jobs=upload_job)
 
 
-@stage(required_stages=[PrepareIcaForDragenAnalysis, UploadDataToIca])
-class AlignGenotypeWithDragen(SequencingGroupStage):
-    # Output object with pipeline ID to GCP
-    def expected_outputs(
-        self,
-        sequencing_group: SequencingGroup,
-    ) -> cpg_utils.Path:
-        sg_bucket: cpg_utils.Path = sequencing_group.dataset.prefix()
-        return sg_bucket / GCP_FOLDER_FOR_RUNNING_PIPELINE / f'{sequencing_group.name}_pipeline_id'
+# @stage(required_stages=[PrepareIcaForDragenAnalysis, UploadDataToIca])
+# class AlignGenotypeWithDragen(SequencingGroupStage):
+#     # Output object with pipeline ID to GCP
+#     def expected_outputs(
+#         self,
+#         sequencing_group: SequencingGroup,
+#     ) -> cpg_utils.Path:
+#         sg_bucket: cpg_utils.Path = sequencing_group.dataset.prefix()
+#         return sg_bucket / GCP_FOLDER_FOR_RUNNING_PIPELINE / f'{sequencing_group.name}_pipeline_id'
 
-    def queue_jobs(self, sequencing_group: SequencingGroup, inputs: StageInput) -> StageOutput | None:
-        dragen_pipeline_id = config_retrieve(['ica', 'pipelines', 'dragen_3_7_8'])
-        dragen_ht_id: str = config_retrieve(['ica', 'pipelines', 'dragen_ht_id'])
+#     def queue_jobs(self, sequencing_group: SequencingGroup, inputs: StageInput) -> StageOutput | None:
+#         dragen_pipeline_id = config_retrieve(['ica', 'pipelines', 'dragen_3_7_8'])
+#         dragen_ht_id: str = config_retrieve(['ica', 'pipelines', 'dragen_ht_id'])
 
-        # Get the correct CRAM reference ID based off the choice made in the config
-        cram_reference_id: str = config_retrieve(
-            ['ica', 'cram_references', config_retrieve(['ica', 'cram_references', 'old_cram_reference'])],
-        )
+#         # Get the correct CRAM reference ID based off the choice made in the config
+#         cram_reference_id: str = config_retrieve(
+#             ['ica', 'cram_references', config_retrieve(['ica', 'cram_references', 'old_cram_reference'])],
+#         )
 
-        user_tags: list[str] = config_retrieve(['ica', 'tags', 'user_tags'])
-        technical_tags: list[str] = config_retrieve(['ica', 'tags', 'technical_tags'])
-        reference_tags: list[str] = config_retrieve(['ica', 'tags', 'reference_tags'])
-        user_reference: str = config_retrieve(['ica', 'tags', 'user_reference'])
+#         user_tags: list[str] = config_retrieve(['ica', 'tags', 'user_tags'])
+#         technical_tags: list[str] = config_retrieve(['ica', 'tags', 'technical_tags'])
+#         reference_tags: list[str] = config_retrieve(['ica', 'tags', 'reference_tags'])
+#         user_reference: str = config_retrieve(['ica', 'tags', 'user_reference'])
 
-        align_genotype_job: PythonJob = get_batch().new_python_job(
-            name='AlignGenotypeWithDragen',
-            attributes=(self.get_job_attrs() or {}) | {'tool': 'Dragen'},
-        )
-        align_genotype_job.image(image=image_path('cpg_workflows'))
-        align_genotype_job.call(
-            run_align_genotype_with_dragen.run,
-            cram_name=sequencing_group.name,
-            cram_id_path=str(
-                inputs.as_path(target=sequencing_group, stage=UploadDataToIca, key='cram_upload_success'),
-            ),
-            cram_index_id_path=str(
-                inputs.as_path(target=sequencing_group, stage=UploadDataToIca, key='crai_upload_success'),
-            ),
-            dragen_ht_id=dragen_ht_id,
-            cram_reference_id=cram_reference_id,
-            dragen_pipeline_id=dragen_pipeline_id,
-            ica_output_folder_id_path=str(
-                inputs.as_path(target=sequencing_group, stage=PrepareIcaForDragenAnalysis, key='analysis_output_fid'),
-            ),
-            user_tags=user_tags,
-            technical_tags=technical_tags,
-            reference_tags=reference_tags,
-            user_reference=user_reference,
-            gcp_bucket=get_path_components_from_gcp_path(path=str(sequencing_group.cram))['bucket'],
-            pipeline_registration_path=GCP_FOLDER_FOR_RUNNING_PIPELINE,
-            api_root=ICA_REST_ENDPOINT,
-        )
-        return self.make_outputs(
-            target=sequencing_group,
-            data=self.expected_outputs(sequencing_group=sequencing_group),
-            jobs=align_genotype_job,
-        )
-
-
-@stage(analysis_type='dragen_align_genotype', required_stages=[PrepareIcaForDragenAnalysis, AlignGenotypeWithDragen])
-class MonitorAlignGenotypeWithDragen(SequencingGroupStage):
-    def expected_outputs(self, sequencing_group: SequencingGroup) -> cpg_utils.Path:
-        sg_bucket: cpg_utils.Path = sequencing_group.dataset.prefix()
-        return sg_bucket / GCP_FOLDER_FOR_RUNNING_PIPELINE / f'{sequencing_group.name}_pipeline_success'
-
-    def queue_jobs(self, sequencing_group: SequencingGroup, inputs: StageInput) -> StageOutput | None:
-        ica_pipeline_id_path: str = str(inputs.as_path(target=sequencing_group, stage=AlignGenotypeWithDragen))
-        ica_output_folder_id_path: str = str(
-            inputs.as_path(target=sequencing_group, stage=PrepareIcaForDragenAnalysis, key='analysis_output_fid'),
-        )
-
-        monitor_pipeline_run: PythonJob = get_batch().new_python_job(
-            name='MonitorAlignGenotypeWithDragen',
-            attributes=(self.get_job_attrs() or {}) | {'tool': 'Dragen'},
-        )
-
-        monitor_pipeline_run.image(image=image_path('cpg_workflows'))
-        monitor_pipeline_run.call(
-            monitor_align_genotype_with_dragen.run,
-            ica_pipeline_id_path=ica_pipeline_id_path,
-            ica_output_folder_id_path=ica_output_folder_id_path,
-            api_root=ICA_REST_ENDPOINT,
-            gcp_bucket=get_path_components_from_gcp_path(path=str(sequencing_group.cram))['bucket'],
-            sg_name=sequencing_group.name,
-            pipeline_registration_path=GCP_FOLDER_FOR_RUNNING_PIPELINE,
-        )
-        return self.make_outputs(
-            target=sequencing_group,
-            data=self.expected_outputs(sequencing_group=sequencing_group),
-            jobs=monitor_pipeline_run,
-        )
+#         align_genotype_job: PythonJob = get_batch().new_python_job(
+#             name='AlignGenotypeWithDragen',
+#             attributes=(self.get_job_attrs() or {}) | {'tool': 'Dragen'},
+#         )
+#         align_genotype_job.image(image=image_path('cpg_workflows'))
+#         align_genotype_job.call(
+#             run_align_genotype_with_dragen.run,
+#             cram_name=sequencing_group.name,
+#             cram_id_path=str(
+#                 inputs.as_path(target=sequencing_group, stage=UploadDataToIca, key='cram_upload_success'),
+#             ),
+#             cram_index_id_path=str(
+#                 inputs.as_path(target=sequencing_group, stage=UploadDataToIca, key='crai_upload_success'),
+#             ),
+#             dragen_ht_id=dragen_ht_id,
+#             cram_reference_id=cram_reference_id,
+#             dragen_pipeline_id=dragen_pipeline_id,
+#             ica_output_folder_id_path=str(
+#                 inputs.as_path(target=sequencing_group, stage=PrepareIcaForDragenAnalysis, key='analysis_output_fid'),
+#             ),
+#             user_tags=user_tags,
+#             technical_tags=technical_tags,
+#             reference_tags=reference_tags,
+#             user_reference=user_reference,
+#             gcp_bucket=get_path_components_from_gcp_path(path=str(sequencing_group.cram))['bucket'],
+#             pipeline_registration_path=GCP_FOLDER_FOR_RUNNING_PIPELINE,
+#             api_root=ICA_REST_ENDPOINT,
+#         )
+#         return self.make_outputs(
+#             target=sequencing_group,
+#             data=self.expected_outputs(sequencing_group=sequencing_group),
+#             jobs=align_genotype_job,
+#         )
 
 
-@stage(analysis_type='dragen_mlr', required_stages=[AlignGenotypeWithDragen])
-class GvcfMlrWithDragen(SequencingGroupStage):
-    def expected_outputs(
-        self,
-        sequencing_group: SequencingGroup,
-    ) -> None:
-        pass
+# @stage(analysis_type='dragen_align_genotype', required_stages=[PrepareIcaForDragenAnalysis, AlignGenotypeWithDragen])
+# class MonitorAlignGenotypeWithDragen(SequencingGroupStage):
+#     def expected_outputs(self, sequencing_group: SequencingGroup) -> cpg_utils.Path:
+#         sg_bucket: cpg_utils.Path = sequencing_group.dataset.prefix()
+#         return sg_bucket / GCP_FOLDER_FOR_RUNNING_PIPELINE / f'{sequencing_group.name}_pipeline_success'
 
-    def queue_jobs(self, sequencing_group: SequencingGroup, inputs: StageInput) -> StageOutput | None:
-        pass
+#     def queue_jobs(self, sequencing_group: SequencingGroup, inputs: StageInput) -> StageOutput | None:
+#         ica_pipeline_id_path: str = str(inputs.as_path(target=sequencing_group, stage=AlignGenotypeWithDragen))
+#         ica_output_folder_id_path: str = str(
+#             inputs.as_path(target=sequencing_group, stage=PrepareIcaForDragenAnalysis, key='analysis_output_fid'),
+#         )
 
+#         monitor_pipeline_run: PythonJob = get_batch().new_python_job(
+#             name='MonitorAlignGenotypeWithDragen',
+#             attributes=(self.get_job_attrs() or {}) | {'tool': 'Dragen'},
+#         )
 
-@stage(required_stages=[GvcfMlrWithDragen])
-class MonitorGvcfMlrWithDragen(SequencingGroupStage):
-    def expected_outputs(
-        self,
-        sequencing_group: SequencingGroup,
-    ) -> None:
-        pass
-
-    def queue_jobs(self, sequencing_group: SequencingGroup, inputs: StageInput) -> StageOutput | None:
-        pass
-
-
-@stage(required_stages=[AlignGenotypeWithDragen, GvcfMlrWithDragen])
-class CancelIcaPipelineRun(SequencingGroupStage):
-    def expected_outputs(
-        self,
-        sequencing_group: SequencingGroup,
-    ) -> None:
-        pass
-
-    def queue_jobs(self, sequencing_group: SequencingGroup, inputs: StageInput) -> StageOutput | None:
-        pass
+#         monitor_pipeline_run.image(image=image_path('cpg_workflows'))
+#         monitor_pipeline_run.call(
+#             monitor_align_genotype_with_dragen.run,
+#             ica_pipeline_id_path=ica_pipeline_id_path,
+#             ica_output_folder_id_path=ica_output_folder_id_path,
+#             api_root=ICA_REST_ENDPOINT,
+#             gcp_bucket=get_path_components_from_gcp_path(path=str(sequencing_group.cram))['bucket'],
+#             sg_name=sequencing_group.name,
+#             pipeline_registration_path=GCP_FOLDER_FOR_RUNNING_PIPELINE,
+#         )
+#         return self.make_outputs(
+#             target=sequencing_group,
+#             data=self.expected_outputs(sequencing_group=sequencing_group),
+#             jobs=monitor_pipeline_run,
+#         )
 
 
-@stage(
-    analysis_type='ica_data_download',
-    required_stages=[PrepareIcaForDragenAnalysis, MonitorAlignGenotypeWithDragen, GvcfMlrWithDragen],
-)
-class DownloadDataFromIca(SequencingGroupStage):
-    def expected_outputs(
-        self,
-        sequencing_group: SequencingGroup,
-    ) -> cpg_utils.Path:
-        bucket_name: cpg_utils.Path = sequencing_group.dataset.prefix()
-        return bucket_name / GCP_FOLDER_FOR_ICA_DOWNLOAD / f'{sequencing_group.name}'
+# @stage(analysis_type='dragen_mlr', required_stages=[AlignGenotypeWithDragen])
+# class GvcfMlrWithDragen(SequencingGroupStage):
+#     def expected_outputs(
+#         self,
+#         sequencing_group: SequencingGroup,
+#     ) -> None:
+#         pass
 
-    def queue_jobs(self, sequencing_group: SequencingGroup, inputs: StageInput) -> StageOutput | None:
-        cram_path_components: dict[str, str] = get_path_components_from_gcp_path(path=str(object=sequencing_group.cram))
-        cram: str = f'{cram_path_components["suffix"]}{cram_path_components["file"]}'
-        bucket_name: str = cram_path_components['bucket']
-        ica_analysis_folder_id_path: str = str(
-            inputs.as_path(target=sequencing_group, stage=PrepareIcaForDragenAnalysis, key='analysis_output_fid'),
-        )
-        batch_instance: Batch = get_batch()
-        ica_download_job: BashJob = batch_instance.new_bash_job(
-            name='DownloadDataFromIca',
-            attributes=(self.get_job_attrs() or {}) | {'tool': 'ICA'},
-        )
-        ica_download_job.storage(storage=calculate_needed_storage(cram=str(sequencing_group.cram)))
-        ica_download_job.image(image=image_path('ica'))
+#     def queue_jobs(self, sequencing_group: SequencingGroup, inputs: StageInput) -> StageOutput | None:
+#         pass
 
-        # Get secrets and folder ID needed for runtime
-        ica_analysis_folder_id = batch_instance.read_input(ica_analysis_folder_id_path)
 
-        # Download an entire folder with ICA. Don't log projectId or API key
-        authenticate_cloud_credentials_in_job(ica_download_job)
-        ica_download_job.command(
-            f"""
-            set +x
-            mkdir -p ~/.icavc2
-            echo "server-url: ica.illumina.com" > ~/.icav2/config.yaml
-            gcloud secrets versions access latest --secret=illumina_cpg_workbench_api --project=cpg-common | jq .apiKey | sed 's/\\\"//g' > key
-            gcloud secrets versions access latest --secret=illumina_cpg_workbench_api --project=cpg-common | jq .projectID | sed 's/\\\"//g' > projectID
-            echo "x-api-key: $(cat key)" >> ~/.icav2/config.yaml
-            icav2 projects enter $(cat projectID)
-            set -x
-            icav2 projectdata download $(cat {ica_analysis_folder_id}) .
-            mv {bucket_name}/{config_retrieve(["ica", "data_prep", "output_folder"])}/{sequencing_group.name}/* {sequencing_group.name}
-            cd {sequencing_group.name} && cat *.md5sum > dragen.md5sum && md5sum -c dragen.md5sum
-            gcloud storage cp --recursive {sequencing_group.name} gs://{bucket_name}/{GCP_FOLDER_FOR_ICA_DOWNLOAD}
-        """,
-        )
+# @stage(required_stages=[GvcfMlrWithDragen])
+# class MonitorGvcfMlrWithDragen(SequencingGroupStage):
+#     def expected_outputs(
+#         self,
+#         sequencing_group: SequencingGroup,
+#     ) -> None:
+#         pass
 
-        return self.make_outputs(
-            target=sequencing_group,
-            data=self.expected_outputs(sequencing_group=sequencing_group),
-            jobs=ica_download_job,
-        )
+#     def queue_jobs(self, sequencing_group: SequencingGroup, inputs: StageInput) -> StageOutput | None:
+#         pass
+
+
+# @stage(required_stages=[AlignGenotypeWithDragen, GvcfMlrWithDragen])
+# class CancelIcaPipelineRun(SequencingGroupStage):
+#     def expected_outputs(
+#         self,
+#         sequencing_group: SequencingGroup,
+#     ) -> None:
+#         pass
+
+#     def queue_jobs(self, sequencing_group: SequencingGroup, inputs: StageInput) -> StageOutput | None:
+#         pass
+
+
+# @stage(
+#     analysis_type='ica_data_download',
+#     required_stages=[PrepareIcaForDragenAnalysis, MonitorAlignGenotypeWithDragen, GvcfMlrWithDragen],
+# )
+# class DownloadDataFromIca(SequencingGroupStage):
+#     def expected_outputs(
+#         self,
+#         sequencing_group: SequencingGroup,
+#     ) -> cpg_utils.Path:
+#         bucket_name: cpg_utils.Path = sequencing_group.dataset.prefix()
+#         return bucket_name / GCP_FOLDER_FOR_ICA_DOWNLOAD / f'{sequencing_group.name}'
+
+#     def queue_jobs(self, sequencing_group: SequencingGroup, inputs: StageInput) -> StageOutput | None:
+#         cram_path_components: dict[str, str] = get_path_components_from_gcp_path(path=str(object=sequencing_group.cram))
+#         cram: str = f'{cram_path_components["suffix"]}{cram_path_components["file"]}'
+#         bucket_name: str = cram_path_components['bucket']
+#         ica_analysis_folder_id_path: str = str(
+#             inputs.as_path(target=sequencing_group, stage=PrepareIcaForDragenAnalysis, key='analysis_output_fid'),
+#         )
+#         batch_instance: Batch = get_batch()
+#         ica_download_job: BashJob = batch_instance.new_bash_job(
+#             name='DownloadDataFromIca',
+#             attributes=(self.get_job_attrs() or {}) | {'tool': 'ICA'},
+#         )
+#         ica_download_job.storage(storage=calculate_needed_storage(cram=str(sequencing_group.cram)))
+#         ica_download_job.image(image=image_path('ica'))
+
+#         # Get secrets and folder ID needed for runtime
+#         ica_analysis_folder_id = batch_instance.read_input(ica_analysis_folder_id_path)
+
+#         # Download an entire folder with ICA. Don't log projectId or API key
+#         authenticate_cloud_credentials_in_job(ica_download_job)
+#         ica_download_job.command(
+#             f"""
+#             set +x
+#             mkdir -p ~/.icavc2
+#             echo "server-url: ica.illumina.com" > ~/.icav2/config.yaml
+#             gcloud secrets versions access latest --secret=illumina_cpg_workbench_api --project=cpg-common | jq .apiKey | sed 's/\\\"//g' > key
+#             gcloud secrets versions access latest --secret=illumina_cpg_workbench_api --project=cpg-common | jq .projectID | sed 's/\\\"//g' > projectID
+#             echo "x-api-key: $(cat key)" >> ~/.icav2/config.yaml
+#             icav2 projects enter $(cat projectID)
+#             set -x
+#             icav2 projectdata download $(cat {ica_analysis_folder_id}) .
+#             mv {bucket_name}/{config_retrieve(["ica", "data_prep", "output_folder"])}/{sequencing_group.name}/* {sequencing_group.name}
+#             cd {sequencing_group.name} && cat *.md5sum > dragen.md5sum && md5sum -c dragen.md5sum
+#             gcloud storage cp --recursive {sequencing_group.name} gs://{bucket_name}/{GCP_FOLDER_FOR_ICA_DOWNLOAD}
+#         """,
+#         )
+
+#         return self.make_outputs(
+#             target=sequencing_group,
+#             data=self.expected_outputs(sequencing_group=sequencing_group),
+#             jobs=ica_download_job,
+#         )
