01b62ae7621f33eeb2648feb5def9abb0f95cce0
MattWellie <mattwellie@gmail.com>
2024-12-20 08:46:17 +1000

    lint

--- cpg_workflows/stages/exomiser.py
+++ cpg_workflows/stages/exomiser.py
@@ -202,7 +202,8 @@ class RunExomiser(DatasetStage):
                 'ped': ped_inputs[family],
                 'pheno': ppk_files[family],
             }
-            for family in output_dict.keys() if '_variants' not in family
+            for family in output_dict.keys()
+            if '_variants' not in family
         }
 
         if exomiser_version == 14:
