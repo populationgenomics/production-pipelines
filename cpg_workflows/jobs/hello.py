655195ec756ea533d2a046fdcfde41aea563d3d1
John Marshall <john.marshall@populationgenomics.org.au>
2025-01-10 16:02:31 +1300

    oops

--- cpg_workflows/jobs/hello.py
+++ cpg_workflows/jobs/hello.py
@@ -2,17 +2,11 @@
 Jobs that say hello.
 """
 
-from hailtop.batch.job import BashJob, Job
-from hailtop.batch.resource import JobResourceFile, Resource, ResourceFile, ResourceGroup
+from hailtop.batch.job import Job
 
 from cpg_utils import Path
-from cpg_utils.config import config_retrieve, get_config, image_path
-from cpg_utils.hail_batch import command, fasta_res_group, get_batch, query_command
+from cpg_utils.hail_batch import command, get_batch
 from cpg_workflows.filetypes import CramPath
-from cpg_workflows.query_modules import seqr_loader
-from cpg_workflows.resources import HIGHMEM
-from cpg_workflows.scripts import upgrade_ped_with_inferred
-from cpg_workflows.utils import can_reuse, chunks
 
 
 def hello_job(
