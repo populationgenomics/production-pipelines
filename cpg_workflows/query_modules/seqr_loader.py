81019f356977d70373126eb1d44a118945c9317c
EddieLF <eddieformaini@gmail.com>
2025-01-30 09:44:56 +1100

    Linting fix, stop black making chr list super long

--- cpg_workflows/query_modules/seqr_loader.py
+++ cpg_workflows/query_modules/seqr_loader.py
@@ -77,33 +77,12 @@ def annotate_cohort(
     # Remove any contigs not in the 22 autosomes, X, Y, M
     if remove_invalid_contigs:
         logging.info('Removing invalid contigs')
+        # fmt: off
         chromosomes = [
-            '1',
-            '2',
-            '3',
-            '4',
-            '5',
-            '6',
-            '7',
-            '8',
-            '9',
-            '10',
-            '11',
-            '12',
-            '13',
-            '14',
-            '15',
-            '16',
-            '17',
-            '18',
-            '19',
-            '20',
-            '21',
-            '22',
-            'X',
-            'Y',
-            'M',
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13',
+            '14', '15', '16', '17', '18', '19', '20', '21', '22', 'X', 'Y', 'M',
         ]
+        # fmt: on
         standard_contigs = hl.literal(
             hl.literal([f'chr{chrom}' for chrom in chromosomes]),
         )
