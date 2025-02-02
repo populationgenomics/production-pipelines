ee2cb9a96817718172eb7ca4a960532ea0f13b21
Michael Harper <michael.harper@populationgenomics.org.au>
2025-02-03 08:07:03 +1100

    Changing back to correct download command

--- cpg_workflows/stages/realign_genotype_with_dragen.py
+++ cpg_workflows/stages/realign_genotype_with_dragen.py
@@ -330,9 +330,8 @@ class DownloadDataFromIca(SequencingGroupStage):
             f"""
             {ICA_CLI_SETUP}
             icav2 projectdata download $(cat {ica_analysis_folder_id_path} | jq -r .analysis_output_fid) {sequencing_group.name} --exclude-source-path
-            gcloud storage cp --recursive {sequencing_group.name}/* gs://{bucket_name}/{GCP_FOLDER_FOR_ICA_DOWNLOAD}/{sequencing_group.name}
+            gcloud storage cp --recursive {sequencing_group.name} gs://{bucket_name}/{GCP_FOLDER_FOR_ICA_DOWNLOAD}/{sequencing_group.name}
         """,
-            # gcloud storage cp --recursive {sequencing_group.name}/*/{sequencing_group.name} gs://{bucket_name}/{GCP_FOLDER_FOR_ICA_DOWNLOAD}/{sequencing_group.name}
         )
 
         return self.make_outputs(
