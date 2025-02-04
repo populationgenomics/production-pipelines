6faf886e3d2ff39a361e6ab3b21c7838381f58ab
EddieLF <eddieformaini@gmail.com>
2025-02-04 11:36:37 +1100

    save with correct extension please

--- scripts/create_index_files.py
+++ scripts/create_index_files.py
@@ -92,7 +92,7 @@ def index_with_samtools(
         )
         cmd = f'samtools index -@ {res.get_nthreads() - 1} {input_file.bam} -o {j.out_bam["bam.bai"]}'
         j.command(command(cmd, monitor_space=True))
-        b.write_output(j.out_bam["bam.bai"], str(file_to_index) + '.bam')
+        b.write_output(j.out_bam["bam.bai"], str(file_to_index) + '.bai')
     elif file_to_index.suffix == '.cram':
         j.declare_resource_group(
             out_cram={
@@ -102,7 +102,7 @@ def index_with_samtools(
         )
         cmd = f'samtools index -@ {res.get_nthreads() - 1} {input_file.cram} -o {j.out_cram["cram.crai"]}'
         j.command(command(cmd, monitor_space=True))
-        b.write_output(j.out_cram["cram.crai"],  str(file_to_index) + '.cram')
+        b.write_output(j.out_cram["cram.crai"],  str(file_to_index) + '.crai')
     else:
         raise ValueError('Resource group must contain a bam or cram file')
 
