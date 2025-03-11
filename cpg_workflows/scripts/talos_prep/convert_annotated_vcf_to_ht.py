895407ff32fa801c4146d81a43b8ef38a1279f43
MattWellie <mattwellie@gmail.com>
2025-03-12 06:52:19 +1000

    consistent with references script

--- cpg_workflows/scripts/talos_prep/convert_annotated_vcf_to_ht.py
+++ cpg_workflows/scripts/talos_prep/convert_annotated_vcf_to_ht.py
@@ -196,9 +196,8 @@ def apply_mane_annotations(ht: hl.Table, mane_path: str | None = None) -> hl.Tab
             transcript_consequences=hl.map(
                 lambda x: x.annotate(
                     mane_status=MISSING_STRING,
-                    mane_id=MISSING_STRING,
                     ensp=MISSING_STRING,
-                    nm_id=MISSING_STRING,
+                    mane_id=MISSING_STRING,
                 ),
                 ht.transcript_consequences,
             ),
@@ -217,10 +216,21 @@ def apply_mane_annotations(ht: hl.Table, mane_path: str | None = None) -> hl.Tab
     return ht.annotate(
         transcript_consequences=hl.map(
             lambda x: x.annotate(
-                mane_status=hl.if_else(key_set.contains(x.transcript), hl_mane_dict[x.transcript]['mane_status'], MISSING_STRING),
-                mane_id=hl.if_else(key_set.contains(x.transcript), hl_mane_dict[x.transcript]['mane_id'], MISSING_STRING),
-                ensp=hl.if_else(key_set.contains(x.transcript), hl_mane_dict[x.transcript]['ensp'], MISSING_STRING),
-                nm_id=hl.if_else(key_set.contains(x.transcript), hl_mane_dict[x.transcript]['nm_id'], MISSING_STRING),
+                mane_status=hl.if_else(
+                    key_set.contains(x.transcript),
+                    hl_mane_dict[x.transcript]['mane_status'],
+                    MISSING_STRING,
+                ),
+                ensp=hl.if_else(
+                    key_set.contains(x.transcript),
+                    hl_mane_dict[x.transcript]['ensp'],
+                    MISSING_STRING,
+                ),
+                mane_id=hl.if_else(
+                    key_set.contains(x.transcript),
+                    hl_mane_dict[x.transcript]['mane_id'],
+                    MISSING_STRING,
+                ),
             ),
             ht.transcript_consequences,
         ),
