9828e799b961d4db326ad7ff92c8a92dfa3118bf
MattWellie <mattwellie@gmail.com>
2025-03-11 20:50:05 +1000

    !?!??!

--- cpg_workflows/scripts/talos_prep/convert_annotated_vcf_to_ht.py
+++ cpg_workflows/scripts/talos_prep/convert_annotated_vcf_to_ht.py
@@ -260,8 +260,7 @@ def main(vcf_path: str, output_path: str, gene_bed: str, alpha_m: str, mane: str
         alpha_m (str): path to the AlphaMissense Hail Table, required
         mane (str | None): path to a MANE Hail Table for enhanced annotation
     """
-
-    hl.default_reference('GRCh38')
+    hl.context.init_spark(master=f'local[4]', default_reference='GRCh38', quiet=True)
 
     # pull and split the CSQ header line
     csq_fields = extract_and_split_csq_string(vcf_path=vcf_path)
