4dcd3fd8e15f962b776cbf755e7d9b880679e79c
MattWellie <mattwellie@gmail.com>
2025-03-11 21:07:11 +1000

    maybe this

--- cpg_workflows/scripts/talos_prep/convert_annotated_vcf_to_ht.py
+++ cpg_workflows/scripts/talos_prep/convert_annotated_vcf_to_ht.py
@@ -195,7 +195,8 @@ def apply_mane_annotations(ht: hl.Table, mane_path: str | None = None) -> hl.Tab
         return ht.annotate(
             transcript_consequences=hl.map(
                 lambda x: x.annotate(
-                    mane=MISSING_STRING,
+                    mane_status=MISSING_STRING,
+                    mane_id=MISSING_STRING,
                     ensp=MISSING_STRING,
                     nm_id=MISSING_STRING,
                 ),
@@ -216,8 +217,10 @@ def apply_mane_annotations(ht: hl.Table, mane_path: str | None = None) -> hl.Tab
     return ht.annotate(
         transcript_consequences=hl.map(
             lambda x: x.annotate(
-                mane=hl.if_else(key_set.contains(x.transcript), hl_mane_dict[x.transcript]['mane'], MISSING_STRING),
+                mane_status=hl.if_else(key_set.contains(x.transcript), hl_mane_dict[x.transcript]['mane_status'], MISSING_STRING),
+                mane_id=hl.if_else(key_set.contains(x.transcript), hl_mane_dict[x.transcript]['mane_id'], MISSING_STRING),
                 ensp=hl.if_else(key_set.contains(x.transcript), hl_mane_dict[x.transcript]['ensp'], MISSING_STRING),
+                gene_id=hl.if_else(key_set.contains(x.transcript), hl_mane_dict[x.transcript]['ensg'], x.gene_id),
                 nm_id=hl.if_else(key_set.contains(x.transcript), hl_mane_dict[x.transcript]['nm_id'], MISSING_STRING),
             ),
             ht.transcript_consequences,
