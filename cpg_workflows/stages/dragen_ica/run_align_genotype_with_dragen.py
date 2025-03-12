8e5b2360db96b22578e2f2f8e0e7a4ecedb53c23
Alex Stuckey <alex.stuckey@populationgenomics.org.au>
2025-03-12 14:20:40 +1300

    update api call as per illumina

--- cpg_workflows/stages/dragen_ica/run_align_genotype_with_dragen.py
+++ cpg_workflows/stages/dragen_ica/run_align_genotype_with_dragen.py
@@ -65,9 +65,11 @@ def submit_dragen_run(
                 AnalysisParameterInput(code='enable_cnv', value='True'),
                 AnalysisParameterInput(code='cnv_enable_self_normalization', value='True'),
                 AnalysisParameterInput(code='enable_sv', value='True'),
+                AnalysisParameterInput(code='enably_cpy2d6', value='True'),
+                AnalysisParameterInput(code='repeat_genotype_enable', value='False'),
                 AnalysisParameterInput(
                     code='additional_args',
-                    value='--read-trimmers polyg --soft-read-trimmers none --vc-hard-filter "DRAGENHardQUAL:all:QUAL<5.0;LowDepth:all:DP<=1" --vc-frd-max-effective-depth 40 --vc-enable-joint-detection true --qc-coverage-ignore-overlaps true --qc-coverage-count-soft-clipped-bases true --enable-cyp2d6 true',
+                    value='--read-trimmers polyg --soft-read-trimmers none --vc-hard-filter "DRAGENHardQUAL:all:QUAL<5.0;LowDepth:all:DP<=1" --vc-frd-max-effective-depth 40 --vc-enable-joint-detection true --qc-coverage-ignore-overlaps true --qc-coverage-count-soft-clipped-bases true',
                 ),
             ],
         ),
