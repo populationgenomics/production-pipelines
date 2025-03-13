6820c3cdb4d5396b754c870a8237fcfde6612c4b
Michael Harper <michael.harper@populationgenomics.org.au>
2025-03-14 09:09:16 +1100

    Fixing config retrieve

--- cpg_workflows/large_cohort/frequencies.py
+++ cpg_workflows/large_cohort/frequencies.py
@@ -73,7 +73,7 @@ def frequency_annotations(
     """
 
     # Exome parsing
-    if config_retrieve(['workflow'], ['sequencing_type']) == 'exome':
+    if config_retrieve(['workflow', 'sequencing_type']) == 'exome':
         print('making it here')
         # clear sample qc filters as this data has not been qc'd yet
         sample_qc_ht = sample_qc_ht.annotate(filters=hl.empty_set(hl.tstr))
