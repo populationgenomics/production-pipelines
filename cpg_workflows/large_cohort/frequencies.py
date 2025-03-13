144d951c698283b1949bdb88c2602fe364f32b65
Michael Harper <michael.harper@populationgenomics.org.au>
2025-03-14 08:20:59 +1100

    Adding debug prints

--- cpg_workflows/large_cohort/frequencies.py
+++ cpg_workflows/large_cohort/frequencies.py
@@ -170,9 +170,12 @@ def frequency_annotations(
 
 def annotate_labels(mt: hl.MatrixTable, inferred_pop_ht: hl.Table, sample_qc_ht: hl.Table) -> hl.MatrixTable:
     # prepare_gnomad_v4_variants_helper requires ancestry to be annotated
-    logging.info(f'{mt.s.collect()}')
-    logging.info(f'{inferred_pop_ht.s.collect()}')
-    logging.info(f'{sample_qc_ht.s.collect()}')
+    print(f'{mt.s.collect()}')
+    print(f'{inferred_pop_ht.s.collect()}')
+    print(f'{sample_qc_ht.s.collect()}')
+    print(mt.show())
+    print(inferred_pop_ht.show())
+    print(sample_qc_ht.show())
     mt = mt.annotate_cols(gen_anc=inferred_pop_ht[mt.s].pop)
     # prepare_gnomad_v4_variants_helper requires sex to be annotated
     mt = mt.annotate_cols(sex=sample_qc_ht[mt.s].sex_karyotype)
