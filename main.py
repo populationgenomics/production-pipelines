7ff733fee2114b784a9b9852419341aba8a2649e
MattWellie <mattwellie@gmail.com>
2025-03-15 12:56:44 +1000

    run the final Stage

--- main.py
+++ main.py
@@ -44,13 +44,13 @@ from cpg_workflows.stages.seqr_loader_long_read.long_read_snps_indels_annotation
 from cpg_workflows.stages.seqr_loader_long_read.long_read_sv_annotation import MtToEsLrSv
 from cpg_workflows.stages.stripy import Stripy
 from cpg_workflows.stages.talos import CreateTalosHTML, MakePhenopackets, MinimiseOutputForSeqr, ValidateMOI
-from cpg_workflows.stages.talos_prep.talos_prep import JumpAnnotationsFromHtToFinalMt
+from cpg_workflows.stages.talos_prep.talos_prep import CompressMtIntoTarball
 from cpg_workflows.workflow import StageDecorator, run_workflow
 
 WORKFLOWS: dict[str, list[StageDecorator]] = {
     'clinvarbitration': [PackageForRelease],
     'talos': [MakePhenopackets, ValidateMOI, CreateTalosHTML, MinimiseOutputForSeqr],
-    'talos_prep': [JumpAnnotationsFromHtToFinalMt],
+    'talos_prep': [CompressMtIntoTarball],
     'exomiser': [ExomiserSeqrTSV, ExomiserVariantsTSV, RegisterSingleSampleExomiserResults],
     'long_read_snps_indels_annotation': [MtToEsLrSNPsIndels],
     'long_read_sv_annotation': [MtToEsLrSv],
