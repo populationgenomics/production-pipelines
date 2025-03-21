3a268e138df9fff5b82b8ef63261fd3855260d0e
EddieLF <eddieformaini@gmail.com>
2025-03-21 16:04:18 +1100

    fix Log skipped sgs

--- cpg_workflows/stages/seqr_loader_long_read/long_read_snps_indels_annotation.py
+++ cpg_workflows/stages/seqr_loader_long_read/long_read_snps_indels_annotation.py
@@ -115,7 +115,7 @@ def find_sgs_to_skip(sg_vcf_dict: dict[str, dict]) -> list[str]:
     sgs_to_skip = set()
     for sg_id, vcf_analysis in sg_vcf_dict.items():
         analysis_meta = vcf_analysis['meta']
-        get_logger().info(f'Checking {sg_id} | Participant: {analysis_meta.get("participant_id", "")} | Joint-called: {analysis_meta.get('joint_called', False)}')
+        get_logger().info(f'Checking {sg_id} | Participant: {analysis_meta.get("participant_id", "")} | Joint-called: {analysis_meta.get("joint_called", False)}')
         if analysis_meta.get('joint_called', False) and not analysis_meta.get('participant_id', '').endswith('00'):
             sgs_to_skip.add(sg_id)
     return list(sgs_to_skip)
