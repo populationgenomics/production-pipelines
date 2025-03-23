a14a4ce42e8bfbb15f10a37ce475965fb2c5ec78
EddieLF <eddieformaini@gmail.com>
2025-03-24 09:29:25 +1100

    Fix index correction for info fields

--- cpg_workflows/query_modules/seqr_loader.py
+++ cpg_workflows/query_modules/seqr_loader.py
@@ -115,7 +115,8 @@ def annotate_cohort(
     # these elements become a 1-element array
     # the index_correction is required to align the AC/AF with the correct allele
     # in the split multi-allelics
-    index_correction = 0 if long_read else 1
+    # index_correction = 0 if long_read else 1
+    index_correction = 1
     mt = mt.annotate_rows(
         info=mt.info.annotate(
             AF=[mt.info.AF[mt.a_index - index_correction]],
