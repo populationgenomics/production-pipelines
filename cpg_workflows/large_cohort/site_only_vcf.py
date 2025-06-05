3c360c2d9a3101b1ae9d50f4003ac8cceb14bdc0
Michael Harper <michael.harper@populationgenomics.org.au>
2025-06-05 14:30:24 +1000

    Using sparse matrix table

--- cpg_workflows/large_cohort/site_only_vcf.py
+++ cpg_workflows/large_cohort/site_only_vcf.py
@@ -234,7 +234,7 @@ def vds_to_site_only_ht(
         return hl.read_table(str(out_ht_path))
 
     # Passing un-densified MatrixTable to default_compute_info
-    mt = hl.vds.to_dense_mt(vds)
+    mt = vds.variant_data
 
     mt = mt.filter_cols(hl.len(sample_qc_ht[mt.col_key].filters) > 0, keep=False)
     mt = mt.filter_cols(hl.is_defined(relateds_to_drop_ht[mt.col_key]), keep=False)
