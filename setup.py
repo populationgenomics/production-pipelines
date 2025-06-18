519be0b26c53d6ce0830d065f20058f3caa59f26
Alex Stuckey <alex.stuckey@populationgenomics.org.au>
2025-06-18 13:20:07 +1200

    Bump version: 1.37.10 → 1.37.11

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 setup(
     name='cpg_workflows',
     # This tag is automatically updated by bumpversion
-    version='1.37.10',
+    version='1.37.11',
     description='CPG workflows for Hail Batch',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
