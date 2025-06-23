1e1bf51e1ff1cf0dcae63734d3d3b89c700aa9b3
Michael Harper <michael.harper@populationgenomics.org.au>
2025-06-24 09:08:56 +1000

    debugging bash

--- scripts/test_run_deepvariant.py
+++ scripts/test_run_deepvariant.py
@@ -78,6 +78,7 @@ def run_pangenome_aware(output_vcf: str, output_gvcf: str) -> 'Job':
     # copy test data
     j.command(
         f"""
+        set -ex
         mkdir -p reference
         FTPDIR=ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids
         curl ${{FTPDIR}}/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz | gunzip > reference/GRCh38_no_alt_analysis_set.fasta
