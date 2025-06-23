ef98cfa41256b55aab30e72969e6c33488e7661c
Michael Harper <michael.harper@populationgenomics.org.au>
2025-06-23 12:33:16 +1000

    listing debugging

--- scripts/test_run_deepvariant.py
+++ scripts/test_run_deepvariant.py
@@ -32,6 +32,10 @@ def run(output_path: str) -> 'Job':
     # copy test data
     j.command(
         f"""
+        ls -l
+        ls -l /opt/
+        ls -l /opt/deepvariant/
+        ls -l /opt/deepvariant/bin/
         INPUT_DIR="$BATCH_TMPDIR/quickstart-testdata"
         CHECKPOINT_DIR="$BATCH_TMPDIR/checkpoint"
         DATA_HTTP_DIR="https://storage.googleapis.com/deepvariant/quickstart-testdata"
