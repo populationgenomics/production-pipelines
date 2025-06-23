85fcd3030483dc2087498d750443a591896e8161
Michael Harper <michael.harper@populationgenomics.org.au>
2025-06-24 08:55:50 +1000

    Implementing pangenome-aware DeepVariant

--- scripts/test_run_deepvariant.py
+++ scripts/test_run_deepvariant.py
@@ -2,8 +2,6 @@
 
 from typing import TYPE_CHECKING
 
-from requests import get
-
 from cpg_utils.config import image_path
 from cpg_utils.hail_batch import get_batch, init_batch
 
@@ -32,10 +30,6 @@ def run(output_path: str) -> 'Job':
     # copy test data
     j.command(
         f"""
-        ls -l
-        ls -l /opt/
-        ls -l /opt/deepvariant/
-        ls -l /opt/deepvariant/bin/
         INPUT_DIR="$BATCH_TMPDIR/quickstart-testdata"
         CHECKPOINT_DIR="$BATCH_TMPDIR/checkpoint"
         DATA_HTTP_DIR="https://storage.googleapis.com/deepvariant/quickstart-testdata"
@@ -74,14 +68,69 @@ def run(output_path: str) -> 'Job':
     return j
 
 
+def run_pangenome_aware(output_vcf: str, output_gvcf: str) -> 'Job':
+    # create a job
+    j = get_batch().new_job('DeepVariant PangenomeAware')
+
+    # choose an image to run this job in (default is bare ubuntu)
+    j.image(image_path('deepvariant_pangenome_aware'))
+
+    # copy test data
+    j.command(
+        f"""
+        mkdir -p reference
+        FTPDIR=ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids
+        curl ${{FTPDIR}}/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz | gunzip > reference/GRCh38_no_alt_analysis_set.fasta
+        curl ${{FTPDIR}}/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.fai > reference/GRCh38_no_alt_analysis_set.fasta.fai
+
+        mkdir -p benchmark
+        FTPDIR=ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/release/AshkenazimTrio/HG003_NA24149_father/NISTv4.2.1/GRCh38
+        curl ${{FTPDIR}}/HG003_GRCh38_1_22_v4.2.1_benchmark_noinconsistent.bed > benchmark/HG003_GRCh38_1_22_v4.2.1_benchmark_noinconsistent.bed
+        curl ${{FTPDIR}}/HG003_GRCh38_1_22_v4.2.1_benchmark.vcf.gz > benchmark/HG003_GRCh38_1_22_v4.2.1_benchmark.vcf.gz
+        curl ${{FTPDIR}}/HG003_GRCh38_1_22_v4.2.1_benchmark.vcf.gz.tbi > benchmark/HG003_GRCh38_1_22_v4.2.1_benchmark.vcf.gz.tbi
+
+        mkdir -p input
+        HTTPDIR=https://storage.googleapis.com/deepvariant/case-study-testdata
+        curl ${{HTTPDIR}}/HG003.novaseq.pcr-free.35x.dedup.grch38_no_alt.chr20.bam > input/HG003.novaseq.pcr-free.35x.dedup.grch38_no_alt.chr20.bam
+        curl ${{HTTPDIR}}/HG003.novaseq.pcr-free.35x.dedup.grch38_no_alt.chr20.bam.bai > input/HG003.novaseq.pcr-free.35x.dedup.grch38_no_alt.chr20.bam.bai
+
+        mkdir -p input
+        HTTPDIR=https://s3-us-west-2.amazonaws.com/human-pangenomics/pangenomes/freeze/freeze1/minigraph-cactus/hprc-v1.1-mc-grch38
+        curl ${{HTTPDIR}}/hprc-v1.1-mc-grch38.gbz > input/hprc-v1.1-mc-grch38.gbz
+
+        # Run pangenome-aware DeepVariant
+        mkdir -p output
+        mkdir -p output/intermediate_results_dir
+        /opt/deepvariant/bin/run_pangenome_aware_deepvariant \
+        --model_type WGS \
+        --ref /reference/GRCh38_no_alt_analysis_set.fasta \
+        --reads /input//HG003.novaseq.pcr-free.35x.dedup.grch38_no_alt.chr20.bam  \
+        --pangenome /input/hprc-v1.1-mc-grch38.gbz \
+        --output_vcf {j.out_vcf} \
+        --output_gvcf {j.out_gvcf} \
+        --num_shards $(nproc) \
+        --regions chr20 \
+        --intermediate_results_dir /output/intermediate_results_dir
+        """,
+    )
+
+    # write the output to the expected location
+    get_batch().write_output(j.out_vcf, output_vcf)
+    get_batch().write_output(j.out_gvcf, output_gvcf)
+
+    # return the job
+    return j
+
+
 def main():
     """
     Main entry point for testing the run function.
     """
     init_batch()
-    output_path = 'gs://cpg-bioheart-test/deepvariant_test/test_out.g.vcf.gz'
-    job = run(output_path)
-    print(f"Job {job.name} created with output path: {output_path}")
+    output_vcf = 'gs://cpg-bioheart-test/deepvariant_test/test_out.vcf.gz'
+    output_gvcf = 'gs://cpg-bioheart-test/deepvariant_test/test_out.g.vcf.gz'
+    job = run_pangenome_aware(output_vcf, output_gvcf)
+    print(f"Job {job.name} created with output paths: {output_vcf}, {output_gvcf}")
 
     get_batch().run()
 
