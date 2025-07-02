0c958b7c42765e81d55a1c6a6f9e558d20ab3692
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2025-07-02 15:10:33 +1000

    tob run

--- str_pca_dataproc_runner.py
+++ str_pca_dataproc_runner.py
@@ -15,7 +15,7 @@ def main():
     dataproc_job(
         function=pca_runner,
         function_path_args=dict(
-            file_path='gs://cpg-bioheart-test/str/polymorphic_run/mt/bioheart_tob/v1_n1925/str_annotated.mt',
+            file_path='gs://cpg-bioheart-test/str/polymorphic_run/mt/bioheart_tob/n950_tob/str_annotated.mt',
         ),
         job_name='STR-PCA',
         num_workers=20,
