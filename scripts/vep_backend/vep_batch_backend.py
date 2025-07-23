20bad2a7f76be4c1fafe407dcf20a446acf8d773
Michael Harper <michael.harper@populationgenomics.org.au>
2025-07-23 20:34:13 +1000

    Removing seqr_loader reference

--- scripts/vep_backend/vep_batch_backend.py
+++ scripts/vep_backend/vep_batch_backend.py
@@ -10,7 +10,8 @@ from cpg_utils import to_path
 from cpg_utils.config import get_config, image_path, output_path
 from cpg_utils.hail_batch import get_batch, query_command
 from cpg_workflows.jobs.vep import add_vep_jobs
-from cpg_workflows.query_modules import seqr_loader
+
+# from cpg_workflows.query_modules import seqr_loader
 
 
 @click.command()
