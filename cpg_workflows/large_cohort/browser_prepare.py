6e94ad22f5956a08143795424ebcb37d919dff30
Michael Harper <michael.harper@populationgenomics.org.au>
2025-07-28 14:25:16 +1000

    Commenting out age and AS_pab_max again to run more tests

--- cpg_workflows/large_cohort/browser_prepare.py
+++ cpg_workflows/large_cohort/browser_prepare.py
@@ -699,7 +699,7 @@ def prepare_gnomad_v4_variants_helper(ds_path: str | None, exome_or_genome: str)
 
     # Summarise the carrier age distribution for each variant.
     logging.info('Summarising the age distribution for variant carriers...')
-    ds = ds.annotate(expanded_age_distribution=hl.struct(het=ds.age_hist_het, hom=ds.age_hist_hom))
+    # ds = ds.annotate(expanded_age_distribution=hl.struct(het=ds.age_hist_het, hom=ds.age_hist_hom))
 
     # Summarise the site quality metrics.
     logging.info('Summarising the site quality metrics...')
@@ -734,7 +734,7 @@ def prepare_gnomad_v4_variants_helper(ds_path: str | None, exome_or_genome: str)
                     "AS_MQ",
                     "AS_FS",
                     "AS_MQRankSum",
-                    "AS_pab_max",
+                    # "AS_pab_max",
                     "AS_QUALapprox",
                     "AS_QD",
                     "AS_ReadPosRankSum",
@@ -782,7 +782,7 @@ def prepare_gnomad_v4_variants_helper(ds_path: str | None, exome_or_genome: str)
             'flags',
             'expanded_freq',
             'fafmax',
-            'expanded_age_distribution',
+            # 'expanded_age_distribution',
             'vep',
             'filters',
             'quality_metrics',
