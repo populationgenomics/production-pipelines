c2d27d0cf9e257406760969fbe0c150aea87b996
Michael Harper <michael.harper@populationgenomics.org.au>
2025-07-28 14:24:00 +1000

    Importing correct and up-to-date VEP that uses SpliceAi plugin. Also changing name of extract transcripts hail table

--- cpg_workflows/stages/large_cohort.py
+++ cpg_workflows/stages/large_cohort.py
@@ -7,7 +7,7 @@ from numpy import require
 from cpg_utils import Path, to_path
 from cpg_utils.config import config_retrieve, get_config, image_path
 from cpg_utils.hail_batch import get_batch, query_command
-from cpg_workflows.jobs.rd_combiner.vep import add_vep_jobs
+from cpg_workflows.jobs.vep import add_vep_jobs
 from cpg_workflows.large_cohort.combiner import combiner
 from cpg_workflows.targets import Cohort
 from cpg_workflows.utils import get_all_fragments_from_manifest, slugify
@@ -562,7 +562,7 @@ class LCAnnotateFragmentedVcfWithVep(CohortStage):
         return cohort.analysis_dataset.prefix() / get_workflow().name / vep_version / 'vep.ht'
 
     def queue_jobs(self, cohort: Cohort, inputs: StageInput) -> StageOutput:
-        outputs = self.expected_outputs(cohort)
+        output = self.expected_outputs(cohort)
         manifest_file = inputs.as_path(target=cohort, stage=ConvertSiteOnlyHTToVcfShards, key='hps_shard_manifest')
 
         input_vcfs = get_all_fragments_from_manifest(manifest_file)
@@ -571,13 +571,15 @@ class LCAnnotateFragmentedVcfWithVep(CohortStage):
             raise ValueError(f'No VCFs in {manifest_file}')
 
         vep_jobs = add_vep_jobs(
+            b=get_batch(),
             input_vcfs=input_vcfs,
-            final_out_path=outputs,
             tmp_prefix=self.tmp_prefix / 'tmp',
+            scatter_count=len(input_vcfs),
+            out_path=output,
             job_attrs=self.get_job_attrs(),
         )
 
-        return self.make_outputs(cohort, data=outputs, jobs=vep_jobs)
+        return self.make_outputs(cohort, data=output, jobs=vep_jobs)
 
 
 @stage(required_stages=[Combiner, Relatedness, Ancestry, LoadVqsr, LCAnnotateFragmentedVcfWithVep])
@@ -781,7 +783,7 @@ class GenerateGeneTable(CohortStage):
         prefix = cohort.analysis_dataset.prefix() / get_workflow().name / gene_table_version
         return {
             'gene_table': prefix / 'gene_table.ht',
-            'base_transcripts_grch38': prefix / 'base_transcripts_grch38.ht',
+            'transcripts_grch38_base': prefix / 'transcripts_grch38_base.ht',
             'mane_select_transcripts': prefix / 'mane_select_transcripts.ht',
         }
 
@@ -800,7 +802,7 @@ class GenerateGeneTable(CohortStage):
                 generate_gene_table.run.__name__,
                 str(inputs.as_path(cohort, Frequencies)),
                 str(self.tmp_prefix / 'browser'),
-                str(self.expected_outputs(cohort)['base_transcripts_grch38']),
+                str(self.expected_outputs(cohort)['transcripts_grch38_base']),
                 str(self.expected_outputs(cohort)['mane_select_transcripts']),
                 str(self.expected_outputs(cohort)['gene_table']),
                 setup_gcp=True,
@@ -841,8 +843,8 @@ class PrepareBrowserTable(CohortStage):
 
         exome_freq_ht_path = config_retrieve(['large_cohort', 'output_versions', 'frequencies_exome'], default=None)
         genome_freq_ht_path = config_retrieve(['large_cohort', 'output_versions', 'frequencies_genome'], default=None)
-        base_transcripts_grch38_path = config_retrieve(
-            ['large_cohort', 'output_versions', 'base_transcripts_grch38'],
+        transcripts_grch38_base_path = config_retrieve(
+            ['large_cohort', 'output_versions', 'transcripts_grch38_base'],
             default=None,
         )
         mane_select_transcripts_path = config_retrieve(
@@ -860,7 +862,7 @@ class PrepareBrowserTable(CohortStage):
                 str(self.expected_outputs(cohort)['browser']),
                 str(self.expected_outputs(cohort)['exome_variants']),
                 str(self.expected_outputs(cohort)['genome_variants']),
-                base_transcripts_grch38_path,
+                transcripts_grch38_base_path,
                 mane_select_transcripts_path,
                 setup_gcp=True,
             ),
