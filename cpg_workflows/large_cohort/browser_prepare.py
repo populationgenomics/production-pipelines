b3e72fdc2ec8728f135a65ad5b94199370ce6c18
Michael Harper <michael.harper@populationgenomics.org.au>
2025-08-01 13:48:22 +1000

    Adding insilico_predictors to empty config

--- cpg_workflows/large_cohort/browser_prepare.py
+++ cpg_workflows/large_cohort/browser_prepare.py
@@ -40,6 +40,147 @@ array<
                 faf99_max: float64,
                 faf99_max_gen_anc: str
             },
+            vep: struct {
+                minimised: int32,
+                assembly_name: str,
+                allele_string: str,
+                ancestral: str,
+                colocated_variants: array<struct {
+                    allele_string: str,
+                    clin_sig: array<str>,
+                    clin_sig_allele: str,
+                    end: int32,
+                    id: str,
+                    minimised: int32,
+                    minor_allele: str,
+                    minor_allele_freq: float64,
+                    phenotype_or_disease: int32,
+                    pubmed: array<int32>,
+                    seq_region_name: str,
+                    somatic: int32,
+                    start: int32,
+                    strand: int32
+                }>,
+                context: str,
+                end: int32,
+                id: str,
+                input: str,
+                intergenic_consequences: array<struct {
+                    allele_num: int32,
+                    consequence_terms: array<str>,
+                    impact: str,
+                    minimised: int32,
+                    variant_allele: str
+                }>,
+                most_severe_consequence: str,
+                motif_feature_consequences: array<struct {
+                    allele_num: int32,
+                    consequence_terms: array<str>,
+                    high_inf_pos: str,
+                    impact: str,
+                    minimised: int32,
+                    motif_feature_id: str,
+                    motif_name: str,
+                    motif_pos: int32,
+                    motif_score_change: float64,
+                    strand: int32,
+                    transcription_factors: array<str>,
+                    variant_allele: str
+                }>,
+                regulatory_feature_consequences: array<struct {
+                    allele_num: int32,
+                    biotype: str,
+                    consequence_terms: array<str>,
+                    impact: str,
+                    minimised: int32,
+                    regulatory_feature_id: str,
+                    variant_allele: str
+                }>,
+                seq_region_name: str,
+                start: int32,
+                strand: int32,
+                transcript_consequences: array<struct {
+                    allele_num: int32,
+                    amino_acids: str,
+                    appris: str,
+                    biotype: str,
+                    canonical: int32,
+                    mane_select: str,
+                    mane_plus_clinical: str,
+                    ccds: str,
+                    cdna_start: int32,
+                    cdna_end: int32,
+                    cds_end: int32,
+                    cds_start: int32,
+                    codons: str,
+                    consequence_terms: array<str>,
+                    distance: int32,
+                    domains: array<struct {
+                        db: str,
+                        name: str
+                    }>,
+                    exon: str,
+                    gene_id: str,
+                    gene_pheno: int32,
+                    gene_symbol: str,
+                    gene_symbol_source: str,
+                    hgnc_id: str,
+                    hgvsc: str,
+                    hgvsp: str,
+                    hgvs_offset: int32,
+                    impact: str,
+                    intron: str,
+                    lof: str,
+                    lof_flags: str,
+                    lof_filter: str,
+                    lof_info: str,
+                    existing_inframe_oorfs: int32,
+                    existing_outofframe_oorfs: int32,
+                    existing_uorfs: int32,
+                    `5utr_consequence`: str,
+                    `5utr_annotation`: dict<str, struct {
+                        type: str,
+                        KozakContext: str,
+                        KozakStrength: str,
+                        DistanceToCDS: str,
+                        CapDistanceToStart: str,
+                        DistanceToStop: str,
+                        Evidence: str,
+                        AltStop: str,
+                        AltStopDistanceToCDS: str,
+                        FrameWithCDS: str,
+                        StartDistanceToCDS: str,
+                        newSTOPDistanceToCDS: str,
+                        alt_type: str,
+                        alt_type_length: str,
+                        ref_StartDistanceToCDS: str,
+                        ref_type: str,
+                        ref_type_length: str
+                    }>,
+                    minimised: int32,
+                    mirna: array<str>,
+                    polyphen_prediction: str,
+                    polyphen_score: float64,
+                    protein_end: int32,
+                    protein_start: int32,
+                    protein_id: str,
+                    sift_prediction: str,
+                    sift_score: float64,
+                    strand: int32,
+                    swissprot: array<str>,
+                    transcript_id: str,
+                    trembl: array<str>,
+                    tsl: int32,
+                    uniparc: array<str>,
+                    uniprot_isoform: array<str>,
+                    variant_allele: str,
+                    am_class: str,
+                    am_pathogenicity: float64,
+                    source: str,
+                    flags: array<str>
+                }>,
+                variant_class: str
+            },
             age_distribution: struct {
                 het: struct {
                     bin_edges: array<float64>,
@@ -127,10 +268,18 @@ array<
                     value: float64
                 }>
             }
+        },
+        insilico_predictors: struct {
+            cadd: struct {
+                phred: float32,
+                raw_score: float32
+            },
+            spliceai_ds_max: float32
         }
     }
 >
 """
+
 PROTEIN_LETTERS_1TO3 = hl.dict(
     {
         "A": "Ala",
