d0148b65a8964504af75dcb67d4b7e8445794cd9
MattWellie <mattwellie@gmail.com>
2025-10-14 09:06:33 +1000

    hard no parallel

--- scripts/dummy_read_write_gcloud.sh
+++ scripts/dummy_read_write_gcloud.sh
@@ -8,6 +8,8 @@ out_file=$2
 
 base_name=$(basename "${in_file}")
 
+gcloud config set storage/parallel_composite_upload_enabled False
+
 gcloud storage cp "${in_file}" "${BATCH_TMPDIR}/${base_name}"
 
 time gcloud storage cp "${BATCH_TMPDIR}/${base_name}" "${out_file}"
